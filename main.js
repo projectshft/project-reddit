0c9b660022a969c292c4dba136a44bba625a3fb4
Justin McCarty <jamccarty99@gmail.com>
2018-01-22 10:06:51 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/* 1. clean looking page
+  2. a. a text box w input of post text
+    b. a text box w input of name
+  3. a submit/post button
+    b. primary color and looking
+  4. added posts display beneath the the previously posted post.
+
+  5.ability to leave comments on each posts
+    a. 
