3f633564589a4d777f0f8467606bce00929f5478
Justin McCarty <jamccarty99@gmail.com>
2018-01-23 15:44:16 -0500

    posts include dynamic elements for remove and comments and create separate divs

--- main.js
+++ main.js
@@ -15,14 +15,12 @@
      be "clickable" and for the page to seem to navigate to a new page that is
      dedicated to just that post and it's comments. Of course, it won't be a new page.
      Perhaps hide the original info and only display the comments of said post.
-
 */
 
-//var text = ;
-//var user = ;
+//attempt to use handlebar to simplify the original code
 var postData = {
   posts: [
-    { post: $('.post-text').val(), name: $('.your-name').val() },
+    { post: "$('.post-text').val()", name: "$('.your-name').val()" },
   ]
 };
 
@@ -30,18 +28,39 @@ var source = $('#post-template').html();
 var template = Handlebars.compile(source);
 var newHTML = template(postData);
 
+/*var commentsContainer = '<div class="comments-container">' + '<div class=comments-list></div>' +
+'<input type="text" class="comment-name">' +
+'<button class="btn btn-primary add-comment">Post Comment</button> </div>';*/ //hidden here as a reference. not my actual code.
 // append our new html to the page
 $('.post').append(newHTML);
-
+//function is run when the 'post' button is clicked
+//function should take 1st input and post them beneath the previous post and take 2nd input and display "posted by:..."
 var addPost = function () {
+//listens to post button when clicked.
   $('.add-post').click(function () {
+    //checks to make sure the are values in both input boxes.
     if ($('.post-text').val() === ""|| $('.your-name').val() === "") {
+      //alerts user of error, that they don't have values in both input boxes.
           alert('Please complete both input fields');
         } else {
-      $('.posts').append($('.post-text').val() + ' Posted By: ' + $('.your-name').val());
+          //when there are 2 values, appends them in a string to an html element
+      $('.posts').append('<div class="post">' + '<a href="#" class="remove">remove</a> ' + '<a href="#" class="show-comments">comments</a> ' + $('.post-text').val() + ' Posted By: ' + '<strong>' + $('.your-name').val() + '</strong>' + '</div>');
       }
     });
 };
+
+//when user clicks on the remove button, it should remove that post.
+//optionally add an alert to warn/confirm removal
+var removePost = function () {
+  //add an event listner to remove button when clicked
+  $('.remove').click(function () {
+    
+  })
+  return false;
+}
+
+
+//calls the function
 addPost();
 
 
