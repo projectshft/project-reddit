d0f634add2309d6ee064a7c125d182f72367f8bb
davieu <davisumana@gmail.com>
2018-04-27 14:22:41 -0400

    added comments in html

--- main.js
+++ main.js
@@ -14,37 +14,36 @@
 
 
 
-var postIt = $('button').on('click', function() {
+var postIt = $('#post-button').on('click', function() {
 
   // var addComment = 'comments';
-  var posted = $('<p>'+ $input +  $('#post').val() + '<p>');
-  var addedName = $('<p>' + 'Posted By: ' + $('#name').val() + '<p>');
+  var posted = $('<p>' + $('#post-input').val() + '<p>');
+  var addedName = $('<p>' + 'Posted By: ' + $('#name-input').val() + '<p>');
 
   // posted.click(function() {
   //   $(this).alert("Hello");
   // })
   // $($input).prependTo(posted);
-$('.posts').append(posted)
-
-  $('#post').val('');
+$('.posts').append(posted);
+$('#post-input').val('');
 
 
   // var name = $('<p>' + 'Posted By: '+ $('#name').val() + '<p>');
-  $('.posts').append(addedName)
-  $('#name').val('');
+  $('.names').append(addedName);
+  $('#name-input').val('');
 })
 
-
-var commentIt = $('button').on('click', function() {
-
-  // var addComment = 'comments';
-  var posted = $('<p>'+ $input +  $('#post').val() + '<p>');
-  var addedName = $('<p>' + 'Posted By: ' + $('#name').val() + '<p>');
-
-
-  $('.posts').append(posted)
-  $('#post').val('');
-
-  $('.posts').append(addedName)
-  $('#name').val('');
-})
+//
+// var commentIt = $('button').on('click', function() {
+//
+//   // var addComment = 'comments';
+//   var posted = $('<p>'+ $input +  $('#post').val() + '<p>');
+//   var addedName = $('<p>' + 'Posted By: ' + $('#name').val() + '<p>');
+//
+//
+//   $('.posts').append(posted)
+//   $('#post').val('');
+//
+//   $('.posts').append(addedName)
+//   $('#name').val('');
+// })
