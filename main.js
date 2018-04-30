e1157d9d886f122a8dd384e015498b5e0f413c6c
Altneuj <34726282+Altneuj@users.noreply.github.com>
2018-04-30 09:01:16 -0400

    final

--- main.js
+++ main.js
@@ -39,64 +39,61 @@ var createPost = function(){
 
 
   $('#post-button').click(function() {
-        createPost();
+    createPost();
 
 
-//looping through multiple times when multiple posts are open.
-        $('.form-group').unbind('click').click(function(e) {
-              if (e.target.classList.contains('comment-button')) {
-                $commentName = $(this).find('.username-input').val();
-                $commentBody = $(this).find('.comment-input').val();
-                $('.comment-input').val('');
-                $('.username-input').val('');
+    //looping through multiple times when multiple posts are open.
+    $('.form-group').unbind('click').click(function(e) {
+      if (e.target.classList.contains('comment-button')) {
+        $commentName = $(this).find('.username-input').val();
+        $commentBody = $(this).find('.comment-input').val();
+        $('.comment-input').val('');
+        $('.username-input').val('');
 
-      var commentTemplate = '<li class="comment">' + $commentBody + '<span class="text-muted"> Posted By: <strong>' + $commentName + '</strong></span>'
-                            + ' <button type="button" class="close Xout" id="close" aria-label="Close"><span class="close close-out" aria-hidden="true">&times;</span>'
-                              '</button> </li>'
-var $comment = $(commentTemplate);
-
-
-$(this).prev('.comment-section').append($comment);
-
-}
-$('.comment-section').click(function(e) {
-if (e.target.classList.contains('close')) {
-  var trashComment = e.target.closest('li')
-  trashComment.remove();
-}
-
-})
-
-})
+        var commentTemplate = '<li class="comment">' + $commentBody + '<span class="text-muted"> Posted By: <strong>' + $commentName + '</strong></span>' +
+          ' <button type="button" class="close Xout" id="close" aria-label="Close"><span class="close close-out" aria-hidden="true">&times;</span>'
+        '</button> </li>'
+        var $comment = $(commentTemplate);
 
 
+        $(this).prev('.comment-section').append($comment);
 
+      }
+      $('.comment-section').click(function(e) {
+        if (e.target.classList.contains('close')) {
+          var trashComment = e.target.closest('li')
+          trashComment.remove();
+        }
 
-//deletes the post
-$('.action-buttons').unbind('click').click(function(e) {
-if (e.target.classList.contains('remove-post')) {
-  var thisPost = $(this).closest('.row')
-  thisPost.remove();
-}
+      })
 
-if (e.target.classList.contains('edit-post')){
-  var edit = prompt("Place edit here");
-  var targetEdit = $(this).closest('.col').find('.post');
-  targetEdit.text(edit);
-}
+    })
 
-//toggles comment section and forms
-if (e.target.classList.contains('view-comments')) {
-  var commentSection = $(this).prev('.show-toggle');
-  if (commentSection.hasClass('show')) {
-    commentSection.addClass('comments').removeClass('show');
-  } else if (commentSection.hasClass('comments')) {
-    commentSection.addClass('show').removeClass('comments');
-  }
-}
-});
+    //deletes the post
+    $('.action-buttons').unbind('click').click(function(e) {
+      if (e.target.classList.contains('remove-post')) {
+        var thisPost = $(this).closest('.row')
+        thisPost.remove();
+      }
+      //edit function
+      if (e.target.classList.contains('edit-post')) {
+        var edit = prompt("Place edit here");
+        var targetEdit = $(this).closest('.col').find('.post');
+        targetEdit.text(edit);
+      }
+
+      //toggles comment section and forms
+      if (e.target.classList.contains('view-comments')) {
+        var commentSection = $(this).prev('.show-toggle');
+        if (commentSection.hasClass('show')) {
+          commentSection.addClass('comments').removeClass('show');
+        } else if (commentSection.hasClass('comments')) {
+          commentSection.addClass('show').removeClass('comments');
+        }
+      }
+    });
 
 
 
 
-});
+  });
