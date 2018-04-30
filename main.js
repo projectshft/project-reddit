544390dd271b08f1b7bed680a08d8d7d67a4d574
davieu <davisumana@gmail.com>
2018-04-29 20:31:31 -0400

    comment toggle

--- main.js
+++ main.js
@@ -1,58 +1,38 @@
-// $('button').on('click', function(){
-//   var post = $('<p>' + $('#post').val() + '<p>');
-//   $('.posts').append(post)
-//   $('#post').val('');
-//
-//
-//   var name = $('<p>' + $('#name').val() + '<p>');
-//   $('.posts').append(name)
-//   $('#name').val('');
-//
-// })
+$(document).ready(() => {
 
+  $('#post-button').on('click', () => {
+    var $postInputed = $('<p>' + $('#post-input').val() + '</p>');
+    var $nameInputed = $('<p>' + '<b>Posted by:</b> ' + $('#name-input').val() + '</p><hr>');
+    var $testing = $(
+      '<div class="comments">' +
+        '<input type="text" id="comment-input" class="comments" placeholder="Comment Text"></input>' +
+        '<input type="text" id="username-input" class="comments" placeholder="User Name"></input>' +
+        '<button type="button" id="comment-button" class="comments btn btn-primary btn-sm">Comment</button>' +
+      '</div>'
+    );
 
-$(document).ready(function(){
-//toggle comments
+    var $comments ='<a href="#" class="toggle-me">comment</a>'
+    $('.post-section').append($comments);
+    $('.post-section').append($postInputed);
+    $('#post-input').val('');
 
+    $('.post-section').append($testing);
 
-  $('#toggle-comment').click(function(){
-      $('.comments').toggle();
+    $('.post-section').append($nameInputed);
+    $('#name-input').val('');
 
-
+      //   $comments.click(function(){
+      // $(this).toggle()
+      // })
   });
 
 
-//click button to post your message and name
-$('#post-button').on('click', function() {
+          $('.comments').hide();
+
+  $('a').click(()=>{
+    $('.comments').toggle();
+  });
 
 
-  var postInputed = $('<p>' + $('#post-input').val() + '</p>');
-  var nameInputed = $('<p>' + 'Posted By: ' + $('#name-input').val() + '</p>');
 
-  var addComment = $( "#toggle-comment" )
-    .html("<a href='#'>Comments</a>");
-
-var hey= $('.posts').append(postInputed);
-
-$('.posts').prepend("<a id='toggle-comment' href='#'>Comments</a>");
-$('#post-input').val('');
-
-  $('.names').append(nameInputed);
-  $('#name-input').val('');
-});
-
-// click button to post comments
-$('#comment-button').on('click', function() {
-
-  var commentInputed = $('<span>' + $('#comment-input').val() + '</span>' +  '<span>'+ ' ' + 'Posted By: ' + $('#username-input').val() + '</span>' + '<p>' + '</p>');
-  // var usernameVar= $('<span>'+ ' ' + 'Posted By: ' + $('#username-input').val() + '</span>');
-
-
-  $('.comment').append(commentInputed );
-  $('#comment-input').val('');
-  $('#username-input').val('');
-
-});
-
-
-});
+})
