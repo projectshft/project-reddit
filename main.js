0d7aba9f433563d89e7b4dd40b7ab0569b23a070
davieu <davisumana@gmail.com>
2018-04-30 08:20:39 -0400

    reworked comments and comment output

--- main.js
+++ main.js
@@ -1,38 +1,53 @@
 $(document).ready(() => {
 
   $('#post-button').on('click', () => {
-    var $postInputed = $('<p>' + $('#post-input').val() + '</p>');
-    var $nameInputed = $('<p>' + '<b>Posted by:</b> ' + $('#name-input').val() + '</p><hr>');
-    var $testing = $(
+
+    var $postInputed = $('<div class="remove"><span><a href="#" class="remove-it">remove</a></span>' + ' ' + '<span><a href="#" class="toggle-me">comments</a></span>' + ' '+ '<span>' + $('#post-input').val() + '</span>');
+    var $nameInputed = $('<p>' + '<b>Posted by:</b> ' + $('#name-input').val() + '</p><hr></div>');
+    var $commentForm = $(
       '<div class="comments">' +
-        '<input type="text" id="comment-input" class="comments" placeholder="Comment Text"></input>' +
-        '<input type="text" id="username-input" class="comments" placeholder="User Name"></input>' +
-        '<button type="button" id="comment-button" class="comments btn btn-primary btn-sm">Comment</button>' +
+      '<input type="text" id="comment-input" class="comments" placeholder="Comment Text"></input>' +
+      '<input type="text" id="username-input" class="comments" placeholder="User Name"></input>' +
+      '<button type="button" id="comment-button" class="comments btn btn-primary btn-sm">Comment</button>' +
       '</div>'
+
     );
 
-    var $comments ='<a href="#" class="toggle-me">comment</a>'
-    $('.post-section').append($comments);
+
+
+//post section
     $('.post-section').append($postInputed);
     $('#post-input').val('');
 
-    $('.post-section').append($testing);
+
+
+    $('.post-section').append($commentForm);
 
     $('.post-section').append($nameInputed);
     $('#name-input').val('');
 
-      //   $comments.click(function(){
-      // $(this).toggle()
-      // })
+
+//comments hide toggle
+    $('.comments').hide();
+    $('.toggle-me').on('click', () => {
+      $('.comments').toggle();
+    });
+//remove post- only works one time
+    $('.remove-it').on('click', () => {
+      $('.post-section').remove();
+    });
+
+    //add comments
+    $('#comment-button').on('click', () => {
+      var $commentInput = $('<p class="comments">' + $('#comment-input').val() + '</p>');
+      var $usernameInput = $('<span class="comments">' + '<b>Posted by:</b> ' + $('#username-input').val() + ' ' + '<a href="#" class="remove-it">remove</a></span>');
+
+      $('.post-section').append($commentInput);
+      $('#comment-input').val('');
+
+      $('.post-section').append($usernameInput);
+      $('#username-input').val('');
   });
 
-
-          $('.comments').hide();
-
-  $('a').click(()=>{
-    $('.comments').toggle();
   });
-
-
-
 })
