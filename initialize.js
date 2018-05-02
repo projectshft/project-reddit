16a902358b063b20c1c12ccd70e95bb9135a6f2f
Matthew Keller <matthewkeller@Rachels-MacBook-Air.local>
2018-05-02 09:09:18 -0400

    added localStorage, added functions to execute for each click, and cleaned up code git push git push origin master

--- initialize.js
+++ initialize.js
@@ -1,26 +1,19 @@
-/*
-        Hello. I'm sure there is an easier way.
-        But this forum functions based on
-        assigning a unique id to every post
-        and comment. It then uses .hover alongside
-        data- to find the numbers associates with
-        the ids and then stores them in variables
-*/
 
-// ------this is the array that stores each post and it's comments as an object------
+var STORAGE_ID = 'project-distillery';
 
+var saveToLocalStorage = function () {
+localStorage.setItem(STORAGE_ID, JSON.stringify(posts));
+}
+
+var getFromLocalStorage = function () {
+  return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+  }
 
 var commentCount=0; //counts amount of comments made
 var postCount=0; //counts amount of posts submitted
 
-var posts = [{
-  'userName': 'Hew',
-  'postMessage': 'Welcome to my project',
-  'comments': [{
-    'commenter': 'Hew',
-    'commentMessage': 'You can make comments here'
-  }],
-}]
+// ------this is the array that stores each post and it's comments as an object------
+
 
 
 //If we want to load the page without any pre-loaded content, simply remove newPost(), newComment, and thisPostId from this page. And delete the object in the area
@@ -31,11 +24,7 @@ console.log("posts array already contains initial object --> pushing to post ski
 
 $( document ).ready(function() {
 
-  let commentSubmit = '#comment-submit-' + 1
-  let commenterName = '#comment-user-' + 1
-  let commenterText = '#post-comment-' + 1
   let thisPostId = 1; //since the page adds the first post object, I initialized thisPostId since there is no hovering that happens to find the variable.
-
-  newPost();
-  newComment(thisPostId);
+  renderPosts();
+  renderComments(thisPostId);
 });
--- postTemplates.js
+++ postTemplates.js
@@ -1,59 +1,77 @@
 /*
         =================== KNOWN ISSUES ===================
-        ---click events happen multiple times
-        ---however, it does not mess with functionality
-        ---Did not delete items from posts array
-        ---ideally posts array re-renders every change made
+        ---Page needs more comments.
+        ---Could document what is happening with more logs.
+        ---Need to work on spacing newlines.
+        ---Create a function: projectApp that holds all the
+            functions that the click events target (make
+            posts private).
+        ---Currently I had more ids in my HTML than I need
+            Started to use .closest() and .sibling(), but
+            need to finish the work.
+        ---Something is wrong with saveToLocalStorage()
+            were I sometimes have to remove a post twice
         ====================================================
 */
-
-// ------creates the html for the new post and appends it to div with class = "posts"------
-var newPost = function() {
-
-  postCount++;
-
-  var source = $('#post-template').html();
-  var template = Handlebars.compile(source);
-  var newHTML = template({postCount:postCount, userName:posts[postCount - 1].userName, message:posts[postCount - 1].postMessage});
-  $(newHTML).appendTo($('.posts')); //------appends the template to the html------
-
-  console.log("appended post # "+postCount+".")
-
-
-  $('input').val('')
-  $('textarea').val('')
-
-  submitComment();
-  doneEdit();
-  editPost();
-  backToPage();
-  removePost();
-  displayCommentBox();
-  targetDisplayPost();
-
-};
-
-var newComment = function(thisPostId) {
-  commentCount++; //keep track of number of comments made
-  let commentsSection = '#comments-section-' + thisPostId
-  let targetPost = posts[thisPostId - 1]
-  let targetComment = targetPost.comments[targetPost.comments.length - 1]
-
-  var source = $('#comment-template').html();
-  var template = Handlebars.compile(source);
-  var newHTML = template({
-    commentCount:commentCount,
-    commenter:targetComment.commenter,
-    commentMessage:targetComment.commentMessage});
-
-  $(newHTML).appendTo($(commentsSection)); //------appends the comments template to the html------
-
-  var commentHover = function() {
-    thisCommentId = $(this).attr('data-comment');
-    deleteComment();
+var renderPosts = function(){
+  let postCheck = getFromLocalStorage();
+  if(postCheck.length==0){ //If statement to pre-load original post if all posts deleted
+    posts = [{
+      'userName': 'Hew',
+      'postMessage': 'Welcome to my project',
+      'comments': [{
+        'commenter': 'Hew',
+        'commentMessage': 'You can make comments here'
+      }],
+    }]
   }
+  else{
+    posts = getFromLocalStorage();
+  }
+  $('.posts').empty();
 
-  $('.a-comment').hover(commentHover)
+  for (var postCount = 0; postCount < posts.length; postCount+= 1){
+
+    var source = $('#post-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template({
+      postCount:postCount+1,
+      userName:posts[postCount].userName,
+      message:posts[postCount].postMessage});
+    $(newHTML).appendTo($('.posts'));
+    /* ------creates the html for the new post and appends it to div with class = "posts"------ */
+
+    $('input').val('')
+    $('textarea').val('')
+  }
+  console.log("appended post # "+posts.length+".")
+}
+
+
+var renderComments = function(thisPostId) {
+  $('.comments-section').empty();
+
+  for (var postCount = 0; postCount < posts.length; postCount+= 1){
+    var post = posts[postCount];
+    var index = posts.indexOf(post);
+    var $post = $('.posts').find('.a-post').eq(index);
+
+    for (var commentCount = 0; commentCount < post.comments.length; commentCount++) {
+      var commentSection = postCount+1
+      var comment = post.comments[commentCount];
+
+      var source = $('#comment-template').html();
+      var template = Handlebars.compile(source);
+      var newHTML = template({
+        commentCount:commentCount+1,
+        commenter:comment["commenter"],
+        commentMessage:comment.commentMessage
+      });
+
+        $(newHTML).appendTo($('#comments-section-'+commentSection));
+        // ------creates the html for the new post and appends it to div with class = "posts"------
+    }
+  }
   $('input').val('')
   $('textarea').val('')
 }
