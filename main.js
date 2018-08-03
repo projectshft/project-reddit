56a925671b9c28c8f3b97731f0530a52dfb7d176
Tanya Cox <tanya54.tb@gmail.com>
2018-08-03 11:31:07 -0400

    first commit-title added, name and message added

---
+++ main.js
@@ -0,0 +1,22 @@
+var button = document.getElementsByTagName('button')[0];
+jj
+button.addEventListener('click', function () { 
+  console.log('button clicked');
+
+  // get name
+	var userInputName = document.getElementById('name').value;
+  console.log('name: ', userInputName);
+
+  // get message
+  var userInputMessage = document.getElementById('message').value;
+	console.log('msg: ', userInputMessage);
+
+  // add name and message to page
+  var userInputElement = document.createTextNode(userInputName + ' - ' + userInputMessage);
+
+  var li = document.createElement('li');
+  li.appendChild(userInputElement);
+
+  var posts = document.getElementsByClassName('posts')[0];
+  posts.append(userInputElement);
+});
