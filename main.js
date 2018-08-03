9049b78c75105adfae4f2838d6cf80d548fad242
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-08-03 15:58:31 -0400

    completing create post functionaility

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 /* ======= 1.user can create posts and comments ====== */
 $(document).ready(function(){
   debugger;
-    $("button").click(function(){ //event handler instructs doc to invoke function when btn clicked
-    var getText = $('#inputPostText').val(); //get value of  #inputPostText text entry and set to variable 'getText'
-    var getName = $('#inputPostText').val(); //get value of  #inputPostText text entry and set to variable 'getText'
-    var $
-
-
-    });
+    $("button").click(function(e){ //event handler instructs doc to invoke function when btn clicked
+      var post = $('#inputPostText').val(); //get value of  #inputPostText text entry and set to variable 'getText'
+      var name = $('#inputName').val(); //get value of  #inputPostText text entry and set to variable 'getText'
+      $('#display-posts').prepend('<div class="row"><div class="post"><p>' + post + '</p><p><b>' + 'Posted By: '+'</b>' + name +'</p></div><hr></div>');
+    }); //tells document to construct and add html to document
 });
+
+
 /* ======= 2.user can leave comments on each posts
 ========== * when user clicks 'comments' (above each post) it toggles comments and input box visible/hidden
 ========== * when user clicks 'x' next to comment, it deletes
