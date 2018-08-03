b486b1a35fc264a864a7fe78bd9f752a4fb032a5
shodan993 <paulnstanley@gmail.com>
2018-08-03 15:34:51 -0400

    part 1 complete

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 //Listen for button clicks, append posts to the list
 var button = $('button');
+var rule = '<hr />';
 
 var clicked = function () {
    var name = $('#name').val();
    var message = $('#message').val();
-   $('.posts').append('<li>' + name + '</li>');
+   $('.posts').append(rule);
    $('.posts').append('<li>' + message + '</li>');
+   $('.posts').append('<li>' + "Posted By: " + '<b>' + name + '</b>'+ '</li>');
 };
 
 $('button').on('click', clicked);
