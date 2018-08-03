59aebec78dbc24fb74aab3a139ffc91ca0b8119e
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-08-03 12:56:48 -0400

    create html,css,jsfiles. structure html page. begin functionality for adding posts

---
+++ main.js
@@ -0,0 +1,17 @@
+/* ======= 1.user can create posts and comments ====== */
+$(document).ready(function(){
+  debugger;
+    $("button").click(function(){ //event handler instructs doc to invoke function when btn clicked
+    var getText = $('#inputPostText').val(); //get value of  #inputPostText text entry and set to variable 'getText'
+    var getName = $('#inputPostText').val(); //get value of  #inputPostText text entry and set to variable 'getText'
+    var $
+
+
+    });
+});
+/* ======= 2.user can leave comments on each posts
+========== * when user clicks 'comments' (above each post) it toggles comments and input box visible/hidden
+========== * when user clicks 'x' next to comment, it deletes
+========== * when user fills out the two comment inputs & clicks post, immediately add comment to list of comments
+========== * when user clicks 'remove' above post should remove post
+ ====== */
