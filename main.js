bcf13cc7f0e0b8a4b9502094796963ba6459f879
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-08-03 21:08:39 -0400

    begin adding functionality to hide and show comment inputs

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-/* ======= 1.user can create posts and comments ====== */
-$(document).ready(function(){
-  debugger;
-    $("button").click(function(e){ //event handler instructs doc to invoke function when btn clicked
-      var post = $('#inputPostText').val(); //get value of  #inputPostText text entry and set to variable 'getText'
-      var name = $('#inputName').val(); //get value of  #inputPostText text entry and set to variable 'getText'
-      $('#display-posts').prepend('<div class="row"><div class="post"><p>' + post + '</p><p><b>' + 'Posted By: '+'</b>' + name +'</p></div><hr></div>');
-    }); //tells document to construct and add html to document
-});
+/* ======= user can create posts ====== */
+// $(document).ready(function(){
+//     $("button").click(function(e){ //event handler instructs doc to invoke function when btn clicked
+//       var post = $('#inputPostText').val(); //get value of  #inputPostText text entry and set to variable 'getText'
+//       var name = $('#inputName').val(); //get value of  #inputPostText text entry and set to variable 'getText'
+//       $('#display-posts').prepend('<div class="row"><div class="post"><p>' + post + '</p><p><b>' + 'Posted By: '+'</b>' + name +'</p></div><hr></div>');//tells document to construct and add html to document
+//     });
+// });
+
+$("button").click(function(){ //event handler instructs doc to invoke function when btn clicked
+$('div#createComments').removeClass('hideComments');
+}); //tells document to construct and add html to document
+
+/* ======= user can create comments ====== */
 
 
 /* ======= 2.user can leave comments on each posts
