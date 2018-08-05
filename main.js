7d2d95933acb9a2779ffe35be8e5c03893a1cb14
Tanya Cox <tanya54.tb@gmail.com>
2018-08-05 15:01:59 -0400

    Modify new posts structure

--- main.js
+++ main.js
@@ -27,13 +27,40 @@ button.addEventListener('click', function () {
 
 $(document).ready(function() {
   console.log('document loaded');
+
   var submitButton = $('#button');
-    console.log(submitButton);
-    submitButton.click(function () { 
-      console.log('jquery button is clicked');
+  console.log(submitButton);
 
-    
+  submitButton.click(function () { 
+    console.log('jquery button is clicked');
 
+    // get value of name-add "Posted by;"
+    var userInputName = $('#name').val();
+    console.log(userInputName);
+    userInputName = "Posted By: " + userInputName;
+
+    //get message input
+    var userInputMessage = $('#message').val();
+    console.log(userInputMessage);
+    userInputMessage = "Post: " + userInputMessage;
+
+    // create message div and add html
+    var message = document.createElement('div');
+    message.innerHTML = userInputMessage;
+
+    //create author div and add html 
+    var author = document.createElement('div');
+    author.innerHTML = userInputName;
+
+    // create outer div to store inner message and author divs
+    var newPost = document.createElement('div');
+    newPost.append(message);
+    newPost.append(author);
+  
+    //add new post to html div for posts 
+    var posts = document.getElementsByClassName('posts')[0];
+    posts.append(newPost);
+   
     });
   
 
