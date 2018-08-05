8ca7fabb01d8899733a6511a605fc07f62f57688
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-08-04 21:29:37 -0400

    adding functionality that displays comment link above posts and shows comment input form when comment link is clicked.

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
-/* ======= user can create posts ====== */
-// $(document).ready(function(){
-//     $("button").click(function(e){ //event handler instructs doc to invoke function when btn clicked
-//       var post = $('#inputPostText').val(); //get value of  #inputPostText text entry and set to variable 'getText'
-//       var name = $('#inputName').val(); //get value of  #inputPostText text entry and set to variable 'getText'
-//       $('#display-posts').prepend('<div class="row"><div class="post"><p>' + post + '</p><p><b>' + 'Posted By: '+'</b>' + name +'</p></div><hr></div>');//tells document to construct and add html to document
-//     });
-// });
+/*======= template for comment section ===== */
 
-$("button").click(function(){ //event handler instructs doc to invoke function when btn clicked
-$('div#createComments').removeClass('hideComments');
-}); //tells document to construct and add html to document
+  var commentFormTemplate =
+  '<div class="container hideComments" id="createComments">'
+  + '<form class="form-inline">'
+  + '<input type="text" class="form-control" id="commentInput" placeholder="Comment">'
+  + '<input type="text" class="form-control" id="commentPosterName" placeholder="Name">'
+  + '<button type="button" class="btn btn-primary">'
+  + 'Submit'
+  + '</button>'
+  + '</form>'
+  + '</div>'
+;
+
+// var postContainerTemplate =
+// '<div class="row">'
+// + '<div class="post">'
+// + '<p>'
+// + '<a href="#" class="comment-link">'
+// + 'Comment'
+// + '</a>'
+// + '</p>'
+// + '</div>'
+// + '</div>'
+
+/* ======= user can create posts ====== */
+
+$(document).ready(function(){
+    $("button").click(function(e){ //event handler instructs doc to invoke function when btn clicked
+      var post = $('#inputPostText').val(); //get value of  #inputPostText text entry and set to variable 'getText'
+      var name = $('#inputName').val(); //get value of  #inputPostText text entry and set to variable 'getText'
+      $('#display-posts').prepend(
+        '<div class="row"><div class="post"><a href="#" class="comment-link">'+'Comment'+'</a><p>'+post+'</p><p><b>'+'Posted By: '+'</b>'+name+'</p></div>'+ commentFormTemplate+'<hr></div>');//tells document to construct and add html to document
+    });
+
+});
 
 /* ======= user can create comments ====== */
 
+//comment form was added with the hideComments class when original post was submitted and added to page
+$(document).on('click','.comment-link', function(){
+  $("#createComments").removeClass('hideComments');
+});
 
 /* ======= 2.user can leave comments on each posts
 ========== * when user clicks 'comments' (above each post) it toggles comments and input box visible/hidden
@@ -20,3 +48,9 @@ $('div#createComments').removeClass('hideComments');
 ========== * when user fills out the two comment inputs & clicks post, immediately add comment to list of comments
 ========== * when user clicks 'remove' above post should remove post
  ====== */
+
+ /* ========= save for later use =============
+
+ $('div#createComments').removeClass('hideComments');
+
+ ====================== */
