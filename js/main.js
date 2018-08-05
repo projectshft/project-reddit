115925fca2fe42c73fbf6754813dfb96379463e9
shodan993 <paulnstanley@gmail.com>
2018-08-05 18:38:19 -0400

    comment function changed from dynamic html to toggled element, remove comment function added, still buggy

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// var postButton = $('#post-button'); //this doesn't get used in function but might have a purpose later?
-
 var rule = '<hr />';
 
 //Listen for post button clicks, append posts to the post list
@@ -9,6 +7,7 @@ var postButtonClicked = function () {
   $('.posts').append(rule);  //add divider
   $('.posts').append('<li>'+'<span id=removePost>'+"remove "+'</span>'+'<span id=commentPost>'+"comments "+'</span>'+'<span id=newMessage>'+message+'</span>'+'</li>');
   $('.posts').append('<li>' + "Posted By: " + '<b>' + name + '</b>'+ '</li>');  //add the post
+  $('.posts').append('<ul id=commentList>' + '</ul>') //placeholder for comments
   $('#name').val('');  //clear the input boxes
   $('#message').val('');
   $('#removePost').on('click', removePost);  //invoke whenever this function runs, for dynamically added elements
@@ -16,41 +15,76 @@ var postButtonClicked = function () {
 };
 
 //only the first remove button is clickable, and it removes all the li items -- tried this/parent/child, revisit later
+//add a check for the comment section -- must hide comments before removing li or you lose comment button
 var removePost = function () {
-  $('li').remove();
+  if ($('#comment-submission').css('display') == 'none') {
+    $('li').remove();
+  } else {
+    $('#comment-submission').hide();
+    $('li').remove();
+  }
 };
 
 //also only works on the first comment button
 //this dynamically creates a comment section, but i think that's wrong -- i need to define it in index.html and toggle class here to show/hide
-//tried that but couldn't get working - revisit later
+// var commentPost = function () {
+//   $('.posts').append(
+//     '<div class="comment-submission">' +
+//       '<div class="container">' +
+//         '<div class="row text-left">' +
+//           '<div class="col-md-4">' +
+//             '<div class="form-group">' +
+//               '<input id="comment" type="text" class="form-control" placeholder="Your Comment">'+'</textarea>' +
+//             '</div>' +
+//           '</div>' +
+//           '<div class="col-md-4">' +
+//             '<div class="form-group">' +
+//                 '<input id="name" type="text" class="form-control" placeholder="Your Name">'+'</input>' +
+//             '</div>' +
+//           '</div>' +
+//           '<div class="col-md-4">' +
+//             '<button type="button" id="post-button" class="btn btn-primary">' + 'Comment' + '</button>' +
+//             '</div>' +
+//           '</div>' +
+//         '</div>' +
+//     '</div>'
+//   );
+//
+//   var name = $('#name').val();
+//   var comment = $('#message').val();
+//
+//   $('li').append('<li>' + comment + " Posted By: " + '<b>' + name + '</b>' + '</li>');
+//
+// };
+
+//rewriting to toggle HTML on or off instead of generating dynamically
+//works! but how do i make it a "child" of a dynamically added li instead of in a fixed location?
+//still only works on the first li
 var commentPost = function () {
-  $('.posts').append(
-    '<div class="comment-submission">' +
-      '<div class="container">' +
-        '<div class="row text-left">' +
-          '<div class="col-md-4">' +
-            '<div class="form-group">' +
-              '<input id="comment" type="text" class="form-control" placeholder="Your Comment">'+'</textarea>' +
-            '</div>' +
-          '</div>' +
-          '<div class="col-md-4">' +
-            '<div class="form-group">' +
-                '<input id="name" type="text" class="form-control" placeholder="Your Name">'+'</input>' +
-            '</div>' +
-          '</div>' +
-          '<div class="col-md-4">' +
-            '<button type="button" id="post-button" class="btn btn-primary">' + 'Comment' + '</button>' +
-            '</div>' +
-          '</div>' +
-        '</div>' +
-    '</div>'
-  );
+  if ($('#comment-submission').css('display') == 'none') {
+    $('#comment-submission').show();
+  } else {
+    $('#comment-submission').hide();
+  }
+};
 
-  var name = $('#name').val();
-  var comment = $('#message').val();
+var commentButtonClicked = function () {
+  var commentName = $('#comment-name').val();
+  var comment = $('#comment').val();
 
-  $('li').append('<li>' + comment + " Posted By: " + '<b>' + name + '</b>' + '</li>');
+  $('#commentList').append('<button id="removeComment" type="button" class="close" aria-label="Close">'+'<span aria-hidden="true">'+'&times;'+'</span>'+'</button>');
+  $('#commentList').append('<li>' + comment + " Posted By: " + '<b>' + commentName + '</b>' + '</li>');
+  $('#removeComment').on('click', removeComment);
+}
 
+var removeComment = function () {
+  if ($('#comment-submission').css('display') == 'none') {
+    $('#commentList').remove();
+  } else {
+    $('#comment-submission').hide();
+    $('commentList').remove();
+  }
 };
 
 $('#post-button').on('click', postButtonClicked);
+$('#comment-button').on('click', commentButtonClicked);
