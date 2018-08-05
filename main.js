ccd3c398cc46ca807b7f7c7f207326d925d17e38
Tanya Cox <tanya54.tb@gmail.com>
2018-08-05 18:35:49 -0400

    finsh updating javascript selectors to jquery, added remove and comments to newPosts

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-// // plain javascript
-
-// var button = document.getElementsByTagName('button')[0];
-
-// button.addEventListener('click', function () { 
-//   console.log('button clicked');
-
-//   // get name
-//   var userInputName = document.getElementById('name').value;
-//   console.log('name: ', userInputName);
-
-//   // get message
-//   var userInputMessage = document.getElementById('message').value;
-//   console.log('msg: ', userInputMessage);
-
-//   // add name and message to page
-//   var userInput = document.createTextNode(userInputName + ' - ' + userInputMessage);
-
-//   var newPost = document.createElement('li');
-//   newPost.appendChild(userInput);
-
-//   var posts = document.getElementsByClassName('posts')[0];
-//   posts.append(newPost);
-// });
-
-// jQuery 
-
 $(document).ready(function() {
   console.log('document loaded');
 
@@ -38,11 +11,22 @@ $(document).ready(function() {
     var userInputName = $('#name').val();
     console.log(userInputName);
     userInputName = "Posted By: " + userInputName;
+    
 
     //get message input
     var userInputMessage = $('#message').val();
     console.log(userInputMessage);
     userInputMessage = "Post: " + userInputMessage;
+  
+
+
+    var remove = document.createElement('div');
+    remove.innerHTML = "Remove";
+    var comments = document.createElement('div');
+    comments.innerHTML = "Comments";
+    var x = document.createElement('div');
+    
+
 
     // create message div and add html
     var message = document.createElement('div');
@@ -54,14 +38,16 @@ $(document).ready(function() {
 
     // create outer div to store inner message and author divs
     var newPost = document.createElement('div');
+    newPost.append(remove);
+    newPost.append(comments);
     newPost.append(message);
     newPost.append(author);
 
     //modify newPost class
     newPost.setAttribute("class", "newPost");
-  
+
     //add new post to html div for posts 
-    var posts = document.getElementsByClassName('posts')[0];
+    var posts = $('#posts');
     posts.append(newPost);
     
     // add line break to end of posts div
@@ -69,14 +55,21 @@ $(document).ready(function() {
     lineBreak.innerHTML = "<hr>";
     posts.append(lineBreak);
 
+  }); // click event
+  
+
+    var posts = $('#posts');
+    posts.click(function () { 
+      console.log(this);
     });
 
+}); //on load event
+    
 
 
-
-});
-
+  
 
 
+  
 
 
