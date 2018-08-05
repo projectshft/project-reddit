9bb657acd5152e3e4a166a8d686dc74211c04a99
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-08-05 12:45:46 -0400

    simplify html & dom, complete functionality for adding comment to an original post

--- main.js
+++ main.js
@@ -1,46 +1,42 @@
 /*======= template for comment section ===== */
 
   var commentFormTemplate =
-  '<div class="container hideComments" id="createComments">'
-  + '<form class="form-inline">'
+  '<form class="form-inline hideComments" id="createComments">'
   + '<input type="text" class="form-control" id="commentInput" placeholder="Comment">'
   + '<input type="text" class="form-control" id="commentPosterName" placeholder="Name">'
-  + '<button type="button" class="btn btn-primary">'
-  + 'Submit'
+  + '<button type="button" class="btn btn-primary" id="submitCommentBtn">'
+  + 'Submit Comment'
   + '</button>'
   + '</form>'
-  + '</div>'
 ;
 
-// var postContainerTemplate =
-// '<div class="row">'
-// + '<div class="post">'
-// + '<p>'
-// + '<a href="#" class="comment-link">'
-// + 'Comment'
-// + '</a>'
-// + '</p>'
-// + '</div>'
-// + '</div>'
-
 /* ======= user can create posts ====== */
-
 $(document).ready(function(){
-    $("button").click(function(e){ //event handler instructs doc to invoke function when btn clicked
-      var post = $('#inputPostText').val(); //get value of  #inputPostText text entry and set to variable 'getText'
-      var name = $('#inputName').val(); //get value of  #inputPostText text entry and set to variable 'getText'
-      $('#display-posts').prepend(
-        '<div class="row"><div class="post"><a href="#" class="comment-link">'+'Comment'+'</a><p>'+post+'</p><p><b>'+'Posted By: '+'</b>'+name+'</p></div>'+ commentFormTemplate+'<hr></div>');//tells document to construct and add html to document
-    });
+  $("button").click(function(e){ //event handler instructs doc to invoke function when btn clicked
+    var post= $('#inputPostText').val(); //value of #inputPostText text entry set to variable 'post'
+    var posterName= $('#inputName').val(); //value of #inputName text entry set to variable 'posterName'
 
+    $('#display-posts').prepend('<div class="row post-container"><p class="post">'+ post +'</p><p><b>'+'Posted By: '+'</b>'+ posterName +'</p>'+'<a href="#" class="comment-link">'+'Comment'+'</a><div class="comment-container"></div>'+commentFormTemplate+'</div><hr>');//tells document to construct and add html to document
+  });
+});
+/* ======= user can create comments ======
+ - when comments link is clicked, show comment input form
+ - when user inputs comment-text and clicks sumbit comment, this comment displays below post
+ - once comment is submitted, comment input form is hidden
+========================================== */
+// a hidden comment form was added when original post was submitted and added to page
+
+$(document).on('click','.comment-link', function(){ //when elements with class comment-link are clicked
+
+  $(this).siblings('#createComments').removeClass('hideComments'); //remove class that hides the comment form
+  $('form').on('click','#submitCommentBtn',function(){ //when comment form submit button is clicked
+    var comment = $('#commentInput').val(); //value of #commentInput entry set to variable 'comment'
+    var commenterName = $('#commentPosterName').val();//value of #commentPosterName text set to variable 'commenterName'
+    $(this).siblings('.comment-container').append('<div class="comment">'+'<p>'+comment+'</p>'+'<p>'+'<b>'+'Posted By: '+'</b>'+commenterName+'</p>'+'<a href="#" class="comment-link">'+'Comment'+'</a>'+'</div>');
+    $("#createComments").addClass('hideComments'); //adds class that is setting comment form display to none
+  });
 });
 
-/* ======= user can create comments ====== */
-
-//comment form was added with the hideComments class when original post was submitted and added to page
-$(document).on('click','.comment-link', function(){
-  $("#createComments").removeClass('hideComments');
-});
 
 /* ======= 2.user can leave comments on each posts
 ========== * when user clicks 'comments' (above each post) it toggles comments and input box visible/hidden
