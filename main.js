bca62bfe08ef1151ab3d2a68aa8c98732fc64bd0
AaronStearns <stearnsconcepts@gmail.com>
2018-08-05 13:21:11 -0400

    adding remove and comment buttons

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
-// <ul class="names"></ul>
-// <input class="name"/>
-// <button>Submit</button>
-//
-// var button = document.getElementsByTagName('button')[0];
-//
-// button.addEventListener('click', function () {
-//   var userInput = document.getElementsByClassName('name')[0].value;
-//   var li = document.createElement('li');
-//   var userInputElement = document.createTextNode(userInput);
-//
-//   li.appendChild(userInputElement);
-//
-//   document.getElementsByClassName('names')[0].append(li);
-// });
-
-// <button id="button1" type="submit" class="btn btn-primary">Post</button>
 var posts = [
-  { text: 'oh yea', name: 'Sean',
+  { text: 'oh yea', name: 'Sean', time: null,
     comments:  [
       {text: 'so what?', name: 'David'}
     ]
-  },
-  { text: 'no!', name: 'Aaron',
-    comments: []
   }
 ]
 
@@ -30,56 +10,38 @@ var $submitButtom = $('#button1');
 var $userName = $('#name');
 var $textInput = $('#message');
 
-
-
-var postsPrimary = { text: null, name: null, comments: [ {text: null, name: null} ] }
-var postsTemplate = postsPrimary.slice()
-
 var dataUpdater = function () {
-
+  postsSendoff = {}
+  var dateTime = new Date().toLocaleString();
   $submitButtom.click(function() {
-    // alert(posts[0].text);
-    postsTemplate.text = $textInput.val()
-    postsTemplate.name = $userName.val()
-    posts.push(postsTemplate)
+    postsSendoff.text = $textInput.val()
+    postsSendoff.name = $userName.val()
+    postsSendoff.datetime = dateTime
+    posts.push(postsSendoff)
   })
 };
 
 dataUpdater();
 
-//
-// var button = document.getElementsByTagName('button')[0];
-//
-// button.addEventListener('click', function () {
-//   var userName = document.getElementById('name').value;
-//   var textInput = document.getElementById('message').value;
-//   var li = document.createElement('li');
-//   var userNameElement = document.createTextNode(userName);
-//   var userTextElement = document.createTextNode(textInput);
-//   li.appendChild(userNameElement);
-//   li.appendChild(userTextElement);
-//   document.getElementById('input-here').append(li);
-//
-// });
-//
-// var makePost = function (yourName, theText) {
-//   var postTemplate =
-//     '<tr class="post-block">'
-//   + ' <td class="post-main" data-post-text="' + theText '">'
-//   + ' <td class="poster" data-post-name=" 'Posted by: + yourName ' ">'
-// }
+var createPost = function (text, name, date) {
+  var template =
+     '<ul>'
+   + '  <li class="li-click">' + 'remove' + ' ' + 'comment' + '</li>'
+   + '  <li class="li-plain">' + text + '</li>'
+   + '  <li class="li-plain">' + "<strong>Posted by: </strong>" + name + '</li>'
+   + '  <li class="li-plain">' + '<i>' + date + '</i>' + '</li>'
+   + '</ul>'
+   ;
+   var $row = $(template);
+   return $row;
+};
 
-//
-// var createSongRow = function (songNumber, songName, songLength) {
-//   var template =
-//      '<tr class="album-view-song-item">'
-//    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-//    + '  <td class="song-item-title">' + songName + '</td>'
-//    + '  <td class="song-item-duration">' + songLength + '</td>'
-//    + '</tr>'
-//    ;
-//
-//    var $row = $(template);
-//
-//    return $row;
-// };
+var postPost = function () {
+  $submitButtom.click(function() {
+    var $postList = $('.post-list');
+    var $postrow = createPost($textInput.val(), $userName.val(), new Date().toLocaleString())
+    $postList.append($postrow)
+  })
+}
+
+postPost();
