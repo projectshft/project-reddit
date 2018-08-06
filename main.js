9c858d52c7671ce5e15659de388c68bc0e61ca8d
Charles Ferringer <charlesferringer@yahoo.com>
2018-08-05 21:48:30 -0400

    create and remove posts

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-//listen for clicks and get values from input boxes
-var onClick = $('button').on('click', function () {
+var commentCommentHtml = "<input id='comment-comment' placeholder='comment'></input>";
+var commentUserHtml = "<input id='comment-user' placeholder='username'></input>";
+var postRemoveHtml = "<a id='remove-post'>remove</a>";
+var postCommentsHtml = "<a id='view-comments'>comments</a>";
+var commentButtonHtml = "<button type='button' class='btn btn-primary' id='create-comment'>Submit Comment</button>"
+
+//listen for clicks and get values from input boxes for posts
+var createPost = $('#create-post').on('click', function () {
   var postText = $('#post-text').val();
   var yourName = $('#your-name').val().bold();
 
@@ -11,19 +17,40 @@ var onClick = $('button').on('click', function () {
     alert('You must enter text in both fields');
   }
   else if (postText != '' && yourName != '') {
-    $('.comments').append('<li>' + "<a>" + "remove" + " " + "</a>" + "<a>"
-    + "comments" + "</a>" + " " + postText + '</li>' + '<li>'
-    + 'Posted by: ' + yourName + '</li>' + '<br>');
+    $('#posts-list').append("<li>" + postRemoveHtml + " " + postCommentsHtml + " " + postText + '</li>' + '<li>'
+    + 'Posted by: ' + yourName + '</li>' + '<br>' + commentCommentHtml + '<br>' + commentButtonHtml);
   }
+//remove post from page
+  var remove = $("#remove-post").click(function(){
+      // for (i = 0; i <)
+      $("li").remove();
+      $("#comment-comment").remove();
+      $("#create-comment").remove();
+  });
 });
 
-var remove = $("#remove").click(function(){
-    $("li").remove();
-});
-//remove items from page
-// $('body').on('click', 'li', function () {
-//   $(this).remove();
+// var createComment = $('#create-comment').on('click', function () {
+//   var postComment = $('#post-text').val();
+//   var commentName = $('#your-name').val().bold();
+//
+// //add post to the page
+//   if (postText == '' && yourName == '') {
+//     alert('You must enter text in both fields.');
+//   }
+//   else if (postText == null && yourName == null) {
+//     alert('You must enter text in both fields');
+//   }
+//   else if (postText != '' && yourName != '') {
+//     $('#posts-list').append("<li>" + "<a>" + "remove" + " " + "</a>" + postComments + " " + postText + '</li>' + '<li>'
+//     + 'Posted by: ' + yourName + '</li>' + '<br>' + commentComment + '<br>' + commentButton);
+//   }
 // });
+
+
+//toggles comment section on and off
+$("#view-comments").click(function(event){
+       $("comment-section").toggle();
+   });
 
 //get user input upon hitting enter
 // function runScript(e) {
