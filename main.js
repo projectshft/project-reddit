cbe7195269c6c3ed0c3eb92ee91ac9df3dbf1c5b
AaronStearns <stearnsconcepts@gmail.com>
2018-08-05 20:48:19 -0400

    added show/hide to toggle comment pane

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-// 'posts' object with one example
-var posts = [
-  { text: 'oh yea', name: 'Sean', time: null,
-    comments:  [
-      {text: 'so what?', name: 'David'}
-    ]
-  }
-]
+// 'posts' object with one dummy example
+var posts = [{
+   text:  null,
+   name: null,
+   time: null,
+   comments: [{
+      text: null,
+      name: null
+   }]
+}];
 
-var $submitButtom = $('#button1');
+var $submitButton = $('#button1');
 var $userName = $('#name');
 var $textInput = $('#message');
 
+// saves displayed post data in the 'posts' object
 var dataUpdater = function () {
   postsSendoff = {}
-  var dateTime = new Date().toLocaleString();
-  $submitButtom.click(function() {
+  var dateTime = new Date().toLocaleString()
+  $submitButton.click(function() {
     postsSendoff.text = $textInput.val()
     postsSendoff.name = $userName.val()
     postsSendoff.datetime = dateTime
@@ -32,28 +35,43 @@ var createPost = function (text, name, date) {
     + '  <li class="li-plain">' + '<i>' + date + '</i>' + '</li>'
     + '</ul>'
     ;
-   var $row = $(post);
-
-   return $row;
+   var $row = $(post)
+   return $row
 };
 
+// display post containing date/time of posting
 var postPost = function () {
-  $submitButtom.click(function() {
-    var $postList = $('.post-list');
+  $submitButton.click(function() {
+    var $postList = $('.post-list')
     var $postrow = createPost($textInput.val(), $userName.val(), new Date().toLocaleString())
     var $commentButton = $('<button/>').addClass('comment-buttton').css({'background-color': 'rgb(222, 243, 251)',
       'width': '100px',
       'height': '20px',
       'border-radius': '5'}).html("Comment")
-    $postList.append($commentButton);
+    $postList.append($commentButton)
     var $removeButton = $('<button/>').addClass('remove-buttton').css({'background-color': 'rgb(222, 243, 251)',
       'width': '100px',
       'height': '20px',
       'padding-right': '5px',
       'border-radius': '5'}).html("Remove")
-    $postList.append($removeButton);
-    $postList.append($postrow);
+    $postList.append($removeButton)
+    $postList.append($postrow)
+  })
+};
+
+postPost();
+
+var showHide = function () {
+  var $commenter = $('.comment-button')
+  var $hidden = $('#hidden-form')
+  $commenter.click(function () {
+    if ($hidden.style.display = "none") {
+      $hidden.style.display = "inline"
+    }
+    else if ($hidden.style.display = "inline") {
+      $hidden.style.display = "none"
+    }
   })
 }
 
-postPost();
+showHide();
