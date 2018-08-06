9d7d0718b2c7198812e47e6f015e97fd85f54da4
ethankpow <ethankpow@gmail.com>
2018-08-06 02:20:13 -0400

    fourth

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var addPost = function (){
   //get text from input on view
   var text = $('#input-box').val();
   var name = $('#input-box1').val();
+
   // create new post
   var post = {
     text: text,
@@ -18,7 +19,39 @@ var addPost = function (){
   var target = document.getElementById('list-target');
   // inject new string HTML into target DOM Element
   target.innerHTML = createListMarkup(posts);
+  // $("#delete-comment").click(function(){
+  //
+  // })
+
+//Adding html elements when hypertext link is clicked
+
+  $(".add-comment").click(function(){
+    $(".comment-row").append( '<div>' + '<input type="text" class="form-control1" id="input-box2" placeholder="Comment" aria-label="Recipient"></input>'
+    + '<input type="text" class="form-control1" id="input-box3" placeholder="User Name" aria-label="Recipient"></input>'
+    + '<button onclick="createComment()" id="com-button" type="button" class="btn btn-primary btn-small">Post</button>' + '</div>');
+
+
+
+
+  })
+
 };
+var comments = [];
+var createComment = function () {
+
+  var userInput = $("#input-box2").val();
+  var userId = $("#input-box3").val();
+
+    var comment = {
+      input: userInput,
+      id: userId
+  }
+debugger;
+
+  comments.push(comment);
+  console.log(comments);
+};
+
 //return unique ID
 // var getId = function () {
 //   seed++
@@ -27,8 +60,9 @@ var addPost = function (){
 var createListMarkup = function(posts){
   var markup = '';
   var namePrepend = "Posted By:";
+
   for(var i = 0; i < posts.length; i++){
-    markup += '<p>' + '<a href="#">Remove</a>' + ' ' + '<a href="#">Comment</a>' + ' ' + posts[i].text + '</p>' + '<p>' + namePrepend + ' ' + posts[i].name + '</p>';
+    markup += '<div class="comment-row">' + '<p>' + '<a href="#" id="delete-comment">Remove</a>' + ' ' + '<a href="#" class="add-comment">Comment</a>' + ' ' + posts[i].text + '</p>' + '<p>' + namePrepend + ' ' + posts[i].name + '</p>' + '</div>';
   }
   return markup;
 }
