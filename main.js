22a8d9406ceb7deed7da84487017538f0c35e7d3
Charles Ferringer <charlesferringer@yahoo.com>
2018-08-05 22:16:13 -0400

    add comments to posts

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var commentCommentHtml = "<input id='comment-comment' placeholder='comment'></input>";
-var commentUserHtml = "<input id='comment-user' placeholder='username'></input>";
+var commentCommentHtml = "<input id='comment-text' placeholder='comment'></input>";
+var commentUserHtml = "<input id='comment-name' placeholder='username'></input>";
 var postRemoveHtml = "<a id='remove-post'>remove</a>";
 var postCommentsHtml = "<a id='view-comments'>comments</a>";
 var commentButtonHtml = "<button type='button' class='btn btn-primary' id='create-comment'>Submit Comment</button>"
@@ -9,7 +9,7 @@ var createPost = $('#create-post').on('click', function () {
   var postText = $('#post-text').val();
   var yourName = $('#your-name').val().bold();
 
-//add post to the page
+  //add post to the page
   if (postText == '' && yourName == '') {
     alert('You must enter text in both fields.');
   }
@@ -18,39 +18,40 @@ var createPost = $('#create-post').on('click', function () {
   }
   else if (postText != '' && yourName != '') {
     $('#posts-list').append("<li>" + postRemoveHtml + " " + postCommentsHtml + " " + postText + '</li>' + '<li>'
-    + 'Posted by: ' + yourName + '</li>' + '<br>' + commentCommentHtml + '<br>' + commentButtonHtml);
+    + 'Posted by: ' + yourName + '</li>' + '<br>' + commentCommentHtml + commentUserHtml + '<br>' + commentButtonHtml);
   }
-//remove post from page
+
+  //remove post from page
   var remove = $("#remove-post").click(function(){
+      //loop through to remove just the post that is clicked on
       // for (i = 0; i <)
       $("li").remove();
-      $("#comment-comment").remove();
+      $("#comment-text").remove();
       $("#create-comment").remove();
+      $("#comment-name").remove();
+  });
+  //listen for clicks and get values from input boxes for comments
+  var createComment = $('#create-comment').on('click', function () {
+    var commentText = $('#comment-text').val();
+    var commentName = $('#comment-name').val().bold();
+
+    //add comment to page
+    if (commentText == '' && commentName == '') {
+      alert('You must enter text in both fields.');
+    }
+    else if (commentText == null && commentName == null) {
+      alert('You must enter text in both fields');
+    }
+    else if (commentText != '' && commentName != '') {
+      $('#comments-list').append("<li>" + commentText + '</li>' + '<li>'
+      + 'Posted by: ' + commentName + '</li>');
+    }
+    // toggle comment section on and off
+    $("#view-comments").click(function(event){
+           $("#comment-section").toggle();
+       });
   });
 });
-
-// var createComment = $('#create-comment').on('click', function () {
-//   var postComment = $('#post-text').val();
-//   var commentName = $('#your-name').val().bold();
-//
-// //add post to the page
-//   if (postText == '' && yourName == '') {
-//     alert('You must enter text in both fields.');
-//   }
-//   else if (postText == null && yourName == null) {
-//     alert('You must enter text in both fields');
-//   }
-//   else if (postText != '' && yourName != '') {
-//     $('#posts-list').append("<li>" + "<a>" + "remove" + " " + "</a>" + postComments + " " + postText + '</li>' + '<li>'
-//     + 'Posted by: ' + yourName + '</li>' + '<br>' + commentComment + '<br>' + commentButton);
-//   }
-// });
-
-
-//toggles comment section on and off
-$("#view-comments").click(function(event){
-       $("comment-section").toggle();
-   });
 
 //get user input upon hitting enter
 // function runScript(e) {
