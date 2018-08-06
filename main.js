f4b188b7f50c0881775420ca9d81c29d0665a042
Carrington Perry <carringtonperry@Carringtons-MacBook-Pro.local>
2018-08-06 08:20:47 -0400

    final commit before pull request

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
 
+
 var nameObject = {};
 var textObject = {};
 var postArray =[];
 var initialPost = [];
 var postNewThread = "";
+var commentersName = "";
+var commentersText = "";
 
+
+//original post
 $('#post-button').click(function(){
-
   var postersName = $('.your-name').val();
   var postersText = $('.post-text').val();
   nameObject = {name:postersName};
   textObject = {text:postersText};
   postArray = [nameObject,textObject]
   initialPost.push(postArray);
-
-
-  $('.post-here').append("<li>"+postersText+"<br>Posted By: "+postersName+ "</li");
-
-
+  $('.post-here').append("<dl>"+postersText+"<br>Posted By: <strong>"+postersName+"</strong></dl>");
+  $('.your-name').val('');
+  $('.post-text').val('');
+// add comment box to orginal post
+  $('.post-here').append('<input class = commenter-text placeholder=Comment-Text></input');
+  $('.post-here').append('<input class = commenter-name placeholder=User-Name></input');
+  $('.post-here').append('<button id=post-comment-button class =btn-primary>post comment</button>');
 });
 
+$('dl').each(function(){
+  $('.post-here').append('<input class = commenter-text placeholder=Comment-Text></input>');
+  $('.post-here').append('<input class = commenter-name placeholder=User-Name></input>');
+  $('.post-here').append('<button id=post-comment-button class =btn-primary>Post Comment</button>');
+});
 
-//outline for post should be:
-// array with objects prop 1 is commenter Name
-// prop 2 is text string.
-
-
-
-
-// $( document ).ready(function() {
-//   console.log( 'ready!' );
-//   alert("page is ready");
-// });
+//comments post to orginal post
+$('#post-comment-button').click(function(){
+  commentersText = $('.commenter-text').val();
+  commentersName = $('.commenter-name').val();
+  $('dl').append("<dd>"+commentersText+"  Posted By: <strong>"+commentersName +"</strong></dl>");
+  $('.commenter-text').val('');
+  $('.commenter-name').val('');
+});
+$( document ).ready(function() {});
