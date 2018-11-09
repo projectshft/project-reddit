6b740f88c253a46eaaa81136b4a665c6e5db3f46
James DeVore <jdevore4592@gmail.com>
2018-11-09 18:08:41 -0500

    Initial commit, needs comment delete button and error checks

---
+++ main.js
@@ -0,0 +1,161 @@
+//TODO: made delete comment buttons
+
+// TODO clean up the comment buttons
+
+
+
+// First, I want to listed for a click event on the submit post button
+//
+// when a click is heard, take the input from the input fields and add to psot data.
+//
+// take data from post data and create the post based off the template
+//
+// then update the board with the posts based off the post data
+
+//Array that will hold all the posts
+var allPostsArray = [];
+postCount = 1;
+
+//I want to gather the information and create a post using handlebars
+
+var generatePost = function(postObject) {
+  var postSource = $('#post-template').html();
+  var postTemplate = Handlebars.compile(postSource);
+  var newPostHTML = postTemplate(postObject);
+  return newPostHTML;
+}
+
+//Now I need to generate the comments html
+
+var generateCommentsHTML = function(commentObject) {
+  var commentSource = $('#comment-template').html();
+  var commentTemplate = Handlebars.compile(commentSource);
+  var newCommentHTML = commentTemplate(commentObject);
+  return newCommentHTML;
+}
+
+//render the comments based on array data
+
+var renderComments = function (thisPost){
+
+  $('.comments-section').empty();
+  var commentsArray = thisPost['postComments']
+  commentsArray.forEach(function(commentInArray){
+    var thisCommentHTML = generateCommentsHTML(commentInArray);
+    $('.comments-section').prepend(thisCommentHTML)
+
+})
+}
+
+
+// I want to gather the post information
+var getPostData = function() {
+  var $postAuthor = $('#post-input-name').val();
+  var $postMessage = $('#post-input-message').val();
+  var postData = {
+    uniquePostNumber:postCount,
+    postAuthor:$postAuthor,
+    postMessage:$postMessage,
+    postComments:[]
+  }
+  return postData;
+}
+
+//I want to add the post data to the overall post database
+var addPostData = function(postObject){
+  allPostsArray.push(postObject);
+}
+
+//This function reads data from the database and returns the post Object
+var getPostObjectFromPostArray = function(dataArray, arrayIndex){
+  var retrievedPostObject = dataArray[arrayIndex];
+  return retrievedPostObject;
+}
+
+//This function clears the board and posts the post based on the array data
+ var renderPosts = function(){
+   //clear the board
+   $postBoard = $('.post-list');
+   $postBoard.empty();
+   //get data - add to database - create post from data in database
+   // for each post in the array, make a post
+   allPostsArray.forEach(function(postInArray){
+     var postHTML = generatePost(postInArray);
+     //and append it to the (emptied) board of posts
+     $postBoard.append(postHTML)
+     $('#post-input-name').val("");
+     $('#post-input-message').val("")
+
+   });
+   postCount++
+ };
+
+ //Get the comment data FOR THAT POST and push it to array
+
+ var addCommentDataToArray = function(commentMessage,commentAuthor,postID) {
+  //Put comments into an object)
+  var commentsObject = {
+    commentAuthor:commentAuthor,
+    commentMessage:commentMessage
+  }
+  var correctPostObject = allPostsArray.find(function(postObjects){
+    return postObjects['uniquePostNumber'] == postID;
+  });
+
+   correctPostObject['postComments'].push(commentsObject);
+ }
+
+ //Deletes the post from the main array
+
+ var deletePost = function(postID){
+   var thisPost = allPostsArray.find(function(post){
+     return post["uniquePostNumber"] == postID
+   });
+   var thisPostIndex = allPostsArray.indexOf(thisPost);
+   allPostsArray.splice(thisPostIndex,1);
+
+ }
+
+$(document).ready(function() {
+      $('.submit-post-btn').on('click', function() {
+        addPostData(getPostData());
+        renderPosts();
+      });
+      //event delegation to listen for created elements
+      $(document).on('click', '.comments-btn', function() {
+        var $commentArea = $(this).parent().parent().find('.add-comment');
+        $commentArea.toggle()
+      });
+      $(document).on('click', '.delete-btn', function() {
+        //need to remove it from the post database
+        var $idNumber = $(this).closest('.post').find('.id-number').html();
+        deletePost($idNumber)
+        renderPosts();
+      });
+      $(document).on('click', '.post-comment-btn', function() {
+        //need to have these vars here because this binding is not working how I would like. It is basically grabbing the post-specific data
+
+        var $thisPostCommentsSection = $(this).parent().parent().find('.comments-section');
+        var $commentInputMessage = $(this).siblings('.comment-input-message');
+        var $commentInputAuthor = $(this).siblings('.comment-input-author');
+        var $idNumber = $(this).closest('.post').find('.id-number').html();
+        addCommentDataToArray($commentInputMessage.val(), $commentInputAuthor.val(), $idNumber);
+        //now comment data is on the master array
+        // now I need to render the comments onto the page by finding the correct object from the database
+
+        var thisPost = allPostsArray.find(function(allPosts) {
+          return allPosts['uniquePostNumber'] == $idNumber;
+        });
+
+        var commentsArray = thisPost['postComments']
+        $thisPostCommentsSection.empty()
+        commentsArray.forEach(function(commentInArray) {
+          var thisCommentHTML = generateCommentsHTML(commentInArray);
+          $thisPostCommentsSection.prepend(thisCommentHTML)
+          //empty the edit forms
+          $commentInputMessage.val("");
+          $commentInputAuthor.val("");
+
+        })
+      })
+    })
