d2bb34fb2b9157e383f2a9ba7cb7614cac72ad99
PGNQ <41378170+PGNQ@users.noreply.github.com>
2018-11-09 13:41:07 -0500

    Complete ability to post messages into the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
+var posts = [];
+var source = $('#post-template').html();
+console.log(source);
+var template = Handlebars.compile(source);
 
+$('#post-button').click(function(e){
+  e.preventDefault();
+  var post = {
+    name: $('#post-name').val(),
+    message: $('#post-message').val()
+  };
+  posts.push(post);
+  renderPosts(post);
+})
+
+var renderPosts = function (post) {
+  var newHTML = template(post);
+  console.log('The newHTML is: ', newHTML);
+  $('.post-container').append(newHTML);
+}
