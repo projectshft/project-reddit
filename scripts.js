e8717926b1aac82bfe9cf383a22c6ea7b8654d2c
Rhys Lutsky <rhyslutsky@gmail.com>
2018-11-09 18:13:10 -0500

    finish part 1

--- scripts.js
+++ scripts.js
@@ -1,6 +1,40 @@
+
+//create an object that houses both the posts and the comments
+// var Postings = {
+//     originalPost: { },
+
+//     comments: [
+//         {},
+//     ]
+// };
+
+//var Postings = [];
+
+var createPost = function () {
+    var post = {
+        text: $('#text').val(),
+        name: $('#name').val()
+    }
+    $('.post-list').append('<li>' + post.text +  '<br>' + '<b>'+ " Posted by: " + '</b>' + post.name + '</li>')
+    //Postings['originalPost'].push(post);
+
+};
+
+
+// console.log(Postings);
+
+//get the name and post text from the form when they click the Post button
+$('.btn-primary').click(function (event) {
+    event.preventDefault();
+    createPost();
+});
+
+
+/*
 //Handlebars template compile and execute
-var source = $("comments-template").html();
+var source = $('#comments-template').html();
 var template = Handlebars.compile(source);
-//var context = {post-text:"", post-name:""};
-var html = template(context);
-$('.post-list').append(html);
+//pass in OJBECT as argument
+var html = template({ Postings });
+$('#new-post').append(html);
+*/
