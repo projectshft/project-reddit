7e4c293fbda99d9e1991b04187f57aa6618425c5
Alicia Hetrick <ahetrick22@gmail.com>
2018-11-10 12:47:44 -0500

    editing posts works

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-var $postButton = $('#post-button');
-var $textInput = $('#message');
-var $usernameInput = $('#name');
-var $postsSection = $('#posts');
-var source = $('#post-template').html();
-var template = Handlebars.compile(source);
 var posts = [];
 
 //master unique id generators
@@ -11,9 +5,9 @@ var id = 0;
 var commentId = 0;
 
 //when post button is clicked, the user input text and name are built into a new post
-$postButton.click(() => {
-  newText = $textInput.val();
-  newUsername = $usernameInput.val();
+$('#post-button').click(() => {
+  newText = $('#message').val();
+  newUsername = $('#name').val();
   id++;
   newPost = {
     postText: newText,
@@ -25,10 +19,8 @@ $postButton.click(() => {
   renderPosts();
 })
 
-//builds the top line of remove post & comments show/hide buttons
+//builds the top line for each post with remove post & comments show/hide links
 var createTopLine = (post => {
-  var lineTemplate = '<div class="top-line"><a href="#" class="remove-post" data-post-id="' + post.id + '">remove </a><a href="#" class="comments-show-hide" data-post-id="post' + post.id + '">comments </a>'
-
   //remove the specific post when the remove link is clicked.
   var handleRemoveClick = function(e) {
     e.preventDefault();
@@ -40,27 +32,58 @@ var createTopLine = (post => {
   //show/hide the comments section by individual post.
   var toggleComments = function(e) {
     e.preventDefault();
-    var postId = '#handlebars-' + $(this).data('post-id');
+    var postId = '#single-' + $(this).data('post-id');
     $(postId).children('.comment-area').toggle();
-    $commentArea.toggle();
   }
 
-  //build the line and individually bind click handlers to each link
-  var $topLine = $(lineTemplate);
+  //allow a user to edit the post & username by individual post
+  var handleEditClick = function(e) {
+    e.preventDefault();
+    $(this).next().show();
+    var $editMessageInput = $(this).next().children('.edit-message');
+    var $editUserInput = $(this).next().children('.edit-user');
+    var $editButton = $(this).next().children('.edit-button');
+    //get this post's id so that we can identify the correct post to edit
+    var postId = '#single-' + $(this).data('post-id');
+    var postToEdit = posts.find(post => 'post' + post.id === $(this).data('post-id'));
+
+    //pre-fill the current values into the inputs
+    $editMessageInput.val(postToEdit.postText);
+    $editUserInput.val(postToEdit.username);
+
+    //when the user clicks to submit changes, update the values, hide the section, & re-render
+    $editButton.click(function() {
+      postToEdit.postText = $editMessageInput.val();
+      postToEdit.username = $editUserInput.val();
+      $(this).hide();
+      renderPosts();
+    })
+  }
+
+  //build the line from the handlebars template
+  var topLineSource = $('#top-line-template').html();
+  var topLineTemplate = Handlebars.compile(topLineSource);
+  var $topLine = $(topLineTemplate(post));
+
+  //individually bind click handlers to each link
   $topLine.on('click', '.remove-post', handleRemoveClick);
   $topLine.on('click', '.comments-show-hide', toggleComments);
+  $topLine.on('click', '.edit-post', handleEditClick);
+
   //return the line to the posting click handler to assemble with the post text
   return $topLine;
 })
 
+// get the text & user name and build a new comment into the comment array, along with pushing the comment input to the bottom
 var createCommentLine = (post => {
-  var commentTemplate = '<p><span class="comment-input-line"><input type="text" placeholder="Comment Text" class="comment-text"></input><input type="text" placeholder="User Name" class="comment-user"></input><button class="btn btn-sm btn-primary comment-button" type="button">Post Comment </button> </span>'
 
-  // get the text & user name and build a new comment into the comment array on the correct post
+  //update the posts array with the new comment
   var postComment = function() {
+    //find the correct post and get its index
     var idToSearch = $(this).closest('.single-post').attr('id');
-    var postToComment = posts.find(post => "handlebars-post" + post.id === idToSearch);
+    var postToComment = posts.find(post => "single-post" + post.id === idToSearch);
     var postToCommentIndex = posts.indexOf(postToComment);
+    //build the new comment object with the values in the inputs
     var newCommentText = $(this).prevAll('.comment-text').val();
     var newCommentUser = $(this).prevAll('.comment-user').val();
     commentId++;
@@ -69,53 +92,69 @@ var createCommentLine = (post => {
       commentUser : newCommentUser,
       commentId : commentId
     }
+    //add it to the correct posts and then render the comments on that post
     posts[postToCommentIndex].comments.push(newComment);
     renderComments(postToComment);
-    }
-
-  var $commentLine = $(commentTemplate);
+  }
+  //build the comment input section to put under the newest comment and return it to append
+  var commentInputSource = $('#comment-input-template').html();
+  var commentInputTemplate = Handlebars.compile(commentInputSource);
+  var $commentLine = $(commentInputTemplate());
+  //individually bind the post comment button to the right post and return it to be rendered
   $commentLine.on('click', '.comment-button', postComment);
   return $commentLine;
 })
 
+//create the comment removal icon for each post
 var createCommentRemovalIcon = function(index,comment) {
-var commentRemovalTemplate = '<span class="glyphicon glyphicon-remove remove-comment" data-comment-id="' + comment.commentId + '"></span></p>';
+
+  //find the correct post and filter it out of the comments array
   var removeComment = function() {
     var removeId = $(this).data('comment-id');
-    posts[index].comments = posts[index].comments.filter(selectedComment => selectedComment.commentId !== comment.commentId);
+    posts[index].comments = posts[index].comments.filter(commentToRemove => commentToRemove.commentId !== comment.commentId);
     renderComments(posts[index]);
   }
-  $commentRemovalIcon = $(commentRemovalTemplate);
+  //build the comment removal icon, bind the click listener, and return it to be rendered
+  var commentRemovalIconSource = $('#comment-removal-icon-template').html();
+  var commentRemovalIconTemplate = Handlebars.compile(commentRemovalIconSource);
+  var $commentRemovalIcon = $(commentRemovalIconTemplate(comment));
   $commentRemovalIcon.on('click', removeComment);
   return $commentRemovalIcon;
 }
 
 //clear out the posts section and re-print the current status
 var renderPosts = function() {
+  var $postsSection = $('#posts');
   $postsSection.empty();
+  var postSource = $('#post-template').html();
+  var postTemplate = Handlebars.compile(postSource);
+  //build each post out of a top line followed by the post text and author, then render the comments for it
   posts.forEach(post => {
     var $topLine = createTopLine(post);
-    var newHTMLPost = template(post);
+    var newPost = postTemplate(post);
     $postsSection.append($topLine);
-    $postsSection.append(newHTMLPost);
+    $postsSection.append(newPost);
     renderComments(post);
   })
 }
 
 //clear out the comments and then re-print them for a particular post
 var renderComments = function(post) {
-  var commentAreaId = "#commentspost" + post.id;
-  $(commentAreaId).empty();
-  var source = $('#comment-template').html();
-  var template = Handlebars.compile(source);
-  var commentInput = createCommentLine(post);
+  //grab the matching post and clear the comments section
+  var $commentAreaId = $("#comments-post" + post.id);
+  $commentAreaId.empty();
+  //build the comment template and then render each comment with its removal icon
+  var commentSource = $('#comment-template').html();
+  var commentTemplate = Handlebars.compile(commentSource);
   post.comments.forEach(comment => {
+    var newComment = commentTemplate(comment);
     var commentRemoval = createCommentRemovalIcon(posts.indexOf(post),comment);
-    var newHTMLComment = template(comment);
-    $(commentAreaId).append(newHTMLComment);
-    $(commentAreaId).append(commentRemoval);
+    $commentAreaId.append(newComment);
+    $commentAreaId.append(commentRemoval);
   })
-  $(commentAreaId).append(commentInput);
+  //add a comment input section at the bottom of all the comments
+  var commentInput = createCommentLine(post);
+  $commentAreaId.append(commentInput);
 };
 
 //prevent enter from submitting
