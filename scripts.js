7c1455f7ec82cf25f026cae65d5dd4c5dca4ff0d
Rhys Lutsky <rhyslutsky@gmail.com>
2018-11-11 09:35:47 -0500

    create Remove buton

--- scripts.js
+++ scripts.js
@@ -1,49 +1,84 @@
 
-//create an object that houses both the posts and the comments
-// var Postings = {
-//     originalPost: { },
+//create an object through JQUERY BELOW that houses both the posts and the comments.  This is my plan.
+// var Postings = [{
+//         text:
+//         name:
+//         comments:[ {
+//             comment:
+//             userName:
+//             }
+//         ]
+//     }
+// ];
 
-//     comments: [
-//         {},
-//     ]
-// };
+//Part 1 - 
 
-//var Postings = [];
-
-
-//Part 1 - Gets name and post input from user when they click the 'post' button. Appends it to the other posts with the post on 1 line and the name on a 2nd line. 
-
+var Postings = [];
 var createPost = function () {
+    // Gets name and post input from user when they click the 'post' button. 
     var post = {
         text: $('#text').val(),
-        name: $('#name').val()
+        name: $('#name').val(),
+        comments: {}
     }
-    $('.post-list').append('<li>' + 'post.text' + '< br > ' + ' < b > ' + " Posted by: " + '</b > ' + post.name + '</li >')
+    Postings.push(post);
+
+    // Appends it to the other posts with the post on 1 line and the name on a 2nd line. 
+    $('.post-list').append('<li>' + post.text + '<br>' + '<b>' + " Posted by: " + '</b> ' + post.name);
+
+    //clears form
+    $('#text').val("");
+    $('#name').val("");
 };
+console.log(Postings);
 
 $('.btn-primary').click(function (event) {
     event.preventDefault();
     createPost();
 });
-console.log('post');
-/*
+
+
 /////////////////PART 2//////////////////////////
-// When "Comment" button is clicked, the comment input box toggles hidden/visible
-$('.btn-comment').click(function (event) {
-    event.preventDefault();
-    $("#comments").toggle(display);
-    //createPost();
-});
-//When the 'Post Comment" button is clicked, the comment is appended to THAT post ALONG with an X button
-$('.btn-postComment').click(function (event) {
-    event.preventDefault();
+//NOW when append a POST:
+function createRemoveButton() {
+    //create button
+    return $('<button/>', {
+        id: "remove",
+        text: "Remove",
+        click: removePost()
+    })
+};
+
+//append it
+$('.post-list').prepend(createRemoveButton());
+
+//event - //When the 'Remove' button is clicked, the entire post (including comments) is deleted.
+// $('#remove').click(function(event){
+function removePost() {
+
 }
+// })
 
-        //ADD class .form-check AND .form-check-inline  for checkbox
-        //UTF-8 dingbat code for X = &#x2716 or &#10006; OR X inside checkbox = &#10062 or &#x274E
+createRemoveButton();
 
 
-    //When "x"button (next to post) is clicked, the comment is deleted.
+    //add X = delete comment after post
 
-//When the 'Remove' button is clicked, the entire post (including comments) is deleted.
-*/
+
+// When "Comment" button is clicked, the comment input box toggles hidden/visible - set CSS
+
+    // $('.btn-comment').click(function (event) {
+    //     event.preventDefault();
+    //     $("#comments").toggle(display);
+    //     //createPost();
+    // });
+
+//When the 'Post Comment" button is clicked, the comment is appended to THAT post ALONG with an X button
+    // $('.btn-postComment').click(function (event) {
+    //     event.preventDefault();
+    // }
+
+
+//When "x"button (next to post) is clicked, the comment is deleted.
+
+
