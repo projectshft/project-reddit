51bc77bcbebbae0e9d24f7681cba8282ad4700b3
Rhys Lutsky <rhyslutsky@gmail.com>
2018-12-27 15:36:38 -0500

    add comment button adds the 1 comment to all posts, not just the  1

--- scripts.js
+++ scripts.js
@@ -1,24 +1,6 @@
-
-//create an object that houses both the posts and the comments.  This is my plan.
-// var Postings = [{
-//     text: "",
-//     name: "",
-//     comments: [{
-//         comment: "",
-//         userName: ""
-//     }
-//     ]
-// }
-// ];
-
-//Part 1 - 
-
 var Postings = [];
 
 //global button creation for non-Bootstrap buttons
-var remove = $('<button class="btn-remove">Remove</button>');
-var comment = $('<button class="btn-comment">Comment</button>');
-
 var checkbox = $('<div class="form-check form-check-inline">' + '<input class="form-check-input" type="checkbox" id="x" value="option1">' + '<label class="form-check-label" for="x">&#x2716</label>' + '</div>');
 
 
@@ -27,20 +9,18 @@ var createPost = function () {
     var post = {
         text: $('#text').val(),
         name: $('#name').val(),
-        comments: ""
-    }
+        comments: []
+    };
+
+    var comments = '<div class="comments">' + '<div class="commentsList"></div>' +'<input type="text" class="commentText" placeholder="Your comment">' + '<input type="text" class="userName" placeholder="User Name"><button class="btn add-comment">Add Comment</button></div>';
 
     //add to array
     Postings.push(post);
 
     // Appends it  
-    //ISSUE - only puts the 3 buttons on the 1st post 
-    $('.post-list').append('<div class="post">' + '<button class="btn-remove">Remove</button>' + '<button class="btn-comment">Comment</button>' + post.text + '<div> Posted by: <b>' + post.name + '</b></div> </div>');
-        // '<li>', remove, comment,
-        // + "" + post.text + '<b>' + " Posted by: " + '</b> ' + post.name, checkbox, '</li>'
-    
-
-    //clears form
+    $('.post-list').append('<div class="post">' + '<button class="btn-remove">Remove</button>' + '<button class="btn-comment">Comment</button>' + post.text + comments + '<div> Posted by: <b>' + post.name + '</b></div> <hr/></div>');
+      
+    //clears form so ready for new post
     $('#text').val("");
     $('#name').val("");
 };
@@ -55,20 +35,19 @@ $('.btn-primary').click(function (event) {
 
 /////////////////PART 2//////////////////////////
 
-var createComment = function () {
+var createComment = function (text, name, postIndex) {
     // Gets name and post input from user when they click the 'post' button. 
     var comment = {
-        commentText: $('#commentText').val(),
-        userName: $('#userName').val(),
+        commentText: text,
+        userName: name
     }
     //ISSUE adds comment as next object
-    Postings.push(comment);
+    Postings[postIndex].comments.push(comment);
     console.log(Postings);
 
     //ISSUE I believe comment is there, but hidden by comment form
-    $('.post-list', 'li').append(
-        '<li>'
-        + "" + comment.commentText + '<b>' + " Posted by: " + '</b> ' + comment.userName, checkbox, '</li>'
+    $('.post-list').find('.comments').append(
+        '<div class="comment">' + comment.commentText + " Posted by: " + '<b> ' + comment.userName + '</b>' + '<input class="form-check-input" type="checkbox" id="x" value="option1"></div>'
     );
 
     //clears form
@@ -77,21 +56,22 @@ var createComment = function () {
 };
 
 // When "Comment" button is clicked on a post, 
-//uncomment display in CSS
-//??How do I get it to add an event listener to the button, not just the document???
-$('btn-comment').click(function () {
-    $('#commentForm').toggle(display);
-    alert('Make a comment.');
-    //toggle the comment form to visible
+// $('.post-list').on('click', '.btn-comment', function(e) {
+//     e.preventDefault();
+//     // $('.comments').toggleClass('show');
+//     alert('Make a comment.');
+//     //toggle the comment form to visible
 
-});
+// });
 
 
 //ISSUE - post and comment disappear
-$('.btn-postComment').click(function (event) {
+$('.post-list').on('click', '.add-comment', function (event) {
     event.preventDefault();
-    createComment();
-    console.log(Postings);
+    var text = $(this).siblings('.commentText').val()
+    var name = $(this).siblings('.userName').val()
+    var postIndex = $(this).closest('.post').index();
+    createComment(text, name, postIndex);
 });
 
 // When "x"button (next to post) is clicked, the comment is deleted.
