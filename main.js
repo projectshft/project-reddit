1cefde7deb7e44322e591caa628490c9c3b0cb5c
Jason Doss <doss2k@gmail.com>
2019-03-01 16:10:36 -0500

    Refactor code using jQuery to shorten it up, removed a few unnecessary things

--- main.js
+++ main.js
@@ -1,50 +1,43 @@
 const posts = $('.posts');
-let counter = 1;
-
-
-// Grabs the input of user and builds the post then adds it to the post list
-document.getElementById('add-post').addEventListener('click', function (e) { 
+/* When add post is clicked this function grabs the values of the username and message inputs,
+   builds a post, and then adds it to the post list */
+$('#add-post').on('click', function (e) { 
   e.preventDefault();
-  const userName = document.getElementById('name').value;
-  const message = document.getElementById('message').value;
+  const userName = $('#username').val();
+  const message = $('#message').val();
 
-  posts[0].innerHTML += 
-  `<div id="comment${counter}" class="userpost">` 
+  posts[0].innerHTML +=
+  '<div class="userpost">'
   + '<div>' + message + '</div>' 
   + '<div>Posted By: <b>' + userName + '</b></div>'
   + '<div class="icon-row"><i class="far fa-window-close delete"></i>'
   + '<i class="far fa-comment comment"></i></div>';
   
-  counter++;
-  addDeleteListeners();
+  deletePostListener();
   addPostMouseOverListeners();
-  });
-
-  // Adds event listens to all delete icons and if clicked removes that post
-  const addDeleteListeners = function() {
-    document.querySelectorAll(".delete").forEach(function(elem) {
-      elem.addEventListener("click", function() {
-        elem.parentElement.parentElement.remove();
-      });
-    });
-  }
-  
-  // Toggles the icon row to show or not show when post is moused over
-  const addPostMouseOverListeners = function() {
-  $('.userpost').each(function () {
-   $(this).mouseenter(function () {
-  if($(this).find(".icon-row").hasClass('show')) {
-    $(this).find(".icon-row").removeClass('show');
-  } else {
-    $(this).find(".icon-row").addClass('show');
-  }
 });
-  $(this).mouseleave(function () {
+
+// Adds event listens to all delete icons and if clicked removes that post
+const deletePostListener = function() {
+  $(".delete").each(function() {
+    $(this).on("click", function() {
+      this.parentElement.parentElement.remove();
+    });
+  });
+}
+
+// Toggles the icon row to show or not when a post area is moused into or out of
+const addPostMouseOverListeners = function() {
+  const checkIconRowShow = function() {
     if($(this).find(".icon-row").hasClass('show')) {
       $(this).find(".icon-row").removeClass('show');
     } else {
       $(this).find(".icon-row").addClass('show');
     }
-});
-});
+  }
+
+  $('.userpost').each(function() {
+    $(this).mouseenter(checkIconRowShow);
+    $(this).mouseleave(checkIconRowShow);   
+  });
 }
