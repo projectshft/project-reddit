20e3c418d64354e50ec8c55fa82279a40b6a324f
mcstreilein <madelinestreilein@Madelines-MacBook-Air.local>
2019-03-01 09:19:28 -0500

    creating index.html, main.js, and style.css files

---
+++ main.js
@@ -0,0 +1,27 @@
+/* Comment Driven Dev
+files needed:
+    -index.html
+    -main.js
+    -style.css
+
+need to link:
+    -bootstrap (above my style.css link in html file)
+    -jquery (at the bottom of my html file)
+
+requirements: 
+    Part 1
+        -use Bootstrap 3 or 4
+        -write clean and pretty html, css, and javascript
+        -use correct comments and logging; (refer to Sean's powerpoint)
+        -utilize patterns to organize javascript if necessary
+        -use JQuery
+
+        functionality:
+        add post (Requires name and input)
+        add comment
+        all posts will go to page, newer ones above
+    Part 2
+        -We now want users to be able to leave comments on each post. When a user clicks 'comments' (above each post) it should toggle the comments and input box visible/hidden.
+        -When a user clicks the 'x' next to a comment, it should delete it.
+        -When a user fills out the two comment inputs and clicks 'Post Comment' it should immediately add the comment to the list of comments.
+        -when a user clicks 'remove' above a post, it should remove the post, too.
