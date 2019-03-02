9809296ed8e7b2192084f5b86c7ce3f58ea6b677
Knox Brand <knox.brand@gmail.com>
2019-03-01 19:15:12 -0500

    Post array, posting function setup

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 postArray = [];
+
+var createPost = function(counter) {
+  var template =	'<a href="">remove </a><h6 class="post font-weight-light" data-post="'+counter+'">'+ postArray[counter-1].postBody +'</h6>'
+                  + '<div>'
+                  + '<h6 class="user" >Posted By: <strong>'+ postArray[counter-1].postUser +'</strong></h6>'
+                  + '</div class="border-bottom">'
+
+var $post = $('.postBox');
+$post.append(template)
+}
+
+$(".btn").click(function() {
+  post = $('#postFormBody').val();
+  name = $('#postFormName').val();
+  postArray.push({postBody: post, postUser: name});
+  createPost(postArray.length);
+});
+
+$('document').ready(function () {
+});
