c49dcee085a7ae2015d30de4ee31859087a70fa8
Richard Eldridge <eldridge.rich@gmail.com>
2019-03-01 19:17:24 -0500

    Added ability to comment on post - bug fix reqd - see TODO line 39

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ const createPost = () => {
   const $newPostName = $("#new-post-name").val();
   const $newPostMessage = $("#new-post-message").val();
   // Place the newPostText and userName into newPostTemplate
-  const newPostTemplate = `<div class="post-wrapper border-bottom pt-3"><div class="post-content"><h4 class="post-heading"><strong class="user-name">${$newPostName}</strong><em> says: </em></h4><p class="post-content lead ml-2">${$newPostMessage}</p></div><div class="form-group text-right"><button type="button" class="btn btn-secondary btn-sm" data-toggle="modal" data-target="#comment-modal">Leave a comment</button></div></div>`;
-  // append the content wrapper with the new post
-  $(".content-wrapper").append(newPostTemplate);
+  const newPostTemplate = `<div class="post-wrapper border-bottom pt-3"><div class="post-content"><h4 class="post-heading"><strong class="user-name">${$newPostName}</strong><em> says: </em></h4><p class="lead ml-2">${$newPostMessage}</p></div><div class="form-group text-right"><button type="button" class="btn btn-secondary btn-sm" data-toggle="modal" data-target="#comment-modal">Leave a comment</button></div></div>`;
+  // append the content wrapper with the new post, if there is content
+  if($newPostName !== "" && $newPostMessage !== ""){
+    $(".content-wrapper").append(newPostTemplate);
+  }
 };
 
 // Add a click event listener to the submit button to create a post
@@ -17,8 +19,24 @@ $newPostButton.click(createPost);
 
 // *** We now want users to be able to leave comments on each post. When a user clicks 'comments' (above each post) it should toggle the comments and input box visible/hidden.
 const createComment = () => {
+  // Capture input from the Bootstrap modal input fields
+  const $commenterName = $("#commenter-name").val();
+  const $commentText = $("#comment-text").val();
+  // Place into commentTemplate
+  const commentTemplate = `<p class="comment ml-5"><strong class="commenter-name">${$commenterName}</strong><em> comments: </em>${$commentText}</p>`
+  // append to the related post, only if there is content
+  if($commentText !== "" && $commenterName !== "") {
+    $(".post-content").append(commentTemplate);
+    // hide comment modal 
+    $("#comment-modal").modal("hide");
+  }
+};
 
-}
+// Add a click event listener to the modal submit button
+const $addCommentButton = $("#add-comment-button");
+$addCommentButton.click(createComment);
+
+// *** TODO *** Comments append to every post on submit ****
 
 
 
