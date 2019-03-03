aa98c883644b6797616265861a864721c986c8ed
Knox <knox.brand@gmail.com>
2019-03-03 10:28:35 -0500

    Comment input box and hide/show working. Delete post working.

--- main.js
+++ main.js
@@ -1,20 +1,57 @@
 postArray = [];
 
 var createPost = function(counter) {
-  var template =	'<a href="">remove </a><h6 class="post font-weight-light" data-post="'+counter+'">'+ postArray[counter-1].postBody +'</h6>'
-                  + '<div>'
-                  + '<h6 class="user" >Posted By: <strong>'+ postArray[counter-1].postUser +'</strong></h6>'
-                  + '</div class="border-bottom">'
+  var template =	 '<div class="border-bottom pb-3">'
+                  + '<div class="d-inline">'
+                  + '<button type="button" id="postRemove" class="btn btn-link p-0">remove </button>'
+                  + '<button type="button" id="postComments" class="btn btn-link p-0">comments </button>'
+                  + '<h6 class="post font-weight-light">'+ postArray[counter-1].postBody +'</h6>'
+                  + '</div>'
+                  + '<h6 class="user">Posted By: <strong>'+ postArray[counter-1].postUser +'</strong></h6>'
+                  + '<div class="commentBox">'
+                  + '<form class="form-inline">'
+                  + '<input type="text" class="form-control w-25" id="commentFormBody" placeholder="Comment Text">'
+                  + '<input type="text" class="form-control w-25" id="commentFormName" placeholder="Your Name">'
+                  + '<button type="button" class="btn btn-primary w-25">Post Comment</button>'
+                  + '</form>'
+                  + '</div>'
+                  + '</div>'
 
-var $post = $('.postBox');
-$post.append(template)
+                             
+
+
+
+  var $post = $('.postBox');
+  $post.prepend(template);
 }
 
-$(".btn").click(function() {
-  post = $('#postFormBody').val();
+var createComments = function(counter) {
+
+  var template = '<div data-comment="1">'
+                + '<h6>test comment</h6>'
+                + '</div>'
+
+  var $comment = $('.commentBox');
+  $comment.prepend(template);
+}
+
+$(".btn").click(function() { //post button click event handler
+  post = $('#postFormBody').val(); 
   name = $('#postFormName').val();
-  postArray.push({postBody: post, postUser: name});
-  createPost(postArray.length);
+  if (post != "" && name != "") {
+    postArray.push({postBody: post, postUser: name});
+    $("#postFormBody").val("");
+    $("#postFormName").val("");
+    createPost(postArray.length);
+  }
+});
+
+$(document).on("click", '#postRemove',function() {
+  $(this).parent().parent('div').remove();
+});
+
+$(document).on("click", '#postComments',function() {
+  $(this).parent().siblings(".commentBox").toggle();
 });
 
 $('document').ready(function () {
