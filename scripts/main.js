67516c5cd282ce2de4a4faf8253c6738b3c0665a
Ritchie Fesperman <ritchie.fes@gmail.com>
2019-03-03 11:02:58 -0500

    add filter to delete assoiacted post from array

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,11 @@ var submissions = [];
 var usernameSubmission = '';
 var messageSubmission = '';
 var messageID = 0;
+var prependedMessageList = '';
 
 //add click listener to capture and push submitted username and message to submissions array.
 $('.btn-submit').on('click', function() {
   //add if statements to check that inputs are populated. If one is empty, output an error but DON'T reset fields -- that shit's annoying.
-  console.log($('#usernameInput').val());
   if ($.trim($('#usernameInput').val()) == '' && $.trim($('#messageInput').val()) == '') {
     alert('Please enter a username and message.');
   } else if ($.trim($('#usernameInput').val()) == ''){
@@ -17,31 +17,27 @@ $('.btn-submit').on('click', function() {
     //assign submissions to relative global variables
     usernameSubmission = $('#usernameInput').val();
     messageSubmission = $('#messageInput').val();
+    //add a messageID
+    messageID += 1;
     //assign them to properties of an object that can be pushed to the submissions array.
     var currentSubmission = {
+      id: messageID,
       username: usernameSubmission,
       message: messageSubmission
     };
-
     console.log('this is the object that was just submitted: ', currentSubmission);
-
     //add the currentSubmission to the submissions array.
     submissions.push(currentSubmission);
-
     console.log('this is the current value of the submissions array: ', submissions);
-
-    //add a messageID
-    messageID += 1;
-
     //in addition to adding the submission to an object and pushing to an array, I think I may also want to immediately add them directly to the page.
-    $('.message-list').prepend(
+    prependedMessageList = $('.message-list').prepend(
       '<div class="post-box">'+
         '<div class="row submit-target" style="padding:0px 10px 0px 20px">'+
           '<div class="username col-11">'
             +usernameSubmission+
           '</div>'+
           '<div class="delete-icon col-1">'+
-            '<button type="button" class="btn delete-btn" style="padding:0px;"><i class="fas fa-trash"></i></button>'+
+            '<button type="button" class="btn delete-btn delete-'+messageID+'" data-id="'+messageID+'" style="padding:0px;"><i class="fas fa-trash"></i></button>'+
           '</div>'+
           '<div class="message-box col-11">'
             +messageSubmission+
@@ -52,15 +48,20 @@ $('.btn-submit').on('click', function() {
             '</div>'+
           '</div>'+
         '</div>'+
-      '</div>');
-
-    console.log('this is the current messageID: ', messageID)
-
-    //add event listener to delete comment
-    $('.delete-btn').on('click', function() {
+      '</div>'
+    );
+    //add event listener to trash icon to delete relevant comment
+    $('.delete-'+messageID).on('click', function() {
+      //assign relative messageID to a variable that can be accessed within the filer() below
+      var relativeID = $(this).attr('data-id')
+      //filter the submissions array to delete the relative object from the array
+      submissions = submissions.filter(function(index){
+        return index.id != relativeID;
+      });
+      console.log('This is the new submissions array:' ,submissions);
+      //remove the post from the html
       $(this).closest('.post-box').remove();
     });
-
     //clear the input fields
     $('#usernameInput').val('');
     $('#messageInput').val('');
