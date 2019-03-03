a406e03a5e6976779bbe283909f03362f5904cd1
marielklem <mariel.steinbeiser@gmail.com>
2019-03-03 17:51:45 -0500

    Fixed comment function

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ var addNewPost = $('button').on('click', function (e) {
   var userName = $('#name').val();
   var comment = $('#message').val();
 
-  $('.posts').append('<div class="individual-post"><strong>'+ userName+ '</strong><br>'+ comment +'<br><div class="icons">'+ iconBar + '</div><br><hr></div>')
+
+  $('.posts').append('<div class="individual-post"><strong>'+ userName+ '</strong><br>'+ comment +'<br><div class="icons"><i class="far fa-trash-alt"></i>  <i class="far fa-comments" id="comment'+userName+'"></i><div  class="individual-comment'+userName+'"</div></div><br><hr></div>')
 
   //when trash icon is clicked, delete posts
   var deletePost = $('.fa-trash-alt').on('click', function() {
@@ -14,8 +15,8 @@ var addNewPost = $('button').on('click', function (e) {
   });
 
   //when comments is clicked small form for text and username (post button)
-  var renderCommentForm = $('.fa-comments').on('click', function() {
-    $(this).closest('.icons').append(commentForm)
+  var renderCommentForm = $('#comment'+userName).on('click', function() {
+    $(".individual-comment"+userName).append(commentForm);
     comments();
   });
 
@@ -23,18 +24,17 @@ var addNewPost = $('button').on('click', function (e) {
 
   //1. click comments link and open small form for text and username & post button
 var comments = function() {
-  $('button').on('click', function(e) {
+  $('.commentSubmit').on('click', function(e) {
     e.preventDefault();
     var commentUser = $('#commentName').val();
     var commentContent = $('#commentContent').val();
 
-  //2. when user clicks post add to list of comments
-  // $('form').html('<div class="individual-comments">'+ commentContent+ ' posted by <strong>'+ commentUser + '</strong> '+ deleteButton +'</div>')
-    $(this).closest('.icons').append('<div class="individual-comments">'+ commentContent+ ' posted by <strong>'+ commentUser + '</strong> '+ deleteButton +'</div>')
+  //2. when user clicks post comment, add to list of comments with a delete button
+    $(this).closest('.commentForm').html('<div class="individual-comment">'+ commentContent+ ' posted by <strong>'+ commentUser + '</strong> '+ deleteButton +'</div>')
 
-  //3. when comment is posted, add X and when clicked, deletes posts
+  //3. clicking delete button next to a comment will delete that individual comment
     var deleteComment = $('.fa-times').on('click', function() {
-      $(this).closest('.individual-comments').remove();
+      $(this).closest('.individual-comment').remove();
     });
   })
 };
@@ -44,5 +44,5 @@ var comments = function() {
 
 
 var iconBar = '<i class="far fa-trash-alt"></i>  <i class="far fa-comments"></i>'
-var commentForm = '<form><input id="commentContent" type="text" class="commentContent" placeholder="Comment Text"><input id="commentName" type="text" class="commentUse" placeholder="User Name"><button class="commentSubmit" type="submit" class="btn btn-primary mb-2">Post Comment</button>'
+var commentForm = '<div class="commentForm"><form><input id="commentContent" type="text" class="commentContent" placeholder="Comment Text"><input id="commentName" type="text" class="commentUse" placeholder="User Name"><button class="commentSubmit" type="submit" class="btn btn-primary mb-2">Post Comment</button></form></div>'
 var deleteButton = '<i class="fas fa-times"></i>'
