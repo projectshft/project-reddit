83ef11573d46f7d3d583bf66d9bad600513a3c24
alice <alicectung@gmail.com>
2019-03-03 20:33:20 -0500

    create comment section

--- main.js
+++ main.js
@@ -1,35 +1,78 @@
-$(document).ready(function(){
+/****************************************************
+Eval #2  |  Project Reddit  |  by Alice Tung
+
+A site where users can create posts and comments.
+
+*****************************************************/
 
 
+
+$(document).ready(function() {
+
+/* Variables to create an array of post objects */
   var $post = [];
   var $postObject = {};
 
+/* When the user submits a post entry, user name and post message
+   are stored as an object in an array.*/
+  $('.btn-post').click(function() {
 
-  $('.btn-post').click(function(){
+    var $postName = $('input#postName').val();
+    var $postText = $('textarea#postText').val();
+
+    $post.push($postObject);
+
+/* User post is rendered as HTML to the virtual DOM */
+    $(function() {
+      $postObject = {
+        postName: $postName,
+        postText: $postText,
+        appendPostObject: function() {
+          $('ul').append(
+            '<br><br><li>' + this.postText + '<br>' +
+            'Posted by: ' + '<strong>' + this.postName + '</strong>' + '</li>' +
+            '<button class="btn btn-primary btn-xs" id="add-comment">Add comment</button>' +
+            '<button class="btn btn-danger btn-xs" id="remove-post">Remove post</button>'
+          );
+
+          $('#add-comment').click(function() {
+            $('#comment-section').toggle();
+          });
+        }
+      };
+      $postObject.appendPostObject();
+    });
+  }.bind(this));
 
 
-      var $postName = $('input#postName').val();
-        //console.log($postName);
+/* Variables to create an array of comment objects */
+  var $comment = [];
+  var $commentObject = {};
 
-      var $postText = $('textarea#postText').val();
-        //console.log($postText);
+/* When the user submits a comment entry, user name and comments
+  are stored as an object in an array.*/
+  $('.btn-comment').click(function() {
 
-      $post.push($postObject);
+    var $commentName = $('input#commentName').val();
+    var $commentText = $('textarea#commentText').val();
 
-      $(function(){
+    $comment.push($commentObject);
 
-        $postObject = {
-          postName: $postName,
-          postText: $postText,
-          init: function(){
-              //console.log(this.postName);
-              $('ul').append(
-                               '<li>'+this.postName+'</li>');
-          }
-        };
-        $postObject.init();
-      });
-  });
-
+/* User comment is rendered as HTML to the virtual DOM */
+    $(function() {
+      $commentObject = {
+        commentName: $commentName,
+        commentText: $commentText,
+        appendCommentObject: function() {
+          $('ul').append(
+            '<li>' + this.commentText + '<br>' +
+            'Posted by: ' + '<strong>' + this.commentName + '</strong>' + '</li>' +
+            '<button class="btn btn-warning btn-xs" id="remove-comment">Remove comment</button>'
+          );
+        }
+      };
+      $commentObject.appendCommentObject();
+    });
+  }.bind(this));
 
 });
