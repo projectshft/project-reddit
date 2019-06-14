5ab963eb6ef494e0b4ea59a38c5d55de6b2a0e46
dposse <dposse.125@gmail.com>
2019-06-14 12:00:07 -0400

    checkForValidText complete

--- main.js
+++ main.js
@@ -4,12 +4,26 @@ const posts = [];
  * { postText: [text], postAuthor: [author] }
  */
 
+const checkForValidText = () => {
+
+  if ($('#new-post-text').val().trim() === '' || $('#new-post-author').val().trim() === '')
+    return false;
+
+  return true;
+
+};
+
 const newPostButtonClickHandler = function(event) {
   event.preventDefault();
 
   //check to see if anything is written to text areas
   //  if no text in one or the other, do nothing
   //    ignore whitespace? so if user only enters spaces/tabs it won't post
+  // use helper function to use with other buttons
+  if (!checkForValidText()) {
+    console.log('Invalid input, post message and author required');
+    return;
+  }
 
   //save postText and postAuthor to variables, maybe not necessary
 
