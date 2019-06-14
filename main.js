e6e9d50b78cda480c96b3cee30e0542b8dbbac8f
Ronnie Scott <ronniescott@Ronnies-MacBook-Pro.local>
2019-06-14 16:19:49 -0400

    fixed issue where username and post werent showing in the new posts

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 
 
 // get the value of the users name and the value of the post they wrote
-var userNameInput = $('#name');
-var userPostInput = $('#message');
 
+
+var foo = function($userNameInput, $userPostInput){
 var template =
 '<article class="row">' +
   '<div class="col-md-10 col-sm-10">' +
     '<div class="panel panel-default">' +
       '<div class="panel-body">' +
         '<header class="text-left">' +
-          '<div class="comment-user"><i class="fa fa-user"></i>' + userNameInput.val() + '</div>' +
+          '<div class="comment-user"><i class="fa fa-user"></i> ' + $userNameInput + '</div>' +
           '<time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i> Dec 16, 2014</time>' +
         '</header>' +
         '<div class="comment-post">' +
-          '<p>'+ userPostInput.val()+'</p>' +
+          '<p>'+ $userPostInput+'</p>' +
         '</div>' +
         '<button type="button" class="btn btn-primary btn-sm" id="btnReply">reply</button>' +
       '</div>' +
@@ -22,9 +22,15 @@ var template =
   '</div>' +
 '</article>'
 ;
+return template;
+}
 
 
 var $btnPost = $('#btnPost');
 $btnPost.on('click', function(){
-  $('.comment-list').prepend(template);
+  var $userNameInput = $('#name').val();
+  var $userPostInput = $('#message').val();
+  var callingFoo = foo($userNameInput, $userPostInput);
+  $('.comment-list').prepend(callingFoo);
+
 });
