9c4ad2567687be08cbc0b9dc3cfccf22a2c89b1b
snpdolan <snpdolan@gmail.com>
2019-06-14 14:08:49 -0400

    added functionality to push posts to the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,28 @@
+$( "#post-form" ).submit(function( event ) {
+	$postItems = $('#post-items');
+	postText = $('#postText').val();
+	userName = $('#userName').val();
+	//console.log(postText, userName);
+	var newPost = createPost(postText, userName);
+  //alert( "Handler for post .submit() called." );
+  console.log(newPost);
+
+  //need to add inner html here somehow
+  $postItems.append(newPost);
+  event.preventDefault();
+});
+
 //function that handles posting creation
-var createPost = function (userName, postContent){
+var createPost = function (postContent, userName){
+	var postTemplate =
+     '<div class="user-post">'
+   + '  <p class="post-content"> ' + postContent + ' </p>'
+	 + '  <h5 class="post-name">Posted By: <em>' + userName + '</em> </h5>'
+   + '</div>'
+   ;
+
+   return postTemplate;
+};
 
 
 	//onHover function that displays the 'x' to delete a post or edit post or post comments
@@ -11,10 +34,6 @@ var createPost = function (userName, postContent){
 	//function that handles comment creation
 
 	//function that handles comment deletion
-
-};
-
-
 
 
 
