fb00229840b6d42859dd207a0c3ce3b52cad0d91
unknown <nathaniel.poli@gmail.com>
2019-06-14 16:17:08 -0400

    Added functionality for creating a post

--- main.js
+++ main.js
@@ -2,14 +2,19 @@
 var posts = [];
 
 // Constructor for a new post
-var Post = function(name, message){
-    this.name = name;
-    this.message = message;
+var Post = function(name, message) {
+  this.name = name;
+  this.message = message;
 }
 
 // Function to add a post to the post div
-var setCurrentPost = function(postName, postMessage) {
-
+var createPost = function(post) {
+  var template =
+  '<div>'
+  + '<p><i>Message: ' + post.message + '</i></p>'
+  + '<p><b>Posted by: ' + post.name + '</b></p>'
+  + '</div>';
+  return template;
 }
 
 // Creating a button click event to post a message when the user fills out the
@@ -20,5 +25,13 @@ $postButton.on('click', function() {
   var $name = $('#name').val();
   var $message = $('#message').val();
   var newPost = new Post($name, $message);
+  var $postList = $('.posts')[0];
+  while($postList.hasChildNodes() && $postList.firstElementChild !== null) {
+    $postList.firstElementChild.remove(); }
+
   posts.push(newPost);
+
+  for(i = 0; i<posts.length; i++) {
+    $('.posts').append(createPost(posts[i]));
+  }
 })
