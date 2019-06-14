833563aae0ec870e9060a0580a5f087b82f26f86
dposse <dposse.125@gmail.com>
2019-06-14 11:44:56 -0400

    Added plan comments

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
+const posts = [];
+/**
+ * Make posts objects like so:
+ * { postText: [text], postAuthor: [author] }
+ */
+
 const newPostButtonClickHandler = function(event) {
   event.preventDefault();
+
+  //check to see if anything is written to text areas
+  //  if no text in one or the other, do nothing
+  //    ignore whitespace? so if user only enters spaces/tabs it won't post
+
+  //save postText and postAuthor to variables, maybe not necessary
+
+  //create post object and add to posts array
+
+  //clear posts section front end
+
+  //render all posts
+
 };
 
 
 
-
+//post button for part 1
 $('#new-post-button').click(newPostButtonClickHandler);
