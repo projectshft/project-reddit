7b2605528e466ef7c51c2d7387e28fa069d757a4
snpdolan <snpdolan@gmail.com>
2019-06-14 11:26:30 -0400

    created base html layout, with minor css styling

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+//function that handles posting creation
+var createPost = function (userName, postContent){
+
+
+	//onHover function that displays the 'x' to delete a post or edit post or post comments
+
+	//function that handles posting deletion
+
+	//EXTENSION: function that enables users to edits
+
+	//function that handles comment creation
+
+	//function that handles comment deletion
+
+};
+
+
+
+
+
+
+
+//EXTENSION: function that displays posts in a "new" screen, with comments below.
