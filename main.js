fda01726bf3fef66b3c9676c79bc7fc53ee52a7c
unknown <nathaniel.poli@gmail.com>
2019-06-14 17:09:32 -0400

    Added a separate function for rendering the posts

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Array for all of the posts to be held in and manipulated
 var posts = [];
+var comments = [];
 
 // Constructor for a new post
 var Post = function(name, message) {
@@ -8,13 +9,25 @@ var Post = function(name, message) {
 }
 
 // Function to add a post to the post div
-var createPost = function(post) {
+var createPost = function(postNumber, post) {
   var template =
-  '<div>'
-  + '<p><i>Message: ' + post.message + '</i></p>'
-  + '<p><b>Posted by: ' + post.name + '</b></p>'
-  + '</div>';
+    '<div class="post-item" data-post-number= "' + postNumber + '">' +
+    '<p>Message: ' + post.message + '</p>' +
+    '<p>Posted by: ' + post.name + '</p>' +
+    '</div>';
   return template;
+}
+
+// Separating the functionality of the model and the view
+var renderPosts = function() {
+  var $postList = $('.posts')[0];
+  // Clearing out the original div of children so it can be repopulated
+  while ($postList.hasChildNodes() && $postList.firstElementChild !== null) {
+    $postList.firstElementChild.remove();
+  }
+  for (i = 0; i < posts.length; i++) {
+    $('.posts').append(createPost(i, posts[i]));
+  }
 }
 
 // Creating a button click event to post a message when the user fills out the
@@ -25,13 +38,6 @@ $postButton.on('click', function() {
   var $name = $('#name').val();
   var $message = $('#message').val();
   var newPost = new Post($name, $message);
-  var $postList = $('.posts')[0];
-  while($postList.hasChildNodes() && $postList.firstElementChild !== null) {
-    $postList.firstElementChild.remove(); }
-
   posts.push(newPost);
-
-  for(i = 0; i<posts.length; i++) {
-    $('.posts').append(createPost(posts[i]));
-  }
+  renderPosts();
 })
