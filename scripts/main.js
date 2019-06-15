d3ca9b680906760f398ee393c76626309fc53ae1
snpdolan <snpdolan@gmail.com>
2019-06-15 11:44:19 -0400

    updated to add unique ID to posts, implemented comment click function

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,68 @@
-$("#post-form").submit(function(event) {
-    var $postItems = $('#post-items');
-    let postText = $('#postText').val();
-    let userName = $('#userName').val();
-    //console.log(postText, userName);
-    var newPost = createPost(postText, userName);
-    //alert( "Handler for post .submit() called." );
-    console.log(newPost);
-
-    //adds post to inner html of post-items
-    $postItems.append(newPost);
-    event.preventDefault();
-});
-
+var postNumber = 0;
 
 //function that handles posting creation
-var createPost = function(postContent, userName) {
-    var postTemplate =
-        '<div class="user-post grey-separator top-padded">' +
-        '<p><button type="button" class="btn btn-link btn-sm remove-post">Remove</button><button type="button" class="btn btn-link btn-sm">Comments</button></p>' +
-        '  <p class="post-content"> ' + postContent + ' </p>' +
-        '<div class="post-comments"></div>' + commentForm +
-        '  <p class="post-name"><b>Posted By:</b> <em>' + userName + '</em> </p>' +
-        '</div>';
+var createPost = function(postContent, userName, postNumber) {
 
-    return postTemplate;
+	var commentForm =
+		'<form class="form-inline comment-form">' +
+		'<input type="text" class="form-control mb-2 mr-sm-2" id="commentText-' + postNumber + '" placeholder="Comment Text" required>' +
+		'<input type="text" class="form-control mb-2 mr-sm-2" id="commentUserName-'+ postNumber + '" placeholder="User Name" required>' +
+		'<button type="button" class="btn btn-primary btn-sm mb-2 comment-form-btn">Post Comment</button></form>';
+
+  var postTemplate =
+      '<div class="user-post grey-separator top-padded" data-post-number="' + postNumber + '">' +
+      '<p><button type="button" class="btn btn-link btn-sm remove-post">Remove</button><button type="button" class="btn btn-link btn-sm">Comments</button></p>' +
+      '  <p class="post-content"> ' + postContent + ' </p>' +
+      '<div class="post-comments">'+ commentForm +'</div>' +
+      '  <p class="post-name"><b>Posted By:</b> <em>' + userName + '</em> </p>' +
+      '</div>';
+
+  return postTemplate;
 };
+
+//submit event to handle creation of new posts
+$("#post-form").submit(function(event) {
+    event.preventDefault();
+    let postItems = $('#post-items');
+    let postText = $('#postText').val();
+    let userName = $('#userName').val();
+    var newPost = createPost(postText, userName, postNumber);
+    //adds post to inner html of post-items
+    postItems.append(newPost);
+    postNumber++;
+});
+
+//submit event to handle creation of new comments
+$("#post-items").on('click','.comment-form-btn',function(){
+		event.preventDefault();
+		console.log("comment button clicked");
+
+		//grab unique post Id #
+		let currentPost = $(this).closest('.user-post');
+    let postId = currentPost.attr('data-post-number');
+    let postComments = $(this).closest('.post-comments');
+    
+    //grab current comment values
+    // debugger
+    let commentText = $("#post-items").find('#commentText-'+ postId).val();
+    let commentUserName = $("#post-items").find('#commentUserName-'+ postId).val();
+
+    var newComment = createComment(commentText, commentUserName);
+
+    //adds comment to inner html of post-comments
+    postComments.prepend(newComment);		
+});
+
+var createComment = function(commentContent, commentUserName) {
+    var commentTemplate =
+        '<div class="user-comment">' +
+        '  <p class="comment-content">' + commentContent + '	<span><b>Commented By: </b><em>' + commentUserName + '</em></p></span>' +
+        '</div>';
+    console.log(commentTemplate);
+    return commentTemplate;
+};
+
+
 
 //function that handles posting deletion
 
@@ -41,36 +79,8 @@ var createPost = function(postContent, userName) {
 //EXTENSION: function that enables users to edit posts
 
 //function that handles comment creation
-$("#comment-form").submit(function(event) {
-		event.preventDefault();
-		console.log('comment posting clicked');
-    var $postComments = $(this).closest(".post-comments");
-    let commentText = $(this).closest('#commentText').val();
-    let commentUserName = $(this).closest('#commentUserName').val();
-    //console.log(commentText, userName);
-    var newcomment = createComment(commentText, commentUserName);
-    alert( "Handler for comment .submit() called." );
-    console.log(newcomment);
 
-    //need to add inner html here somehow
-    $postComments.append(newcomment);
-    event.preventDefault();
-});
-
-var createComment = function(commentContent, commentUserName) {
-    var commentTemplate =
-        '<div class="user-comment">' +
-        '  <p class="comment-content"> ' + commentContent + '	<b>Commented By:</b><em>' + commentUserName + '</em></p>' +
-        '</div>';
-
-    return commentTemplate;
-};
-
-var commentForm =
-	'<form class="form-inline" id="comment-form">' +
-	'<input type="text" class="form-control mb-2 mr-sm-2" id="commentText" placeholder="Comment Text" required>' +
-	'<input type="text" class="form-control mb-2 mr-sm-2" id="commentUserName" placeholder="User Name" required>' +
-	'<button type="submit" class="btn btn-primary btn-sm mb-2">Post Comment</button></form>';
+		
 
 //function that handles comment deletion
 
