837bea9c74dda633b42789b69b5713fbdf0a9ff9
Jake Galligan <galligan1996@gmail.com>
2019-06-16 22:18:13 -0400

    Cleaning up javascript code

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
- //creating a base html for each post. The function takes in the post text and author name and adds formats them within an html element including a removeand comment button
+//this function is run once the post button is clicked
  var createPost = function(userName, userText) {
+   //setting base html for when the post button is clicked
    var postTemplate = '<div id = "all-post-content"><button type = "button" class ="attached-buttons" id = "remove-button">remove</button><button type = "button" class = "attached-buttons" id = "comment-button">comment</button><div id = "post-box">' + userText + '<br>' + 'Posted By: ' +'<b>' + userName + '</b></div></div>'
-
+   //setting base html for when the comment butotn is clicked
    var commentFormTemplate = '<div id = "beforecommentform"><form class = "commentBox" id ="commentform"><input id = "comment-text" class="comment-input" type = "text" placeholder= "Comment Text"><input id = "comment-name" class = "comment-input" type = "text" placeholder = "User Name"><button type = "button" id ="post-comment-button" class = "btn btn-primary">Post Comment</button></form></div>'
 
-
    $posts = $(postTemplate);
-
-
 
    //set a function which changes the text color to navy and adds underline when hovered over
    var onHover = function() {
@@ -21,18 +19,12 @@
      $(this).css('text-decoration', 'none')
    };
 
-   //when the comment button is a comment and username input boxes appear under the post along with a post comment button
-   //one the comment button is clicked again the form to add a comment is toggled off
+   //function is ran when the 'comment' button is clicked. This displays the base html and contains a function which allows the user to click a 'post comment' button
    var commentClick = function() {
      $(this).parent('#all-post-content').after(commentFormTemplate);
 
      //when the post-comment button is clicked the post comment funciton runs
      $('#post-comment-button').on('click', postComment)
-
-
-
-
-
    };
 
    //when the remove button is clicked the post is then removed
@@ -40,21 +32,20 @@
      $('#all-post-content').remove();
    };
 
-   //when post comment button is clicked the comment and then username of commenter is posted direclty underneath the post
+   //when post comment button is clicked the comment and then username of commenter is posted diretly underneath the post
    var postComment = function () {
      console.log('test comment button')
      var commentText = $('#comment-text').val();
      var commentName = $('#comment-name').val();
      var commentTextTemplate = '<p class = "commentBox" id = "comment">' + commentText + ' Posted By: ' + '<b>' + commentName + ' ' + '</b><i class="fas fa-trash-alt"></i></p>';
 
-     //adds individual comment underneath each posts
      $(this).closest('#beforecommentform').prepend(commentTextTemplate);
-     //once someone clicks the "post comment" the input form is then toggled off
-     //I had tried to make it so when the "comment" button is clicked that the input form is toggled off, but did not have time to complete sufficiently. My code for this attempt is commented at the bottom of the file
+     //after the comment is posted the input form is then toggled off
+     //NOTE: I had tried to make it so when the "comment" button is clicked that the input form is toggled off, but did not have time to complete sufficiently. My code for this attempt is commented at the bottom of the file
      $('#commentform').toggle();
 
 
-     //when the trash icon is clicked the comment closest to it is deleted
+     //when the trash icon is clicked the comment is deleted
      $('#comment').find('i').on('click', function() {
      $(this).closest('#comment').remove();
    })
@@ -76,7 +67,7 @@
  });
 
 
- // toggle feature stil needs to be worked on (it works the first time)
+//NOTE: this is the code I had to try and get the input form to toggle when the comment button is clicked. It works the first time and only on the first comment. I believe I would need to use an if statement and check the state of the input form display in order to succesfully do this
  // $(this).on('click', function () {
  //   console.log("toggle check");
  //   $this.closest('#commentform').toggle();//ends up deleting all form need to get id
