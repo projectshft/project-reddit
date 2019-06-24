2f295bd2639801df87328f4a6e1081ba2e39783b
unknown <nathaniel.poli@gmail.com>
2019-06-24 12:52:56 -0400

    refactored code to make it easier to understand

--- main.js
+++ main.js
@@ -1,67 +1,67 @@
-// Array for all of the posts to be held in and manipulated
-var posts = [];
-var comments = [];
 
-// Constructor for a new post
-var Post = function(name, message) {
-  this.name = name;
-  this.message = message;
+var PostList = function() {
+  // Array for all of the posts to be held in and manipulated
+  var posts = [];
+
+  // Template for creating posts
+  var postTemplate = function(p) {
+    var pTemplate =
+      '<div class="post-item">' +
+      '<p>' + '<a class="comment-toggle" href="#">' + 'Comments' + '</a> ' + 'Message: ' + p.text + '</p>' +
+      '<p>Posted by: ' + p.name + '</p>'
+    return pTemplate;
+  }
+
+  //Template for adding comments to posts
+  var commentTemplate = function(p) {
+    var cTemplate = '';
+  }
+
+  //Function to render the posts
+  var renderPosts = function() {
+    $('.posts').empty();
+    posts.forEach(function(p) {
+      $('.posts').append(postTemplate(p));
+    })
+  }
+
+  //Function to add a post to the posts array when the button is clicked
+  var addPost = function(name, text) {
+    var post = {
+      name : name,
+      text : text,
+      comments : []
+    }
+    posts.push(post);
+    renderPosts();
+  }
+
+  //Function to add a comment to the posts
+  var addComment = function(name, text) {
+
+  }
+
+  var toggleComments = function() {
+    if ($('.comments').css('display') === 'block') {
+      $('.comments').css('display', 'none');
+    } else {
+      $('.comments').css('display', 'block');
+    }
+  }
+
+  return {
+    addPost: addPost,
+    posts: posts,
+    toggleComments: toggleComments
+  }
 }
 
-// Function to add a post to the post div
-var createPost = function(post) {
-  var template =
-    '<div class="post-item">' +
-    '<p>' + '<a class="btn btn-primary btn-sm" href="#comments">' + 'Comments' + '</a> ' + 'Message: ' + post.message + '</p>' +
-    '<p>Posted by: ' + post.name + '</p>' +
-    '</div>' +
-    '<div class = "comments">' + 'This is a comment' + '<div>';
-  return template;
-}
-
-//<a class="btn btn-primary" href="#" role="button">Link</a>
-
-// Separating the functionality of the model and the view
-var renderPosts = function() {
-  var $postList = $('.posts')[0];
-  // Clearing out the original div of children so it can be repopulated
-  while ($postList.hasChildNodes() && $postList.firstElementChild !== null) {
-    $postList.firstElementChild.remove();
-  }
-  for (i = 0; i < posts.length; i++) {
-    $('.posts').append(createPost(posts[i]));
-  }
-}
-
-// Creating a function to toggle comment content
-var commentToggle = function() {
-  var $comment = $('.comments');
-  if ($comment === undefined) {
-    return true;
-  }
-  if ($comment.css('display') === "none") {
-    $comment.css('display', 'block');
-  } else {
-    $comment.css('display', 'none');
-  }
-  return true;
-}
-
-//Creating a button click event to toggle comments along with a text box to post
-//comments
-var $commentButton = $('.btn-sm');
-$commentButton.on('click', commentToggle());
-
-
-// Creating a button click event to post a message when the user fills out the
-// name and the message
+var postObject = PostList();
 var $postButton = $('.post-button');
+var $body = $('body');
 
 $postButton.on('click', function() {
-
   var $name = $('#name').val();
-  var $message = $('#message').val();
-  var newPost = new Post($name, $message);
-  posts.push(newPost);
-  renderPosts();
-})
+  var $msg = $('#message').val();
+  postObject.addPost($name, $msg);
+});
