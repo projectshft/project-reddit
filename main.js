058aeb7da4b7dec5c58f328daf3380804413cca9
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-09-27 16:19:52 -0400

    Add check for form text content

--- main.js
+++ main.js
@@ -2,11 +2,16 @@
 postClicked = function() {
   let postInput = $("#post-text").val()
   let nameInput = $("#name-text").val()
+  if (!postInput || !nameInput) {
+    alert('Please fill in all fields')
+    return;
+  }
+
   let postHtml  = '<div class="page-header">' +
                   '<p>'+ postInput + '</p>' +
   '<p>Posted By: <span><strong>' + nameInput + '</strong></span></p>' +
     '</div>'
-    
+
 // Add our saved inputs to the page
   $("#post-container").prepend(postHtml);
 
