62c000e5c73985551cb4cb97f38b79786e330776
Billy DeLashmutt <delashmuttwa@vt.edu>
2019-09-28 20:43:35 -0400

    Add post removal link & functionality

--- main.js
+++ main.js
@@ -7,23 +7,29 @@ postClicked = function() {
     return;
   }
 
-  let postHtml =  '<a href="#" class="comment-link" role="button">Comments</a>' + '<div class="page-header">' +
+  let postHtml =  '<div class="post">' +
+                  '<a href="#" class="remove-link" role="button">Remove</a>' +
+                  '<a href="#" class="comment-link" role="button">Comments</a>' + '<div class="page-header">' +
                   '<p>'+ postInput + '</p>' +
                   '<p>Posted By: <span><strong>' + nameInput + '</strong></span></p>' +
                   '<form class="comment-form hidden">' +
                   '<input class="comment-text" type="text" placeholder="Comment Text">' +
                   '<input class="comment-user" type="text" placeholder="Name">' +
                   '<button class="btn btn-primary comment-button" type="button">Add Comment</button>' +
-                  '</form></div>'
+                  '</form></div>' +
+                  '</div>'
 
   // Add our saved inputs to the page and clear form
   $("#post-container").prepend(postHtml);
   $("#post-form")[0].reset();
 }
 
+///////////////////////////////////////////////////
+/* MASTER CLICK HANDLER FOR ALL POST INTERACTION */
+///////////////////////////////////////////////////
 // Check for comment link click and only open comments for that post on click
 // Add comment to it's parent post on 'Add Comment'
-checkCommentClick = function(event) {
+postContainerClick = function(event) {
   if ($(event.target).hasClass('comment-link')) {
     $(event.target).next(".page-header").children(".comment-form").toggleClass("hidden")
   }
@@ -32,12 +38,16 @@ checkCommentClick = function(event) {
     let commentUserInput = $(event.target).prev('.comment-user').val()
     let commentTextInput = $(event.target).prevAll('.comment-text').val()
 
-    let commentHtml = '<p>' + commentTextInput + ' <em>' + 'Commented By: <strong>' + commentUserInput + '</strong></em></p>';
+    let commentHtml = '<p>' + commentTextInput + ' <em>' + 'Comment By: <strong>' + commentUserInput + ' </strong></em></p>';
     $(event.target).prevAll(".comment-text").before(commentHtml)
+  }
+
+  if($(event.target).hasClass('remove-link')) {
+    $(event.target).closest('.post').remove();
   }
 
 }
 
 
 $(".post-button").on("click", postClicked);
-$("#post-container").on("click", checkCommentClick)
+$("#post-container").on("click", postContainerClick)
