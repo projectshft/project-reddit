92a0d950d9ec655abb15aa0c96557b6c01120200
daveman235 <awad.david@gmail.com>
2019-09-29 15:34:11 -0400

    2nd commit that includes form reset

--- main.js
+++ main.js
@@ -1,60 +1,58 @@
-//Create function that takes in input in text fields when clicked 
+//Create function that takes in input in text fields when clicked and alerts user if fields are empty
 var $postButtonClicked = function() { 
 	var $postText = $('#postText').val();
+		if ($postText.length < 1) {
+			alert('Please enter a new post')
+			return false;
+	};
     var $yourName = $('#your-name').val();
-    
+    	if ($yourName.length < 1) {
+			alert('Please enter your name')
+			return false;
+	};
 
-    //Variable that takes in the users inputs
+
+//Variable that stores in the users inputs
     var addPost = [{
     Name: $yourName,
     Post: $postText
   	}];
 
-// console.log(addPost);
+//Variables that are able to take in and display the data that was previously stored in the array
+	var $postDisplay = $('.added-post').prepend($postText);
+	var $postNameDisplay = $('.user-name').prepend('Posted By: ' + '<b>' + $yourName + '</b>');
 
-	var $postDisplay = $('.added-post').append($postText);
-	var $postNameDisplay = $('.user-name').append('Posted By: ' + '<b>' + $yourName + '</b>');
+	// var newPost = $postDisplay
 
-
-  //   var addPost = 
-		// console.log($postText);
-		// console.log('Posted By: ' + $yourName);
 };
 
-// Click listener that invokes postButtonClicked variable 
-$('#postbtn').click($postButtonClicked);
+// Click listener that invokes postButtonClicked variable when click is heard
+$('#postBtn').click($postButtonClicked);
+
+
+// function that resets form after submission
+$(document).ready(function(){
+    $("#postBtn").click(function(){
+        $("#configform").trigger("reset");
+    });
+});
 
 
 
-	
-
-
-// var $pushPost = addPost.push($postText, $yourName);
-
-// console.log($pushPost);
-
-////////////////////////////////////////////////////////////
-
-//console.log('the value =' + '' + postText + yourName);
-
-// var postBtn= $("#postbtn").click(function() {
-// 	var postText= $('.form-control').val();
-
-// // 	console.log("the value of the post text is ", postText, postBtn );
-// });
 
 
 
-//HTML + CSS 
-// Add a post
-	//click function on post button 
-	// collect values from form 
-	//put values in a posts array 
-	//clear text boxes 
+
+//HTML + CSS ----------------------------check
+// Add a post----------------------------check
+	//click function on post button -----check
+	// collect values from form ---------check 
+	//put values in a posts array -------check 
+	//clear text boxes ------------------check
 // display posts
-//add commit to post 
+//add comment to post 
 // display comments 
-//delete a comment ]
+//delete a comment 
 // toggle comments box show/hide 
 //delete a post 
 
