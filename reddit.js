b521d1692239b8503e8d8902d078158cdbcaaf21
Jenesis Bonitto <user@users-air.home>
2019-09-29 17:24:02 -0400

    2nd commit, JS started

--- reddit.js
+++ reddit.js
@@ -1,10 +1,21 @@
 //add post
+var button = document.getElementsByTagName("button")[0];
+
+button.addEventListener("click", function( {
+    var userInput = document.getElementsByClassName('')
+})
+console.log(captured)
 
 
-function GetDynamicTextBox1(value) {
+/*<input type="text" id="userInput"=> give me input</input>
+<button onclick="test()">Submit</button>
 
-    return '<input name = "DynamicTextBox1"  id="ingr_name" type="text" value = "' + value + '" required/>&nbsp'  +
-    '<select name="selecval" onchange="selectres($(this))" id="selecval"><option value="">Select</option><option value="1">Yes</option><option value="2">No</option></select>'
-    +'<input name = "DynamicTextBox2" style="display:none" id="res_name" type="text" value = "' + value + '" required/>&nbsp'+ '<input type="button" value="Remove" class="remove" />'
+<!-- add this line for function to write into -->
+<p id="demo"></p>   
 
-    }
+<script type="text/javascript">
+function test(){
+    var userInput = document.getElementById("userInput").value;
+    document.getElementById("demo").innerHTML = userInput;
+}
+</script>//
