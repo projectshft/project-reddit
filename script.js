a12bf0b09a57743e3b00ff1f3c2678548aec804b
George Green <greezy@Georges-MacBook-Air.local>
2019-09-29 17:41:59 -0400

    Final copy. Still having issues associating comments to the correct posts.

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ let createNewCard = function(postNumber, name, post){
     return newCardTemplate;
 };
 
+//teamplate for creating a new comment based on values passed in by the user
 let createNewComment = function(name, post) {
   let newComment =
       '<div class="card-comment">'
@@ -62,13 +63,14 @@ let createNewComment = function(name, post) {
     +     '<span aria-hidden="true" class="close-this">&times;</span>'
     +   '</button></p>'
     + '</div>'
+
   return newComment;
 }
 
 
-//add click event to submit post material
+//adds click event to submit post material
 $postButton.click(function(event) {
-      //prevent html from sending the form via post to ensure it can post on the page
+      //prevents html from sending the form via post to ensure it can post on the page
       event.preventDefault();
 
       //places the information for each post into an object
@@ -78,79 +80,56 @@ $postButton.click(function(event) {
         comments: []
       };
 
-      console.log(allPosts.name, allPosts.post);
       //places each object holding post information into an array
       posts.push(allPosts);
       //loops through the array of posts, posting them and comments
       for (var i = posts.length - 1; i < posts.length; i++) {
         let newPost = createNewCard([i + 1], posts[i].name, posts[i].post);
-        console.log(newPost)
+
         //adds post to the page
         $postForum.append(newPost);
 
-      }
+  }
+      //resets input fields to default
       $('.user-name').val('');
       $('.post-text').val('');
 })
-console.log(posts);
 
+//function to add user comments
 $postForum.click(function(event) {
-  console.log(event.target.className);
   if (event.target.className == 'btn btn-primary card-link') {
     let postComments = {
       name: $('.comment-name').val(),
       comment: $('.comment-remark').val()
     };
-    console.log(postComments);
-    //adds comment object to the comments array for that item
-    posts[0].comments.push(postComments);
-        let userComment = createNewComment(postComments.name, postComments.comment);
-        console.log(userComment);
-        //adds comment to the end of the post
-        $('.card-comments').prepend(userComment)
-      //  event.target.closest('div').prepend(userComment)
+
+    /*
+    adds comment object to the comments array for that item, commented out because I was having
+    difficulty getting through the object for the right values.
+    */
+    //posts[0].comments.push(postComments);
+
+    //stores the HTML for adding the comment into a variable
+    let userComment = createNewComment(postComments.name, postComments.comment);
+
+    //adds comment to the end of the post
+    /*
+    I had a lot of trouble trying to target the comment to the correct post. I still haven't figured out what the problem is
+    but left some of the code commented out to talk through.
+    */
+    //$('.card-comments').prepend(userComment)
+    $(this).attr('class', '.card-comments').prepend(userComment);
+    //removes the post if clicked
   } else if (event.target.className == 'btn btn-primary card-link2'){
     $('.card-style').remove();
   }
 });
-
+//function to delete comments
 $postForum.click(function(event) {
   if (event.target.className == 'close-this') {
-  console.log('true');
   event.target.closest('div').remove();
-}
-$('.comment-name').val(''),
-$('.comment-remark').val('');
+  }
+  //sets comments input back to default
+  $('.comment-name').val('');
+  $('.comment-remark').val('');
 });
-
-
-//
-//         //new click event for comment button on each post
-//         $('.card-link').click(function(event) {
-//           event.preventDefault();
-//           //creates a new object to store comments on each post
-//           let postComments = {
-//             name: $('.comment-name').val(),
-//             comment: $('.comment-remark').val()
-//           };
-//           //adds comment object to the comments array for that item
-//           posts[i - 1].comments.push(postComments);
-//           //loops through the comments for each post item
-//           for (var j = posts[i - 1].comments.length - 1; j < posts[i - 1].comments.length; j++) {
-//             //creates html string to add comment to the comments section
-
-//           }
-//           //sets values in comment fields back to default
-
-//           //adds click function to remove posts on delete
-//           $('.card-link2').click(function() {
-//             $('.card-style').remove();
-//           });
-//           //adds click function to remove comments on delete
-//           $('.delete-comment').click(function() {
-//             $('.card-comment').remove();
-//           });
-//          });
-//        }
-
-// });
