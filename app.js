922b84822edcbb2fee96f380a73caddb6ffd8c9b
Greg Larsen <gslarsen2718@gmail.com>
2019-09-29 21:02:49 -0400

    add comment and post remove and toggle functionality

--- app.js
+++ app.js
@@ -1,150 +1,13 @@
+// Project Reddit
+
 const posts = [];
-const postTextEl = $('#post-text')[0];
-const postNameEl = $('#post-name')[0];
-const buttonEl = $('button')[0];
+let JSONposts = '';
+let postsDivEl = $('#posts')[0];
+let postTextEl = $('#post-text')[0];
+let postNameEl = $('#post-name')[0];
+let buttonEl = $('button')[0];
 
-
-// create/append DOM elements 
-const renderPosts = (posts) => {
-  // debugger;
-  
-  // clear posts container before rendering
-  let postEl = $('#posts');
-  postEl.remove();
-
-  // set up posts container
-  postEl = document.createElement('div');
-  postEl.setAttribute('id', 'posts');
-  postContainer = document.querySelector('#post-container');
-  postContainer.append(postEl);
- 
-  // create elements
-  posts.forEach((post, idx) => {
-    // debugger;
-    
-    // create container for each post
-    const postContainer = document.createElement('div');
-    postContainer.setAttribute('id', idx);
-
-    // set up post elements
-  
-    const textEl = document.createElement('p');
-    textEl.setAttribute('id', 'text');
-    textEl.setAttribute('class', 'posts');
-    textEl.innerHTML = post.text;
-
-    const postedByEl = document.createElement('span');
-    postedByEl.setAttribute('class', 'posts');
-    postedByEl.innerHTML = 'Posted By: ';
-
-    const divEl = document.createElement('div');
-    divEl.setAttribute('class', `${post.name}: ${post.text}`);
-    divEl.setAttribute('style', 'display: none');
-    postedByEl.prepend(divEl);
-
-    const commentFormText = createCommentForm('Comment Text', `${post.name}: ${post.text}`);
-    const commentFormName = createCommentForm('User Name', `${post.name}: ${post.text}`);
-    divEl.appendChild(commentFormText);
-    divEl.appendChild(commentFormName);
-
-    const commentsEl = document.createElement('a');
-    commentsEl.setAttribute('class', 'posts');
-    commentsEl.setAttribute('class', 'comments');
-
-    // toggle comment input forms
-    commentsEl.addEventListener('click', function() {
-      if (divEl.style.display === 'none') divEl.style.display = 'block';
-      else divEl.style.display = 'none';
-    });
-    commentsEl.innerText = 'comments';
-
-    const commentsButton = document.createElement('button');
-    commentsButton.setAttribute('class', "btn btn-primary");
-    commentsButton.setAttribute('style', 'margin-left: 5px')
-    commentsButton.innerText = 'Post Comment';
-
-    // add event listener to comments button
-    commentsButton.addEventListener('click', function () {
-      // debugger;
-      // add to posts if not blank text
-      if (commentFormText.value) {
-        if (commentFormName.value == '') commentFormName.value = 'Anonymous';
-        if (!post.hasOwnProperty('comments')) var comments = [];
-        else comments = post.comments;
-        comments.unshift({ name: commentFormName.value, text: commentFormText.value });
-        post.comments = comments;
-        renderComments(comments, post, idx, textEl);
-        commentFormText.value = '';
-        commentFormName.value = '';
-        commentsEl.click();
-      }
-    })
-    
-    const renderComments = (comments, post, idx, textEl) => {
-      //debugger;
-      // clear current comments container
-      
-      let container = document.querySelector(`#${CSS.escape(idx+post.text)}`);
-      console.log(container)
-
-      if (container) container.remove();
-      
-      // create comments container for rendering comments
-      const commentsSection = document.createElement('div');
-      commentsSection.setAttribute('id', idx+post.text);
-      textEl.append(commentsSection);
-      
-      console.log(Array.isArray(comments));
-      for (let i=0; i < comments.length; i++) {
-        let text = comments[i].text;
-        let name = comments[i].name;
-    
-        let commentLine = document.createElement('p');
-        commentLine.setAttribute('id', idx);
-        commentLine.innerText = `${text} Posted By: ${name}`;
-    
-        commentsSection.append(commentLine);
-      }
-      // comments.forEach((comment, idx) => {
-      //   let text = comment.text;
-      //   let name = comment.name;
-    
-      //   let commentLine = document.createElement('p');
-      //   commentLine.setAttribute('id', idx);
-      //   commentLine.innerText = `${text} Posted By: ${name}`;
-    
-      //   commentsSection.append(commentLine);
-    
-      // })
-
-      const postTextEl = $('#post-text')[0];
-      const postNameEl = $('#post-name')[0];
-      postTextEl.value = '';
-      postNameEl.value = '';
-    };
-
-    divEl.append(commentsButton);
-
-
-    const nameEl = document.createElement('strong');
-    nameEl.setAttribute('class', 'posts');
-    nameEl.innerText = post.name;
-  
-    const horizontalLine = document.createElement('hr');
-    horizontalLine.setAttribute('class', 'posts');
-  
-    postContainer.append(textEl);
-    textEl.prepend(commentsEl);
-    postContainer.append(postedByEl);
-    postContainer.append(nameEl);
-    postContainer.append(horizontalLine);
-    postEl.append(postContainer);
-    renderComments(post.comments, post, idx, textEl);
-    
-  })
-}
-
-// prevent default behavior on forms
+// prevent default entry on form
 const checkIfFormReturnIsPressed = (event) => {
   if (event.keyCode === 13) {
     buttonEl.click();
@@ -152,46 +15,160 @@ const checkIfFormReturnIsPressed = (event) => {
   }
 };
 
+// add new posts
 buttonEl.addEventListener('click', function () {
-  // select form text and name
-  const postTextEl = $('#post-text')[0];
-  const postNameEl = $('#post-name')[0];
-
   // add to posts if not blank text
   if (postTextEl.value) {
     if (postNameEl.value == '') postNameEl.value = 'Anonymous';
-    posts.push({ name: postNameEl.value, text: postTextEl.value });
+    posts.push({ name: postNameEl.value, text: postTextEl.value, id: postNameEl.value + postTextEl.value, comments: [] });
+    sessionStorage['posts'] = JSON.stringify(posts);
     renderPosts(posts);
     postTextEl.value = '';
     postNameEl.value = '';
+    buttonEl.blur();
   }
 });
 
-const createCommentForm = (placeholder, id) => {
+function renderPosts(posts) {
+  debugger;
+  postsDivEl.innerHTML = '';
+  for (let idx = 0; idx < posts.length; ++idx) {
+    const post = posts[idx];
+
+    // create container for individual post
+    let postContainerDiv = document.createElement('div');
+    postContainerDiv.setAttribute('id', `${idx}${post.id}`);
+    console.log(postContainerDiv);
+
+    // create top lone of post
+    let firstLineOfPostRemove = document.createElement('span');
+    firstLineOfPostRemove.setAttribute('style', "color: blue");
+    firstLineOfPostRemove.innerText = 'remove';
+    firstLineOfPostRemove.addEventListener('click', function () {
+      removePost(`${idx}`);
+    })
+
+    function removePost(idx) {
+      posts.splice(idx, 1);
+      let postEl = $('div[id*="' + idx + post.name + post.text + '"]'); 
+      postEl.remove();
+    }
+
+    postContainerDiv.append(firstLineOfPostRemove);
+
+    let firstLineOfPostComments = document.createElement('span');
+    firstLineOfPostComments.setAttribute('style', 'color: blue');
+    firstLineOfPostComments.innerText = ' comments';
+    firstLineOfPostComments.addEventListener('click', function () {
+      debugger;
+      if (commentsSection.style.display === 'none' || commentsSection.style.display == '') commentsSection.style.display = 'block';
+      else commentsSection.style.display = 'none';
+    })
+
+    postContainerDiv.append(firstLineOfPostComments);
+
+    let firstLineOfPostText = document.createElement('span');
+    firstLineOfPostText.innerText = ` ${post.text}`;
+    postContainerDiv.append(firstLineOfPostText);
+
+    // create COMMENTS section
+    let commentsSection = document.createElement('div');
+    commentsSection.setAttribute('name', `${post.id}`);
+    commentsSection.setAttribute('style', "display: block");
+    commentsSection.innerHTML = `<div name="comments-${post.id}"></div>`;
+    postContainerDiv.append(commentsSection);
+
+
+    // create comments form and button for section
+    const commentFormText = createCommentForm('Comment Text');
+    commentFormText.setAttribute('onkeypress', "return checkIfFormReturnIsPressed(event)");
+    const commentFormName = createCommentForm('User Name');
+    commentFormText.setAttribute('onkeypress', "return checkIfFormReturnIsPressed(event)");
+
+    const commentsButton = document.createElement('button');
+    commentsButton.setAttribute('class', "btn btn-primary");
+    commentsButton.setAttribute('style', 'margin-left: 5px')
+    commentsButton.innerText = 'Post Comment';
+    commentsButton.addEventListener('click', function (e) {
+      let commentsParagraphs = $('p');
+      if (commentsParagraphs.length > 0) commentsParagraphs.remove();
+
+      // add to post comments if not blank text
+      if (commentFormText.value || post.comments.length > 0) {
+        if (commentFormName.value == '' && commentFormText.value) {
+          commentFormName.value = 'Anonymous';
+          post.comments.unshift({ name: commentFormName.value, text: commentFormText.value, id: post.id + commentFormName.value + commentFormText.value });
+        } else if (commentFormText.value) { post.comments.unshift({ name: commentFormName.value, text: commentFormText.value, id: post.id + commentFormName.value + commentFormText.value }); }
+        commentFormText.value = '';
+        commentFormName.value = '';
+
+        // render comments
+        for (let index = 0; index < post.comments.length; ++index) {
+          const comment = post.comments[index];
+          let commentEl = document.createElement('p');
+          commentEl.setAttribute('name', comment.id);
+          commentEl.setAttribute('style', "list-style-type:none");
+
+          commentEl.innerHTML = comment.text + ' Posted By: ' + `<strong>${comment.name}</strong>` + `<strong style="color: blue"> x</strong>`;
+          commentEl.children[1].addEventListener(('click'), function (e) {
+            debugger;
+            e.target.parentNode.remove();
+
+            post.comments.splice(index, 1);
+          })
+
+          let lineBreakEl = document.createElement('br');
+          let commentsDiv = $('div[name*="comments-' + post.id + '"]')[0];
+          if (commentsDiv) commentsDiv.append(commentEl);
+        }
+      }
+    })
+
+    commentsSection.appendChild(commentFormText);
+    commentsSection.appendChild(commentFormName);
+    commentsSection.appendChild(commentsButton);
+
+    // create post posted-by section
+    let postPostedBySection = document.createElement('div');
+    postPostedBySection.innerHTML = 'Posted By: ' + `<strong>${post.name}</strong>`;
+    postContainerDiv.append(postPostedBySection);
+    let horizontalLine = document.createElement('hr');
+    postContainerDiv.append(horizontalLine);
+
+
+    // append post to main posts container
+    postsDivEl.appendChild(postContainerDiv);
+
+    // render comments for each post
+    post.comments.forEach((comment) => {
+      let commentsDiv = $('div[name*="comments-' + post.id + '"]')[0];
+      if (commentsDiv) {
+        let commentEl = document.createElement('p');
+        commentEl.setAttribute('name', comment.id);
+        commentEl.setAttribute('style', "list-style-type:none");
+
+        commentEl.innerHTML = comment.text + ' Posted By: ' + `<strong>${comment.name}</strong>` + `<strong style="color: blue"> x</strong>`;
+        commentEl.children[1].addEventListener(('click'), function (e) {
+          debugger;
+          e.target.parentNode.remove();
+
+          post.comments.splice(index, 1);
+        })
+        commentsDiv.append(commentEl);
+        commentsDiv.parentNode.style.display = 'block';
+        commentsDiv.style.display = 'block';
+      }
+    });
+  }
+}
+
+
+
+const createCommentForm = (placeholder) => {
   const commentInputEl = document.createElement('input');
-  commentInputEl.setAttribute('id', id);
-  commentInputEl.setAttribute('placeholder', placeholder);  
+  commentInputEl.setAttribute('placeholder', placeholder);
   return commentInputEl;
 };
 
 
 
-
-
-
-
-
-
-
-
-  // Create from peg form
-  // const fromPegFormEl = document.createElement('form');
-  // const fromPegInputEl = document.createElement('input');
-  // const fromPegInputSubmitEl = document.createElement('input');
-  // fromPegFormEl.setAttribute('id', 'fromPeg');
-  // fromPegInputEl.placeholder = 'enter peg to move from';
-  // fromPegInputEl.name = 'fromPeg';
-  // fromPegInputSubmitEl.setAttribute('type', 'submit');
-  // fromPegFormEl.appendChild(fromPegInputEl);
-  // fromPegFormEl.appendChild(fromPegInputSubmitEl);
-  // document.body.appendChild(fromPegFormEl);
