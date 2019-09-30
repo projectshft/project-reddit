5ccf70c74a449ad6e595269f51fe50907eb5c110
pjcrawford <pcrawford94@gmail.com>
2019-09-30 11:35:42 -0400

    commit

---
+++ main.js
@@ -0,0 +1,84 @@
+
+//jQuery for posts and comments 
+
+var $posts = $('.posts');
+  
+var createPost = function (text, user) {
+
+	//demo comment 
+	  var demo = Model({ text: text, name: user, comments: []});
+  
+	  demo.change(function () {
+		app.renderComments();
+	  });
+  
+	  posts.add(demo);
+	  renderComments();
+	};
+
+// To put the new post in the html so the user can see it.  i think the comments containter part is wrong
+var renderPosts = function () {
+	$posts.empty();
+  
+	for (var i = 0; i < posts.models.length; i += 1) {
+		var post = posts.models[i];
+  
+	var commentsContainer = '<div class="comments-container">' + '<div cass=comments-list></div>' +
+		'<input type="text" class="comment-name" placeholder="Comment Text">' + '<input type="text" class="comment-user" placeholder>'
+  
+ // to render to comments after a post 
+var renderComments = function () {
+	$('.comments-list').empty();
+  
+	for (var i = 0; i < posts.models.length; i += 1) {
+		
+	var post = posts.models[i];
+  
+		var $post = $('.posts').find('.post').eq(index);
+  
+		for (var x = 0; x < post.get('comments').length; x += 1) {
+		
+		  var comment = post.get('comments')[x];
+  
+		};
+	  };
+	};
+  
+//variable to create comment  
+var createComment = function (text, name, postIndex) {
+	var comment = { text: text, name: name };
+  
+	var currentComments = posts.models[postIndex].get('comments');
+  
+	}
+ //variable to remove comment that is closest  
+	var removeComment = function (commentButton) {
+
+	  var $clickedComment = $(commentButton).closest('.comment');
+  		var commentIndex = $clickedComment.index();
+  
+	  var postIndex = $clickedComment.closest('.post').index();
+  
+	   $clickedComment.remove();
+  
+	  posts.models[postIndex].get('comments').splice(commentIndex, 1);
+	}
+  
+	
+//to render posts and invoke removing posts
+
+	  return createPost;
+
+	  return renderPosts;
+
+	  return removePost;
+
+//to render the comments, I think this part may need fixing
+  
+	  return createComment; 
+
+	  return renderComments;
+}
+
+};
+
