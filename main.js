48740be7f2f7951741a3a07db38d233c0f25890e
daveman235 <awad.david@gmail.com>
2019-09-30 22:26:24 -0400

    added comments for individual posts

--- main.js
+++ main.js
@@ -10,8 +10,7 @@ var $postButtonClicked = function() {
 			alert('Please enter your name')
 			return false;
 	};
-
-
+   
 //Variable that stores in the users inputs
    //  var addPost = [{
    //  Name: $yourName,
@@ -19,15 +18,21 @@ var $postButtonClicked = function() {
   	// }];
 
 //Variables that are able to take in and display the data that was previously stored in the array
-  var template = '<p>'
-   + '  <div class="added-post">' + $postText + '</div>'
-   + '  <div class="pb-3 mt-4 mb-2 border-bottom">' + 'Posted By: ' + '<b>' + $yourName + '</b>' + '</div>';
-   + '  <div class="container">';
-   + '</p>' ;
+  var template = '<p>';
+   template += '  <div class="added-post">' + $postText + '</div>';
+   template += '  <div class="pb-3 mt-4 mb-2 border-bottom">' + 'Posted By: ' + '<b>' + $yourName + '</b>' + '</div>';
+   template += '	<form class="navbar-form navbar-left" id="config-comment" role="search">';
+   template += '  <div class="form-group"></div>';
+   template += '	<input type="text" class="comment-text" placeholder="Comment Text">';
+   template += '	<input type="text" class="usernameInput" placeholder="Username">';
+   template += '  <button type="button" class="btn btn-primary" id="commentBtn">Post Comment</button>';
+   template += '	</form>'
+   template += '</p>' ;
 
+	
 //This invokes the template variable and prepends the data to the site
    $('.post-display').prepend(template);
-
+   $('#commentBtn').click($commentOnPost);
 };
 
 // Click listener that invokes postButtonClicked function when click is heard
@@ -41,10 +46,46 @@ $(document).ready(function(){
     });
 });
 
+//function for add comment events 
+var $commentOnPost = function () {
+	var $commentText = $('.comment-text').val();
+    	if ($commentText.length < 1) {
+			alert('Please enter text')
+			return false;
+	};
+	var $usernameInput = $('.usernameInput').val();
+    	if ($usernameInput.length < 1) {
+			alert('Please enter username')
+			return false;
+		}
+ 	  var templateComment = '<p>'
+   + '	<div class="add-post"></div>'
+   + '  <div class="added-post">' + $commentText + '</div>'
+   + '  <div class="pb-3 mt-4 mb-2 border-bottom">' + 'Posted By: ' + '<b>' + $usernameInput + '</b>' + '</div>';
+   + '  <div class="container">';
+   + '</p>' ;
+
+
+   	$('form').prepend(templateComment);
+ 	};
+
+
+// form reset for comment buttons 
+$(document).ready(function(){
+    $("#commentBtn").click(function(){
+        $("#config-comment").trigger("reset");
+
+
+
+
+
+});
+});
+
 
   // var template = '<p>'
   //  + '	<form class="navbar-form navbar-left" role="search">';
-  //  + '  <div class="form-group">' + '</div>';
+  //  + '  <div class="form-group"></div>';
   //  + '  <div class="added-post">' + $postText + '</div>';
   //  + '  <div class="pb-3 mt-4 mb-2 border-bottom">' + 'Posted By: ' + '<b>' + $yourName + '</b>' + '</div>';
   //  + '	<input type="text" class="form-control" placeholder="Search">';
