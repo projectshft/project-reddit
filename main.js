baece361ecb118f0ddb604a0ef497940945b0364
Paul Storm <paul.w.storm@biola.edu>
2020-01-24 13:09:05 -0500

    Added html outline and submit-post functionality.

--- main.js
+++ main.js
@@ -5,3 +5,29 @@ var posts = {
   post4: {postUser: "Hanna", postContent: "Storm", postUpvotes: 1, postComments: []}
 };
 var postCounter = 4;
+
+// When user presses post the a new object is created which stores the post content, user name, comments and upvote count
+$("#submit-post").click(function() {
+    if ($("#message").val().length !== 0 && $("#name").val().length !== 0) {
+    postCounter += 1;
+    var postContent = $("#message").val();
+    var postUser = $("#name").val();
+    var postId = "post" + postCounter;
+    postUpvotes = 1;
+    postComments = [];
+
+    posts[postId] = {
+      postUser: postUser,
+      postContent: postContent,
+      postUpvotes: postUpvotes,
+      postComments: postComments
+    };
+
+    $("#message").val("");
+    $("#name").val("");
+
+    renderPosts(posts);
+  } else {
+    alert("Your post must have a name and message!")
+  }
+});
