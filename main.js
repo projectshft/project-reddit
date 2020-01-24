a8d13fec3d4aa20952482bf8b08c54527f1393f1
Paul Storm <paul.w.storm@biola.edu>
2020-01-24 12:46:32 -0500

    Created index.html and main.js. Added html template and first js elements.

---
+++ main.js
@@ -0,0 +1,7 @@
+var posts = {
+  post1: {postUser: "Paul", postContent: "Storm", postUpvotes: 1, postComments: []},
+  post2: {postUser: "Johanna", postContent: "Storm", postUpvotes: 1, postComments: []},
+  post3: {postUser: "Charles", postContent: "Storm", postUpvotes: 1, postComments: []},
+  post4: {postUser: "Hanna", postContent: "Storm", postUpvotes: 1, postComments: []}
+};
+var postCounter = 4;
