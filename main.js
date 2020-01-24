5a3917c609f37094272591de55ed96042e535cfa
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-01-24 14:13:30 -0500

    remove button now text but still overdeletes

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
 //this is an array of objects containing the posts information
 
-var posts = [
-  //the text of the post
-  {message: 'message',
-  // the name of the poster
-  name:'name',
-  //
-  comment: [{commentText:'commenttext', commentName:'commentname' }]
-  }
-]
+var posts = [];
+// var posts = [
+//   //the text of the post
+//   {
+//     userMessage: 'userMessage',
+//     // the name of the poster
+//     userName: 'userName',
+//     //
+//     userComment: [{
+//       commenterMessage: 'commenterMessage',
+//       commenterName: 'commenterName'
+//     }]
+//   }
+// ]
+//the addPost function pushes new posts into the post array
+var addPost = function(post) {
+  posts.push(post);
+}
 
-$('.btn-primary').click(function () {
+var post = $(this).closest('.post').data();
+addPost(post);
+console.log(posts);
+
+$('.btn-primary').click(function() {
+  //creates a new remove button
+  // var remove = $('<input class="remove" type="text" id="remove" value="remove"/>');
+  //appends the remove button to the post
+  $('.post').append('<p>' + 'remove' + '</p>').addClass('remove');
+  //posts user button
   var userMessage = $('#message').val();
-  $('.post').append('<div>' +  userMessage + '</div>' );
-
+  //appends the user message to the post
+  $('.post').append('<p>' + userMessage + '</p>');
+  //posted user Names
   var userName = $('#name').val();
-  $('.post').append('Posted By:'+'<div> <strong>'+ userName +'</strong> </div>');
-
-  $('.post').on('click', 'div', function(){
-  $(this).remove();
-
-  })
+  //appends the user name to the post
+  $('.post').append('<p>' +' Posted By:' + '</p>' + '<p> <strong>' + userName + '</strong> </p>');
+  //deletes the post when the remove button is clicked
+  //TODO!! make it not delete every post when clicked
+  //TODO!! make it text that when clicked deletes the entire post
+  $('.remove').on('click', function() {
+    $('.post').remove();
+  });
 
 });
