7a8d9e1babed17f769f1ec58d1023d8b97c686fb
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-01-24 14:43:28 -0500

    updated with comments

--- main.js
+++ main.js
@@ -15,15 +15,14 @@ var posts = [];
 //   }
 // ]
 //the addPost function pushes new posts into the post array
-var addPost = function(post) {
-  posts.push(post);
-}
 
-var post = $(this).closest('.post').data();
-addPost(post);
-console.log(posts);
+//
+// var post = $(this).closest('.post').data();
+// addPost(post);
+// console.log(posts);
 
-$('.btn-primary').click(function() {
+//addPosts
+var addPost = $('.btn-primary').click(function() {
   //creates a new remove button
   // var remove = $('<input class="remove" type="text" id="remove" value="remove"/>');
   //appends the remove button to the post
@@ -35,12 +34,20 @@ $('.btn-primary').click(function() {
   //posted user Names
   var userName = $('#name').val();
   //appends the user name to the post
-  $('.post').append('<p>' +' Posted By:' + '</p>' + '<p> <strong>' + userName + '</strong> </p>');
+  $('.post').append('<p>' + ' Posted By:' + '</p>' + '<p> <strong>' + userName + '</strong> </p>');
+
   //deletes the post when the remove button is clicked
   //TODO!! make it not delete every post when clicked
   //TODO!! make it text that when clicked deletes the entire post
   $('.remove').on('click', function() {
-    $('.post').remove();
+    $(this).closest('.post').remove();
   });
 
+  //pushes new posts in Posts array
+  posts.push({
+    name: userName,
+    message: userMessage,
+    comment: null
+  });
+  console.log(posts);
 });
