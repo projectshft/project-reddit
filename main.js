4856abd3eab0c16c4c8909b3c1f97a3acf182eef
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-01-24 18:56:14 -0500

    addToArray + renderPosts functions are in this version

--- main.js
+++ main.js
@@ -1,59 +1,70 @@
 //this is an array of objects containing the posts information
 //empty array where we push in the objects
 var postsArray = [];
+var order = 0;
+
 
 //addPosts
-var addPost = $('.btn-primary').click(function() {
-  //creates a new remove button
-  // var remove = $('<input class="remove" type="text" id="remove" value="remove"/>');
-  //appends the remove button to the post
-  $('.post').append('<p>' + 'remove' + '</p>').addClass('remove');
-  //posts user button
+
+var addToPostArrays = function() {
+  //takes the values from the input field on the forum
   var userMessage = $('#message').val();
-  //appends the user message to the post
-  $('.post').append('<p>' + userMessage + '</p>');
-  //posted user Names
   var userName = $('#name').val();
-  //appends the user name to the post
-  $('.post').append('<p>' + ' Posted By:' + '</p>' + '<p> <strong>' + userName + '</strong> </p>');
-
-  //deletes the post when the remove button is clicked
-  //TODO!! make it not delete every post when clicked
-  //TODO!! make it text that when clicked deletes the entire post
-  $('.remove').on('click', function() {
-    $(this).closest('.post').remove();
-  });
-
-  //pushes new posts in Posts array
+  var postComment = $()
+  var postNumber =$()
+  //pushes the values into Posts array
   postsArray.push({
     name: userName,
     message: userMessage,
-    comment: [
-
-    ]
+    comment: [],
+    order: order++
   });
-  console.log(postsArray);
-  //rerenders the post
   renderPost();
+};
+
+
+$('.btn-primary').click(function() {
+  addToPostArrays();
 });
-
-
-//take as input this array objects
-//using
 
 var renderPost = function() {
   $('.posts').empty();
-  $('.post').html('0');
 
   var posts = '';
   //looping through the array to find stuff
   for (var i = 0; i < postsArray.length; i++) {
-    posts += '<div>' + postsArray[i].name +
-      ' - ' + postsArray[i].message + ' Comment - ' + postsArray[i].comment + '</div>';
-  }
-  $('.posts').append(posts)
+    addPost(posts[i]);
+    $('.posts').append(posts);
+  };
+};
 
-  console.log(posts);
-}
+var addPost = function(post) {
+  //creates a new remove button
+  // var remove = $('<input class="remove" type="button" id="removeButton" value="remove"/>');
+  // //appends the remove button to the post
+  // $('.post').append(remove);
+  // $('.post').append('<p>' + 'remove' + '</p>').addClass('remove');
+  //posts user button
+  //appends the user message to the post
+  $('.post').append('<p>' + userMessage + '</p>');
+  //posted user Names
+  //appends the user name to the post
+  $('.post').append('<p>' + ' Posted By:' + '</p>' + '<p> <strong>' + userName + '</strong> </p>');
+  //pushes new posts in Posts array
+};
 
-renderPost();
+console.log(addPost());
+console.log(postsArray);
+//
+//
+//   //deletes the post when the remove button is clicked
+//   //TODO!! make it not delete every post when clicked
+//   //TODO!! make it text that when clicked deletes the entire post
+//   $('.remove').on('click', function() {
+//     $(this).closest('.zzzpost').remove();
+//   });
+//take as input this array objects
+//using
+// var toogleComments = $('.view-cart').on('click', function () {
+//   $('.shopping-cart').toggleClass('show');
+// });
