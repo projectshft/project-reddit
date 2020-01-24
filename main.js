e1246ad7144b4b82fe5367c60c88cc96b9b10851
Hannah Cline <han@Hannahs-MacBook-Air-2.local>
2020-01-24 12:58:50 -0500

    center the text and added to the html file also added a function to remove posts

---
+++ main.js
@@ -0,0 +1,14 @@
+var revealRemove = $('.remove').click(function (){
+  $(this).remove()
+  //$(this).find("post").remove()
+
+  /*if ($('.remove').hasClass(" show") === true){
+    $('.remove').hide();
+  }else {
+    $('.remove').show()
+  }
+
+  } else {
+    shoppingCart.className += ' show';
+  }*/
+});
