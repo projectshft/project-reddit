956cd3046f4cc533266b1f33864158d41a2eda13
Paul Storm <paul.w.storm@biola.edu>
2020-01-24 13:38:20 -0500

    Added editPost function.

--- main.js
+++ main.js
@@ -31,3 +31,20 @@ $("#submit-post").click(function() {
     alert("Your post must have a name and message!")
   }
 });
+
+var editPost = function(button) {
+  $(".edit-post").off();
+
+  var editForm =
+  "<form style='margin-top:30px' onsubmit='event.preventDefault()';>"
+  + "<textarea type='text' id='edited-content' class='form-control'>" + posts[button.parent().attr('id')].postContent + "</textarea>"
+  + "<button type='submit' id='submit-edit' class='btn btn-primary'>Submit Edit</button>"
+  + "</form>";
+
+  button.parent().append(editForm);
+
+  $("#submit-edit").click(function(button) {
+    posts[$(this).parent().parent().attr('id')].postContent = $("#edited-content").val();
+    renderPosts(posts);
+  })
+}
