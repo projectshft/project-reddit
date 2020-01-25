8763eac8d45662324b2364dde340175b5a532631
Hannah Cline <han@Hannahs-MacBook-Air-2.local>
2020-01-25 15:08:09 -0500

    added to the renderBoard function, can now add comments to first post. commentspost  button still does not work for post passed the first post and the comments toggle is still not fixed. still need to add the remove x button to comments

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //post button is clicked and then the input data goes into an object.
 //then calls add post function to push it into my empty array
-$('.btn-primary').on('click', function () {
+$('#post-btn').on('click', function() {
   var postedMessageUserName = $('#post-name').val()
   var postedMessage = $('#post-message').val()
   var post = {
-    name:postedMessageUserName,
-    message:postedMessage,
-    comments:[]
+    name: postedMessageUserName,
+    message: postedMessage,
+    comments: []
   }
   addPost(post);
 });
@@ -15,37 +15,73 @@ $('.btn-primary').on('click', function () {
 
 var posts = [];
 
-
 //appends the new html also called remove and comment to invoke the ability to click them
-var renderBoard = function () {
+var renderBoard = function() {
   $('.posts').empty();
-
+  console.log('renderboard function was called')
+  //loops through the comments to format them and add to html append line
+  var commentsFormated = ""
   for (var i = 0; i < posts.length; i++) {
     var newPost = posts[i];
-    $('.posts').append('<div class = post> <p class = "btn-remove inline-text">Remove</p><p class = "btn-comment inline-text"> Comment </p><p class = "inline-text"> '+ newPost.message + ' </p><form class = "form-inline comment"><input id = "comment-name" type = "text" class = "form-control" placeholder = "User Name"><input id="comment" type = "text" class = "form-control" placeholder = "Comment"><button type="button" class = "btn btn-primary">Post</button></form><p>Posted by:<strong>' + newPost.name + '</strong></p>');
+    newPost.comments.forEach(function(array) {
+      commentsFormated = commentsFormated + '<p class = comment-message>' + array.message + 'Posted by:' + array.name + '</p>';
+      return commentsFormated
+    })
+    //if there are no comments it is not added to the html append
+    if (commentsFormated == undefined) {
+      $('.posts').append('<div class = post> <p class = "btn-remove inline-text">Remove</p><p class = "btn-comment inline-text"> Comment </p><p class = "inline-text"> ' + newPost.message + ' </p><form class = "form-inline comment"><input id = "comment-name" type = "text" class = "form-control" placeholder = "User Name"><input id="comment-message" type = "text" class = "form-control" placeholder = "Comment"><button id = "comment-post-btn" type="button" class = "btn btn-primary">Post</button></form><p>Posted by:<strong>' + newPost.name + '</strong></p>');
+      //if there are comments it add to it
+    } else {
+      $('.posts').append('<div class = post> <p class = "btn-remove inline-text">Remove</p><p class = "btn-comment inline-text"> Comment </p><p class = "inline-text"> ' + newPost.message + commentsFormated + ' </p><form class = "form-inline comment"><input id = "comment-name" type = "text" class = "form-control" placeholder = "User Name"><input id="comment-message" type = "text" class = "form-control" placeholder = "Comment"><button id = "comment-post-btn" type="button" class = "btn btn-primary">Post</button></form><p>Posted by:<strong>' + newPost.name + '</strong></p>');
+    }
   }
-removeAndComment()
+  removeAndComment()
 };
 
 //pushes the posts into the posts array. Then calls the renderBoard function to post to screen
-var addPost = function (post) {
+var addPost = function(post) {
   posts.push(post);
-  console.log('this is the new posts array' + posts)
   renderBoard()
+  //commentClickButton()
 }
-//removes all post and comments when hiting the word remove and removes from posts array
-var removeAndComment = function(){
-$('.btn-remove').on('click', function (){
+//removes all post and comments when hiting the word remove and removes from posts array. also calls the click function for the comments post button
+var removeAndComment = function() {
+  $('.btn-remove').on('click', function() {
     var postArrayIndex = $(this).closest(".post").index()
     $(this).closest(".post").remove()
     posts.splice(postArrayIndex, 1);
-});
-//reveals the comment form when users presses the comment button
-//TODO
-$('.btn-comment').on('click',function(){
-  $(this).closest(".comment").toggleClass(' show')
-  console.log('comment button clicks')
-})
+  });
+  //reveals the comment form when users presses the comment button
+  //TODO
+  $('.btn-comment').on('click', function() {
+    $(this).closest(".comment").toggleClass(' show')
+    console.log('comment button clicks')
+    //commentClickButton()
+  })
+  //allows user to click on comment button and adds them into and object. then calls the push function
+  $('#comment-post-btn').on('click', function() {
+    console.log('the comment post button clicks')
+    var postedCommentUserName = $('#comment-name').val()
+    var postedComment = $('#comment-message').val()
+    var postCommentedOnArrayIndex = $(this).closest(".post").index()
+    console.log(postCommentedOnArrayIndex)
+    var comment = {
+      name: postedCommentUserName,
+      message: postedComment,
+    }
+    console.log(comment)
+    addComment(comment, postCommentedOnArrayIndex);
+
+  });
 }
+
+//function will add comments to the posts array
+var addComment = function(comment, postCommentedOnArrayIndex) {
+  posts[postCommentedOnArrayIndex].comments.push(comment)
+  //  posts.forEach(item => item.comments.forEach(array => console.log(array.message + "Posted by: " + array.name)))
+  renderBoard()
+}
+
+
 //renders the post at the start
 renderBoard();
