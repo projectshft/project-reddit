30c7be5e518ad52597f38d60c66635bdf9fd624c
Hannah Cline <han@Hannahs-MacBook-Air-2.local>
2020-01-24 19:47:18 -0500

    remove button removes post but need to delete post from posts array too. changed css display for comments section.

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-
-//Template for adding comments
-//var templateForComments = '<p>' + <userComment> + 'by:<strong>' + userName + '</strong></p>'
-
-//Template for adding posts
-//var templateForPost =
-//'<div class="post form-group"><div><p class="remove">remove</p><p class="comment-button">comments </p><p>' + postedMessage + '</p></div><div class="enter-comment"><div class="form-group"><input id="commenters-name" type="text" class="form-control comment" placeholder="User Name"></input></div><div class="form-group"><input id="message" type="text" class="form-control comment" placeholder="Comment Text" </input></div><button type="submit" class="btn btn-comment">Post Comment</button></div><p>Posted by: <strong>' + postedMessageUserName + '</strong></p></div>'
-
-
-//removes all post and comments when hiting the word remove
-var removePost = $('.remove').on('click', function (){
- $(this).closest(".post").remove()
- console.log('hi')
-});
-
-//reveals the comment form when users presses the comment button
-//TODO
-var showComment = $('.comment-button').click(function(){
-  $(this).closest(".post").closest('.enter-comment').remove()
-})
-
 //post button is clicked and then the input data goes into an object.
 //then calls add post function to push it into my empty array
 $('.btn-primary').on('click', function () {
@@ -26,46 +5,50 @@ $('.btn-primary').on('click', function () {
   var postedMessage = $('#post-message').val()
   var post = {
     name:postedMessageUserName,
-    message:postedMessage
+    message:postedMessage,
+    comments:[]
   }
-  console.log('this is the post' + post)
   addPost(post);
 });
 
-
-
-
-
-
 //Get user input from main post and stores in variables
 
-
-var posts = [
-{name: 'Hannah', message:'Is the moon landing real?'},
-{name:'April', message:'Hello'}
-];
+var posts = [];
 
 
-//addends the new html
+//appends the new html also called remove and comment to invoke the ability to click them
 var renderBoard = function () {
   $('.posts').empty();
 
   for (var i = 0; i < posts.length; i++) {
     var newPost = posts[i];
-
-    $('.posts').append('<div class = post> <p class = remove>Remove</p><p class = comment>Comment</p><p>'+ newPost.message + '</p><p>Posted by:<strong>' + newPost.name + '</strong></p>');
+    $('.posts').append('<div class = post> <p class = "btn-remove inline-text">Remove</p><p class = "btn-comment inline-text"> Comment </p><p class = "inline-text"> '+ newPost.message + ' </p><form class = "form-inline comment"><input id = "comment-name" type = "text" class = "form-control" placeholder = "User Name"><input id="comment" type = "text" class = "form-control" placeholder = "Comment"><button type="button" class = "btn btn-primary">Post</button></form><p>Posted by:<strong>' + newPost.name + '</strong></p>');
   }
+removeAndComment()
 };
+
 //pushes the posts into the posts array. Then calls the renderBoard function to post to screen
 var addPost = function (post) {
   posts.push(post);
   console.log('this is the new posts array' + posts)
+  debugger;
   renderBoard()
 }
-//clears the post to rerender it
-/*var clearPostBoard = function () {
-  posts = [];
-  updatePostBoard();
-}*/
+//removes all post and comments when hiting the word remove
+//DOESNT WORK TODO
+var removeAndComment = function(){
+$('.btn-remove').on('click', function (){
+    $(this).closest(".post").remove()
+    console.log(posts)
+//// TODO: REMOVE BUTTON REMOVES FROM HTML BUT DOESNOT REMOVE FROM POST ARRAY******
+});
 
+//reveals the comment form when users presses the comment button
+//TODO
+$('.btn-comment').on('click','.btn-comment',function(){
+  console.log('The comment button works')
+  $(this).closest(".comment").toggleClass('show')
+})
+}
+//renders the post at the start
 renderBoard();
