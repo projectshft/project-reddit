a054a13cbd4f392a289b3103924400d89f17b7d6
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2020-01-25 16:09:40 -0500

    init commit, remove comment- work in progress

---
+++ scripts/data.js
@@ -0,0 +1,28 @@
+
+
+// let posts = [
+//   {
+//     text: "Happy Friday",
+//     name: "Henry",
+//     comments: [
+//       {
+//         text: "Hurray!",
+//         name: "Olga"
+//       }
+//     ]
+//   },
+//   {
+//     text: "What is Reddit?",
+//     name: "Tom",
+//     comments: [
+//       {
+//         text: "Who knows...",
+//         name: "Alan"
+//       },
+//       {
+//         text: "Google it!",
+//         name: "Judson"
+//       }
+//     ]
+//   },
+// ]
---
+++ scripts/main.js
@@ -0,0 +1,159 @@
+let countPosts = 0;
+let posts = [];
+let $postsList = $('.posts');
+let showInputs = false;
+let comments= [];
+let countComments = 0;
+let $commentsList = $('');
+let $commentInput = $('');
+
+let renderComment = function(num, postNum, text, author) {
+  let commentRow =
+   '<span>' + text + '</span>'
+    + '<span> Comment By: ' + author + '</span>'
+    + '<span id="remove-comment-'+ postNum + '-' + num + '" class="remove">x</span><br>';
+
+  let $row = $(commentRow);
+
+  return $row;
+}
+
+let renderPost = function(postNumber, postText, postAuthor, comments) {
+
+    let template =
+     '<div class="post" id="'+ postNumber +'">'
+   + '<div class="post-text"><span id="remove-post'+ postNumber +'" class="remove-post">remove</span>'
+   + '<span id="show-post-comments'+ postNumber +'" class="show-comments">comments</span>'
+   + postText + '</div>'
+   + '<div class="comments'+ postNumber +'" style="display: none;"></div>'
+   + '<div id="input'+ postNumber +'" style="display: none;></div>'
+   + '<div id="post-comment-row'+ postNumber +'"></div>'
+   + '<div class="post-author">Posted By: ' + postAuthor + '</div>'
+   + '</div>';
+
+   let $row = $(template);
+
+   return $row;
+}
+
+let displayPosts = function() {
+  $postsList.empty();
+
+  for (let i = 0; i < posts.length; i++) {
+    let $postRow = renderPost(i + 1, posts[i].text, posts[i].author, posts[i].comments);
+    $postsList.append($postRow);
+  }
+}
+
+let displayComments = function(postId) {
+  $commentsList.empty();
+  //console.log('From display comments function: '+postId);
+  for (let i = 0; i < posts[postId].comments.length; i++) {
+    let $commentRow = renderComment(i + 1, postId, posts[postId].comments[i].text, posts[postId].comments[i].author);
+    $commentsList.append($commentRow);
+  }
+}
+
+let displayCommentInput = function(index) {
+  $commentInput.empty();
+
+  let commentRow =
+      '<form style="margin-top:5px;" onsubmit="event.preventDefault();">'
+      + '<input id="comment-text'+ index +'" type="text" class="comment-input" placeholder="Comment Text">'
+      + '<input id="comment-user'+ index +'" type="text" class="comment-input" placeholder="User Name">'
+      + '<button id="addComment'+ index +'" type="submit" class="btn btn-primary postCommentBtn">Post Comment</button>'
+      + '</form>';
+
+    $commentInput.append(commentRow);
+}
+
+// on post submit event save input to the posts array and render posts
+$('#postBtn').on('click', function() {
+  //get input values
+  let $postText = $('#post-text').val();
+  let $postAuthor = $('#author').val();
+  let post = {
+    text: '',
+    author: '',
+    comments: []
+  };
+  //post object to add to the posts array
+  post.text = $postText;
+  post.author = $postAuthor;
+  post.comments = [];
+
+  //add new post to a posts array
+  posts.push(post);
+
+  displayPosts();
+  countPosts++;
+  //clear input fields
+  $('#post-text').val('');
+  $('#author').val('');
+})
+
+$('.posts').on('click', '.show-comments', function(e) {
+  e.preventDefault();
+
+  let currentIndex = $(this).attr('id').slice(-1);
+  $commentsList = $('#comments' + currentIndex);
+  $commentInput = $('#input' + currentIndex);
+
+  displayCommentInput(currentIndex);
+
+  $('.comments' + currentIndex).toggle('slow');
+  $('#input' + currentIndex).toggle('slow');
+});
+
+//add comments
+$('.posts').on('click', '.postCommentBtn', function(e) {
+   e.preventDefault();
+
+   let currentIndex = $(this).attr('id').slice(-1);
+   $commentsList = $('.comments' + currentIndex);
+   $commentInput = $('#input' + currentIndex);
+
+   //console.log('show index of a post to add the comment: '+ currentIndex);
+   let $commentText = $('#comment-text' + currentIndex).val();
+   let $commentAuthor = $('#comment-user' + currentIndex).val();
+   //alert($commentText)
+   //debugger;
+   let comment = {};
+   comment.text = $commentText;
+   comment.author = $commentAuthor;
+
+   //add new comment to comments array for the post id = countPosts
+   posts[currentIndex-1].comments.push(comment);
+   //console.log('Print the array of comments for index: ' +currentIndex +' , comment: ' + comment );
+   countComments++;
+   displayComments(currentIndex-1);
+   $commentText = $('#comment-text' + currentIndex).val('');
+   $commentAuthor = $('#comment-user' + currentIndex).val('');
+})
+
+//delete a post with comments
+$('.posts').on('click', '.remove-post', function(e) {
+  e.preventDefault();
+  let post = $(this).attr('id').slice(-1);
+
+  posts.splice(post-1, 1);
+
+  displayPosts();
+  countPosts--;
+});
+
+//delet the comment of the post
+$('.posts').on('click', '.remove', function(e) {
+  e.preventDefault();
+
+  let comment = $(this).attr('id').slice(-1);
+  console.log('Comment index: ' + comment);
+
+  let postIndex = $(this).attr('id');
+  let temp = postIndex.split('-');
+  console.log('Post index: ' + temp[2]);
+
+  let temp2 = posts[temp[2]].comments.splice(comment-1, 1);
+  displayComments(temp[2]-1);
+
+});
