f2b768c2a29dcdc57206ed2197ceee2a911f0486
Hannah Cline <han@Hannahs-MacBook-Air-2.local>
2020-01-26 15:43:21 -0500

    fixed delete button, complete

--- main.js
+++ main.js
@@ -16,7 +16,8 @@ $('#post-btn').on('click', function() {
 var posts = [];
 
 //appends the new html also called remove and comment to invoke the ability to click them
-var renderBoard = function() {
+var renderBoard = function(postOrComment) {
+  checkTheToggle();
   $('.posts').empty();
   console.log('renderboard function was called');
   //loops through the posts and comments to format them and add to html append line
@@ -26,33 +27,17 @@ var renderBoard = function() {
     var commentsFormated = "";
     newPost.comments.forEach(function(array) {
       commentsFormated = commentsFormated +
-        '<div>' +
-        '<p class = "comment-message inline-text">' +
+        '<div class = "erase-comment">' +
+        '<p class = "inline-text">' +
         array.message + '  Posted by:  ' + array.name + '</p>' +
         '<p class = "delete inline-text"><strong> X </strong></p>' +
         '</div>';
       return commentsFormated;
     })
-    //if there are no comments it is not added to the html append
-    if (commentsFormated == undefined) {
+    //conditon to check if the comments button had been clicked before rerendering the board
+    if (toggleTracker[i] === 'off') {
       $('.posts').append(
-        '<div class = post>' +
-        '<p class = "btn-remove inline-text">Remove</p>' +
-        '<p class = "btn-comment inline-text"> Comment </p>' +
-        '<p class = "inline-text">' + newPost.message + ' </p>' +
-        '<div class="hide">' +
-        '<form class = "form-inline">' +
-        '<input id = "comment-name" type = "text" class = "form-control" placeholder = "User Name">' +
-        '<input id="comment-message" type = "text" class = "form-control" placeholder = "Comment">' +
-        '<button id = "comment-post-btn" type="button" class = "comment-post-btn btn-primary">Post</button>' +
-        '</form>' +
-        '</div>' +
-        '<p>Posted by:<strong>' + newPost.name + '</strong></p>' +
-        '</div>');
-      //if there are comments its add to the posts
-    } else {
-      $('.posts').append(
-        '<div class = post>' +
+        '<div class = "post">' +
         '<p class = "btn-remove inline-text">Remove</p>' +
         '<p class = "btn-comment inline-text"> Comment </p>' +
         '<p class = "inline-text">' + newPost.message + ' </p>' +
@@ -66,11 +51,28 @@ var renderBoard = function() {
         '</form>' +
         '</div>' +
         '<p>Posted by:<strong>' + newPost.name + '</strong></p>' +
-        '</div>')
-        debugger;
+        '</div>');
+    } else {
+      $('.posts').append(
+        '<div class = "post on">' +
+        '<p class = "btn-remove inline-text">Remove</p>' +
+        '<p class = "btn-comment inline-text"> Comment </p>' +
+        '<p class = "inline-text">' + newPost.message + ' </p>' +
+        '<div class = "hide show">' +
+        commentsFormated +
+        '<p class="comment">' +
+        '<form class = "form-inline">' +
+        '<input id = "comment-name" type = "text" class = "form-control" placeholder = "User Name">' +
+        '<input id="comment-message" type = "text" class = "form-control" placeholder = "Comment">' +
+        '<button id = "comment-post-btn" type="button" class = "comment-post-btn btn-primary">Post</button>' +
+        '</form>' +
+        '</div>' +
+        '<p>Posted by:<strong>' + newPost.name + '</strong></p>' +
+        '</div>');
+
     }
   }
-  removeAndComment();
+  allTheButtons();
 };
 
 //pushes the posts into the posts array. Then calls the renderBoard function to post to screen
@@ -79,23 +81,26 @@ var addPost = function(post) {
   renderBoard()
 }
 //removes all post and comments when hiting the word remove and removes from posts array. also calls the click function for the comments post button
-var removeAndComment = function() {
+var allTheButtons = function() {
   $('.btn-remove').on('click', function() {
     var postArrayIndex = $(this).closest(".post").index();
     $(this).closest(".post").remove();
     posts.splice(postArrayIndex, 1);
   });
 
-
+  //makes the comment section appear and disappear
   $('.btn-comment').on('click', function() {
     $(this).siblings(".hide").toggleClass("show");
-    console.log('comment button clicks');
+    $(this).parent(".post").toggleClass("on");
   })
-  //TODO delete button doesnt work!!!!
+  //Deletes comment from html and from post array
   $('.delete').on('click', function() {
-    $(this).closest("comment-message").remove();
-    console.log('delete button clicks');
-  })
+    var postArrayIndex = $(this).closest(".post").index();
+    var commentArrayIndex = $(this).parent().index();
+    $(this).parent().remove();
+    posts[postArrayIndex].comments.splice(commentArrayIndex, 1);
+    console.log('post index:' + postArrayIndex + 'comments Index:' + commentArrayIndex);
+  });
 
   //allows user to click on comment button and adds them into and object. then calls the push function
   $('.comment-post-btn').on('click', function() {
@@ -115,10 +120,22 @@ var removeAndComment = function() {
 //function will add comments to the posts array
 var addComment = function(comment, postCommentedOnArrayIndex) {
   posts[postCommentedOnArrayIndex].comments.push(comment);
-  console.log(posts);
   renderBoard();
 }
-
-
+//keeps track if the comments are open or closed
+var toggleTracker = []
+//check to see if comments are hiden or not pushes anser into array to use when rendering the board
+var checkTheToggle = function() {
+toggleTracker = []
+posts.forEach(function(post, index) {
+  //  if($('.posts')(index).find('.hide')==true){
+  var toggleChecker = $(".post:eq(" + index + ")").hasClass('on')
+  if (toggleChecker == true) {
+    toggleTracker.push('on')
+  } else {
+    toggleTracker.push('off')
+  }
+});
+};
 //renders the post at the start
 renderBoard();
