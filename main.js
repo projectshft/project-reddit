2250f9a1d29b6452103d0286a1afa373dcfbf366
Katy Andersen <katyandersen@Katys-MacBook-Air.local>
2020-01-26 17:39:56 -0500

    render function

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
+var posts = []
+
+
+var render = function () {
+    $('.posts').empty();
+
+ for (var i = 0; i < posts.length; i ++) {
+   // for (var j = 0; j < posts.comments.length; j ++) {
+   //   if(posts[i] == comments[j]) {
+
+   $('.posts').append(
+       '<div class="post">' + '<a href="#" class="removes">remove</a> ' + '<button class="btn-comments">comments</button>' + ' ' + '<br>' +
+       posts.message + '<div class="comments-container hide">' + '<input type="text" class="comment-input" placeholder="Comment">' +
+       '<input type="text" class="commenter-name" placeholder="Name"><button class="btn btn-primary submit-comment">Submit</button></div>' +
+       '<div> Posted By: <strong>' + posts.name + '</strong></div>' + '<hr>' + '</div>');
+
+   $('.posts').append('<div class = "comments">' + posts.comments.comment + ' Posted By: <strong>' +
+      posts.comments.name + '</strong>' + '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>')
+      }
+    }
+  
 // Events
 // Submit Post Event
+
 $('button').click(function() {
 // Edge cases for if user leaves post message blank, post name blank, or both blank
   if ($('#message').val() == '' && $('#name').val() == '') {
@@ -14,13 +36,8 @@ $('button').click(function() {
   var userName = $('#name').val();
   var userMessage = $('#message').val();
 
-// Add post div containing, user input, remove link, comments button and comments container
-// Publish post on the page
-  $('.posts').append(
-    '<div class="post">' + '<a href="#" class="removes">remove</a> ' + '<button class="btn-comments">comments</button>' + ' ' + '<br>' +
-    userMessage + '<div class="comments-container hide">' + '<input type="text" class="comment-input" placeholder="Comment">' +
-    '<input type="text" class="commenter-name" placeholder="Name"><button class="btn btn-primary submit-comment">Submit</button></div>' +
-    '<div> Posted By: <strong>' + userName + '</strong></div>' + '</div>')
+  posts.push({ message: userMessage, name: userName, comments: []});
+  render()
   }
 });
 
@@ -51,8 +68,10 @@ $('.posts').on('click', '.submit-comment', function() {
 
 // Add comments div containing, user comment, commenter name and close button
 // Publish comment on the page
-  $('.posts').append('<div class = "comments">' + userComment + ' Posted By: <strong>' +
-    commenterName + '</strong>' + '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>')
+  // $('.posts').closest('.post').append('<div class = "comments">' + userComment + ' Posted By: <strong>' +
+  //   commenterName + '</strong>' + '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>')
+    posts.comments.push([{comment: userComment , name: userName}])
+    render()
   }
 })
 
