b1ade927ae41856abca9106d62e18aab91ef6c63
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-01-26 22:06:35 -0500

    rewrote the append functions but cant get comments to show in main.js file. test.js is working

--- main.js
+++ main.js
@@ -1,146 +1,171 @@
-//this is an array of objects containing the posts information
-//empty array where we push in the objects
+//creates an empty data structure for posts + comments
 var postsArray = [];
-var order = 1;
-var commentNumber = 1;
-
-//add Posts to Array
-var addToPostsArrays = function() {
-  //takes the values from the input field on the forum
-  var userMessage = $('#message').val();
-  var userName = $('#name').val();
-
+var $postsElement = $('.posts');
+//this function adds the posts to postsArray data structure
+var addToPostsArray = function() {
+  //takes the values from the inputs fields on the forum
+  var $userMessage = $('#message').val();
+  var $userName = $('#name').val();
   //pushes the values into Posts array
   postsArray.push({
-    name: userName,
-    message: userMessage,
+    name: $userName,
+    message: $userMessage,
     comment: [],
-    order: order++
   });
-
-  // var userCommentText = event.target.find('#commenttext').val();
-  // var userCommentName = event.target.find('#commentname').val();
-  // postArray[2].push({
-  //   commenttext: userCommentText,
-  //   commentname: userCommentName,
-  //   commentnumber: commentNumber++
-  // });
   renderPost();
 };
-
-
-//when you press both the post and the post message button
-//it adds the input values to the postsArray
-//TODO!!! could call by id and be separate
+//when you hit the post button it adds the posts to the data structure
+// and renders the post which appends the html
 $('#submit').click(function() {
-  addToPostsArrays();
+  addToPostsArray();
 });
 
-$('#postcomment').click(function(event) {
+var addCommentsToPostsArray = function() {
+  var $userCommentName = $('#commentname').val();
+  var $userCommentText = $('#commentname').val();
+  //shouldn't be zero should somehow access each of the array indexes
+  postsArray[index].comment.push({
+    commenttext: $userCommentText,
+    commentname: $userCommentName,
+  });
+};
 
-  // var postIndex = commentnumber
-  //
-  // var addCommentsToArray = function() {
-  //   var userCommentText = event.target.find('#commenttext').val();
-  //   var userCommentName = event.target.find('#commentname').val();
-  //   postArray[2].push({
-  //     commenttext: userCommentText,
-  //     commentname: userCommentName,
-  //     commentnumber: commentnumber++
-  //   })
-  // };
-});
+var createPosts = function(post, postIndex) {
+  var $postTemplate = $('<div class="post"> ' +
+    '<a class="postInput" type ="link" id="removePost"> remove </a>' +
+    '<a class="postInput"" type="link" id="commentLink"> comment(s) </a>' +
+    '<p class="postInput" id="postMessage">' + post.message + '<p>' +
+    '<p class="postInput" id="postName">' + ' Posted By: ' + '<strong>' + post.name + '</strong></p>' +
+  '<hr>' + '<p class="hide postInput" id="postIndex">' + postIndex + '</p>' +
+    '</div>');
 
+    $postsElement.append($postTemplate);
+  };
+
+var createComments = function(comment, commentIndex) {
+    var $commentTemplate = $('<div class ="comments form-group" id="comments">' +
+    '<input id="commentText" class="commentInput form-control" type="Text" placeholder="Comment Text">' + comment.commenttext + '</input>' +
+    '<input id="commentName" class="commentInput form-control" type="Text" placeholder="User Name">' + comment.commentname + '</input>' +
+    '<button class="btn btn-primary commentInput" type="submit" id="postcomment"> Post Comment </button>' + '<p class="hide commentIndex">' +
+    commentIndex + '</p>' +
+    '</div>');
+  $postTemplate.append($commentTemplate);
+};
 //the render function does x
 var renderPost = function() {
   //empties posts when you render the page
-  $('.posts').empty();
-  //looping through the array to find stuff
+  $postsElement.empty();
+  //looping through outerarray to get the info from posts and append the new posts from the template
   for (var i = 0; i < postsArray.length; i++) {
-    //loops through the array to posts info
-    var commentsElement = addPostToPage(postsArray[i], i)
-    //loops through to find comment info
+    var $postElements = createPosts(postsArray[i], i + 1);
+    // $postsElement.append($postElements);
+    console.log("what is happening in the outer loop?" + postsArray[i]);
+    ///looping through inner array to get the info from posts and append the new posts from the template
     for (var j = 0; j < postsArray[i].comment.length; j++) {
-
-      addCommentToPage(postsArray[i].comment[j], commentsElement, j);
+      var $commentElements = createComments(postsArray[i].comment[j], j + 1);
+      // $postElements.append($commentElements);
     };
+
   };
 
-  console.log(postsArray);
 };
 
 
-$('.post').each(function () {
-      $(this).closest('a').find('a.view').append(this);
-  });
-
-//this function adds the posts to the webpage by creating new divs
-//and the nested comment posts
-var addPostToPage = function(post) {
-  //creates a new div in the posts div for posts
-  var $postDiv = $('<div class ="post">' + '</div>');
-  //adds a remove and comment button to the post
-  $postDiv.append('<a class="removepost" type ="link" id="removepost"> remove </a>');
-  //
-  var $commentsElement = $postDiv.append('<a class=comments type="link" id="comments"> comment(s) </a>');
-  //adds the userInput (message + name ) to the post
-  $postDiv.append('<p class="userinput" id="postmessage">' + post.message + '<p>' + '<p class="userinput" id="postname">' + ' Posted By: ' + '<strong>' + post.name + '</strong></p>' + '<hr>');
-  $('.posts').append($postDiv);
-  
-
-  $commentsElement.append('<input id="commenttext" class="commentinput form-control" type="Text" placeholder="Comment Text">' + '</input>');
-  $commentsElement.append('<input id="commentname" class="commentinput form-control" type="Text" placeholder="User Name">' + '</input>');
-  $commentsElement.append('<button class="btn btn-primary commentinput" type="submit" id="postcomment"> Post Comment</button>');
-  $('.post').append($commentsElement);
-  //take in the loop index
-
-
-};
-
-// var addCommentToPage = function(commentsInfo, $commentsElement, index) {
-//   // creates the two user inputs (text + name) and the post comment button
-//   $commentsElement.append('<input id="commenttext" class="commentinput form-control" type="Text" placeholder="Comment Text">' + '</input>');
-//   $commentsElement.append('<input id="commentname" class="commentinput form-control" type="Text" placeholder="User Name">' + '</input>');
-//   $commentsElement.append('<button class="btn btn-primary commentinput" type="submit" id="postcomment"> Post Comment</button>');
-//   $('.post').append($commentsElement);
-// };
-
-
-//when the remove button is clicked it removes the post
-$('.removepost').on('click', function() {
-  $(this).closest('.post').remove();
+$postsElement.on('click', '#removepost', function() {
+  $(this).closest($postElement).remove();
+  // renderPost();
 });
 
-// $('.commentinput').click(function() {
-//   $(this).closest('.hide').toggleClass('show');
-// });
+// +posts[Object.keys(posts)[i]].postComments[j][0]+“: “+posts[Object.keys(posts)[i]].postComments[j][1]
 
 
-// $('.post').on('click', '.commentlink', function() {
-//   $('#comments').css("display", "");
-// });
 //
-// //when you click on the link comments(s) it should display the hidden comments or .commentinfo
 //
-// $('div').show();
-// // $('.commentlink').click(function() {
-// //   $('.comments').css("display", "");
-// // });
 //
-// // $("#commentlink").click(function() {
-// //   $("#comments").toggle();
+// // var $postDiv = $('<div class ="post">' + '</div>');
+// // //adds a remove and comment button to the post
+// // $postDiv.append('<a class="removepost" type ="link" id="removepost"> remove </a>');
+// // //
+// // $postDiv.append('<a class="commentlink"" type="link" id="commentlink"> comment(s) </a>');
+// // //adds the userInput (message + name ) to the post
+// // $postDiv.append('<p class="userinput" id="postmessage">' + post.message + '<p>' + '<p class="userinput" id="postname">' + ' Posted By: ' + '<strong>' + post.name + '</strong></p>' + '<hr>');
+// // //
+// // $('.posts').append($postDiv);
+// // //
+// //
+// // $('.posts').on('click', '.removepost', function() {
+// //   $(this).closest('.post').remove();
+// //   // renderPost();
 // // });
 // //
-// //
-// // $(".commentlink").click(function() {
-// //   $(".comments").toggle("slow", function() {
-// //
-// //   });
+// // $('.posts').on('click', '.comments', function(event) {
+// //   $(this).find('.hide').toggleClass('show');
 // // });
-//
-// $('.commentlink').on('click', function() {
-//   $('.comments').toggleClass('show');
-// });
-//
-
-//postUserComment =
+// // // //this function adds the posts to the webpage by creating new divs
+// // // //and the nested comment posts
+// // var addPostToPage = function(post, index) {
+// //   //creates a new div in the posts div for posts
+// //
+// //   var $postDiv = $('<div class ="post">' + '</div>');
+// //   //adds a remove and comment button to the post
+// //   $postDiv.append('<a class="removepost" type ="link" id="removepost"> remove </a>');
+// //   //
+// //   $postDiv.append('<a class="commentlink"" type="link" id="commentlink"> comment(s) </a>');
+// //   //adds the userInput (message + name ) to the post
+// //   $postDiv.append('<p class="userinput" id="postmessage">' + post.message + '<p>' + '<p class="userinput" id="postname">' + ' Posted By: ' + '<strong>' + post.name + '</strong></p>' + '<hr>');
+// //   //
+// //   $('.posts').append($postDiv);
+// //
+// //   //make all these elements children of the commentsElement
+// //   var $commentsDiv = $('<div class ="comments form-group" id="comments">' + '</div>');
+// //   $commentsDiv.append('<input id="commenttext" class="commentinput form-control" type="Text" placeholder="Comment Text">' + '</input>');
+// //   $commentsDiv.append('<input id="commentname" class="commentinput form-control" type="Text" placeholder="User Name">' + '</input>');
+// //   $commentsDiv.append('<button class="btn btn-primary commentinput" type="submit" id="postcomment"> Post Comment</button>');
+// //
+// //   $($postDiv).append($commentsDiv);
+// //   //appends them as children to $commentsElement & grandchildren of .post
+// //   // //take in the loop index
+// //   // $('.commentinput').addClass("hide");
+// // };
+// //
+// // var addCommentToPage = function(commentsInfo, $commentsElement, index) {
+// //   var $commentDiv = $('<div class ="comments form-group" id="comments">' + '</div>');
+// //   $commentDiv.append('<input id="commenttext" class="commentinput form-control" type="Text" placeholder="Comment Text">' + '</input>');
+// //   $commentDiv.append('<input id="commentname" class="commentinput form-control" type="Text" placeholder="User Name">' + '</input>');
+// //   $commentDiv.append('<button class="btn btn-primary commentinput" type="submit" id="postcomment"> Post Comment</button>');
+// //   $('.commentlink').append($commentDiv);
+// //
+// //
+// // //when the remove button is clicked it removes the post
+// //
+// //
+// //
+// // // $(this).siblings(“.hide”).toggleClass(“show”)
+// // // $('.post').on('click', '.commentlink', function() {
+// // //   $('#comments').css("display", "");
+// // // });
+// // //
+// // // //when you click on the link comments(s) it should display the hidden comments or .commentinfo
+// // //
+// // // $('div').show();
+// // // // $('.commentlink').click(function() {
+// // // //   $('.commentinput').css("display", "");
+// // // // });
+// // //
+// // // // $("#commentlink").click(function() {
+// // // //   $("#comments").toggle();
+// // // // });
+// // // //
+// // // //
+// // // // $(".commentlink").click(function() {
+// // // //   $(".comments").toggle("slow", function() {
+// // // //
+// // // //   });
+// // // // });
+// // //
+// // // $('.commentlink').on('click', function() {
+// // //   $('.comments').toggleClass('show');
+// // // });
+// // //
+// //
+// // //postUserComment =
--- test.js
+++ test.js
@@ -1,103 +1,141 @@
-//this is an array of objects containing the posts information
-//empty array where we push in the objects
+//creates an empty data structure for posts + comments
 var postsArray = [];
-var order = 1;
-var commentnumber = 1;
 
-//add Posts to Array
-var addToPostsArrays = function() {
+
+//this function adds the posts to postsArray data structure
+var addToPostsArray = function() {
   //takes the values from the input field on the forum
-  var userMessage = $('#message').val();
-  var userName = $('#name').val();
-  // var userCommentText = $('#commenttext').val();
-  // var userCommentName = $('#commentname').val();
+  var $userMessage = $('#message').val();
+  var $userName = $('#name').val();
+
   //pushes the values into Posts array
   postsArray.push({
-    name: userName,
-    message: userMessage,
-    comment: [
+    name: $userName,
+    message: $userMessage,
+    comment: [],
 
-    ],
-    order: order++
   });
+
   renderPost();
 };
+
 
 //when you press both the post and the post message button
 //it adds the input values to the postsArray
 //TODO!!! could call by id and be separate
 $('#submit').click(function() {
-  addToPostsArrays();
+  addToPostsArray();
 });
 
-$('#postcomment').click(function() {
-  // addToPostsArrays();
-  console.log("where do i put this info for post comment?")
-});
+
+var addCommentsToPostsArray = function() {
+  var $userCommentName = $('#commentname').val();
+  var $userCommentText = $('#commentname').val();
+  //shouldn't be postsArray.length-1 should somehow access each of the array indexes
+  postsArray[postsArray.length-1].comment.push({
+    commenttext: $userCommentText,
+    commentname: $userCommentName,
+  });
+};
+
+// this click handler adds the comments input the data structure
+// and renders the post which appends the html
+$('.posts').on('click', '#postcomment', function() {
+   addCommentsToPostsArray();
+//   // renderPost();
+ });
+
 
 //the render function does x
 var renderPost = function() {
-  //
+  //empties posts when you render the page
   $('.posts').empty();
-  // $('.commentinput').empty();
-
   //looping through the array to find stuff
   for (var i = 0; i < postsArray.length; i++) {
-    var newPost = postsArray[i];
-      $('.posts').append(newPost);
+    //loops through the array to posts info
+    var commentsElement = addPostToPage(postsArray[i], i)
+    //loops through to find comment info
     for (var j = 0; j < postsArray[i].comment.length; j++) {
-      var newComment = postsArray[i].comment[j];
+
+      addCommentToPage(postsArray[i].comment[j], commentsElement, j);
     };
   };
+
+  console.log(postsArray);
 };
 
 
-var $post = $('<div id="parent">0</div>');
-$post.append('<div id="child1">1</div>');
-$post.append('<div id="child2">2</div>');
-$post.append('<div id="child3">3</div>');
-$('.posts').append($post);
+// $('.post').each(function () {
+//       $(this).closest('a').find('a.view').append(this);
+//   });
 
-//   var addPostToPage = function(post) {
-//     //creates a new div in the posts div for posts
-//     var postElement = $('.posts').append('<div class ="post">' + '</div>');
-//     //adds a remove and comment button to the post
-//     postElement.append('<a class="removepost" type ="link" id="removepost"> remove </a>');
-//     //
-//     var $commentsElement = $postElement.append('<a class="comments"  type="link" id="comments"> comment(s) </a>');
-//     //adds the userInput (message + name ) to the post
-//     postElement.append('<p class="userinput" id="postmessage">' + post.message + '<p>' + '<p class="userinput" id="postname">' + ' Posted By: ' + '<strong>' + post.name + '</strong></p>' + '<hr>');
-//     //
-//     return $commentsElement;
-//
-//     //take in the loop index
-//   };
-//
-// var addCommentToPage = function(commentsElement){
-//
-// $commentsElement.append('<input id="commenttext" class="commentinput form-control" type="Text" placeholder="Comment Text">' + '</input>');
-// $commentsElement.append('<input id="commentname" class="commentinput form-control" type="Text" placeholder="User Name">' + '</input>');
-// $commentsElement.append('<button class="btn btn-primary commentinput" type="submit" id="postcomment"> Post Comment </button>');
-//
+//this function adds the posts to the webpage by creating new divs
+//and the nested comment posts
+var addPostToPage = function(post) {
+  //creates a new div in the posts div for posts
+  var $postDiv = $('<div class ="post">' + '</div>');
+  //adds a remove and comment button to the post
+  $postDiv.append('<a class="removepost" type ="link" id="removepost"> remove </a>');
+  //
+  var $commentsElement = $postDiv.append('<a class=comments type="link" id="comments"> comment(s) </a>');
+  //adds the userInput (message + name ) to the post
+  $postDiv.append('<p class="userinput" id="postmessage">' + post.message + '<p>' + '<p class="userinput" id="postname">' + ' Posted By: ' + '<strong>' + post.name + '</strong></p>' + '<hr>');
+  $('.posts').append($postDiv);
+
+
+  $commentsElement.append('<input id="commenttext" class="commentinput form-control" type="Text" placeholder="Comment Text">' + '</input>');
+  $commentsElement.append('<input id="commentname" class="commentinput form-control" type="Text" placeholder="User Name">' + '</input>');
+  $commentsElement.append('<button class="btn btn-primary commentinput" type="submit" id="postcomment"> Post Comment</button>');
+  $('.post').append($commentsElement);
+  //take in the loop index
+
+
+};
+
+// var addCommentToPage = function(commentsInfo, $commentsElement, index) {
+//   // creates the two user inputs (text + name) and the post comment button
+//   $commentsElement.append('<input id="commenttext" class="commentinput form-control" type="Text" placeholder="Comment Text">' + '</input>');
+//   $commentsElement.append('<input id="commentname" class="commentinput form-control" type="Text" placeholder="User Name">' + '</input>');
+//   $commentsElement.append('<button class="btn btn-primary commentinput" type="submit" id="postcomment"> Post Comment</button>');
+//   $('.post').append($commentsElement);
 // };
 
 
-
 //when the remove button is clicked it removes the post
-$('.posts').on('click', '.removepost', function() {
+$('.removepost').on('click', function() {
   $(this).closest('.post').remove();
 });
 
-//when you click on the link comments(s) it should display the hidden comments or .commentinfo
+// $('.commentinput').click(function() {
+//   $(this).closest('.hide').toggleClass('show');
+// });
 
-// $('.post').on('click', '.commentlink', function () {
-//   $(this).closest('.comments').toggleClass('show');
+
+// $('.post').on('click', '.commentlink', function() {
+//   $('#comments').css("display", "");
 // });
 //
-// $( ".commentlink" ).click(function() {
-//   $( ".comments" ).toggle( "slow", function() {
-//     // Animation complete.
-//   });
+// //when you click on the link comments(s) it should display the hidden comments or .commentinfo
+//
+// $('div').show();
+// // $('.commentlink').click(function() {
+// //   $('.comments').css("display", "");
+// // });
+//
+// // $("#commentlink").click(function() {
+// //   $("#comments").toggle();
+// // });
+// //
+// //
+// // $(".commentlink").click(function() {
+// //   $(".comments").toggle("slow", function() {
+// //
+// //   });
+// // });
+//
+// $('.commentlink').on('click', function() {
+//   $('.comments').toggleClass('show');
 // });
 //
-// $( "#commentlink" ).toggle('display');
+
+//postUserComment =
