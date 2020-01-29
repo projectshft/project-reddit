0c9cd514060cd9d1ea013bdaa7691d7e5ba12fb2
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-01-28 20:04:24 -0500

    final.js with comments appearing on the page

--- final.js
+++ final.js
@@ -1,13 +1,16 @@
 //creates an empty data structure for posts + comments
 var postsArray = [];
 
+
 var $postDiv = $('<div class ="post container">' + '</div>');
 var $commentDiv = $('<div class ="comments container">' + '</div>');
 var $posts = $('.posts');
 
 //this function adds the posts to postsArray data structure
 var addToPostsArray = function() {
+  var postIndex = $(this).closest($postDiv).index();
   //takes the values from the input field from the posts
+  console.log(postIndex);
   var $userMessage = $('#message').val();
   var $userName = $('#name').val();
   //pushes the values into postsArray
@@ -16,6 +19,9 @@ var addToPostsArray = function() {
     message: $userMessage,
     comments: [],
   });
+  //
+  return postIndex;
+
 };
 
 //when you click on the Post button it submits the information to the datastructure
@@ -32,16 +38,16 @@ $('#submit').click(function() {
   }
 });
 
-var addCommentsToPostsArray = function() {
-  //
+var addCommentsToPostsArray = function(postIndex) {
+  var commentIndex = $(this).closest($commentDiv).index();
   var $userCommentName = $('#commentname').val();
   var $userCommentText = $('#commentname').val();
   //shouldn't be postsArray.length-1 should somehow access each of the array indexes
-  postsArray[i].comments.push({
+  postsArray[postIndex].comments[commentIndex].push({
     commenttext: $userCommentText,
     commentname: $userCommentName,
   });
-
+  return commentIndex
 };
 
 // this click handler adds the comments input the data structure
@@ -49,7 +55,7 @@ var addCommentsToPostsArray = function() {
 $posts.on('click', '#postcomment', function() {
   if ($('#commentname').val().length !== 0 && $('#commentname').val().length !== 0) {
     addToPostsArray();
-    renderPost();
+    renderComments();
   } else {
     //edge case for comments
     alert("you can't submit an empty comment! add your name or comment and then resubmit!")
@@ -62,8 +68,7 @@ $posts.on('click', '#postcomment', function() {
 var renderPost = function() {
   //empties posts when you render the page
   $posts.empty();
-  //
-  $commentDiv.empty();
+
   //looping through the array to find stuff
   $posts.append($postDiv);
   //
@@ -71,45 +76,52 @@ var renderPost = function() {
     //loops through the array to posts info
     addPostToPage(postsArray[i], i);
     //loops through to find comment info
-    for (var j = 0; j < postsArray[i].comments.length; j++) {
-      //can't get my inner loop to extract data
-      addCommentsToPage(postsArray[i].comments[j], j);
-    }
-  };
+  }
 };
+
+var renderComments = function(postIndex, commentIndex) {
+  //
+  $commentDiv.empty();
+  //can't get my inner loop to extract data
+  if (postsArray[postIndex].comments === undefined) {
+    postsArray[postIndex].comments = 0;
+    for (var j = 0; j < postsArray[postIndex].comments[commentIndex].length; j++) {
+      addPostToPage(postsArray[postIndex].comments[commentIndex], j);
+    }
+  }
 };
 
 //this function adds the posts to the webpage by creating new divs
 //and the nested comment posts
-var addPostToPage = function(post, index) {
+var addPostToPage = function(post, postIndex, comment, commentIndex) {
+  //
+  $posts.append($postDiv);
   //adds a remove and comment button to the post
   $postDiv.append('<a class="removepost" type ="link" id="removepost"> remove </a>');
-  console.log($postDiv);
-  $postDiv.append('<a class=comments type="link" id="comments"> comment(s) </a>');
+
+  $postDiv.append('<a class=comments type="link" id="commentslink"> comment(s) </a>');
   //adds the userInput (message + name ) to the post + creates a data attribute to index the posts
   $postDiv.append('<p class="userinput" id="postmessage">' + post.message + '<p>' +
     '<p class="userinput" id="postname">' + ' Posted By: ' + '<strong>' + post.name +
-    '</strong></p>' + '<hr>' + '<p class="hide userinput" id="postIndex" data-post-index-number>' +
-    index + '</p>');
-  $postDiv.append($commentDiv);
-  console.log($postDiv);
-};
-
-var addCommentsToPage = function(comment, index) {
+    '</strong></p>' + '<hr>' + '</p>');
+  // '<p class="hide userinput" id="postIndex" data-post-index-number>' +
+  // // postIndex +
   //
-  $commentDiv.append('<input id="commenttext" class="commentsinput" type="Text" placeholder="Comment Text">' + comment.commenttext +
+  $postDiv.append($commentDiv);
+
+  //
+  $commentDiv.append('<button class="btn btn commentsinput" type="submit" id="deletecomment"> X </button>');
+  $commentDiv.append('<input id="commenttext" class="commentsinput" type="Text" placeholder="Comment Text">' +
     '</input>');
 
-  $commentDiv.append('<input id="commentname" class= "commentsinput" type="Text" placeholder="User Name">' + comment.commentname +
+  $commentDiv.append('<input id="commentname" class= "commentsinput" type="Text" placeholder="User Name">' +
     '</input>');
   //
   $commentDiv.append('<button class="btn btn-primary commentsinput" type="submit" id="postcomment"> Post Comment</button>');
-  $commentDiv.append('<button class="btn btn commentsinput" type="submit" id="deletecomment"> X </button>');
-  //create a data attribute to index the comments
-  $commentDiv.append('<p class="hide commentsinput" id ="commentIndex" data-comment-index-number>' +
-    index + '</p>');
 
-  ;
+  //create a data attribute to index the comments
+  // $commentDiv.append('<p class="hide commentsinput" id ="commentIndex" data-comment-index-number>' +
+  //   commentIndex + '</p>');
 };
 
 
@@ -119,12 +131,13 @@ $('#removepost').on('click', function() {
   $(this).closest('.post').remove();
 });
 
-//adds a hidden class to the comment
-$commentDiv.addClass('hide');
-$commentDiv.on('click', function() {
-  //if the comments were showing up, i would hide them and then toggle to show them
-  $(this).closest('.hide').toggleClass('show');
-});
+// //adds a hidden class to the comment
+// $commentDiv.addClass('hide');
+// $('#commentslink').on('click', function() {
+//   console.log("what is the comments div doing?")
+//   //if the comments were showing up, i would hide them and then toggle to show them
+//   $(this).closest('.hide').toggleClass('show');
+// });
 
 //deletes the comments when you hit x
 $('#deletecomment').on('click', function() {
