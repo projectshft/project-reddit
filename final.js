33fa911f367f8874679d17a95c5e23fba49691a8
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-02-02 19:54:50 -0500

    with comments posting to both data structure and page but not deleting

--- final.js
+++ final.js
@@ -1,9 +1,11 @@
   //creates an empty data structure for posts + comments
   var postsArray = [];
-//
+  //
   var $posts = $('.posts');
+
   //this function adds the posts to postsArray data structure
   var addToPostsArray = function() {
+
     //takes the values from the input field from the posts
     var $userMessage = $('#message').val();
     var $userName = $('#name').val();
@@ -13,7 +15,7 @@
       message: $userMessage,
       comments: [],
     });
-    //
+
   };
 
   //when you click on the Post button it submits the information to the datastructure
@@ -32,9 +34,9 @@
   });
 
   var addCommentsToPostsArray = function(postIndex) {
-    //
-    var $userCommentName = $('#commentname').val();
-    var $userCommentText = $('#commentname').val();
+    //when you add a comment you are adding to a post
+    var $userCommentName = $(this).closest('.commentlist').find('.commentname').val();
+    var $userCommentText = $(this).closest('.commentlist').find('.commenttext').val();
     //
     postsArray[postIndex].comments.push({
       commenttext: $userCommentText,
@@ -46,25 +48,23 @@
 
   // this click handler adds the comments input the data structure
   // and renders the post which appends the html
-  $posts.on('click','.postcomment', function() {
+  $posts.on('click', '.postcomment', function() {
+    var postIndex = $(this).closest('.post').index();
+    //
+    var postElement = $(this).closest('.post');
+    var postObject = postsArray[postIndex];
     //edge cases tests to see what will happen
-    console.log('this button works');
-    if ($('#commentname').val().length !== 0 && $('#commenttext').val().length !== 0) {
-      //
-      var postIndex = $(this).closest('.post').index();
-      //
-      var commentIndex = addCommentsToPostsArray(postIndex);
-      var postElement = $(this).closest('.post');
-      var postObject= postsArray[postIndex];
+    debugger;
+    if ($(this).siblings('.commentname').val().length !== 0 && $(this).siblings('.commenttext').val().length !== 0) {
 
       renderComments(postElement, postObject);
     } else {
       //edge case for comments
       alert("you can't submit an empty comment! add your name or comment and then resubmit!")
     }
-    return postIndex;
     return postElement;
-    return postObject;
+    postObject;
+    postIndex;
   });
 
   //the render function should be looping through and appending but I couldn't figure out
@@ -83,12 +83,13 @@
   // postDataModel -- one post object in the array
   var renderComments = function(postElement, postObject) {
     //TODO !! change it to just empty the comment relative to the element
-    $('.comments').empty();
-    //can't get my inner loop to extract data
-    
+
+    //need to empty a comment list which is separate from the commentslink
+    //render comment function isn't working based on the data structure
+    $('.commentslist').empty();
     //loops through to find comment info
     for (var position = 0; position < postObject.comments.length; position++) {
-      //
+
       addCommentsToPage(postObject.comments[position]);
     }
     addCommentInputToPage();
@@ -103,35 +104,35 @@
       '<p class="userinput" id="postmessage">' + postObject.message + '<p>' +
       '<p class="userinput" id="postname">' + ' Posted By: ' + '<strong>' + postObject.name +
       '</strong></p>' + '<hr>' + '</p>' + '</div>');
-    };
-var addCommentInputToPage = function() {
-    $('.post').append('<div class ="comments hide">' + '<input id="commenttext" class="commenttext" type="Text" placeholder="Comment Text">' +
-      '</input>' + '<input id="commentname" class= "commentname" type="Text" placeholder="User Name">' +
+  };
+
+  var addCommentInputToPage = function() {
+    $('.post').append('<div class ="comments hide">' + '<div class=commentslist>' + '</div>' + '<input class="commenttext" type="Text" placeholder="Comment Text">' +
+      '</input>' + '<input class= "commentname" type="Text" placeholder="User Name">' +
       '</input>' + '<button class="btn btn-primary postcomment" type="submit" > Post Comment </button>' + '</div>');
   };
 
   var addCommentsToPage = function(postElement) {
-    //debugger;
-    $posts.find('.comments').append('<div class="comment">' + postElement.commenttext + ' Posted By: <strong> ' + postElement.commentname +
-     + '</strong>' + '<button type=“button” class=“close” aria-label=“Close”>'+'<span aria-hidden=“true”>&times;</span>'+'</button>'+'</div>');
-};
-//this removes the post
-$('.posts').on('click', '.removes', function() {
-    $(this).closest('.post').remove();
-    //postsArray (slice)
-    //removes from the data datastructure!
-    //postsArray.slice(postIndex);
 
+    $posts.find('.commentslist').append('<div class="comment">' + postElement.commenttext + ' Posted By: <strong> ' + postElement.commentname + '</strong>' + '<a class="remove-comment">' + '<i class="fa fa-times" aria-hidden="true">' + '</i>' + '</a>' + '</div>');
+  };
+  //this removes the post
+  $('.posts').on('click', '.removes', function() {
+    var index = $(this).closest('.post');
+    //removes the post from the posts data structure
+    postsArray.splice(index, 1);
+    //removes the post from the DOM
+    $(this).closest('.post').remove();
   });
 
-//when you click on the comments link it shows the comments
+  //when you click on the comments link it shows the comments
   $('.posts').on('click', '.commentslink', function() {
     $(this).siblings('.hide').toggleClass("show");
   });
 
   //deletes the comments when you hit x
-  $('.posts').on('click', '.close', function() {
+  $('.commentlist').on('click', '.remove-comment', function() {
     console.log('this delete button works!');
-    $(this).find('.comment').remove();
+    $(this).find('.commentlist').remove();
     //this also deletes the commment from the datastructure
   });
