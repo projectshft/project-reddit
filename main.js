e0d662503ae39b39ecc543bab77f45fc4bde2934
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-05-08 18:49:32 -0400

    Adding functionality to comment link to show/hide comment section

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-
-//Creating post when post button is clicked
+//Creating post element when post button is clicked
 $('#post-button').click(function() {
-  //Create variable for user name input
-  var originalPosterName = $('#name').val();
 
-  //Create variable for message input
-  var originalPosterMessage = $('#message').val();
+  var postName = $('#name').val();
 
-  //Create variable for remove button
-  var removeButton = '<a id="remove-button" role="button" href="#">remove </a>'
+  var postMessage = $('#message').val();
 
-  //Create variable for comments button
-  var commentButton = '<a id="comment-button" role="button" href="#">comments </a>'
+  var removeLink = '<a class="remove-link" role="button" href="#">remove </a>';
+
+  var commentLink = '<a class="comment-link" role="button" href="#">comments </a>';
+
+  var commentForm = '<form id="comment-form">' +
+    '<h3>Add a New Comment</h3>' +
+    '<div class="form-group">' +
+    '<textarea type="text" class="form-control comment-message" placeholder="Comment Text" required></textarea>' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<input type="text" class="form-control comment-name" placeholder="Your Name" required></input>' +
+    '</div>' +
+    '<button type="button" class="btn btn-primary comment-button">Post</button>' +
+    '</form>'
+
+  //Create comment section
+  var commentSection = '<section class="comments hide">' + commentForm + '</section>'
 
   //Create element to hold post html
-  var originalPost = '<article><p class="post">' + removeButton + commentButton +
-    originalPosterMessage + '</p><p class="username">Posted By: <strong>' +
-    originalPosterName + '</strong></p></article>'
+  var post = '<article  class="post"><p>' + removeLink + commentLink +
+    postMessage + '</p>' + commentSection + '<p class="username">Posted By: <strong>' +
+    postName + '</strong></p></article><hr>';
 
   //Add post to posts section
-  $('#posts').append(originalPost);
+  $('#posts').append(post);
+
+  //Make comment section and form appear and disappear when comment button is clicked
+  $('.comment-link').click(function() {
+    console.log("I was clicked")
+    var comments = $(this).parent().siblings('.comments');
+    console.log(comments);
+    $(comments).toggleClass("hide");
+  });
+
 });
