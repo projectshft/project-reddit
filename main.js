5c9dc2fcb343631b49828a28288ac571892913bb
Pierre Lourens <plourens3@gmail.com>
2020-05-08 10:48:07 -0400

    Grabbing values of author & name from form

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Register a click event on the post button
 $("#submit").click(function () {
   // Grab the value of the message upon click
-  console.log("hi");
+  let $message = $("#message").val();
+
   // Grab the value of the author box upon click
+  let $name = $("#name").val();
 });
