3f93bea6f638d046bd7aee8df88630dd80867fe5
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-05-08 15:53:37 -0400

    comment inputs appear on comment button click

--- main.js
+++ main.js
@@ -3,14 +3,19 @@ $('#post-button').click(function() {
   var $postText = $('#post').val();
   var $postName = $('#name').val();
   $('#post-container').append(
-    '<div> <p> <a href="#" class="delete">delete</a> <a href="#" class="comment">comment </a>' + $postText + '</p> <p>Posted By: <b>' + $postName + '</b> </p> <hr> </div>'
+    '<div> <p> <a href="#" class="delete">delete</a> <a href="#" class="comment">comment </a>' + $postText + '</p> <div class="comment-section"> </div> <p>Posted By: <b>' + $postName + '</b> </p> <hr> </div>'
   )
 
+  // Put delete and comment click functions inside post button click function for scope
   $('.delete').click(function() {
     $(this).closest('div').empty();
   })
 
   $('.comment').click(function() {
-    alert($(this).val())
+    alert('it\'s been clicked');
+    $(this).closest('p').closest('div').find('.comment-section').append(
+      '<form class="form-inline"> <div class="form-group"> <input type="comment" class="form-control" id="comment" placeholder="Comment Text"> </div> <div class="form-group"> <input type="user-name" class="form-control" id="user-name" placeholder="User Name"> </div> <button type="button" class="btn btn-primary">Post Comment</button> </form>'
+    )
+
   })
 });
