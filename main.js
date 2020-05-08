37d2b9afe6aa412e00d123386f92b198bd6fd819
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-05-08 16:54:59 -0400

    comments get added when post comment is clicked

--- main.js
+++ main.js
@@ -12,13 +12,17 @@ $('#post-button').click(function() {
   })
 
   $('.comment').click(function() {
-    alert('it\'s been clicked');
-    $(this).closest('p').closest('div').find('.comment-section').append(
-      '<form class="form-inline"> <div class="form-group"> <input type="comment" class="form-control" id="comment" placeholder="Comment Text"> </div> <div class="form-group"> <input type="user-name" class="form-control" id="user-name" placeholder="User Name"> </div> <button type="button" class="btn btn-primary post-comment-btn">Post Comment</button> </form>'
+    $(this).closest('p').closest('div').find('.comment-section').toggle(
+      '<form class="form-inline"> <div class="form-group comment-group"> <input type="comment" class="form-control" id="comment" placeholder="Comment Text"> </div> <div class="form-group user-group"> <input type="user-name" class="form-control" id="user-name" placeholder="User Name"> </div> <button type="button" class="btn btn-primary post-comment-btn">Post Comment</button> </form>'
     )
-
+    //put post-comment click function inside comment click function for scope
     $('.post-comment-btn').click(function() {
-      alert('button clicked. dog is barking outside')
+      var $commentText = $(this).closest('form').find('.comment-group').find('input').val()
+      var $commentName = $(this).closest('form').find('.user-group').find('input').val()
+      $(this).closest('div').prepend(
+        '<p>' + $commentText + ' By: <b>' + $commentName + '</b> </p>'
+      )
+
     })
 
   })
