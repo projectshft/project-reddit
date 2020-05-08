2fdbc5b0d4f50ba326528e52658f49861db59b70
Pierre Lourens <plourens3@gmail.com>
2020-05-08 10:45:05 -0400

    Links JS file and registers click event on submit button

---
+++ main.js
@@ -0,0 +1,6 @@
+// Register a click event on the post button
+$("#submit").click(function () {
+  // Grab the value of the message upon click
+  console.log("hi");
+  // Grab the value of the author box upon click
+});
