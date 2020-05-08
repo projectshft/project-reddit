5e6fed29f451fc01360e7adf5e084dbb7ed92967
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-05-08 15:56:12 -0400

    post comment button engaged

--- main.js
+++ main.js
@@ -14,8 +14,12 @@ $('#post-button').click(function() {
   $('.comment').click(function() {
     alert('it\'s been clicked');
     $(this).closest('p').closest('div').find('.comment-section').append(
-      '<form class="form-inline"> <div class="form-group"> <input type="comment" class="form-control" id="comment" placeholder="Comment Text"> </div> <div class="form-group"> <input type="user-name" class="form-control" id="user-name" placeholder="User Name"> </div> <button type="button" class="btn btn-primary">Post Comment</button> </form>'
+      '<form class="form-inline"> <div class="form-group"> <input type="comment" class="form-control" id="comment" placeholder="Comment Text"> </div> <div class="form-group"> <input type="user-name" class="form-control" id="user-name" placeholder="User Name"> </div> <button type="button" class="btn btn-primary post-comment-btn">Post Comment</button> </form>'
     )
+
+    $('.post-comment-btn').click(function() {
+      alert('button clicked. dog is barking outside')
+    })
 
   })
 });
