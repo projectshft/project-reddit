f1a545abaa7250588ffa2def8fdef4f609624caf
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-05-08 14:01:21 -0400

    delete button works

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ $('#post-button').click(function() {
   var $postText = $('#post').val();
   var $postName = $('#name').val();
   $('#post-container').append(
-    '<div> <p> <a href="#" id="delete">delete</a> <a href="#" id="comment">comment </a>' + $postText + '</p> <p>Posted By: <b>' + $postName + '</b> </p> </div> <hr>'
+    '<div> <p> <a href="#" class="delete">delete</a> <a href="#" class="comment">comment </a>' + $postText + '</p> <p>Posted By: <b>' + $postName + '</b> </p> <hr> </div>'
   )
 
-
+  $('.delete').click(function() {
+    $(this).closest('div').empty();
+  })
 });
