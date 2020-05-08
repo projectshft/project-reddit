0d37fdf8cd075a39eced2956950cd76c114c29ef
MCola <mcolapietro502@gmail.com>
2020-05-08 19:35:48 -0400

    correct id number and class name

--- main.js
+++ main.js
@@ -4,13 +4,12 @@ var userPostNumber = 0;
 $postButton.click(function() {
   var $userName = $('#name').val();
   var $userMessage = $('#message').val()
-  //add a user count to make a unique ID number
+  //add a user count to make a unique post number for the class
   userPostNumber ++;
-
 
   //create a div for each post that will contain everything inside.
   $('.posts').append('<div class="userPost"></div>');
-
-  $('.userPost').last().append('<div class="individualPosts" id="' + userPostNumber +'">' + $userMessage + '</div>');
-  $('.userPost').last().append('<div class="individualPosts" id="' + userPostNumber +'"> Posted By: <strong>' + $userName + '</strong></div>');
+  //append message and name to newly created userPost div
+  $('.userPost').last().append('<div class="individualPostMessage" id="message' + userPostNumber +'">' + $userMessage + '</div>');
+  $('.userPost').last().append('<div class="individualPostName" id="name' + userPostNumber +'"> Posted By: <strong>' + $userName + '</strong></div>');
 });
