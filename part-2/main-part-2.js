56d291b2285aebf66e51dd77322504b5f8a4075a
Joe Krall <kralljr@outlook.com>
2020-05-09 17:38:09 -0400

    Initial commit, part 2

---
+++ part-2/main-part-2.js
@@ -0,0 +1,14 @@
+var button = $('button');
+
+$('button').on('click', function () {
+
+  var userMessage = "<li>" + $('#message').val() + "</li>";
+  var userName = "<li>" + "<strong>Posted by:</strong> " + $('#name').val() + "</li>";
+  var post = '<li><ul>' + userMessage + userName + '<hr>' + '<ul><li>';
+
+  // $('.post-list').append(userMessage);
+  // $('.post-list').append(userName);
+  // $('.post-list').append('<hr>');
+
+  $('.post-list').prepend(post);
+});
