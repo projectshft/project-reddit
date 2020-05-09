07c2f7d873a0855a50cf195db8225907b996511e
ADH <61984231+adholton@users.noreply.github.com>
2020-05-09 13:21:36 -0400

    finished all functionality, need css

--- main.js
+++ main.js
@@ -14,17 +14,11 @@ $('#submit').click(function() {
 
   //the post will contain the author name and post content, and 2 links to either remove or add comments
   //consider creating a comment section that will be part of the post template but hidden from view until the comments button is clicked
-  const postTemplate = '<div class="post-container"><a class="comments-link"><span>Comments   </span></a><a class="remove-link"><span>Remove</span></a><p class="post-content">' + postContent + '</p><form class="form-class" style="margin-top:10px;" onsubmit="event.preventDefault();"><input id="" type="text" class="form-control short-input comment-content" placeholder="Comment Text"></input><input id="" type="text" class="form-control short-input comment-author" placeholder="User Name"><button class="btn btn-primary comment-button">Post Comment</button></form><p class="post-author">Posted by: <strong>' + postAuthor + '</strong></p></div>';  
+  const postTemplate = '<div class="post-container"><a class="comments-link"><span>Comments   </span></a><a class="remove-link"><span>Remove</span></a><p class="post-content">' + postContent + '</p><section class="comments-section"><div class="comments-div"></div><form class="form-class" style="margin-top:10px;" onsubmit="event.preventDefault();"><input id="" type="text" class="form-control short-input comment-content" placeholder="Comment Text"></input><input id="" type="text" class="form-control short-input comment-author" placeholder="User Name"><button class="btn btn-primary comment-button">Post Comment</button></form></section><p class="post-author">Posted by: <strong>' + postAuthor + '</strong></p></div>';  
 
-
+  //create a variable that will point to the post template created for each post click. (we need this to specify which post we are manipulating and adding click events to)
   const $postTemplate = $(postTemplate);
   
-  // const commentTemplate = '<form style="margin-top:30px;" onsubmit="event.preventDefault();"><input id="" type="text" class="form-control" placeholder="Comment Text"></input><input id="" type="text" class="form-control" placeholder="User Name"><button class="btn btn-primary">Post Comment</button></form>';
-
-  //$('.post-content').append($(commentTemplate));
-  //$(commentTemplate).insertAfter('.post-content');
-  
- 
  
    //if the remove link is clicked, the post will be removed from the page 
   $postTemplate.find('.remove-link').click(function() {
@@ -33,37 +27,49 @@ $('#submit').click(function() {
 
   //if the comments link is clicked, a name and text field and comment button will be shown and if the comments are showing they will be hidden
   $postTemplate.find('.comments-link').click(function() {
-    if ($(this).nextAll('.form-class').is(':hidden')) {
-      $(this).nextAll('.form-class').show();
+    if ($(this).nextAll('.comments-section').is(':hidden')) {
+      $(this).nextAll('.comments-section').show();
     } else {
-      $(this).nextAll('.form-class').hide();
+      $(this).nextAll('.comments-section').hide();
     }
   })
 
-   //the post will be posted under the heading, child of the .post-div and above the form, each new post will be added below
+  //the post will be posted under the heading, child of the .post-div and above the form, each new post will be added below
   $('.posts').append($postTemplate);
 
-   //if the comment button is clicked the comments/commenter will appear under the post with an "x", top to bottom. 
+
+  //if the comment button is clicked the comments/commenter will appear under the post with an "x", comments listed top to bottom. 
   $postTemplate.find('.comment-button').click(function() {
-    //using $(this) might cause an issue
+    
     const commentContent = $(this).siblings('.comment-content').val();
     const commentAuthor = $(this).siblings('.comment-author').val();
-    //use span?
-    const commentTemplate = '<div class="comment-div"><span class="comment-span">' + commentContent + '</span><span>  Posted by: <strong>' + commentAuthor + '</strong></span><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></div>';
+    
+    const commentTemplate = '<div><span class="comment-span">' + commentContent + '</span><span>  Posted by: <strong>' + commentAuthor + '</strong></span><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></div>';
+
+    //add the comment template to the comments div
     const $commentTemplate = $(commentTemplate);
-    $postTemplate.find('.post-content').append($commentTemplate);
+    $postTemplate.find('.comments-div').append($commentTemplate);
 
     //if the "x" is clicked, the comment will be removed
     $commentTemplate.find('.glyphicon-remove').click(function() {
-      $(this).closest('.comment-div').remove();
+      $(this).parent().remove();
     })
 
   })
   
+})
 
 
+  //use css and/or bootstrap to..
+  // style the page (border btw posts),
+  
+  // change input field heights/ widths and margins
+  
+  // make glyph icon blue  
 
-  //use css and/or bootstrap to style the page (border btw posts), input field widths, icons, etc. 
+  //clear all the text input fields after clicking post or comment
+
+
 
 
     /* postTemplate = 
@@ -105,7 +111,7 @@ $('#submit').click(function() {
   // })
 
  
-})
+
 
 
 
