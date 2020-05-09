f4c49f940dc6f27f9d1954f83822d7f576da1b5d
MCola <mcolapietro502@gmail.com>
2020-05-09 13:49:49 -0400

    added feature to reply to a posts comment and cleared input values after posting

--- main.js
+++ main.js
@@ -7,32 +7,31 @@ $postButton.click(function() {
     return alert('Please enter a name & post text');
   };
   
+  //retrieving values of the form.
   var $userName = $('#name').val();
   var $userMessage = $('#message').val();
   //add a user count to make a unique post number for the class
   userPostNumber ++;
 
-  //creating a parent div element for each user post that will contain the form details inside.
-  $('.posts').append('<div class="userPost"></div><hr />');
+  //creating a parent div element for each user post that will contain the user's form details inside.
+  $('.posts').append('<div class="userPost"></div>');
   
   //append message to newly created userPost div
   $('.userPost').last().append('<div class="individualPostMessage" id="message' + userPostNumber +'">' + $userMessage + '</div>');
   
   //add reply comment form that is hidden by default to newly created userPost div
   //first setting up variables to make jQuery more readable for adding reply comment form
-  var replyForm = '<div class="replyComment hidden"><form class="replyCommentForm form-inline" onsubmit="event.preventDefault();">';
-  var replyComment = '<input type="text" class="form-control mb-2 mr-sm-2" placeholder="Comment Text">'
-  var replyUserName = '<input type="text" class="form-control"  placeholder="Username">'
-  var postReplyCommentButton = '<button id="post" class="btn btn-primary">Post Comment</button>'
+  var $replyForm = '<div class="replyComment hidden"><form class="replyCommentForm form-inline" onsubmit="event.preventDefault();">';
+  var $replyComment = '<input type="text" class="replyCommentDetails form-control mb-2 mr-sm-2" placeholder="Comment Text">'
+  var $replyUserName = '<input type="text" class="replyCommentUserName form-control"  placeholder="Username">'
+  var $postReplyCommentButton = '<button class="postReply btn btn-primary">Post Comment</button>'
 
-
-  $('.userPost').last().append(replyForm + replyComment + replyUserName + postReplyCommentButton + '</form></div>');
+  $('.userPost').last().append($replyForm + $replyComment + $replyUserName + $postReplyCommentButton + '</form></div>');
   
   //append name to newly created userPost div
   $('.userPost').last().append('<div class="individualPostName" id="name' + userPostNumber +'"> Posted By: <strong>' + $userName + '</strong></div>');
   
-
-  //adding "remove" and "comments" to the user's post message
+  //adding the words "remove" and "comments" to the user's post message from the form
   var $newMessage = $('.individualPostMessage').last();
   
   $('<span class="addComments">comment </span>').prependTo($newMessage);
@@ -45,10 +44,10 @@ $postButton.click(function() {
   });
 
   //adding css text-decoration to "comments" and "remove" on hover
-  $('.removeOption, .addComments').on('mouseenter', function(event) {
+  $('.removeOption, .addComments').on('mouseenter', function (event) {
     $(event.currentTarget).css('text-decoration', 'underline');
     $(event.currentTarget).css('text-decoration', 'underline');
-  }).on('mouseleave', function(event) {
+  }).on('mouseleave', function (event) {
     $(event.currentTarget).css('text-decoration', 'none');
   });
 
@@ -65,10 +64,20 @@ $postButton.click(function() {
     var $parentElement = $(event.currentTarget).closest('.individualPostMessage')
     
     $($parentElement).siblings('.replyComment').toggleClass('hidden');
-    //console.log($(event.currentTarget).parent('.replyCommentForm').toggle())
   })
 
- 
+  //setting up functionality for users to reply to post with in the original post's comment form.
+  $('.postReply').click(function(event) {
+    var $replyInput = $('.replyCommentDetails').val();
+    var $replyUserName = $('.replyCommentUserName').val();
+
+    //adding user's reply comments to the post that the user filled the form in
+    $(event.currentTarget).closest('.replyComment').prepend(`<p>${$replyInput} Posted By: <strong>${$replyUserName}</strong></p>`);
+
+    //clearing input values
+    $('input').val('');
+
+  })
 
   //clearing form fields for next user input
   $('input').val('');
