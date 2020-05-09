3a66dbcdd15e9c9312e44ce4f9a4cfc05600c19d
MCola <mcolapietro502@gmail.com>
2020-05-08 21:18:36 -0400

    added css formatting to remove and comments words

--- main.js
+++ main.js
@@ -7,17 +7,19 @@ $postButton.click(function() {
   //add a user count to make a unique post number for the class
   userPostNumber ++;
 
-  //create a div for each post that will contain everything inside.
+  //creating a div element for each post that will contain the form details inside.
   $('.posts').append('<div class="userPost"></div>');
   //append message and name to newly created userPost div
   $('.userPost').last().append('<div class="individualPostMessage" id="message' + userPostNumber +'">' + $userMessage + '</div>');
   $('.userPost').last().append('<div class="individualPostName" id="name' + userPostNumber +'"> Posted By: <strong>' + $userName + '</strong></div>');
 
-  //adding "remove" and "comments" to post message
+  //adding "remove" and "comments" to the user's post message
   var $newMessage = $('.individualPostMessage').last();
   
-  $('<span>comments </span>').prependTo($newMessage);
+  $('<span class="addComments">comments </span>').prependTo($newMessage);
   $('<span class="removeOption">remove </span>').prependTo($newMessage);
   
+  //adding css to "comments" and "remove" class
+  $('.removeOption, .addComments').css('color', 'blue');
 
 });
