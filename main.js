4ef4084d1ed0d3b12d59f5f4bbd13cf9843581b4
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-05-09 17:03:48 -0400

    Adding functionality to post comment button

--- main.js
+++ main.js
@@ -1,90 +1,92 @@
 //Creating post element when post button is clicked
 $('#post-button').click(function() {
 
-      var postName = $('#name').val();
+  var postName = $('#name').val();
 
-      var postMessage = $('#message').val();
+  var postMessage = $('#message').val();
 
-      //using jquery object to hold data for remove link in order to bind click event to it
-      var $removeLink = $('<a/>', {
-        type: 'button',
-        href: '#',
-        class: 'remove-link',
-        text: 'remove ',
-        click: function() {
-          $(this).parent().remove();
-        },
+  //Using jquery object to hold data for remove link in order to bind click event to it
+  var $removeLink = $('<a/>', {
+    type: 'button',
+    href: '#',
+    class: 'remove-link',
+    text: 'remove ',
+    click: function() {
+      $(this).parent().remove();
+    },
+  })
 
-      })
+  //Using jquery object to hold data for comment link in order to bind click event to it
+  var $commentLink = $('<a/>', {
+    type: 'button',
+    href: '#',
+    class: 'comment-link',
+    text: ' comments ',
+    click: function() {
+      $(this).siblings('.comment-section').toggleClass('hide');
+    }
+  });
 
-      //using jquery object to hold data for comment link in order to bind click event to it
-      var $commentLink = $('<a/>', {
-        type: 'button',
-        href: '#',
-        class: 'comment-link',
-        text: ' comments ',
-        click: function() {
-          $(this).siblings('.comments').toggleClass('hide');
-        }
+  //Creating comment form object to append functional post comment button to it
+  var $commentForm = $('<form/>', {
+    class: 'comment-form',
+    html: '<h3>Add a New Comment</h3>' +
+      '<div class="form-group">' +
+      '<textarea type="text" class="form-control comment-message" placeholder="Comment Text" required></textarea>' +
+      '</div>' +
+      '<div class="form-group">' +
+      '<input type="text" class="form-control comment-name" placeholder="Your Name" required></input>' +
+      '</div>'
+  });
+
+  //Create object for comment button in order to attach click handler to it
+  var $commentButton = $('<button/>', {
+    type: 'button',
+    class: 'btn btn-primary comment-button',
+    text: 'Post',
+    click: function() {
+      //find values of comment form input and add them to the comment section when post is clicked
+      var commentName = $(this).siblings().find('.comment-name').val();
+      var commentMessage = $(this).siblings().find('.comment-message').val();
+
+      //Creating object to hold comment data which remove comment button can be appended to
+      var $comment = $('<p/>', {
+        class: 'comment',
+        html: commentMessage + ' <p><em>Comment By:</em> <strong>' + commentName + '</strong></p>'
       });
 
-      //creating comment form
-      var $commentForm = $('<form/>',
-       {
-          class: 'comment-form',
-          html:
-            '<h3>Add a New Comment</h3>' +
-            '<div class="form-group">' +
-            '<textarea type="text" class="form-control comment-message" placeholder="Comment Text" required></textarea>' +
-            '</div>' +
-            '<div class="form-group">' +
-            '<input type="text" class="form-control comment-name" placeholder="Your Name" required></input>' +
-            '</div>'
-        });
+      //Adding comment to comment section on click
+      $(this).parent().siblings('.comments-container').append($comment);
+    }
+  });
 
-        //create object for comment button in order to attach click handler to it
-        var $commentButton = $('<button/>', {
-          type: 'button',
-          class: 'btn btn-primary comment-button',
-          text: 'Post'
-          // click: function() {
-          //   $(this).
-          // }
-        });
+  //Attach comment button to comment form
+  $commentForm.append($commentButton);
 
-        //attach comment button to comment form
-        $commentForm.append($commentButton);
+  //Create comment section object in order to be able to append comment form to it
+  var $commentSection = $('<section/>', {
+    class: 'comment-section hide'
+  });
 
-        //Create comment section object to be able to append form with functional button to  it
-        var $commentSection = $('<section/>', {
-          class: 'comments hide',
-          text: 'Comments:'
-        });
-
-        $commentSection.append($commentForm);
-
-        //find values of comment form input and add them to the comment section when post is clicked
-        var commentName = $('.comment-name').val();
-        var commentMessage = $('.comment-message').val();
-
-        var $comment = $('<p/>', {
-          class: 'comment',
-          html: commentMessage + 'Posted by: <strong>' + commentName + '</strong>'
-        });
-
-        //Creating jquery object for post in order to append remove and comment links to it before adding to DOM
-        var $post = $('<article/>', {
-          class: 'post',
-          html: '<p>' +
-            postMessage + '</p>' + '<p class="username">Posted By: <strong>' +
-            postName + '</strong></p><hr>'
-        });
-
-        //adding functional comment and remove links to post
-        $post.prepend($commentLink); $post.prepend($removeLink); $post.append($commentSection)
-
-        //adding post to posts section
-        $('#posts').append($post);
+  $commentSection.append('<section class="comments-container"><h4>Comments:</h4></section><hr>')
+  $commentSection.append($commentForm);
 
 
-      });
+  //Creating jquery object for post in order to append remove and comment links to it before adding to DOM
+  var $post = $('<article/>', {
+    class: 'post',
+    html: '<p class="user-message">' +
+      postMessage + '</p>' + '<p class="username">Posted By: <strong>' +
+      postName + '</strong></p><hr>'
+  });
+
+  //adding functional comment and remove links to post
+  $post.prepend($commentLink);
+  $post.prepend($removeLink);
+  $post.append($commentSection).append('<hr>');
+
+  //adding post to posts section
+  $('#posts').append($post);
+
+
+});
