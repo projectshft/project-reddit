836ceb51774c086413a78adcd7dc9491610697da
Christina Kurpiel <kurpielcm@gmail.com>
2020-05-09 11:04:59 -0400

    add remove post button and functionality

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-//
-// var createPost = function (post, name) {
-//   var template =
-//  
-//
-// }
 
-$('button').on('click', function () {
-  post = $('#post-input').val();
-  name = $('#name-input').val();
+var createPost = $('button').on('click', function () {
+  var post = $('#post-input').val();
+  var name = $('#name-input').val();
+
+    // the template for post, name, and remove/comment buttons
+  var template =
+  '<div class="post-item comment-form"><li><button class="remove">remove</button class="comment"><button>comments</button>'
+  +post+ ' Posted By '+ '<strong>'+name+ '</strong></li></div>';
 
     //if the user post and name input are not empty, add them to the list
-  if (post !== "" && name !== "") {
-    $('.list-group').append('<div class="post-item comment-form"><li><button>remove</button><button>comments</button>' +post+ '</li><li> Posted By '+ '<strong>'+name+ '</strong></li></div>');
-  } else {
-    //if the post and name input are empty, alert the user
-    alert('Please input your name and post content.');
-  };
+      if (post !== "" && name !== "") {
+        $('.list-group').append(template);
+      } else {
+        //if the post and name input are empty, alert the user
+        alert('Please input your name and post content.');
+      };
 
 
-// the users post, name, and remove and comments button are added to the list
+      var removePost = $('.remove').on('click', function () {
+        $(this).parent().remove();
+      });
 
-
-});
+  });
