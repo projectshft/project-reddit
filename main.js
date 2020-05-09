204560b60e2869b3d68fec209d3db609d1849d8b
Christina Kurpiel <kurpielcm@gmail.com>
2020-05-09 12:44:11 -0400

    add input for post comments

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 
-var createPost = $('button').on('click', function () {
+var managePosts = $('button').on('click', function () {
   var post = $('#post-input').val();
   var name = $('#name-input').val();
 
     // the template for post, name, and remove/comment buttons
   var template =
-  '<div class="post-item comment-form"><li><button class="remove">remove</button class="comment"><button>comments</button>'
-  +post+ ' Posted By '+ '<strong>'+name+ '</strong></li></div>';
+  '<div class="post-item comment-form"><li><span class="remove special-word">remove</span>'
+  + " " + '<span class="comment special-word">comments</span> '
+  + post + '<br> Posted By '+ '<strong>'+ name + '</strong></li></div>';
 
     //if the user post and name input are not empty, add them to the list
       if (post !== "" && name !== "") {
@@ -16,9 +17,21 @@ var createPost = $('button').on('click', function () {
         alert('Please input your name and post content.');
       };
 
+      //removes the parent post when clicked
+    var removePost = $('.remove').on('click', function () {
+      $(this).parent().remove();
+    });
 
-      var removePost = $('.remove').on('click', function () {
-        $(this).parent().remove();
-      });
+    var addComment = $('.comment').on('click', function () {
+      //need new template for input and buttons
+      // var newContent =
+      // '<'
+
+
+      //adds additional input and post button to the bottom of the post when clicked
+      var newContent = $('.post-controls').html()
+      $(this).parent().append(newContent);
+    });
+
 
   });
