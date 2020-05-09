791a9608c12959c98f5d1c4bd51b3e16be506a51
MCola <mcolapietro502@gmail.com>
2020-05-08 20:23:03 -0400

    added comments and remove options before each new message post

--- main.js
+++ main.js
@@ -12,4 +12,12 @@ $postButton.click(function() {
   //append message and name to newly created userPost div
   $('.userPost').last().append('<div class="individualPostMessage" id="message' + userPostNumber +'">' + $userMessage + '</div>');
   $('.userPost').last().append('<div class="individualPostName" id="name' + userPostNumber +'"> Posted By: <strong>' + $userName + '</strong></div>');
+
+  //adding "remove" and "comments" to post message
+  var $newMessage = $('.individualPostMessage').last();
+  
+  $('<span>comments </span>').prependTo($newMessage);
+  $('<span class="removeOption">remove </span>').prependTo($newMessage);
+  
+
 });
