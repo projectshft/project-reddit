6dea014a4ccfd0df074f3df11f3d768811f57048
ADH <61984231+adholton@users.noreply.github.com>
2020-05-09 11:47:51 -0400

    finished post button, comments/remove links

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 ////create a global object that keeps the posts, maybe
-const posts = {};
+//const posts = {};
 // const postTemplate = '<div class="post-container"><p class="post-content">Test</p><p class="post-author">Test</p></div>';
 
 //when the user clicks Post button.. 
@@ -14,38 +14,89 @@ $('#submit').click(function() {
 
   //the post will contain the author name and post content, and 2 links to either remove or add comments
   //consider creating a comment section that will be part of the post template but hidden from view until the comments button is clicked
-  const postTemplate = '<div class="post-container"><a class="comments-link"><span>Comments   </span></a><a class="remove-link"><span>Remove</span></a><p class="post-content">' + postContent + '</p><form class="form-class" style="margin-top:30px;" onsubmit="event.preventDefault();"><input id="" type="text" class="form-control short-input" placeholder="Comment Text"></input><input id="" type="text" class="form-control short-input" placeholder="User Name"><button class="btn btn-primary">Post Comment</button></form><p class="post-author">Posted by: <strong>' + postAuthor + '</strong></p></div>';  
+  const postTemplate = '<div class="post-container"><a class="comments-link"><span>Comments   </span></a><a class="remove-link"><span>Remove</span></a><p class="post-content">' + postContent + '</p><form class="form-class" style="margin-top:10px;" onsubmit="event.preventDefault();"><input id="" type="text" class="form-control short-input" placeholder="Comment Text"></input><input id="" type="text" class="form-control short-input" placeholder="User Name"><button class="btn btn-primary comment-button">Post Comment</button></form><p class="post-author">Posted by: <strong>' + postAuthor + '</strong></p></div>';  
+
+
+  const $postTemplate = $(postTemplate);
   
   // const commentTemplate = '<form style="margin-top:30px;" onsubmit="event.preventDefault();"><input id="" type="text" class="form-control" placeholder="Comment Text"></input><input id="" type="text" class="form-control" placeholder="User Name"><button class="btn btn-primary">Post Comment</button></form>';
 
   //$('.post-content').append($(commentTemplate));
   //$(commentTemplate).insertAfter('.post-content');
-  //look up how to add line between posts in bootstrap
+  
  
-  //the post will be posted under the heading, child of the .post-div and above the form, each new post will be added below
-  $('.posts').append(postTemplate);
-
-  //if the remove link is clicked, the post will be removed from the page AND from the post object
-  $('.remove-link').click(function() {
-    $('.post-container').remove();
-  })  
+ 
+   //if the remove link is clicked, the post will be removed from the page 
+  $postTemplate.find('.remove-link').click(function() {
+    $(this).parent().remove();
+  });
 
   //if the comments link is clicked, a name and text field and comment button will be shown and if the comments are showing they will be hidden
-  $('.comments-link').click(function() {
-    if ($('.form-class').is(':hidden')) {
-    $('.form-class').show();
+  $postTemplate.find('.comments-link').click(function() {
+    if ($(this).nextAll('.form-class').is(':hidden')) {
+      $(this).nextAll('.form-class').show();
     } else {
-      $('.form-class').hide();
-    } 
+      $(this).nextAll('.form-class').hide();
+    }
   })
 
- //if the comment button is clicked the comments/commenter will appear under the post with an "x". The comment and comment author will go into the posts object as well.
+   //the post will be posted under the heading, child of the .post-div and above the form, each new post will be added below
+  $('.posts').append($postTemplate);
 
-//if the "x" is clicked, the comment will be removed
+   //if the comment button is clicked the comments/commenter will appear under the post with an "x", top to bottom. 
 
-//create templates that will be used for each post and comment section 
+  
+
+  //if the "x" is clicked, the comment will be removed
+
+
+
+
+  //use css and/or bootstrap to style the page (border btw posts), input field widths, icons, etc. 
+
+
+    /* postTemplate = 
+  <div class="post-container">
+    <a class="comments-link"><span>Comments   </span></a> 
+    <a class="remove-link"><span>Remove</span></a> 
+    <p class="post-content">' + postContent + '</p>
+    <form class="form-class" style="margin-top:30px;" onsubmit="event.preventDefault();">
+      <input id="" type="text" class="form-control short-input" placeholder="Comment Text"></input>
+      <input id="" type="text" class="form-control short-input" placeholder="User Name">
+      <button class="btn btn-primary comment-button">Post Comment</button></form>
+    <p class="post-author">Posted by: <strong>' + postAuthor + '</strong></p>
+  </div>';
+  */
+
+ 
+  // var $post = $(postTemplate);
+  // $post.find('.remove-link').click(function() {
+  //   $(this).parentNode.remove();
+  // })
+
+
+  // $('.remove-link').click(function() {
+  //   $(this).parent().remove();
+  // })  
+
+
+  
+
+  // $('.comments-link').click(function() {
+  //   //$(this).siblings('.form-class').toggle();
+  //  // $(this).nextAll('.form-class').toggle();
+
+  //   if ($(this).nextAll('.form-class').is(':hidden')) {
+  //     $(this).nextAll('.form-class').show();
+  //   } else {
+  //     $(this).nextAll('.form-class').hide();
+  //   }
+  // })
+
  
 })
+
+
 
 
 $('#submit').click();
