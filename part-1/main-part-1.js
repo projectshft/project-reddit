857c5b35919ba7a57a46db3fe3f2340a97f9ff94
Joe Krall <kralljr@outlook.com>
2020-05-09 18:15:47 -0400

    fixed text bolding, margin between comments and Add New

--- part-1/main-part-1.js
+++ part-1/main-part-1.js
@@ -1,15 +1,10 @@
 var button = $('button');
-var allPosts = [];
 
 $('button').on('click', function () {
 
   var userMessage = "<li>" + $('#message').val() + "</li>";
   var userName = "<li>" + "<strong>Posted by:</strong> " + $('#name').val() + "</li>";
   var post = '<li><ul>' + userMessage + userName + '<hr>' + '<ul><li>'; 
-
-  // $('.post-list').append(userMessage);
-  // $('.post-list').append(userName);
-  // $('.post-list').append('<hr>');
 
   $('.post-list').prepend(post);
 });
--- part-2/main-part-2.js
+++ part-2/main-part-2.js
@@ -1,14 +1,15 @@
 var button = $('button');
+var allPosts = [];
 
+// On submitting the form, the user's post stacks
 $('button').on('click', function () {
 
   var userMessage = "<li>" + $('#message').val() + "</li>";
-  var userName = "<li>" + "<strong>Posted by:</strong> " + $('#name').val() + "</li>";
-  var post = '<li><ul>' + userMessage + userName + '<hr>' + '<ul><li>';
+  var userName = "<li>" + "Posted by: <strong> " + $('#name').val() + "</strong></li>";
+  var postComments = null;
+  var postRemove = null; // on same <li> as userMessage
 
-  // $('.post-list').append(userMessage);
-  // $('.post-list').append(userName);
-  // $('.post-list').append('<hr>');
+  var post = '<li><ul>' + userMessage + userName + '<hr>' + '<ul><li>';
 
   $('.post-list').prepend(post);
 });
