5c662d5c939704606540d187fc289992ebfee42f
Joe Krall <kralljr@outlook.com>
2020-05-10 00:50:52 -0400

    able to post li element to ul in index

--- part-2/main-part-2.js
+++ part-2/main-part-2.js
@@ -6,16 +6,16 @@ var allPosts = [];
 $(postButton).on('click', function () {
 
   // Getting user input into HTML format
-  var postHTML = $('#message').val() + '<br>' + 'Posted by: <strong> ' + $('#name').val() + '<strong>' + '<br>' + '<hr>';
-  // don't forget <hr>
+  var postHTML = $('#message').val() + '<br>Posted by: <strong> ' + $('#name').val() + '<strong><br><hr>';
 
-  // var post = $('<li></li>').
-  //
-  //
-  // var postComments = $('<a data-target="#comments" data-toggle="collapse" id="comment-button">comments</a>').find('#comment-button').click(function() {alert('click');}).end();
+  var post = document.createElement('li');
+  $(post).html(postHTML);
+
+  // I want this button to be bound to only its list item.
+  //var postComments = $('<a data-target="#comments" data-toggle="collapse" id="comment-button">comments</a>').find('#comment-button').click(function() {alert('click');}).end();
 
   //$('.post').prepend(postComments);
 
-  $('.post-list').prepend(postHTML);
+  $('.post-list').prepend(post);
 
 });
