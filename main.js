3449137cdeaa4d963f89f4e1b1adb3e45223f719
bclewis54 <lewisbradleychanning@gmail.com>
2020-05-10 13:38:43 -0400

    Comment section still-in-progress(SIP). Posts push to  array properly(contain name, message, and a comment area).

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-//Create data structure to maintain userPosts
-var $userPosts = [
+//Create array to maintain userPosts
+var $userPosts = []
 
-];
-
-//A post userMessage will need to be submitted when the button is clicked
+//A post including userMessage and userName will need to be submitted when the button is clicked
 $('#submit').click(function () {
   var userMessageInput = $('#postMessage').val();
   var userNameInput = $('#postName').val();
-  //Edge cases for if userName or userMessage are empty
+  //Edge cases for if userName or userMessage are empty will alert
   if (userMessageInput.length === 0 || userNameInput.length === 0) {
     alert('You must enter your name and a message to post.')
   } else {
     //Post the name and message to the website
-    $('.userPosts').append('<p>' + userMessageInput + '</p>');
-    $('.userPosts').append('<p class="userName">' + 'Posted by: ' + userNameInput + '</p>');
-    //push the userMessage and userName into $userPosts
-    $userPosts.push(userMessageInput);
-    $userPosts.push(userNameInput);
+    $('.userPosts').append('<p>' + userMessageInput + '</p>' + '<p class="userName">' + 'Posted by: ' + userNameInput + '</p>');
+    //push the userMessage, userName, and a comment area(to be filled later) into $userPosts
+    $userPosts.push({
+        message: userMessageInput,
+        name: userNameInput,
+        comments: []
+      })
 
+    //add a (TODO: UNIQUE) comment section for each post
     $('.userName').append('<div class="postComments">' +
     '<input id="commentMessage" type="text" class="form-control comment" placeholder="Comment">' + '</input>' +
     '<input id="commentName" type="text" class="form-control comment" placeholder="Name">' + '</input>' + '</div>' +
