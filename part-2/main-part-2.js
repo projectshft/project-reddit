8c608c6c16525aab9396d473ad3fb1c3b4843dea
Joe Krall <kralljr@outlook.com>
2020-05-10 15:11:04 -0400

    experimenting with more jquery objects for form

--- part-2/main-part-2.js
+++ part-2/main-part-2.js
@@ -1,29 +1,31 @@
-var postButton = $('button');
-//var commentButton = $('#comments')
+var postButton = $('#submit');
 var allPosts = [];
 
-// On submitting the form, the user's post stacks
 $(postButton).on('click', function () {
 
   // This is where we would empty $(.post-list)
 
-  // Getting user input into HTML format
-  //
-  var formForComments = '<form class="form-inline comments" style="display:none;"><input id="message" type="text" class="form-control" placeholder="Post Text"></input><input id="name" type="text" class="form-control" placeholder="Your Name"></input><button id="submit" class="btn btn-primary">Post Comment</button></form>';
+  //This 'form' gets me the right button but is not being created dynamically.
+  var forSubmittingComments = '<form class="form-inline comments" onsubmit="event.preventDefault();" style="display:none;"><input id="message-comment" type="text" class="form-control" placeholder="Comment Text"></input><input id="name-comment" type="text" class="form-control" placeholder="User Name"></input><button id="submit-comment" class="btn btn-primary">Post Comment</button></form>';
 
-  var postHTML = $('#message').val() +  formForComments + '<p>Posted by: <strong> ' + $('#name').val() + '</strong><br><hr>';
+  var postHTML = $('#message').val() + '<p id="comment-1">' + forSubmittingComments + 'Posted by: <strong> ' + $('#name').val() + '</strong><br><hr>';
 
-  // I want this button to be bound to only its list item.
-  // I can't let go of this test case!
+  // Trying to append the form stuff dynamically
+  $(function() {
+     $('#comment-1').append(
+         $('<form />', { class: 'form-inline comments', onsubmit: 'event.preventDefault();', style:'display:none;'}).append(
+             $('<input />', { id: 'message-comment', type: 'text', class:'form-control', placeholder:'Comment Text'}),
+         )
+     )
+ })
+
   var postComments = $('<a/>',
     {
         text: 'comment ',
         click: function () { $(".comments").toggle(); }
     });
 
-
-    var post = $('<li><p id="comment-1">' + postHTML).find('#comment-1').prepend(postComments).end();
-
+    var post = $('<li><p id="post-1">' + postHTML).find('#post-1').prepend(postComments).end();
 
   // This is where using allPosts might come in handy. We create a separate ID above using a for loop,
   // and then prepend in a loop down here. The ID would be for matching dropdowns.
