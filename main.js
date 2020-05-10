184ad7aff2774a763848aca943864d9f21f9d7cb
bclewis54 <lewisbradleychanning@gmail.com>
2020-05-10 12:18:09 -0400

    Edge cases functional

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
+//Create data structure to maintain userPosts
+var $userPosts = [
+
+];
 
 //A post userMessage will need to be submitted when the button is clicked
 $('#submit').click(function () {
   var userMessageInput = $('#postMessage').val();
+  if (userMessageInput.length === 0) {
+    alert('You must enter a message to post.')
+  } else {
   $('.userPosts').append('<p>' + userMessageInput + '</p>');
-});
+}});
 //A post userName will need to be submitted when the button is clicked
 $('#submit').click(function () {
   var userNameInput = $('#postName').val();
-  $('.userPosts').append('<p>' + 'Posted By: ' + userNameInput + '</p>');
-});
-//A unique comment section with commentorName and commentorMessage will need to be appended to each post
-$('#comment').click(function () {
-  var commentMessage = $('#commentMessage').val();
-  $('.userPosts').append('<p>' + commentMessage + '</p>');
-});
+  if (userNameInput.length === 0) {
+    alert('You must enter a name to post.')
+  } else {
+  $('.userPosts').append('<p>' + 'Posted by: ' + userNameInput + '</p>');
+}});
 
-$('#comment').click(function () {
-  var commentName = $('#commentName').val();
-  $('.userPosts').append('<p>' + 'Posted By: ' + commentName + '</p>');
-});
+
+
+
+
+
+
+
+
+//A unique comment section with commentorName and commentorMessage will need to be appended to each post
+// $('#comment').click(function () {
+//   var commentMessage = $('#commentMessage').val();
+//   $('.userPosts').append('<p>' + commentMessage + '</p>');
+// });
+//
+// $('#comment').click(function () {
+//   var commentName = $('#commentName').val();
+//   $('.userPosts').append('<p>' + 'Posted By: ' + commentName + '</p>');
+// });
