728e4f4724882661f99b0a6392a1a64acec99ea6
Joe Krall <kralljr@outlook.com>
2020-05-10 00:38:43 -0400

    rewrote post as a single line of HTML, no longer wrapped in a list tag

--- part-2/main-part-2.js
+++ part-2/main-part-2.js
@@ -5,20 +5,17 @@ var allPosts = [];
 // On submitting the form, the user's post stacks
 $(postButton).on('click', function () {
 
-  var post = $('<li><ul class="post"></ul></li>');
+  // Getting user input into HTML format
+  var postHTML = $('#message').val() + '<br>' + 'Posted by: <strong> ' + $('#name').val() + '<strong>' + '<br>' + '<hr>';
+  // don't forget <hr>
 
-  var userMessage = $('#message').val(); // on same line as postComments
-  var userMessageLine = $('<li class="your-post-here"></li>');
+  // var post = $('<li></li>').
+  //
+  //
+  // var postComments = $('<a data-target="#comments" data-toggle="collapse" id="comment-button">comments</a>').find('#comment-button').click(function() {alert('click');}).end();
 
-  $('.your-post-here').append(userMessage);
-  $('.post').append(userMessageLine);
+  //$('.post').prepend(postComments);
 
-  //var userName = '<li>' + 'Posted by: <strong> ' + $('#name').val() + '</strong></li>';
-
-  var postComments = $('<li><a data-target="#comments" data-toggle="collapse" id="comment-button">comments</a>').find('#comment-button').click(function() {alert('click');}).end();
-
-  $('.post').prepend(postComments);
-
-  $('.post-list').prepend(post);
+  $('.post-list').prepend(postHTML);
 
 });
