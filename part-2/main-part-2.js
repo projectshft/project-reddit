80a2e6b235e15ac388c2c16a1d6c8741b794f156
Joe Krall <kralljr@outlook.com>
2020-05-10 00:58:18 -0400

    creating new li element using jquery

--- part-2/main-part-2.js
+++ part-2/main-part-2.js
@@ -5,17 +5,21 @@ var allPosts = [];
 // On submitting the form, the user's post stacks
 $(postButton).on('click', function () {
 
+  // This is where we would empty $(.post-list)
+
   // Getting user input into HTML format
   var postHTML = $('#message').val() + '<br>Posted by: <strong> ' + $('#name').val() + '<strong><br><hr>';
 
-  var post = document.createElement('li');
+  var post = $('<li id="comments"></li>');
   $(post).html(postHTML);
 
   // I want this button to be bound to only its list item.
-  //var postComments = $('<a data-target="#comments" data-toggle="collapse" id="comment-button">comments</a>').find('#comment-button').click(function() {alert('click');}).end();
+  var postComments = $('<a data-target="#comments" data-toggle="collapse" id="comment-button">comments</a>').find('#comment-button').click(function() {alert('click');}).end();
 
-  //$('.post').prepend(postComments);
+  $('.post').prepend(postComments);
 
+  // This is where using allPosts might come in handy. We create a separate ID above using a for loop,
+  // and then prepend in a loop down here.
   $('.post-list').prepend(post);
 
 });
