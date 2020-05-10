50fcf675757a3fd59aa4f7970c59e9828582b841
Joe Krall <kralljr@outlook.com>
2020-05-10 11:52:31 -0400

    rewrote using find()

--- part-2/main-part-2.js
+++ part-2/main-part-2.js
@@ -8,7 +8,7 @@ $(postButton).on('click', function () {
   // This is where we would empty $(.post-list)
 
     // Getting user input into HTML format
-  var postHTML = $('#message').val() + '<input style="display:none;" id="message" type="text" class="form-control comments" placeholder="Post Text"><input id="name" style="display:none;" type="text" class="form-control comments" placeholder="Your Name"></input><button style="display:none;" id="submit" class="btn btn-primary comments">Post</button>' + '<br>' + 'Posted by: <strong> ' + $('#name').val() + '<strong><br><hr>';
+  var postHTML = $('#message').val() + '<input style="display:none;" id="message" type="text" class="form-control comments" placeholder="Post Text"><input id="name" style="display:none;" type="text" class="form-control comments" placeholder="Your Name"></input><button style="display:none;" id="submit" class="btn btn-primary comments">Post</button>' + '<br>' + 'Posted by: <strong> ' + $('#name').val() + '</strong><br><hr>';
 
   // I want this button to be bound to only its list item.
   // I can't let go of this test case!
@@ -18,7 +18,7 @@ $(postButton).on('click', function () {
         click: function () { $(".comments").toggle(); }
     });
 
-    var post = $('<li><p>' + postHTML + '</p></li>').children().first().prepend(postComments).end();
+    var post = $('<li><p id="comment-1">' + postHTML + '</p></li').find('#comment-1').prepend(postComments).end();
 
 
   // This is where using allPosts might come in handy. We create a separate ID above using a for loop,
