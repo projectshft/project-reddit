585208ea4907d6c22d05d8ddcf1fa94a76caf6b3
Spalding Vance <spaldingvance@Spaldings-MBP.nc.rr.com>
2020-05-10 11:26:34 -0400

    added functionality for removing comments

--- main.js
+++ main.js
@@ -13,12 +13,16 @@ var commentObj = {
 
 //builds post object instance and adds object to posts array to be rendered
 var addPost = function() {
+  console.log("in add post, comment object is " + commentObj);
   var myPost = Object.create(postObj);
+  myPost.comments = [];
+  console.log(myPost.comments);
   var postName = $('.post-form-name').val();
   var postText = $('.post-form-text').val();
   myPost.postName = postName;
   myPost.postText = postText;
   myPost.postID = 'post' + (posts.length);
+  console.log("in add post, unadded template comments are " + myPost.comments);
   posts.push(myPost);
   renderPosts();
 }
@@ -36,6 +40,9 @@ var renderPosts = function() {
     var postID = post.postID;
     var $postTemplate = buildPostTemplate(postID, postText, postName);
     $postsContainer.append($postTemplate);
+    console.log("calling rendComments from renderPosts for " + postID);
+    console.log("comments for " + postID + " are " + post.comments[0]);
+    renderComments(postID, post);
   }
   $(".remove-post").click(removePost);
   $(".show-comments").click(showComments);
@@ -75,6 +82,16 @@ var removePost = function() {
   renderPosts();
 }
 
+var removeComment = function() {
+  var commentID = $(this).parent().attr("id");
+  var postID = $(this).closest(".list-group-item").attr("id");
+  var myPost = posts.find(post=> post.postID === postID);
+  console.log(myPost.comments);
+  myPost.comments = myPost.comments.filter(comment => comment.commentID !== commentID);
+  resetCommentIDs(myPost);
+  renderComments(postID, myPost);
+}
+
 //sets postIDs based off the order they exist in the posts array
 //postID is set based off of lenght of posts array, creating duplicate IDs
 var resetPostIDs = function() {
@@ -83,14 +100,22 @@ var resetPostIDs = function() {
   }
 }
 
+var resetCommentIDs = function(post) {
+  for(let i = 0; i < post.comments.length; i++) {
+    post.comments[i].commentID = "comment" + i;
+  }
+}
+
 var addComment = function() {
   var postID = $(this).closest(".list-group-item").attr("id");
+  console.log("adding comment for " + postID);
   var commentName = $(this).siblings(".comment-form-name").val();
   var commentText = $(this).siblings(".comment-form-text").val();
   var myComment = Object.create(commentObj);
   myComment.commentName = commentName;
   myComment.commentText = commentText;
   var myPost = posts.find(post => post.postID === postID);
+  console.log("in addComment: myPost is " + myPost.postID);
   myPost.comments.push(myComment);
   renderComments(postID, myPost);
 }
@@ -98,22 +123,26 @@ var addComment = function() {
 //removes all comments from comment-messages-container, then loops through comments array
 // to update comments in the container
 var renderComments = function(postID, myPost) {
+  console.log("rendering comments for " + postID);
   var $commentContainer = $('#'+postID).find(".comment-messages-container");
   $commentContainer.empty();
   for(let i = 0; i < myPost.comments.length; i++) {
     var commentName = myPost.comments[i].commentName;
     var commentText = myPost.comments[i].commentText;
-    myPost.comments[i].commentID = "comment" + i;
-    var $commentTemplate = buildCommentTemplate(commentText, commentName);
+    var commentID = "comment" + i;
+    myPost.comments[i].commentID = commentID;
+    console.log("in renderComments: post comments are " + myPost.comments[i].commentID);
+
+    var $commentTemplate = buildCommentTemplate(commentText, commentName, commentID);
     $commentContainer.append($commentTemplate);
   }
-  //$(".delete-comment-btn").click(removeComment);
+  $(".delete-comment-btn").click(removeComment);
 }
 
 //builds jquery dom object for comment template, taking in values passed by renderComments
-var buildCommentTemplate = function(commentText, commentName) {
+var buildCommentTemplate = function(commentText, commentName, commentID) {
   var commentTemplate =
-  '<p>' + commentText + ' Posted By: ' + commentName +
+  '<p id="'+ commentID + '">' + commentText + ' Posted By: ' + commentName +
   '<button type="button" class="close delete-comment-btn" aria-label="Close">' +
   '<span aria-hidden="true">&times;</span></button>' + '</p>';
   return $(commentTemplate);
