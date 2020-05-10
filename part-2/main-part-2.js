92db6e9ccd836a33f7696e67160080fc5b54f5c7
Joe Krall <kralljr@outlook.com>
2020-05-10 02:46:38 -0400

    Posting directly without delay. Took out postComments button, and created the post variable with .html(postHTML). Commenting back in soon ...

--- part-2/main-part-2.js
+++ part-2/main-part-2.js
@@ -7,23 +7,18 @@ $(postButton).on('click', function () {
 
   // This is where we would empty $(.post-list)
 
-
-  // Getting user input into HTML format
-  var post = $('<ul><li id="comment"></li></ul>');
-
+    // Getting user input into HTML format
   var postHTML = $('#message').val() + '<br>Posted by: <strong> ' + $('#name').val() + '<strong><br><hr>';
-
-  $('#comment').html(postHTML);
+  var post = $('<li id="comment"></li>').html(postHTML);
 
   // I want this button to be bound to only its list item.
-  var postComments = $('<a data-target="#comments" data-toggle="collapse" id="comment-button">comments </a>').find('#comment-button').click(function() {
-    alert('click');
-  }).end();
+  //var postComments = $('<li><a data-target="#comments" data-toggle="collapse" id="comment-button">comments</a></li>').find('#comment-button').click(function() { alert('click');}).end();
 
-  $('#comment').prepend(postComments);
+
+//  $('#comment').prepend(postComments);
+
   // This is where using allPosts might come in handy. We create a separate ID above using a for loop,
   // and then prepend in a loop down here.
   $('.post-list').prepend(post);
-
 
 });
