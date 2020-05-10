66de152f2fbaa8bbd87215d5be91b805987a4a81
Joe Krall <kralljr@outlook.com>
2020-05-10 04:51:55 -0400

    able to toggle span

--- part-2/main-part-2.js
+++ part-2/main-part-2.js
@@ -8,18 +8,15 @@ $(postButton).on('click', function () {
   // This is where we would empty $(.post-list)
 
     // Getting user input into HTML format
-  var postHTML = $('#message').val() + '<span style="display:none;"><br>blue</span>' + '<br>Posted by: <strong> ' + $('#name').val() + '<strong><br><hr>';
+  var postHTML = $('#message').val() + '<span id=comments style="display:none;"><br></span>' + '<br>Posted by: <strong> ' + $('#name').val() + '<strong><br><hr>';
 
   // I want this button to be bound to only its list item.
-
-  var postComments = $('<a href="#comments" data-toggle="collapse" id="comment-button">comments </a>').find('#comment-button').click(function() {alert('click');}).end();
-
   // I can't let go of this test case!
-  // var postComments = $('<a/>',
-  //   {
-  //       text: 'comment ',
-  //       click: function () { alert('hi'); }
-  //   });
+  var postComments = $('<a/>',
+    {
+        text: 'comment ',
+        click: function () { $("#comments").toggle(); }
+    });
 
     var post = $('<li><p>' + postHTML + '</p></li>').children().first().prepend(postComments).end();
 
