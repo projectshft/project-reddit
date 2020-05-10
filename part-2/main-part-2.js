c64e7e653564455358d52fae54f8e408ea3e0fac
Joe Krall <kralljr@outlook.com>
2020-05-09 23:37:22 -0400

    comment button dynamically created, lots of bugs

--- part-2/main-part-2.js
+++ part-2/main-part-2.js
@@ -5,15 +5,19 @@ var allPosts = [];
 // On submitting the form, the user's post stacks
 $(postButton).on('click', function () {
 
-  var userMessage = '<li>' + $('#message').val() + '</li>'; // on same line as postComments, note closing tag
-  var userName = '<li>' + 'Posted by: <strong> ' + $('#name').val() + '</strong></li>';
+  var post = $('<li><ul class="post"></ul></li>');
 
-  var post = '<li><ul class="post">' + userMessage + userName + '<hr>' + '<ul><li>';
+  var userMessage = $('#message').val(); // on same line as postComments
+  var userMessageLine = $('<li class="your-post-here"></li>');
+
+  $('.your-post-here').append(userMessage);
+  $('.post').append(userMessageLine);
+
+  //var userName = '<li>' + 'Posted by: <strong> ' + $('#name').val() + '</strong></li>';
 
   var postComments = $('<li><a data-target="#comments" data-toggle="collapse" id="comment-button">comments</a>').find('#comment-button').click(function() {alert('click');}).end();
 
   $('.post').prepend(postComments);
-  //var commentSection = '<li class="collapse in" id="comments"><p>Dummy text.</p></li>';
 
   $('.post-list').prepend(post);
 
