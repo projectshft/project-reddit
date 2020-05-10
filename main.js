03e5c2b2ca702d06f3c102bb7c25d298e9e94eb5
Spalding Vance <spaldingvance@Spaldings-MBP.nc.rr.com>
2020-05-10 19:18:23 -0400

    added styling, added validation preventing empty posts/comments from being accepted

--- main.js
+++ main.js
@@ -13,18 +13,19 @@ var commentObj = {
 
 //builds post object instance and adds object to posts array to be rendered
 var addPost = function() {
-  console.log("in add post, comment object is " + commentObj);
   var myPost = Object.create(postObj);
   myPost.comments = [];
-  console.log(myPost.comments);
   var postName = $('.post-form-name').val();
   var postText = $('.post-form-text').val();
+  if(!postName || !postText) {
+    alert("Inavlid input: both fields must be filled out");
+  } else {
   myPost.postName = postName;
   myPost.postText = postText;
   myPost.postID = 'post' + (posts.length);
-  console.log("in add post, unadded template comments are " + myPost.comments);
   posts.push(myPost);
   renderPosts();
+  }
 }
 
 //feeds post information to buildPostTemplate and appends result to $postsContainer
@@ -32,7 +33,6 @@ var addPost = function() {
 var renderPosts = function() {
   var $postsContainer = $('.posts-container');
   $postsContainer.empty();
-
   for(let i = 0; i < posts.length; i++) {
     var post = posts[i];
     var postName = post.postName;
@@ -40,8 +40,6 @@ var renderPosts = function() {
     var postID = post.postID;
     var $postTemplate = buildPostTemplate(postID, postText, postName);
     $postsContainer.append($postTemplate);
-    console.log("calling rendComments from renderPosts for " + postID);
-    console.log("comments for " + postID + " are " + post.comments[0]);
     renderComments(postID, post);
   }
   $(".remove-post").click(removePost);
@@ -72,7 +70,7 @@ var buildPostTemplate = function(postID, postText, postName) {
     return $postTemplate;
 }
 
-//fetches id for post to be removed, and removes it from the posts array
+//fetches id for post to be removed and removes it from the posts array
 var removePost = function() {
   var id = $(this).parent().attr("id");
   posts = posts.filter(post => {
@@ -82,11 +80,11 @@ var removePost = function() {
   renderPosts();
 }
 
+//fetches id for comment to be removed and removes it from the comments array
 var removeComment = function() {
   var commentID = $(this).parent().attr("id");
   var postID = $(this).closest(".list-group-item").attr("id");
   var myPost = posts.find(post=> post.postID === postID);
-  console.log(myPost.comments);
   myPost.comments = myPost.comments.filter(comment => comment.commentID !== commentID);
   resetCommentIDs(myPost);
   renderComments(postID, myPost);
@@ -100,30 +98,33 @@ var resetPostIDs = function() {
   }
 }
 
+//resets comment IDs to prevent duplicate IDs on the same post
 var resetCommentIDs = function(post) {
   for(let i = 0; i < post.comments.length; i++) {
     post.comments[i].commentID = "comment" + i;
   }
 }
 
+//grabs comment text and name values from form and adds them to comments array
 var addComment = function() {
   var postID = $(this).closest(".list-group-item").attr("id");
-  console.log("adding comment for " + postID);
   var commentName = $(this).siblings(".comment-form-name").val();
   var commentText = $(this).siblings(".comment-form-text").val();
+  if (!commentName || !commentText) {
+    alert("Invalid input: both fields must be filled out");
+  } else {
   var myComment = Object.create(commentObj);
   myComment.commentName = commentName;
   myComment.commentText = commentText;
   var myPost = posts.find(post => post.postID === postID);
-  console.log("in addComment: myPost is " + myPost.postID);
   myPost.comments.push(myComment);
   renderComments(postID, myPost);
+  }
 }
 
 //removes all comments from comment-messages-container, then loops through comments array
 // to update comments in the container
 var renderComments = function(postID, myPost) {
-  console.log("rendering comments for " + postID);
   var $commentContainer = $('#'+postID).find(".comment-messages-container");
   $commentContainer.empty();
   for(let i = 0; i < myPost.comments.length; i++) {
@@ -131,8 +132,6 @@ var renderComments = function(postID, myPost) {
     var commentText = myPost.comments[i].commentText;
     var commentID = "comment" + i;
     myPost.comments[i].commentID = commentID;
-    console.log("in renderComments: post comments are " + myPost.comments[i].commentID);
-
     var $commentTemplate = buildCommentTemplate(commentText, commentName, commentID);
     $commentContainer.append($commentTemplate);
   }
@@ -142,7 +141,7 @@ var renderComments = function(postID, myPost) {
 //builds jquery dom object for comment template, taking in values passed by renderComments
 var buildCommentTemplate = function(commentText, commentName, commentID) {
   var commentTemplate =
-  '<p id="'+ commentID + '">' + commentText + ' Posted By: ' + commentName +
+  '<p id="'+ commentID + '">' + commentText + ' <b>Posted By:</b> ' + commentName +
   '<button type="button" class="close delete-comment-btn" aria-label="Close">' +
   '<span aria-hidden="true">&times;</span></button>' + '</p>';
   return $(commentTemplate);
@@ -154,4 +153,3 @@ var showComments = function() {
 }
 
 $(".post-button").click(addPost);
-console.log("end of program reached");
