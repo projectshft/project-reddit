0baf80f8221bb89108cc0b997d073cca38f99d6f
ADH <61984231+adholton@users.noreply.github.com>
2020-05-10 14:17:56 -0400

    clraify comments and fix formatting

--- main.js
+++ main.js
@@ -12,9 +12,20 @@ $('#submit').click(function() {
   const postContent = $('#message').val();
   const postAuthor = $('#name').val();
 
-  //the post will contain the author name and post content, and 2 links to either remove or add comments
-  //consider creating a comment section that will be part of the post template but hidden from view until the comments button is clicked
-  const postTemplate = '<div class="post-container"><a class="comments-link links"><span>Comments</span></a><a class="remove-link links"><span>Remove</span></a><p class="post-content">' + postContent + '</p><section class="comments-section"><div class="comments-div"></div><form class="form-class form-inline" style="margin-top:10px;" onsubmit="event.preventDefault();"><input type="text" class="form-control-sm comment-content" placeholder="Comment Text"></input><input type="text" class="form-control-sm comment-author" placeholder="User Name"><button class="btn btn-primary comment-button">Post Comment</button></form></section><p class="post-author">Posted by: <strong>' + postAuthor + '</strong></p></div>';  
+  //the post will contain the author name and post content, and 2 links to either remove or add comments. Comment section will be part of the post template but hidden from view until the comments button is clicked
+  const postTemplate = 
+  '<div class="post-container">' + 
+    '<a class="comments-link links"><span>Comments</span></a>' +
+    '<a class="remove-link links"><span>Remove</span></a>' + 
+    '<p class="post-content">' + postContent + '</p>' + 
+    '<section class="comments-section">' + 
+      '<div class="comments-div"></div>' +
+      '<form class="form-class form-inline" style="margin-top:10px;" onsubmit="event.preventDefault();">' +
+        '<input type="text" class="form-control-sm comment-content" placeholder="Comment Text"></input>' + 
+        '<input type="text" class="form-control-sm comment-author" placeholder="User Name">' + 
+        '<button class="btn btn-primary comment-button">Post Comment</button>' +
+      '</form></section>' + 
+    '<p class="post-author">Posted by: <strong>' + postAuthor + '</strong></p></div>';  
 
   //create a variable that will point to the post template created for each post click. (we need this to specify which post we are manipulating and adding click events to)
   const $postTemplate = $(postTemplate);
@@ -25,26 +36,27 @@ $('#submit').click(function() {
     $(this).parent().remove();
   });
 
-  //if the comments link is clicked, a name and text field and comment button will be shown and if the comments are showing they will be hidden
+  //if the comments link is clicked, the comments, comment input fields and comment button will toggle (hide/show)
   $postTemplate.find('.comments-link').click(function() {
-    if ($(this).nextAll('.comments-section').is(':hidden')) {
-      $(this).nextAll('.comments-section').show();
-    } else {
-      $(this).nextAll('.comments-section').hide();
-    }
+    $(this).nextAll('.comments-section').toggle();
   })
 
-  //the post will be posted under the heading, child of the .post-div and above the form, each new post will be added below
+  //the post will the child of the .posts div and above the form, each new post will be added below the prevoius post
   $('.posts').append($postTemplate);
 
 
-  //if the comment button is clicked the comments/commenter will appear under the post with an "x", comments listed top to bottom. 
+  //if the comments button is clicked the comments/commenter will be added under the post, with an "x" for comment deletion, comments listed top to bottom. 
   $postTemplate.find('.comment-button').click(function() {
     
     const commentContent = $(this).siblings('.comment-content').val();
     const commentAuthor = $(this).siblings('.comment-author').val();
     
-    const commentTemplate = '<div class="comment-row"><span class="comment-span">' + commentContent + '</span><span>  Posted by: <strong>' + commentAuthor + '</strong></span><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></div>';
+    const commentTemplate = 
+    '<div class="comment-row">' +
+      '<span class="comment-span">' + commentContent + '</span>' +
+      '<span>Posted by: <strong>' + commentAuthor + '</strong></span>' +
+      '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>' +
+    '</div>';
 
     //add the comment template to the comments div
     const $commentTemplate = $(commentTemplate);
@@ -55,72 +67,19 @@ $('#submit').click(function() {
       $(this).parent().remove();
     })
 
+    //clear the comment input fields after clicking Comment button
     $(this).siblings('.comment-content').val('');
     $(this).siblings('.comment-author').val('');
 
   })
+  //clear the post input fields after clicking Post button
   $('#message').val('');
   $('#name').val('');
 })
 
 
-  //use css and/or bootstrap to..
-  // style the page (border btw posts),
-  
-  // change input field heights/ widths and margins
-  
-  // make glyph icon blue  
-
-  //clear all the text input fields after clicking post or comment
 
 
-
-
-    /* postTemplate = 
-  <div class="post-container">
-    <a class="comments-link"><span>Comments   </span></a> 
-    <a class="remove-link"><span>Remove</span></a> 
-    <p class="post-content">' + postContent + '</p>
-    <form class="form-class" style="margin-top:30px;" onsubmit="event.preventDefault();">
-      <input id="" type="text" class="form-control short-input comment-content" placeholder="Comment Text"></input>
-      <input id="" type="text" class="form-control short-input comment-author" placeholder="User Name">
-      <button class="btn btn-primary comment-button">Post Comment</button></form>
-    <p class="post-author">Posted by: <strong>' + postAuthor + '</strong></p>
-  </div>';
-  */
-
- 
-  // var $post = $(postTemplate);
-  // $post.find('.remove-link').click(function() {
-  //   $(this).parentNode.remove();
-  // })
-
-
-  // $('.remove-link').click(function() {
-  //   $(this).parent().remove();
-  // })  
-
-
-  
-
-  // $('.comments-link').click(function() {
-  //   //$(this).siblings('.form-class').toggle();
-  //  // $(this).nextAll('.form-class').toggle();
-
-  //   if ($(this).nextAll('.form-class').is(':hidden')) {
-  //     $(this).nextAll('.form-class').show();
-  //   } else {
-  //     $(this).nextAll('.form-class').hide();
-  //   }
-  // })
-
- 
-
-
-
-
-
-// $('#submit').click();
 
 
 
