0e650445cc7a6bfcbdbaf1cd7e54da6b81a8c815
bclewis54 <lewisbradleychanning@gmail.com>
2020-05-10 22:34:34 -0400

    Rendering not functioning. Trying approach without array.

--- main.js
+++ main.js
@@ -1,36 +1,23 @@
-//Create array to maintain userPosts
-var $userPosts = []
-
-var show$userPostsAndComments = function() {
-  for (postIndex = 0; postIndex < $userPosts.length; postIndex++) {
-    //publish the post userName and userMessage to the html
-    // TODO: add a FUNCTIONAL remove button
-    $('.userPosts').append('<p>' + userMessageInput + '</p>' + '<p class="userName">' + 'Posted by: ' + userNameInput + '</p>'
-    + '<button class="delete">' + '&#10007' + '</button>');
-    }
-  }
-
 //A post including userMessage and userName will need to be submitted when the button is clicked
-//Edge cases for if userName or userMessage are empty will alert
-if ($('#postMessage').val() === 0 || $('#postName').val() === 0) {
-  alert('You must enter your name and a message to post.')
-} else {
-  //Save the name and message to the $userPosts array
+$('#submit').click(function () {
   var userMessageInput = $('#postMessage').val();
   var userNameInput = $('#postName').val();
-  //push the userMessage, userName, and a comment area(to be filled later) into $userPosts
-  $userPosts.push({
-    message: userMessageInput,
-    name: userNameInput,
-    comments: []
-  })
+  //Edge cases for if userName or userMessage are empty will alert
+  if (userMessageInput.length === 0 || userNameInput.length === 0) {
+    alert('You must enter your name and a message to post.')
+  } else {
+    //Post the name and message to the website
+    // TODO: add a remove button
+    $('.userPosts').append('<p>' + userMessageInput + '</p>' +
+    '<p class="userName">' + 'Posted by: ' + userNameInput + '</p>' +
+    '<button class="delete">' + '&#10007' + '</button>');
+
     //add a (TODO: UNIQUE) comment section for each post
-    // $('.userName').append('<div class="postComments">' +
-    // '<input id="commentMessage" type="text" class="form-control comment" placeholder="Comment">' + '</input>' +
-    // '<input id="commentName" type="text" class="form-control comment" placeholder="Name">' + '</input>' + '</div>' +
-    // '<button id="comment" class="btn btn-primary">' + 'Comment' + '</button>')
-}
-show$userPostsAndComments();
+    $('.userName').append('<div class="postComments">' +
+    '<input id="commentMessage" type="text" class="form-control comment" placeholder="Comment">' + '</input>' +
+    '<input id="commentName" type="text" class="form-control comment" placeholder="Name">' + '</input>' + '</div>' +
+    '<button id="comment" class="btn btn-primary">' + 'Comment' + '</button>')
+}});
 
 //A (TODO: unique) comment section with commentorName and commentorMessage will need to be appended to each post
 $('.userPosts').on('click', '#comment', (function () {
@@ -39,15 +26,7 @@ $('.userPosts').on('click', '#comment', (function () {
   if (commentMessage.length === 0 || commentName.length === 0) {
     alert('You must enter your name and a message to post a comment.')
   } else {
-  $('.postComments').append('<p>' + commentMessage + '</p>' + '<p>' + 'Posted By: ' + commentName + '</p>');
+  $('.postComments').append('<p>' + commentMessage + '</p>'
+  + '<p>' + 'Posted By: ' + commentName + '</p>');
   //push the commentName and commentMessage into $userPosts at the postIndex to match comment with post
-  // $userPosts[postIndex].push(commentMessage, commentName);
 }}));
-
-
-//A user should be able to remove the post
-// $('.delete').click(function() {
-//   $(this).closest('.userPosts').remove();
-// });
-
-//A user should be able to hide/show comments
