60192215552204626bc8e71213d714b396ee9e6d
Christina Kurpiel <kurpielcm@gmail.com>
2020-05-10 22:03:37 -0400

    add post, comment, and remove functions to comments section

--- main.js
+++ main.js
@@ -13,48 +13,47 @@ $('#post-btn').on('click', function () {
   var commentForm = commentInput + commentName + commentPost + '<p class="comments-here"></p>';
 
 
-    //if the user post and name input are not empty, add them to the list
-      if (post !== "" && name !== "") {
-        $('.list-group').append(template);
-      } else {
-        //if the post and name input are empty, alert the user
-        alert('Please input your name and post content.');
-      };
+  // if the post and name fields are filled, add the post, poster's name, and the delete/comment buttons to the forum
+    if (mainPost !== "" && mainName !== "") {
+      $('.post-item').append('<div class="parent">' +deleteButton +
+      commentButton + mainPost + '<p> '+commentForm+' </p><p>Posted by:<strong> ' +mainName+ '</strong></p</div>');
+    } else {
+      //if the post and name input are empty, alert the user
+      alert('Please input your name and post content.');
+    };
 
-      //removes the parent post when clicked
-    var removePost = $('.remove').on('click', function () {
-      $(this).parent().remove();
+
+
+  //removes the post from the forum
+  $('.remove-button').on('click', function () {
+    $(this).parent().remove();
+  });
+
+  //toggles the comments section when clicked
+  $('.comment-button').unbind().on('click', function () {
+    $(this).next().toggle();
+  });
+
+
+  // posts comment and user associated with comment to the post when "post comment is clicked"
+  $('.comment-post').unbind().on('click', function () {
+    var postedComment = $('.comment-input').val();
+    var postedCommentName = $('.comment-name').val();
+    var closeButton = '<span class="glyphicon glyphicon-remove" style="color:#337ab7"></span>'
+
+    if (postedComment !== "" && postedCommentName !== "") {
+      $(this).parent().append('<p class="posted-content">'+postedComment + ' | Posted By: <strong>'
+      + postedCommentName +'</strong>' +  closeButton +'</p>');
+    } else {
+      alert('Please fill out comment and name forms before posting.')
+    };
+
+
+
+    //removes comments posted to main post
+  $('.glyphicon-remove').unbind().on('click', function () {
+    $(this).parent().remove();
+
     });
-
-
-
-    var addComment = $('.comment').unbind().on('click', function () {
-
-      $(this).next().toggle();
-      });
-
-
-
-var postComment = $('#comment-button').on('click', function () {
-
-      var comment = $('#comment-text').val();
-      var commentName = $('#name-text').val();
-      if (comment !== "" && name !== "") {
-        $('.comment-section').append(comment + ' Posted by:' +commentName + '<br></br>');
-      } else {
-        alert('Please fill out comment and name forms before posting.')
-      }
-    });
-    });
-
-
-
-
-
-
-
-
-
-
-// var post = $('#post-board').html();
-// console.log(post);
+  });
+});
