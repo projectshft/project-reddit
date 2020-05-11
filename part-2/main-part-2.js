96f085e02714207edebd8fe02ce493d08a2b7b6c
Joe Krall <kralljr@outlook.com>
2020-05-10 21:25:17 -0400

    last version before attempting a rewrite with div

--- part-2/main-part-2.js
+++ part-2/main-part-2.js
@@ -8,24 +8,27 @@ $(postButton).on('click', function () {
   //This 'form' gets me the right button but is not being created dynamically.
   var forSubmittingComments = '<form class="form-inline comments" onsubmit="event.preventDefault();" style="display:none;"><input id="message-comment" type="text" class="form-control" placeholder="Comment Text"></input><input id="name-comment" type="text" class="form-control" placeholder="User Name"></input><button id="submit-comment" class="btn btn-primary">Post Comment</button></form>';
 
-  var postHTML = $('#message').val() + '<p id="comment-1">' + forSubmittingComments + 'Posted by: <strong> ' + $('#name').val() + '</strong><br><hr>';
+  var postHTML = $('#message').val() + forSubmittingComments + 'Posted by: <strong> ' + $('#name').val() + '</strong><br><hr>';
 
   // Trying to append the form stuff dynamically
-  $(function() {
-     $('#comment-1').append(
-         $('<form />', { class: 'form-inline comments', onsubmit: 'event.preventDefault();', style:'display:none;'}).append(
-             $('<input />', { id: 'message-comment', type: 'text', class:'form-control', placeholder:'Comment Text'}),
-         )
-     )
- })
+  // var submitComment = $('<form />',
+  //   {
+  //     class: 'form-inline comments',
+  //     onsubmit: 'event.preventDefault();',
+  //     style:'display:none;'
+  //
+  //   });
+  //
+  // var postHTML = $('<span id="commentForm"' + basicComment).find('#comment-form').children().append(submitComment).end();
 
-  var postComments = $('<a/>',
+  
+  var commentButton = $('<a/>',
     {
         text: 'comment ',
         click: function () { $(".comments").toggle(); }
     });
 
-    var post = $('<li><p id="post-1">' + postHTML).find('#post-1').prepend(postComments).end();
+    var post = $('<li><p id="post-1">' + postHTML).find('#post-1').prepend(commentButton).end();
 
   // This is where using allPosts might come in handy. We create a separate ID above using a for loop,
   // and then prepend in a loop down here. The ID would be for matching dropdowns.
