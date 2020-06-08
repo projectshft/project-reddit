7f012ca16bcdf447afb0a7a9bd1d100e6b01958c
Joe Krall <kralljr@outlook.com>
2020-06-07 21:33:58 -0400

    created postObject and allPosts array with for loop

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 var postButton = $('#submit-post');
 
+var postCount = 0;
 var allPosts = [];
 
 $(postButton).on('click', function () {
+
+  $('.post-list').empty();
+
+  var postMessage = $('#message').val();
+  var postName = $('#name').val();
+  postCount += 1;
+
+  var postObject = {
+    message: postMessage,
+    name: postName,
+    id: postCount
+  }
+
+  allPosts.push(postObject);
 
   var removeButton = '<a href="#" id="remove-button">remove </a>';
   var commentButton = '<a href="#" id="comment-button">comment </a>';
   var commentForm = '<form class="form-inline comments" onsubmit="event.preventDefault();" style="display:none;"><input id="message-comment" type="text" class="form-control" placeholder="Comment Text"></input><input id="name-comment" type="text" class="form-control" placeholder="User Name"></input><button id="submit-comment" class="btn btn-primary">Post Comment</button></form>';
 
-  var postMessage = $('#message').val();
-  var postName = $('#name').val();
-
   // This is where using allPosts might come in handy. We create a separate ID above using a for loop,
   // and then prepend in a loop down here. The ID woul+ d be for matching dropdowns.
-  $('.post-list').append('<li><p id="post-1">' + '<p>' + removeButton + commentButton + postMessage + '</p>' + '<ul class="comment-list"><ul>' + commentForm + '<p>Posted by: <strong>' + postName + '</strong></p><hr></li>');
+  for (i = 0; i < allPosts.length; i++) {
+    $('.post-list').append('<li><p id="post-"' + allPosts[i].id + ' >' + '<p>' + removeButton + commentButton + allPosts[i].message + '</p>' + '<ul class="comment-list"><ul>' + commentForm + '<p>Posted by: <strong>' + allPosts[i].name + '</strong></p><hr></li>');
+  }
 
   var commentButton = $('#comment-button');
 
