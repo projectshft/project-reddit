0bc1324ee7f9bfd447ed24f9b7068102859aaf36
Joe Krall <kralljr@outlook.com>
2020-06-14 21:54:09 -0400

    commenting on each individual post working

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var postButton = $('#submit-post');
 var allPosts = [];
+var postIndex = 0;
 
 // Creating Post objects
 var PostModule = function(message, name, id) {
@@ -10,11 +11,23 @@ var PostModule = function(message, name, id) {
     listElementId: null,
     removeButton: null,
     commentButton: null,
-    submitComment: null
+    submitComment: null,
+    allComments: []
   }
 }
 
-var postIndex = 0;
+// Creating Comment objects
+var CommentModule = function(message, name, id) {
+  return {
+    message: message,
+    name: name,
+    id: id,
+    listElementId: null,
+    removeButton: null,
+    commentIndex: 0
+  }
+}
+
 
 $(postButton).on('click', function() {
 
@@ -29,37 +42,63 @@ $(postButton).on('click', function() {
 
   $('.post-list').empty();
 
-  allPosts.forEach (function (post) {
+  allPosts.forEach(function(post) {
 
     $('.post-list').append('<li id="' + post.id + '" class="post">' + '<p>' + removeButtonHTML + commentButtonHTML + post.message + '</p>' + commentFormHTML + '<p>Posted by: <strong>' + post.name + '</strong></p><hr></li>');
 
     //
     post.listElementId = '#' + post.id;
 
+    // Dynamically creates commentButton based on listElementId
     post.commentButton = $(post.listElementId).find('.comment-button');
+    // Toggles .comments (child of selected post)
     $(post.commentButton).on('click', function() {
       $(post.listElementId).find(".comments").toggle();
     });
 
+    // Dynamically creates removeButton based on listElementId
     post.removeButton = $(post.listElementId).find('.remove-button');
+    // On click, removes post from DOM and the selected post from allPosts
     $(post.removeButton).on('click', function() {
       $(post.listElementId).remove();
       allPosts = allPosts.filter(function(item) {
-          if (item.id !== post.id) {
-            return item
-          }
-        })
+        if (item.id !== post.id) {
+          return item
+        }
+      })
     });
 
     post.submitComment = $(post.listElementId).find('#submit-comment');
     $(post.submitComment).on('click', function() {
-      // $('.comment-list').empty();
-      var commentMessage = $('.comment-message').val();
-      var commentName = $('.comment-name').val();
 
-      $(post.listElementId).find('.comment-list').append('<li><p id="comment">' + '<p>' + commentMessage + ' Posted by: <strong>' + commentName + '</strong></p></li>')
+      var commentMessage = $(post.listElementId).find('.comment-message').val();
+      var commentName = $(post.listElementId).find('.comment-name').val();
+      post.allComments.push(CommentModule(commentMessage, commentName, post.commentIndex));
+      post.commentIndex++;
+
+      // Emptying before for loop
+      $(post.listElementId).find('.comment-list').empty();
+
+      post.allComments.forEach(function(comment) {
+        $(post.listElementId).find('.comment-list').append('<li id="' + comment.id + '" class="comment">' + '<p>' + comment.message + ' Posted by: <strong>' + comment.name + ' </strong><i class="fa fa-times"></i></p></li>');
+
+        comment.listElementId = '#' + comment.id;
+
+        comment.removeButton = $(comment.listElementId).find('.fa-times');
+        // On click, removes post from DOM and the selected post from allPosts
+        $(comment.removeButton).on('click', function() {
+          $(comment.listElementId).remove();
+          post.allComments = post.allComments.filter(function(item) {
+            if (item.id !== comment.id) {
+              return item;
+            }
+          })
+        });
+
+      });
+
     });
 
-  });
+  })
 
-});
+})
