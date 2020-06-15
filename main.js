b35dc31d3ca0b1823a867a1f11791642384cd95e
Joe Krall <kralljr@outlook.com>
2020-06-15 08:42:37 -0400

    commented out the submit comment button. Rewrote delete

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var PostModule = function(message, name, id) {
     message: message,
     name: name,
     id: id,
-    listElementId: null,
+    postListElementId: null,
     removeButton: null,
     commentButton: null,
     submitComment: null,
@@ -24,8 +24,8 @@ var CommentModule = function(message, name, id) {
     message: message,
     name: name,
     id: id,
-    listElementId: null,
-    removeButton: null,
+    commentListElementId: null,
+    deleteComment: null,
   }
 }
 
@@ -39,65 +39,68 @@ $(postButton).on('click', function() {
 
   var removeButtonHTML = '<a href="#" class="remove-button">remove </a>';
   var commentButtonHTML = '<a href="#" class="comment-button">comment </a>';
-  var commentFormHTML = '<form class="form-inline comments" onsubmit="event.preventDefault();" style="display:none;">' + '<ul class="comment-list"></ul>' + '<input type="text" class="form-control comment-message" placeholder="Comment Text"></input>' + '<input type="text" class="form-control comment-name" placeholder="User Name"></input>' + '<button id="submit-comment" class="btn btn-primary">Post Comment</button>' + '</form>';
+  var commentListHTML = '<ul class="comment-list"></ul>';
+  var commentFormHTML = '<form class="form-inline comments" onsubmit="event.preventDefault();" style="display:none;">' + '<input type="text" class="form-control comment-message" placeholder="Comment Text"></input>' + '<input type="text" class="form-control comment-name" placeholder="User Name"></input>' + '<button id="submit-comment" class="btn btn-primary">Post Comment</button>' + '</form>';
 
   $('.post-list').empty();
 
   allPosts.forEach(function(post) {
 
-    $('.post-list').append('<li id="' + post.id + '" class="post">' + '<p>' + removeButtonHTML + commentButtonHTML + post.message + '</p>' + commentFormHTML + '<p>Posted by: <strong>' + post.name + '</strong></p><hr></li>');
+    $('.post-list').append('<li id="' + post.id + '" class="post">' + removeButtonHTML + commentButtonHTML + post.message  + commentListHTML + commentFormHTML + 'Posted by: <strong>' + post.name + '</strong><hr></li>');
 
     //
-    post.listElementId = '#' + post.id;
+    post.postListElementId = '#' + post.id;
 
     // Dynamically creates commentButton based on listElementId
-    post.commentButton = $(post.listElementId).find('.comment-button');
+    post.commentButton = $(post.postListElementId).find('.comment-button');
     // Toggles .comments (child of selected post)
     $(post.commentButton).on('click', function() {
-      $(post.listElementId).find(".comments").toggle();
+      $(post.postListElementId).find(".comments").toggle();
     });
 
     // Dynamically creates removeButton based on listElementId
-    post.removeButton = $(post.listElementId).find('.remove-button');
+    post.removeButton = $(post.postListElementId).find('.remove-button');
     // On click, removes post from DOM and the selected post from allPosts
     $(post.removeButton).on('click', function() {
-      $(post.listElementId).remove();
+
       allPosts = allPosts.filter(function(item) {
         if (item.id !== post.id) {
           return item
         }
       })
+
+      $('.post-list').find(post.postListElementId).remove();
     });
 
-    post.submitComment = $(post.listElementId).find('#submit-comment');
-    $(post.submitComment).on('click', function() {
-
-      var commentMessage = $(post.listElementId).find('.comment-message').val();
-      var commentName = $(post.listElementId).find('.comment-name').val();
-      post.allComments.push(CommentModule(commentMessage, commentName, post.commentIndex)); // 0
-      post.commentIndex++;
-
-      // Emptying before for loop
-      $(post.listElementId).find('.comment-list').empty();
-
-      post.allComments.forEach(function(comment) {
-        $(post.listElementId).find('.comment-list').append('<li id="' + comment.id + '" class="comment">' + comment.message + ' Posted by: <strong>' + comment.name + ' </strong><i class="fa fa-times remove-comment-button"></i></li>');
-
-        comment.listElementId = '#' + comment.id;
-
-        comment.removeButton = $(comment.listElementId).find('.remove-comment-button');
-        // On click, removes post from DOM and the selected post from allPosts
-        $(comment.removeButton).on('click', function() {
-          $(comment.listElementId).remove();
-          post.allComments = post.allComments.filter(function(item) {
-            if (item.id !== comment.id) {
-              return item
-            }
-          })
-        });
-      });
-
-    });
+    // post.submitComment = $(post.postListElementId).find('#submit-comment');
+    // $(post.submitComment).on('click', function() {
+    //
+    //   var commentMessage = $(post.postListElementId).find('.comment-message').val();
+    //   var commentName = $(post.postListElementId).find('.comment-name').val();
+    //   post.allComments.push(CommentModule(commentMessage, commentName, post.commentIndex)); // 0
+    //   post.commentIndex++;
+    //
+    //   // Emptying before for loop
+    //   $(post.postListElementId).find('.comment-list').empty();
+    //
+    //   post.allComments.forEach(function(comment) {
+    //     $(post.postListElementId).find('.comment-list').append('<li id="' + comment.id + '" class="comment">' + comment.message + ' Posted by: <strong>' + comment.name + ' </strong><i class="fa fa-times remove-comment-button"></i></li>');
+    //
+    //     comment.commentListElementId = '#' + comment.id;
+    //
+    //     comment.deleteComment = $(comment.commentListElementId).find('.remove-comment-button');
+    //     // On click, removes post from DOM and the selected post from allPosts
+    //     $(comment.deleteComment).on('click', function() {
+    //       $(comment.commentListElementId).remove();
+    //       post.allComments = post.allComments.filter(function(item) {
+    //         if (item.id !== comment.id) {
+    //           return item
+    //         }
+    //       })
+    //     });
+    //   });
+    //
+    // });
 
   })
 
