dfe2d314f7063f63e4b903c7b551bffc99fe34e1
Joe Krall <kralljr@outlook.com>
2020-06-15 22:48:09 -0400

    added edge cases, more comments

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-
 var allPosts = [];
 var postIndex = 0;
 
-// HTMl reused consistently
+// HTMl for individual buttons and forms
+// Created for readability
 var removeButtonHTML = '<a href="#" class="remove-button">remove </a>';
 var commentButtonHTML = '<a href="#" class="comment-button">comment </a>';
 var commentListHTML = '<ul class="comment-list"></ul>';
@@ -24,18 +24,19 @@ var PostModule = function(message, name, id) {
 }
 
 // Creating Comment objects
-var CommentModule = function(message, name, id) {
+var CommentModule = function(message, name, postId, commentId) {
   return {
     message: message,
     name: name,
-    id: "comment" + id,
-    commentListElementId: "#comment" + id,
+    id: "post-" + postId + "-comment-" + commentId,
+    commentListElementId: "#post-" + postId + "-comment-" + commentId,
     deleteComment: null,
   }
 }
 
-var renderPosts = function (post) {
-  $('.post-list').append('<li id="' + post.id + '" class="post">' + removeButtonHTML + commentButtonHTML + post.message  + commentListHTML + commentFormHTML + 'Posted by: <strong>' + post.name + '</strong><hr></li>');
+// Appends all posts in allPosts to the DOM
+var renderPosts = function(post) {
+  $('.post-list').append('<li id="' + post.id + '" class="post">' + removeButtonHTML + commentButtonHTML + post.message + commentListHTML + commentFormHTML + 'Posted by: <strong>' + post.name + '</strong><hr></li>');
 
   // Dynamically creates buttons based on listElementId
   renderRemoveButton(post);
@@ -44,8 +45,9 @@ var renderPosts = function (post) {
   renderSubmitCommentButton(post);
 }
 
-//
-var renderRemoveButton = function (post) {
+// On click this button removes selected post from the DOM
+// and filters it from allPosts
+var renderRemoveButton = function(post) {
 
   post.removeButton = $(post.postListElementId).find('.remove-button');
   $(post.removeButton).on('click', function() {
@@ -57,29 +59,29 @@ var renderRemoveButton = function (post) {
     })
 
     $('.post-list').find(post.postListElementId).remove();
-    console.log("Post" + post.postListElementId + " was clicked on remove")
   });
 }
 
-var renderCommentButton = function (post) {
+// On click, this button toggles the comment form
+var renderCommentButton = function(post) {
   post.commentButton = $(post.postListElementId).find('.comment-button');
-  // Toggles .comments (child of selected post)
+
   $(post.commentButton).on('click', function() {
     $(post.postListElementId).find(".comments").toggle();
-    console.log("Post" + post.postListElementId + " was clicked on toggle");
   });
 }
 
-
-var renderComments = function (post) {
+// Appends all comments in the argument's allComments array
+// to the DOM
+var renderComments = function(post) {
 
   post.allComments.forEach(function(comment) {
     $(post.postListElementId).find('.comment-list').append('<li id="' + comment.id + '" class="comment">' + comment.message + ' Posted by: <strong>' + comment.name + ' </strong><i class="fa fa-times remove-comment-button"></i></li>');
 
-    comment.deleteComment =  $(post.postListElementId).find('.comment-list').find(comment.commentListElementId).find('.remove-comment-button');
+    comment.deleteComment = $(post.postListElementId).find('.comment-list').find(comment.commentListElementId).find('.remove-comment-button');
+
     // On click, removes post from DOM and the selected post from allPosts
     $(comment.deleteComment).on('click', function() {
-
       post.allComments = post.allComments.filter(function(item) {
         if (item.id !== comment.id) {
           return item
@@ -97,62 +99,52 @@ var renderComments = function (post) {
 // based on the posts commentIndex. Then, re-renders
 // comments for that post only.
 var renderSubmitCommentButton = function(post) {
+
   post.submitComment = $(post.postListElementId).find('#submit-comment');
   $(post.submitComment).on('click', function() {
 
-    var commentMessage = $(post.postListElementId).find('.comment-message').val();
-    var commentName = $(post.postListElementId).find('.comment-name').val();
-    post.allComments.push(CommentModule(commentMessage, commentName, post.commentIndex)); // 0
-    post.commentIndex++;
+      var commentMessage = $(post.postListElementId).find('.comment-message').val();
+      var commentName = $(post.postListElementId).find('.comment-name').val();
 
-    $(post.postListElementId).find('.comment-list').empty();
-    renderComments(post);
+      // User must fill in all fields
+      if (commentMessage === "" || commentName === "") {
+        alert("Please enter all fields")
+      } else {
+        // creates unique comment id based on the original post's id and the post's commentIndex
+        post.allComments.push(CommentModule(commentMessage, commentName, post.id, post.commentIndex));
+        post.commentIndex++;
 
-  });
+        $(post.postListElementId).find('.comment-list').empty();
+        renderComments(post);
+
+      });
+  }
 }
-
 
 var postButton = $('#submit-post');
 // On click, create new post with user entry
-// add post to allPosts with unique id based on
-// postIndex, increase post index, then empty
-// .post-list and rerendr DOM.
 $(postButton).on('click', function() {
 
   var postMessage = $('#message').val();
   var postName = $('#name').val();
-  var newPost = PostModule(postMessage, postName, postIndex);
-  allPosts.push(newPost);
-  postIndex++;
 
-  $('.post-list').empty();
+  // User must fill in all fields
+  if (postMessage === "" || postName === "") {
+    alert("please enter all fields")
+  } else {
 
-  allPosts.forEach(function(post) {
-    renderPosts(post);
-  });
+    // add post to allPosts with unique id based on
+    // postIndex, increase post index,
+    var newPost = PostModule(postMessage, postName, postIndex);
+    allPosts.push(newPost);
+    postIndex++;
+
+    // then empty .post-list and rerender DOM.
+    $('.post-list').empty();
+
+    allPosts.forEach(function(post) {
+      renderPosts(post);
+    });
+  }
 
 });
-
-
-
-//
-// Weird bug...
-// If the first existing post has more comments, we can't get toggle to work
-// and it takes two clicks to delete the post wiht fewer commetns
-//
-// It's like that extra comment throws off the comment toggle and the remove button
-// Clicks are being registered though
-//
-//
-// And if I delete the number of comments on the first post, such that
-// it is less than or equal to the number of comments on the post below it,
-// it works
-//
-//
-// I think the way I'm doing it, I have to refresh the whole page
-//
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// The delete button HIDES the first comment on the first postName
-// the second time deletes the post it was clicked on.
-//
-// Something's not being dynamically created when the second comment is made
