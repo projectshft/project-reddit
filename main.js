8285f5da1aa270b06d7910cc4357e2b08c734390
Aissa Feldmann <aissaf@umich.edu>
2020-08-21 12:13:51 -0400

    commented out js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-let button = document.getElementsByTagName("button")[0];
+// let button = document.getElementsByTagName("button")[0];
 
-button.addEventListener("click", function () {
-  let userName = document.getElementById("name").value;
-  let userPost = document.getElementById("message").value;
+// button.addEventListener("click", function () {
+//   let userName = document.getElementById("name").value;
+//   let userPost = document.getElementById("message").value;
 
-  let userNameAndPost = userName.concat(": " + userPost);
+//   let userNameAndPost = userName.concat(": " + userPost);
 
-  let userPostElement = document.createTextNode(userNameAndPost);
-  let li = document.createElement("li");
-  li.appendChild(userPostElement);
+//   let userPostElement = document.createTextNode(userNameAndPost);
+//   let li = document.createElement("li");
+//   li.appendChild(userPostElement);
 
-  document.getElementById("posts").append(li);
-});
+//   document.getElementById("posts").append(li);
+// });
