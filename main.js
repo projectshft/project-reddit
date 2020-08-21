82937bf9b19d155c3d4fe60c8be2348989c3af80
Aissa Feldmann <aissaf@umich.edu>
2020-08-21 12:12:10 -0400

    basic form

---
+++ main.js
@@ -0,0 +1,14 @@
+let button = document.getElementsByTagName("button")[0];
+
+button.addEventListener("click", function () {
+  let userName = document.getElementById("name").value;
+  let userPost = document.getElementById("message").value;
+
+  let userNameAndPost = userName.concat(": " + userPost);
+
+  let userPostElement = document.createTextNode(userNameAndPost);
+  let li = document.createElement("li");
+  li.appendChild(userPostElement);
+
+  document.getElementById("posts").append(li);
+});
