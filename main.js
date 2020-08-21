b39833fb79e3e395c8c0de8ec7b93ac2dd206138
PJ Foley <pjfoley@gmail.com>
2020-08-21 13:32:15 -0700

    Part 1 functional. Code not cleaned

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 
 
-let deletePost;
-let tweetCount = 0;
-var button = document.getElementById('submit')[0];
-submit.addEventListener('click', function () {   // maybe onsubmit
-  var userName = document.getElementById('name').value;  // needs [0]?
+var deletePost;
+var tweetCount = 0;  // check if we need this still
+// var button = document.getElementById('submit')[0];  
+var $postButton = $('#submit');  // needed?
+
+//submit.addEventListener('click', function () {   // maybe onsubmit
+var addNewPostEntry = function () {
+  var $postText = $('#message').val();
+  console.log($postText);
+  var $postersName = $('#name').val();
+  console.log($postersName);
+  $('.posts').append('<p>' + $postText + '</p>').addClass('post-entry');
+  $('.posts').append('<div class="comments">test</div>');  // find me by nearest?
+  $('.posts').append('Posted By: <b>' + $postersName + '</b><hr>');
+};  
+/*   var userName = document.getElementById('name').value;  // needs [0]?
   var userTextNode = document.createTextNode(userName);
   var userElementP = document.createElement('p');
   userElementP.appendChild(userTextNode);
@@ -25,14 +36,16 @@ submit.addEventListener('click', function () {   // maybe onsubmit
   document.getElementsByClassName('tweet')[tweetCount].append(delBtnElementBtn);
   deletePost = document.getElementsByClassName('deleter')[tweetCount];
   makeEvents();
-  tweetCount++;
-});
-var makeEvents = function() {
+  tweetCount++; */
+
+/* var makeEvents = function() {
 deletePost.addEventListener('click', function () {
 console.log('working');
 document.getElementsByClassName('tweet')[0].remove();
 document.getElementsByClassName('user-name')[0].remove();
 //document.getElementsByClassName('deleter')[0].remove();  // surprise! it deletes itself
 });
-}
+} */
 
+$('#submit').click(addNewPostEntry);
+ 
