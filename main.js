7335317e5087721c8a748094183cc30f2bef88cf
Aissa Feldmann <aissaf@umich.edu>
2020-08-21 17:38:14 -0400

    changed table to ol

--- main.js
+++ main.js
@@ -12,3 +12,21 @@
 
 //   document.getElementById("posts").append(li);
 // });
+let arrayOfPostsAndNames = [];
+
+$('button').click(function () {
+  let userPost = $('#post-text').val();
+  let userName = $('#post-name').val();
+  let userPostAndName = {
+    post: userPost,
+    name: userName,
+  };
+  arrayOfPostsAndNames.push(userPostAndName);
+
+  // $('.candy').append('<strong>Chocolate</strong>'); //how to add to innerHTML of a class
+
+  // <li data-id="5">cigar</li>  
+  // $('li').data().id; // returns {id: 5}
+  
+});
+
