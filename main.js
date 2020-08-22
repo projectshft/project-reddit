c043a97ef365ecc5f89deba251d709a1198b831d
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-08-22 13:23:57 -0400

    adds comment functionality

--- main.js
+++ main.js
@@ -7,8 +7,25 @@ $('button').click( function() {
         alert('You need to enter a name and message before posting')
     }
     else {
-    $('.posts').append('<p>' + messageInput + '</p>')
+    $('.posts').append('<button id="remove" class="btn">remove</button>' + '<button id="comment" class="btn">comment</button>' + '<p>' + messageInput + '</p>')
     $('.posts').append('<p>' + 'Posted By: <b>' + nameInput + '</b></p>')}
     //once text is posted, resets the default value of the form
     $('form').find('input').val('');
+})
+// the below adds functionality that when comment is clicked, a box for a comment and username pops up as well as a post comment button
+$('.posts').on( 'click', '#comment', function (){
+    $('.posts').append('<input id="comment-text" type="text" class="form-control" placeholder="Comment Text"></input>' + 
+    '<input id="name" type="text" class="form-control" placeholder="User Name"></input>' + 
+    '<button id="submit-comment" class="btn btn-primary">Post Comment</button>')
+})
+// when post comment button is clicked, comment text and username text appears
+$('.posts').on( 'click', '#submit-comment', function (){
+    console.log('hi');
+} ) 
+
+
+
+$('.posts').on('click', '#remove', function () {
+ //$(this).remove($('.posts.children'));
+ console.log('hi');
 })
