3f8d28d4e3e55da171baf42ff5182de9bbc39180
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-08-22 09:32:13 -0400

    adds an edge case to prevent blank posts

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 $('button').click( function() {
     let nameInput = $('#name').val();
     let messageInput = $('#message').val();
+    //checks to make sure nameInput and messageInput have a value so user can't blank post
+    if (nameInput == '' || messageInput == '') {
+        alert('You need to enter a name and message before posting')
+    }
+    else {
     $('.posts').append('<p>' + messageInput + '</p>')
-    $('.posts').append('<p>' + 'Posted By: <b>' + nameInput + '</b></p>')
+    $('.posts').append('<p>' + 'Posted By: <b>' + nameInput + '</b></p>')}
 })
