e3c15b114152024b10f9862dc28fec0954ee2fd5
unknown <joshkamrass@gmail.com>
2021-03-25 10:42:43 -0400

    adds remove

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-//Add post to posts section
+
 var posts = [];
+var $postButton = $('.submit-post');
+var $textInput = $('.post-text');
+var $authorInput = $('.post-author');
+var $posts = $('.posts');
+
+//Add post to posts array
 var addPost = function (author, content) {
   var postObj = {
     author: author,
@@ -7,18 +13,25 @@ var addPost = function (author, content) {
   }
   posts.push(postObj);
 }
-var $postButton = $('.submit-post');
-var $textInput = $('.post-text');
-var $authorInput = $('.post-author');
+
+//renders posts array to page
+var renderPosts = function () {
+  var postsHTML = posts.reduce(function (htmlString, post, index) {
+    htmlString += '<div class="post" data-post="' + index.toString() + '"><p><span role="button" tabindex="0" class="remove-button">remove</span> ' + post.content + ' - Posted By: ' + post.author + '</p><hr></div>';
+    return htmlString;
+  }, '<hr>')
+  $posts.html(postsHTML);
+}
+
+//Updates page with new post
 $postButton.on('click', function() {
   addPost($authorInput.val(), $textInput.val());
   renderPosts();
 })
 
-var renderPosts = function () {
-  var postsHTML = posts.reduce(function (htmlString, post) {
-    htmlString += '<p>' + post.content + ' - Posted By: ' + post.author + '</p><hr>';
-    return htmlString;
-  }, '<hr>')
-  $('.posts').html(postsHTML);
-}
+//Removes Post
+$posts.on('click', 'span', function(e) {
+  var postNumber = $(e.target).closest('.post').data().post;
+  posts.splice(postNumber,1);
+  renderPosts();
+})
