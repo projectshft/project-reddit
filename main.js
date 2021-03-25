70540f773d5393eeb87abc19c9f096ea6531211a
unknown <joshkamrass@gmail.com>
2021-03-25 09:14:06 -0400

    completes part 1 functionality

---
+++ main.js
@@ -0,0 +1,24 @@
+//Add post to posts section
+var posts = [];
+var addPost = function (author, content) {
+  var postObj = {
+    author: author,
+    content: content
+  }
+  posts.push(postObj);
+}
+var $postButton = $('.submit-post');
+var $textInput = $('.post-text');
+var $authorInput = $('.post-author');
+$postButton.on('click', function() {
+  addPost($authorInput.val(), $textInput.val());
+  renderPosts();
+})
+
+var renderPosts = function () {
+  var postsHTML = posts.reduce(function (htmlString, post) {
+    htmlString += '<p>' + post.content + ' - Posted By: ' + post.author + '</p>';
+    return htmlString;
+  }, '')
+  $('.posts').html(postsHTML);
+}
