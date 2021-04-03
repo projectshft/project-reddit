f8ff3f1d011864f066288a97d98cef680569b4d6
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-04-03 11:31:56 -0400

    Added comments section

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 $( document ).ready(function() {
 
+  // Create new post //
   $("#submit").on("click", function() {
     // Collect input from user //
     var $name = $('#name').val();
     var $message = $('#message').val();
 
     // Create new div for post //
-    var $newPostDiv = $("<div></div>");
+    var $newPostDiv = $('<div class="newPostDiv"></div>');
     var $newPost = $('<p></p>');
     var $hr = $('<hr></hr>');
+    var $editDiv = $('<div class="editDiv"></div>');
+    var $editDivComments = $('<span class="comments"></span>');
+    var $editDivDelete = $('<span class="delete"></span>');
+    var $commentsDiv = $('<div class=comments-div></div>');
     
+    $editDivComments.text('Comments ');
+    $editDivDelete.text('Delete');
     $newPost.text($message + ' - Posted By: ' + $name);
+
+    $editDiv.append($editDivComments);
+    $editDiv.append($editDivDelete);
     $newPostDiv.append($newPost);
+    $newPostDiv.append($editDiv);
+    createCommentsSection($commentsDiv);
+    $newPostDiv.append($commentsDiv);
     $newPostDiv.append($hr);
 
     // Add post //
@@ -20,6 +33,40 @@ $( document ).ready(function() {
 
     // Reset input fields //
     $('form :input').val('');
+
+    $editDivComments.on("click", function() {
+      $commentsDiv.toggle();
+    });
+
+
   });
-  
+
+  // Create/View comments //
+  var createCommentsSection = function ($commentsDiv) {
+    var $commentsPosts = $('<div class="comments-posts"></div>');
+    var $commentForm = $('<form style="margin-top:30px;" onsubmit="event.preventDefault();"></form>');
+
+    // Comment Form Text Input //
+    var $commentTextDiv = $('<div class="form-group"></div>');
+    var $commentTextInput = $('<input id="comment-text" type="text" class="form-control" placeholder="Comment Text"></input>');
+
+    // Comment Form Name Input //
+    var $commentNameDiv = $('<div class="form-group"></div>');
+    var $commentNameInput = $('<input id="comment-name" type="text" class="form-control" placeholder="Your Name"></input>');
+  // Comment Form Submit Button //
+    var $submitCommentButton = $('<button id="submit-comment" class="btn btn-primary">Submit Comment</button>');
+    
+    $commentTextDiv.append($commentTextInput);
+    $commentNameDiv.append($commentNameInput);
+    $commentForm.append($commentTextDiv);
+    $commentForm.append($commentNameDiv);
+    $commentForm.append($commentTextDiv);
+    $commentForm.append($commentNameDiv);
+    $commentForm.append($submitCommentButton);
+    $commentsDiv.append($commentsPosts);
+    $commentsDiv.append($commentForm);
+  };
+
+ 
+
 });
