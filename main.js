53e88cd8ed8b0b3aa0961524052e298c74151d32
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-04-05 12:03:22 -0400

    comment and remove links structure added via JS

--- main.js
+++ main.js
@@ -7,10 +7,27 @@
 $('#submit').on('click', function () {
   var userText = $('#post-text').val();
   var userName = $('#name').val(); 
-  $('.posts').append('<p>' + userText + ' - Posted By: ' + userName + '</p'); 
+
+  var $postElement = $('.posts').append('<div data-id="post">'); 
+
+  $postElement.append('<p>' + '<button type="button" class="btn btn-link" id="remove">remove</button>' + '<button type="button" class="btn btn-link" id="comment">comment</button>'  + userText + ' - Posted By: ' + userName + '</p'); 
+
   $('.posts').append('<hr>');
 });
 
 //PART 2
 //add remove and comments buttons in front of post
   // 'remove comments I like chees - Posted By: aaron'
+//maybe add a <span> before the <p> with the posts and fill it with the 2 links??? 
+//<button type="button" class="btn btn-link">Link</button>
+//add a new <div> with each post and comments???? 
+
+//onClick event listener for the comment and remove
+  //changes 'comment' to 'comments'
+  //opens up comments if there are comments
+    //add a new div <div 'id="comments">
+    //has form with posts and name boxes
+    //submit comment button 
+      //onClick event listener that posts a comment 
+  //remove button removes comments 
+  
