f7ae6886113a97ad190ed76a06d2c4232405af8f
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-04-08 13:39:22 -0400

    modified remove buttons to not delete comment form

--- main.js
+++ main.js
@@ -10,50 +10,35 @@ $('#submit').on('click', function () {
   var userName = $('#name').val(); 
 
   $('.posts').append('<div id="post-el"><p id="post">' + '<button type="button" class="btn btn-link" id="remove">remove</button>' + '<button type="button" class="btn btn-link" id="comment">comment</button>'  + userText + ' - Posted By: ' + userName +'<hr></p></div>'); 
+  //$('#post').append('<div id="comment-el">')
+  $('.posts').append($('#comments-form')); 
 
 }); 
 
-
 //PART 2
-//add remove and comments buttons in front of post
-  // looks like --- 'remove comment I like chees - Posted By: aaron'
-// use this for remove and comment <button type="button" class="btn btn-link">Link</button>
-//add a new <div> with each post and comments inside a <p>???? 
-
-//onClick event listener for the comment and remove
-  //changes 'comment' to 'comments'
-  //opens up comments if there are comments
-    //add a new div <div 'id="comments">
-    //has a form with posts and name boxes
-    //submit comment button 
-      //onClick event listener that posts a comment 
-  //remove button removes comments
-  
-
 //remove button 
 $('.posts').on('click', 'p', function (e) {
   var $element = $(e.target);
   
   if($element.attr('id') === 'remove') {
+    $('.posts').append($('#comments-form')); 
     $element.closest('div').remove(); 
-    //$element.closest('hr').remove();
   } else if($element.attr('id') === 'remove-comment') {
     $element.closest('p').remove();
   }
-}); //not working great. when you remove comments, it won't let you add more???
-
+}); 
 
 //comment button
 $('.posts').on('click', 'p', function (e) {
   var $element = $(e.target);
-  //console.log($element)
   
   if($element.attr('id') === 'comment') {
-   $element.closest('p').append($('#comments-form'));   
+   $element.closest('p').append($('#comments-form'));
+   $('#comment-el').show();   
    $('#comments-form').show();
-  };
+  // $('#comments-form').toggle();
+  } 
 });
-
 
 //submit comment 
 $('#comments-form').on('click', 'button', function (e) {
@@ -62,8 +47,10 @@ $('#comments-form').on('click', 'button', function (e) {
   var userComment = $('#post-comment').val();
   var userCommentName = $('#name-comment').val();
 
-  $element.closest('p').append('<p id="posted-comment">' + '<button type="button" class="btn btn-link" id="remove-comment">remove</button>' + userComment + ' - Posted by: ' + userCommentName + '</p>');
+  $element.closest('p').append('<div id="comment-el">' + '<p id="posted-comment">' + '<button type="button" class="btn btn-link" id="remove-comment">remove</button>' + userComment + ' - Posted by: ' + userCommentName + '</p>' + '</div>');
 
+  $('.posts').append($('#comments-form')); 
+  $('#comment-el').show(); 
   $('#comments-form').hide();
 });
 //not working great
