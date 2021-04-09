57b00ec301d2d8d9a2bee6a3c464df31c1e71bd3
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-04-09 18:44:43 -0400

    reworked structure and separated eventhandlers from functions

--- main.js
+++ main.js
@@ -1,57 +1,76 @@
-//PART 1
-//grab text user input
-//grab name user input
-//put that in posts div
-  //'I like cheese - Posted By: Aaron'
 
-//POSTS
-$('#submit').on('click', function () {
-  var userText = $('#post-text').val();
-  var userName = $('#name').val(); 
+//named functions
 
-  $('.posts').append('<div id="post-el"><p id="post">' + '<button type="button" class="btn btn-link" id="remove">remove</button>' + '<button type="button" class="btn btn-link" id="comment">comment</button>'  + userText + ' - Posted By: ' + userName +'<hr></p></div>'); 
-  //$('#post').append('<div id="comment-el">')
+var submitPost = function () {
+  var $userText = $('#post-text').val();
+  var $userName = $('#name').val(); 
+
+  $('.posts').append('<div class="post-el"><p class="post">' + '<button type="button" class="btn btn-link remove-button">remove</button>' + '<button type="button" class="btn btn-link comment-button">comment</button><button type="button" class="btn btn-link comments-button">comments</button>'  + $userText + ' - Posted By: ' + $userName +'<hr></p></div>'); 
+  //$('.posts').append($('#comments-form')); 
+};
+
+var removePost = function () {
+  $('.posts').append($('#comments-form')); //moves comment-form so it doesn't get deleted
+  $(this).parents('.post-el').remove(); 
+};
+
+var openComment = function () {
+  $(this).parent('.post').append($('#comments-form')); //comments form opens beneath the comment selected
+  $('#comments-form').show();
+  //add opening current comments as well
+};
+
+var removeComment = function () {
+  $(this).parents('.comment-el').remove();
+};
+
+var submitComment = function () {
+
+  var $userComment = $('#post-comment').val();
+  var $userCommentName = $('#name-comment').val();
+
+  //adds comments
+  $(this).parents('.post').append('<div class="comment-el"><p class="posted-comment">' + '<button type="button" class="btn btn-link remove-comment">remove</button>' + $userComment + ' - Posted by: ' + $userCommentName + '</p></div>');
+
+  $(this).parents('.post').children('.comments-button').css('display', 'inline'); 
+  $(this).parents('.post').children('.comment-button').css('display', 'none'); 
+
+  $(this).parents('.post').children('.comment-el').show();
+
+  //moves comment form 
   $('.posts').append($('#comments-form')); 
 
-}); 
+  //$('#comments-form').hide();
+};
 
-//PART 2
-//remove button 
-$('.posts').on('click', 'p', function (e) {
-  var $element = $(e.target);
-  
-  if($element.attr('id') === 'remove') {
-    $('.posts').append($('#comments-form')); 
-    $element.closest('div').remove(); 
-  } else if($element.attr('id') === 'remove-comment') {
-    $element.closest('p').remove();
-  }
-}); 
 
-//comment button
-$('.posts').on('click', 'p', function (e) {
-  var $element = $(e.target);
-  
-  if($element.attr('id') === 'comment') {
-   $element.closest('p').append($('#comments-form'));
-   $('#comment-el').show();   
-   $('#comments-form').show();
-  // $('#comments-form').toggle();
-  } 
-});
+var toggleComments = function () {
+  $(this).parents('.post').children('.comment-el').toggle();
+  $(this).parent('.post').append($('#comments-form'));
+  $('#comments-form').toggle();
+};
 
-//submit comment 
-$('#comments-form').on('click', 'button', function (e) {
-  var $element = $(e.target);
 
-  var userComment = $('#post-comment').val();
-  var userCommentName = $('#name-comment').val();
+//event listeners
 
-  $element.closest('p').append('<div id="comment-el">' + '<p id="posted-comment">' + '<button type="button" class="btn btn-link" id="remove-comment">remove</button>' + userComment + ' - Posted by: ' + userCommentName + '</p>' + '</div>');
+//submits posts
+$('#submit').on('click', submitPost); //work on preventing empty submissions
 
-  $('.posts').append($('#comments-form')); 
-  $('#comment-el').show(); 
-  $('#comments-form').hide();
-});
-//not working great
+//removes posts
+$('.posts').on('click', '.remove-button', removePost); 
+
+//opens initial comments button
+$('.posts').on('click', '.comment-button', openComment);
+
+//submits comments
+$('#submit-comment').on('click', submitComment); 
+
+//removes comments
+$('.posts').on('click', '.remove-comment', removeComment); 
+
+//toggles comments and opens comment form
+$('.posts').on('click', '.comments-button', toggleComments);
+
+
+
 
