b8c2004d6e662b4ab0d0b2e0a0941ebb49ce0c20
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-04-10 00:06:58 -0400

    startind edit functionality

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-
 //named functions
-
 var submitPost = function () {
   var $userText = $('#post-text').val();
   var $userName = $('#name').val(); 
 
-  $('.posts').append('<div class="post-el"><p class="post">' + '<button type="button" class="btn btn-link remove-button">remove</button>' + '<button type="button" class="btn btn-link comment-button">comment</button><button type="button" class="btn btn-link comments-button">comments</button>'  + $userText + ' - Posted By: ' + $userName +'<hr></p></div>'); 
+  $('.posts').append('<div class="post-el"><p class="post">' + '<button type="button" class="btn btn-link edit-button">edit</button>' + '<button type="button" class="btn btn-link remove-button">remove</button>' + '<button type="button" class="btn btn-link comment-button">comment</button><button type="button" class="btn btn-link comments-button">comments</button>'  + $userText + ' - Posted By: ' + $userName +'<hr></p></div>'); 
+
+  $('form :input').val(''); //resets inputs in main form  
   //$('.posts').append($('#comments-form')); 
+};
+
+var openEditPost = function () {
+  //console.log('edit')
+  $(this).parent().append(('<input type="text" placeholder="Edit post" class="edit-post form-control"/>'));
+  $(this).parent().append(("<button type=\"button\" class=\"btn btn-primary btn-xs save-edit-button\">Submit Edit</button>")); 
+};
+
+var submitEdits = function () {
+  var $edits = $('.edit-post').val();
+  $(this).parent(':not(button, input)').html($edits);
 };
 
 var removePost = function () {
@@ -17,7 +28,6 @@ var removePost = function () {
 var openComment = function () {
   $(this).parent('.post').append($('#comments-form')); //comments form opens beneath the comment selected
   $('#comments-form').show();
-  //add opening current comments as well
 };
 
 var removeComment = function () {
@@ -32,17 +42,17 @@ var submitComment = function () {
   //adds comments
   $(this).parents('.post').append('<div class="comment-el"><p class="posted-comment">' + '<button type="button" class="btn btn-link remove-comment">remove</button>' + $userComment + ' - Posted by: ' + $userCommentName + '</p></div>');
 
+  $('form :input').val(''); //resets input in comment form
+
   $(this).parents('.post').children('.comments-button').css('display', 'inline'); 
   $(this).parents('.post').children('.comment-button').css('display', 'none'); 
+  $(this).parents('.post').children('.comment-el').hide(); 
 
-  $(this).parents('.post').children('.comment-el').show();
-
+  $('#comments-form').hide();
   //moves comment form 
   $('.posts').append($('#comments-form')); 
 
-  //$('#comments-form').hide();
 };
-
 
 var toggleComments = function () {
   $(this).parents('.post').children('.comment-el').toggle();
@@ -50,11 +60,24 @@ var toggleComments = function () {
   $('#comments-form').toggle();
 };
 
-
 //event listeners
-
 //submits posts
-$('#submit').on('click', submitPost); //work on preventing empty submissions
+$('#submit').on('click', function () {
+  $('#main-form').submit(function(e) {
+    if ($.trim($("#post-text").val()) === "" || $.trim($("#name").val()) === "") {
+        e.preventDefault();
+        return false;
+    } else {
+      submitPost(); 
+    };
+  });
+});
+
+//open edit posts
+$('.posts').on('click', '.edit-button', openEditPost); 
+
+//submit edit posts
+$('.posts').on('click', '.save-edit-button', submitEdits); 
 
 //removes posts
 $('.posts').on('click', '.remove-button', removePost); 
@@ -63,7 +86,7 @@ $('.posts').on('click', '.remove-button', removePost);
 $('.posts').on('click', '.comment-button', openComment);
 
 //submits comments
-$('#submit-comment').on('click', submitComment); 
+$('#submit-comment').on('click', submitComment);
 
 //removes comments
 $('.posts').on('click', '.remove-comment', removeComment); 
