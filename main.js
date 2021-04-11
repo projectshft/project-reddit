87f70fc888aaa9ad7f4dc062e22a04abced3b6a5
Kris Yerkie <kyerkie@myncca.com>
2021-04-11 16:41:42 -0400

    adding in form field reset upon post

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ $('#submit').on('click', function () {
   //Creates a div, then one paragraph element to contain the message, a second paragraph element
   //to contain the poster name, then adds on a button for Removing the Comment and a buttong for
   //adding a comment to that specific comment.
-  $('.posts').append('<div class="post"><p>' + $text + '<p>Posted By: ' + $name + '<p><button class="remove-button" id="removeButton">Delete Comment</button>' + '<button class="add-comments-button" id="removeButton">Add Comment</button>' + '</p></p></p><hr></div>');
+  $('.posts').append('<div class="post"><p>' + $text + '<p>Posted By: ' + $name + '<p><button class="remove-button" id="removeButton">Delete Comment</button>' + '<button class="add-comments-button" id="addCommentsButton">Add Comment</button>' + '</p></p></p><hr></div>');
+
+  //Resets the form fields back to blank
+  $('form :input').val('');
 
 });
 
-$('.posts').on('click', 'button', function (e) {
+$('.posts').on('click', '.remove-button', function (e) {
   $(this).closest('.post').remove();
+});
+
+$('.posts').on('click', '.add-comments-button', function (e) {
+  console.log('its working');
 });
