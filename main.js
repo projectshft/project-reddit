9651e2650e37f3d7ec5cdd5d818ac78c984ee057
Kris Yerkie <kyerkie@myncca.com>
2021-04-11 23:44:14 -0400

    commit for eval

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+/*
+I know that I need to add unique IDs to every post div that is created so that I can then post the reply
+comments to the associated unique post/thread. I tried utilizing the data attributes option, and also
+considered converting the posts div into an array and using index numbers for each post ID so I could
+call them later on, but couldn't get anything to function properly. 
+
+I also know that the comment reply form is showing up at the end of the posts div instead of directly after
+the comment that the "Add Comment" button is clicked on. I tried moving it to the end of the post div, but
+without the unique ID number issue fixed and functioning, I couldn't figure out how to append the form
+to the single unique post (which is the same thing that is happening when I do reply to an existing comment,
+it posts my reply to all comments instead of just the single one it is supposed to - I know this is related to
+the unique ID issue) - considered adding the same index on the end of the add-comment button click
+function and iterate through using i and i++, but couldn't figure out the correct way to do the syntax. 
+
+There were alot of challenges for me on this project. Conceptually I feel like I know what I need to do, I 
+just am really struggling with the syntax and specific way to write what needs to happen.
+Will continue to work through these issues but wanted to commit where I was at.
+*/
+
 //When Post button is clicked, this will post the message as a parent thread.
 $('#submit').on('click', function () {
   //Gets the user input for both the name and message boxes.
@@ -11,7 +30,7 @@ $('#submit').on('click', function () {
 
   //Adds on a blank comment form to the comment but hides it. 
   $('#blank-comment-form').hide();
-  $('.post').append($('#blank-comment-form'));
+  $('.posts').append($('#blank-comment-form'));
 
   //Adds a horizontal line after hidden comment box
   $('.posts').append('<hr>');
@@ -20,7 +39,7 @@ $('#submit').on('click', function () {
    $('form :input').val('');
 });
 
-//When Post button is clicked, this will post the message as a parent thread.
+//When Post Reply button is clicked, this will post the message as reply to the specific comment.
 $('#submit-comment').on('click', function () {
   //Gets the user input for both the name and message boxes.
   var $commentName = $('#comment-name').val();
@@ -36,7 +55,7 @@ $('#submit-comment').on('click', function () {
 
   //Adds on a blank comment form to the comment but hides it. 
   $('#blank-comment-form').hide();
-  $('.posts').append($('#blank-comment-form'));
+  $('.post').append($('#blank-comment-form'));
 });
 
 //Deletes the entire thread when the delete button is removed.
