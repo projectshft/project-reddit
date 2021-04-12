f2b0625f414fd72bae39317fd37087916ce0707c
Kris Yerkie <kyerkie@myncca.com>
2021-04-11 21:28:06 -0400

    Sub comment box reordered

--- main.js
+++ main.js
@@ -9,15 +9,15 @@ $('#submit').on('click', function () {
   //adding a comment to that specific comment.
   $('.posts').append('<div class="post"><p>' + $text + '<p>Posted By: ' + $name + '<p><button class="remove-button" id="removeButton">Delete Comment</button>' + '<button class="add-comments-button" id="addCommentsButton">Add Comment</button>' + '</p></p></p></div>');
 
-  //Resets the form fields back to blank
-  $('form :input').val('');
-
   //Adds on a blank comment form to the comment but hides it. 
   $('#blank-comment-form').hide();
-  $('.posts').append($('#blank-comment-form'));
+  $('.post').append($('#blank-comment-form'));
 
   //Adds a horizontal line after hidden comment box
   $('.posts').append('<hr>');
+
+   //Resets the form fields back to blank
+   $('form :input').val('');
 });
 
 //When Post button is clicked, this will post the message as a parent thread.
@@ -29,7 +29,7 @@ $('#submit-comment').on('click', function () {
   //Creates a div, then one paragraph element to contain the message, a second paragraph element
   //to contain the poster name, then adds on a button for Removing the Comment and a buttong for
   //adding a comment to that specific comment.
-  $('.post').append('<div class="sub-post"><p>' + $commentText + '<p>Posted By: ' + $commentName + '<p><button class="remove-button" id="removeButton">Delete Comment</button>' + '<button class="add-comments-button" id="addCommentsButton">Add Comment</button>' + '</p></p></p><hr></div>');
+  $('.post').append('<div class="sub-post"><p>' + $commentText + '<p>Posted By: ' + $commentName + '<p><button class="remove-button" id="removeButton">Delete Comment</button>' + '<button class="add-comments-button" id="addCommentsButton">Add Comment</button>' + '</p></p></p></div>');
 
   //Resets the form fields back to blank
   $('form :input').val('');
