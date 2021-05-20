f831f4c24760db32fd6ee87c00f4bbd140fa0882
Stefania Spetetchi <steffi@balerion.lan>
2021-05-19 22:23:08 -0400

    updates jquery

--- main.js
+++ main.js
@@ -1,148 +1,96 @@
 var numberOfPosts = 0;
+// keep track of the number of posts that have been added
 
-$('#submit-post').on('click', function () {
+$("#submit-post").on("click", function () {
   numberOfPosts = numberOfPosts + 1;
-  var userInputMesg = $('#message').val();
-  var userInputName = $('#name').val();
-  var post = document.createElement('div');
-  post.classList.add('new-post' + numberOfPosts);
-  post.setAttribute('id', 'new-post' + numberOfPosts);
-  var divElement = document.createElement('p');
-  divElement.classList.add(numberOfPosts);
-  var commentButton = document.createElement('button');
-  commentButton.classList.add('btn', 'btn-link', 'comment');
-  commentButton.classList.add(numberOfPosts);
-  commentButton.setAttribute('id', 'comment-on-post' + numberOfPosts);
-  commentButton.setAttribute('number', numberOfPosts);
-  commentButton.textContent = 'comment';
-  commentButton.setAttribute('action', 'create');
-  var removeButton = document.createElement('button');
-  removeButton.classList.add('btn', 'btn-link', 'remove');
-  removeButton.setAttribute('id', 'remove-post' + numberOfPosts);
-  removeButton.textContent = 'remove';
-  var allComments = document.createElement('div');
-  allComments.classList.add('all-comments' + numberOfPosts);
-
-  var postsDiv = $('.submitted-posts');
+  var postsCenter = $(".submitted-posts");
+  var userInputMesg = $("#message").val();
+  var userInputName = $("#name").val();
+  var divElement = $("<p>", { "name": "Submitted Post", class: numberOfPosts, id: "post-to-submit" });
+  var commentButton = $("<button>", { id: "comment-on-post-" + numberOfPosts, "number": numberOfPosts, "text": "comment", "action": "create" });
+  commentButton.addClass("btn btn-link comment");
+  var removeButton = $("<button>", { id: "remove-post" + numberOfPosts, "text": "remove", })
+  removeButton.addClass("btn btn-link remove");
+  var post = $("<div>", { "name": "Post " + numberOfPosts, class: "new-post", id: "new-post" + numberOfPosts })
   var userInputElement = document.createTextNode(userInputMesg + " - Posted by: " + userInputName);
 
-  divElement.appendChild(commentButton);
-  divElement.appendChild(removeButton);
-  divElement.appendChild(userInputElement);
-  divElement.appendChild(allComments);
-
+  divElement.append(commentButton, removeButton, userInputElement);
   post.append(divElement);
-  postsDiv.append(post);
+  postsCenter.append(post);
 
-  removePost();
   addCommentForm();
+  removePost();
 });
 
 var removePost = function () {
-  $('#remove-post' + numberOfPosts).on('click', function (e) {
+  $("#remove-post" + numberOfPosts).on('click', function (e) {
     if (e.target.classList.contains('remove')) {
       var postToRemove = e.target.closest('div');
     }
     postToRemove.remove();
   });
-}
-
+};
 
 var addCommentForm = function () {
-
-  $('#comment-on-post' + numberOfPosts).on('click', function (e) {
-    var $element = $(e.target);
-    var number = $element.attr('number');
+  $('#comment-on-post-' + numberOfPosts).on('click', function (e) {
+    var element = $(e.target);
+    var number = element.attr('number');
     var commentForm = $('.comment-form-' + number);
 
-    if ($element.attr('action') === 'create') {
-      var form = document.createElement('form');
-      form.classList.add('comment-form-' + numberOfPosts);
-      form.classList.add('show');
-      form.setAttribute('id', 'comments');
-      form.setAttribute('style', 'margin-top:10px');
-      form.setAttribute('onsubmit', 'event.preventDefault()');
-      var divComment = document.createElement('div');
-      divComment.classList.add('form-group');
-      var inputDivComment = document.createElement('input');
-      inputDivComment.classList.add('form-control');
-      inputDivComment.setAttribute('id', 'comment-text');
-      inputDivComment.setAttribute('type', 'text');
-      inputDivComment.setAttribute('placeholder', 'Comment Text');
-      var divCommentName = document.createElement('div');
-      divCommentName.classList.add('form-group');
-      var inputDivCommentName = document.createElement('input');
-      inputDivCommentName.classList.add('form-control');
-      inputDivCommentName.setAttribute('id', 'comment-name');
-      inputDivCommentName.setAttribute('type', 'text');
-      inputDivCommentName.setAttribute('placeholder', 'Name');
-      var formButton = document.createElement('button');
-      formButton.classList.add('btn', 'btn-primary');
-      formButton.setAttribute('id', 'submit-comment');
-      formButton.setAttribute('number', numberOfComments);
-      formButton.setAttribute('type', 'submit');
-      formButton.textContent = 'Submit Comment';
+    if (element.attr('action') === 'create') {
+      var form = $("<form>", { id: "comments", class: "show", class: "comment-form-" + number, "onsubmit": "event.preventDefault()" });
+      var divComment = $("<div>", { class: "form-group" });
+      var inputDivComment = $("<input>", { class: "form-control", id: "comment-text", type: "text", placeholder: "Comment Text" });
+      var divCommentName = $("<div>", { class: "form-group" });
+      var inputDivCommentName = $("<input>", { class: "form-control", id: "comment-name", type: "text", placeholder: "Name" });
+      var formButton = $("<button>", { id: "submit-comment", class: "submit-comment-" + number, "number": number, type: "submit", "text": "Submit Comment" });
+      formButton.addClass("btn btn-primary");
+      var commentsListDiv = $("<div>", { class: "commentary-" + number });
+      var posts = $("#new-post" + number);
 
-      divComment.appendChild(inputDivComment);
-      divCommentName.appendChild(inputDivCommentName);
-      form.append(divComment);
-      form.append(divCommentName);
-      form.append(formButton);
-      var posts = $('.new-post' + numberOfPosts);
       posts.append(form);
+      form.append(commentsListDiv, divComment, divCommentName, formButton);
+      divComment.append(inputDivComment);
+      divCommentName.append(inputDivCommentName);
 
-      $element.attr('action', 'hide');
-    }
-    else if ($element.attr('action') === 'hide') {
-      commentForm.addClass('hide');
-      commentForm.removeClass('show');
-      $element.attr('action', 'show');
+      element.attr("action", "hide");
+      submitComment();
+    } else if (element.attr("action") === "hide") {
+      commentForm.addClass("hide");
+      commentForm.removeClass("show");
+      element.attr("action", "show");
     } else {
-      commentForm.addClass('show');
-      commentForm.removeClass('hide');
-      $element.attr('action', 'hide');
+      commentForm.addClass("show");
+      commentForm.removeClass("hide");
+      element.attr("action", "hide");
     }
-    submitComment();
-  });
-}
+  })
+};
 
-var numberOfComments = 0;
 var submitComment = function () {
-  $('#submit-comment').on('click', function (event) {
-    numberOfComments = numberOfComments + 1;
-    var $comment = $(event.target);
-    var number = $comment.attr('number');
+  var count = $('#submit-comment').attr('number');
+  $(".submit-comment-" + count).on("click", function () {
     var userInputComment = $('#comment-text').val();
     var userInputCommentName = $('#comment-name').val();
-    var comment = document.createElement('div');
-    comment.classList.add('new-comment' + numberOfComments);
-    comment.setAttribute('id', 'new-comment' + numberOfComments);
-    var commentDiv = document.createElement('p');
-    commentDiv.classList.add(numberOfComments);
-    var removeCommentButton = document.createElement('button');
-    removeCommentButton.classList.add('btn', 'btn-link', 'remove-comment');
-    removeCommentButton.setAttribute('id', 'remove-comment');
-    removeCommentButton.textContent = 'remove this comment';
-
-    var commentSection = $('.submitted-posts').find('.all-comments' + numberOfPosts);
-
+    var comment = $("<div>", { id: "new-comment", class: "new-comment-" + count, "onsubmit": "event.preventDefault()" });
+    var commentDiv = $("<p>", { class: "comms" + count });
+    var removeCommentButton = $("<button>", { id: "remove-comment", "count": count, text: "remove this comment" });
+    removeCommentButton.addClass("btn btn-link remove-comment");
+    var commentSection = $(".commentary-" + count);
     var userInputCommentElement = document.createTextNode(userInputComment + " - Posted by: " + userInputCommentName);
-    commentDiv.appendChild(removeCommentButton);
-    commentDiv.appendChild(userInputCommentElement);
-    comment.append(commentDiv);
+
     commentSection.append(comment);
-
+    comment.append(commentDiv);
+    commentDiv.append(removeCommentButton, userInputCommentElement);
     removeComment();
-  });
-}
-
+  })
+};
 
 var removeComment = function () {
-  $('#remove-comment').on('click', function (e) {
-    console.log('click remove');
+  $('.remove-comment').on('click', function (e) {
     if (e.target.classList.contains('remove-comment')) {
-      var commentToRemove = e.target.closest('p');
+      var commentToRemove = e.target.closest('div');
     }
     commentToRemove.remove();
-  });
+  })
 };
