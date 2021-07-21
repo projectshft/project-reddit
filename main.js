2fc13e533a80db8cad99278704f6f24e0bb9127d
Benjamin Pryor <bscott23@gmail.com>
2021-07-21 17:59:47 -0400

    adding remove

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ $('.submit-post').on('click', function () {
   var $input = $('#input-post').val();
   var $name = $('#input-name').val();
 
-  $('.posts').append('<div> <button type="button" class="btn btn-link comment">comment</button> <button type="button" class="btn btn-link remove">remove</button>' + $input + ' -Posted by: ' + $name + '</div><div class="comment-section" style="display: none"><div class="comments"></div><input type="text"class="form-control input-area"id="input-comment"placeholder="Your comment"/> <input type="text"class="form-control input-area"id="input-name2" placeholder="Your name"/> <button type="button" class="btn btn-primary submit-comment"> Submit Comment </button></div><hr />');
+  $('.posts').append('<div> <button type="button" class="btn btn-link comment">comment</button> <button type="button" class="btn btn-link remove">remove</button>' + $input + ' -Posted by: ' + $name + '</div> <div> <div class="comment-section" style="display: none"> <div class="comments"> </div><input type="text"class="form-control input-area"id="input-comment"placeholder="Your comment"/> <input type="text"class="form-control input-area"id="input-name2" placeholder="Your name"/> <button type="button" class="btn btn-primary submit-comment"> Submit Comment </button> </div> <hr /> </div>');
+  bindRemove();
 });
 
 $('.posts').on('click', '.comment', function () {
   var $comments = $('.comment-section');
-  $comments.toggle();
+  $comments.closest('div').toggle();
 });
 
 $('.posts').on('click','.submit-comment', function () {
@@ -16,7 +17,9 @@ $('.posts').on('click','.submit-comment', function () {
   $('.comments').append('<button type="button" class="btn btn-link remove">remove</button>' + $commentInput + ' -Posted by: ' + $commentName + '</div>');
 })
 
-$('.posts').on('click', '.remove', function () {
-  var $comments = $('.comment-section');
-  $comments.hide();
-});
+var bindRemove = function () {
+  $('.remove').on('click', function () {
+    $('.remove').closest('div').remove();
+  });
+  
+}
