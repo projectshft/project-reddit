f8baa53ce4eabc96346ce1fae53ba1b510d64737
Benjamin Pryor <bscott23@gmail.com>
2021-07-22 10:51:23 -0400

    fixed bindings added all required functions

--- main.js
+++ main.js
@@ -2,24 +2,29 @@ $('.submit-post').on('click', function () {
   var $input = $('#input-post').val();
   var $name = $('#input-name').val();
 
-  $('.posts').append('<div> <button type="button" class="btn btn-link comment">comment</button> <button type="button" class="btn btn-link remove">remove</button>' + $input + ' -Posted by: ' + $name + '</div> <div> <div class="comment-section" style="display: none"> <div class="comments"> </div><input type="text"class="form-control input-area"id="input-comment"placeholder="Your comment"/> <input type="text"class="form-control input-area"id="input-name2" placeholder="Your name"/> <button type="button" class="btn btn-primary submit-comment"> Submit Comment </button> </div> <hr /> </div>');
+  $('.posts').append('<div> <button type="button" class="btn btn-link comment">comment</button> <button type="button" class="btn btn-link remove">remove</button>' + $input + ' -Posted by: ' + $name + '<div class="comment-section" style="display: none"> <div class="comments"> </div> <div ="comment-input-area"> <input type="text"class="form-control input-area input-comment" placeholder="Your comment"/> <input type="text"class="form-control input-area input-name2" placeholder="Your name"/> <button type="button" class="btn btn-primary submit-comment"> Submit Comment </button> </div>  </div> <hr /> </div> ');
   bindRemove();
 });
 
 $('.posts').on('click', '.comment', function () {
-  var $comments = $('.comment-section');
-  $comments.closest('div').toggle();
+  var $commentSection =  $(this).closest('div').find('.comment-section');
+  if ($commentSection.css('display') ==='none') {
+    $commentSection.show();
+  }else {
+    $commentSection.hide();
+  }
 });
 
-$('.posts').on('click','.submit-comment', function () {
-  var $commentInput = $('#input-comment').val();
-  var $commentName = $('#input-name2').val();
-  $('.comments').append('<button type="button" class="btn btn-link remove">remove</button>' + $commentInput + ' -Posted by: ' + $commentName + '</div>');
+$('.posts').on('click', '.submit-comment', function () {
+  var $commentInput = $(this).closest('.comment-section').find('.input-comment').val();
+  var $commentName = $(this).closest('.comment-section').find('.input-name2').val();
+  $(this).closest('.comment-section').find('.comments').append('<div> <button type="button" class="btn btn-link remove">remove</button>' + $commentInput + ' -Posted by: ' + $commentName + '</div>');
+  bindRemove();
 })
+
 
 var bindRemove = function () {
   $('.remove').on('click', function () {
-    $('.remove').closest('div').remove();
-  });
-  
-}
+    $(this).closest('div').remove();
+  });  
+};
