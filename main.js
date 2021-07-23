87ce38c9aab88ab1800c4e0895b50ca21ebff3fb
Ryan Passer <rpasser93@gmail.com>
2021-07-23 15:47:07 -0400

    Adds remove and comment buttons next to Post text

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var newPostTemplate = function(postText,nameText) {
-  var insertPost = '<p><h4>' + postText + '<small> - Posted by: ' + nameText + '</small></h4></p><hr>';
+  var postRemoveButton = '<button type="button" class="btn btn-link rem-post-btn">[X]</button>'
+  var postCommentButton = '<button type="button" class="btn btn-link view-comms-btn">[comments]</button>'
+  var insertPost = '<p><h4><strong>'+ postRemoveButton + postCommentButton + postText + '<small> - Posted by: ' + nameText + '</small></strong></h4></p><hr>';
+
   $('.posts').append(insertPost);
 }
 
-$('button').click(function(){
+$('.submit-post-btn').click(function(){
   var postInput = $('#input-post').val();
   var nameInput = $('#input-name').val();
   newPostTemplate(postInput,nameInput);
