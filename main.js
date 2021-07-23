9063fe06a04fba3b8a707a39b9929e9f1fb51da4
Otto Frankel <ottofrankel@comcast.net>
2021-07-23 10:58:29 -0700

    Added toggleable comment section

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var posts = [];
+var currId = 1;
 
 var getPost = function (postId) {
   for (var i = 0; i < posts.length; i++) {
@@ -11,16 +12,39 @@ var getPost = function (postId) {
 $('#submit').on('click', function () {
   var message = $('#message').val();
   var name = $('#name').val();
-  var id = posts.length;
+  var id = currId;
+  currId++;
 
   if (message && name) {
+    var postComments = 
+     '<div class="comment-section" style="display: none;">'
+      +'<form id="comment-form" style="margin-top:30px;" onsubmit="event.preventDefault();">'
+            
+        +'<div class="form-group">'
+          +'<input id="comment" type="text"'
+          +'class="form-control"'
+          +'placeholder="Comment text"></input>'
+        +'</div>'
+
+        +'<div class="form-group">'
+          +'<input id="comment-name" type="text"'
+          +'class="form-control"'
+          +'placeholder="Your name"></input>'
+        +'</div>'
+      
+        +'<button id="submit-comment" class="btn btn-primary">Submit Post</button>'
+      +'</form>'
+    +'</div>';
+
     $('.posts').append(
         '<div class="post" id="' + id + '">'
         + '<p>' + message + ' - ' + 'Posted By: ' + name + '</p>'
         + '<a class="remove-post">remove</a><span> --- </span>'
-        + '<a class="show-comments">comments</a><hr>'
+        + '<a class="show-comments">comments</a>'
+        + postComments + '<hr>'
         +'</div>'
     );
+
     $('#post-form')[0].reset();
 
     posts.push(
@@ -42,4 +66,10 @@ $('body').on('click', '.remove-post', function () {
     posts.splice(index, 1);
   
   $postDiv.remove(); 
+})
+
+$('body').on('click', '.show-comments', function () {
+  var $postDiv = $(this).closest('.post');
+  var $commentSection = $postDiv.find('.comment-section');
+  $commentSection.toggle();
 })
