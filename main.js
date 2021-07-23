06976c7fc07802377fbc6b5e478267a4901b6fce
Otto Frankel <ottofrankel@comcast.net>
2021-07-23 09:38:49 -0700

    Added ability to delete post

--- main.js
+++ main.js
@@ -2,5 +2,18 @@ $('#submit').on('click', function () {
   var message = $('#message').val();
   var name = $('#name').val();
 
-  $('.posts').append('<p>' + message + ' - ' + 'Posted By: ' + name + '</p><hr>')
+  if (message && name) {
+    $('.posts').append(
+        '<div class="post">'
+        + '<p>' + message + ' - ' + 'Posted By: ' + name + '</p>'
+        + '<a class="remove-post">remove</a><span> --- </span>'
+        + '<a class="show-comments">comments</a><hr>'
+        +'</div>'
+    )
+    $('#post-form')[0].reset();
+  }
+})
+
+$('body').on('click', '.remove-post', function () {
+  $(this).closest('.post').remove();
 })
