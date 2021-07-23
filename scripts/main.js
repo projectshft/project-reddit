de2baddeffec2e1c51165ac5f6e52b116cc5c7e8
Philip Krynicki <philipkrynicki@Philips-MacBook-Air.local>
2021-07-23 15:11:27 -0500

    More Javascript added

--- scripts/main.js
+++ scripts/main.js
@@ -4,25 +4,57 @@ $('#submit').on('click', function () {
   if (authorName === '' || postMessage === '') {
     alert('Need to add a value!');
   } else {
-    $('.posts').append("<div><a class='removePost' href = '#'>Remove</a> <a class='comments' href = '#'>Comments</a> " + postMessage + " - Posted By: " + authorName + "</div>");
+    var $newPost = $(postTemplate(postMessage, authorName)).appendTo('.posts');
+    var $commentForm = $newPost.find('.comment-form').hide();
+
+    $newPost.find('.comments').on('click', function () {
+      if($commentForm.is(":visible")) {
+        $commentForm.hide()     
+      } else {
+        $commentForm.show();
+      }
+    })
+    $newPost.find('.comment-submit').on('click', handleCommentSubmit);
   }
   
 });
 
+function handleCommentSubmit () {
+  var $form = $(this).closest('.comment-form');
+  var yourName = $form.find('#commentAuthorName').val().trim();
+  var commentText = $form.find('#commentMessage').val().trim();
+  if (yourName === '' || commentText === '') {
+    alert('Need to add a value!');
+  } else {
+    var $comments = $form.find('.comments');
+    var $newComment = $(commentTemplate(commentText, yourName)).appendTo($comments);
+  }  
+};
+
+function commentTemplate (commentText, yourName) {
+  return "<div>\
+    <a class='removePost' href = '#'>Remove</a> " + commentText + " - Posted By: " + yourName + "</div>";
+};
 
 
-var commentInputTemplate = '<form style="margin-top:30px;" onsubmit="event.preventDefault();">\
-<h3>Add a new post</h3>\
+
+function postTemplate (postMessage, authorName) {
+  return "<div>\
+    <a class='removePost' href = '#'>Remove</a> <a class='comments' href = '#'>Comments</a> " + postMessage + " - Posted By: " + authorName + commentInputTemplate + 
+    "</div>";
+};
+
+var commentInputTemplate = '<form class="comment-form" style="margin-top:30px;" onsubmit="event.preventDefault();">\
+<div class="comments"></div>\
 <div class="form-group">\
-  <input id="postMessage" type="text"\
+  <input id="commentMessage" type="text"\
     class="form-control"\
-    placeholder="Post Text"></input>\
+    placeholder="Comment Text"></input>\
 </div>\
 <div class="form-group">\
-  <textarea id="authorName" type="text"\
+  <textarea id="commentAuthorName" type="text"\
   class="form-control"\
   placeholder="Your Name"></textarea>\
 </div>\
-<button id="submit" class="btn btn-primary">Submit Post</button>\
+<button class="btn btn-primary comment-submit">Submit Comment</button>\
 </form>';
-
