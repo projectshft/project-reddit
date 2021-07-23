26afa366ca1a0113b51914252cc211dd46891dc0
zdresser <42179244+zdresser@users.noreply.github.com>
2021-07-23 18:36:33 -0400

    Added delete post

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-var posts = [{author: 'Zach', text: 'Blah blah', comments:[]}];
+var posts = [];
+var id = 1;
 
 
-$("#submit-post").click(function () {  
+$("#submit-post").click(function () { 
+   
   var author = $("#post-author").val();
   var text = $("#post-text").val();
-  var postData = {'author': author, 'text': text, comments: []};
+  var postData = {'id': id, 'author': author, 'text': text, comments: []};
 
   posts.push(postData);
-  
+
   $("#post-author").html('Your Name');
   $("#post-text").html('Post Text');
 
+  var remove = '<button class="remove">Remove</button>';
+  var comments = '<button>Comments</button>';
   
-  // showPosts(); Will need to show all posts after new one is submitted
+  $("#new-post").append('<p>' + remove + comments + text + " - Posted By: " + author + '<hr>'+ '</p>');
+
+  id += 1;
 });
 
-var showPosts = function(posts){};//iterate through posts array and update html
+$(document).on('click','.remove',  function() {  
+  $(this).parent().next("hr").remove();
+  $(this).closest('p').remove();
+
+});
+
+
+//click comments link then show html section with comments associated with post. 
