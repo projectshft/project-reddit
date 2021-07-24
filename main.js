f0d0624c7e71419fddd0e67f5704409579cc85e0
Ryan Passer <rpasser93@gmail.com>
2021-07-24 19:37:14 -0400

    Add remove functionality for comments

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ var newPostTemplate = function(postText,nameText) {
   var commentFormDisplay = '<div class="toggler hide-element"><div class="comments"></div><form><div class="form-group"><input type="text" class="form-control comment-post comment-width-adj" id="comment-post" placeholder="Comment Text" /></div><div class="form-group"><input type="text" class="form-control comment-name comment-width-adj" id="comment-name" placeholder="Your Name" /></div></form>';
   var commentSubmitButton = '<button type="button" class="btn btn-info submit-comm-btn">Submit Comment</button></div>';
   var combinedCommentDisplay = commentFormDisplay + commentSubmitButton;
-
   var insertPost = '<h4><p>'+ postRemoveButton + postCommentButton + '<strong>'+ postText + '</strong><small> - posted by: ' + nameText + '</small></p></h4>' + combinedCommentDisplay + '<hr>';
 
   $('.posts').append(insertPost);
@@ -14,9 +13,10 @@ var newPostTemplate = function(postText,nameText) {
 
 var newCommentTemplate = function(commentText,commentName,matchCommentToPost) {
   var commentRemoveButton = '<button type="button" class="btn btn-link rem-comm-btn">[X]</button>';
-  var insertComment = '<h5><p>' + commentRemoveButton + '<em>"' + commentText + '"</em>' +'<small> - posted by: ' + commentName + '</small></p></h5>';
+  var insertComment = '<h5 class="new"><p>' + commentRemoveButton + '"' + commentText +'"<small> - posted by: ' + commentName + '</small></p></h5>';
 
   matchCommentToPost.append(insertComment);
+  initCommentRemoveListener();
 
 };
 
@@ -31,15 +31,6 @@ var initPostSubmitListener = function () {
   });
 };
 
-var initCommentToggleListener = function() {
-  $('.view-comms-btn').off('click');
-  $('.view-comms-btn').on('click', function() {
-  var $associatedCommentDiv = $(this).closest('h4').next('.toggler');
-  $associatedCommentDiv.toggleClass('hide-element');
-
-})
-};
-
 var initCommentSubmitListener = function() {
   $('.submit-comm-btn').off('click');
   $('.submit-comm-btn').on('click', function() {
@@ -49,8 +40,24 @@ var initCommentSubmitListener = function() {
     for(i=0; i < $('form').length; i++) {
       $('form')[i].reset();
       };
-      
     newCommentTemplate(commentTextInput,commentNameInput,$associatedPostForComment);
+  });
+};
+
+var initCommentToggleListener = function() {
+  $('.view-comms-btn').off('click');
+  $('.view-comms-btn').on('click', function() {
+  var $associatedCommentDiv = $(this).closest('h4').next('.toggler');
+  $associatedCommentDiv.toggleClass('hide-element');
+});
+};
+
+var initCommentRemoveListener = function() {
+  $('.rem-comm-btn').off('click');
+  $('.rem-comm-btn').on('click', function() {
+    console.log('click!!!');
+  var removedComment = $(this).closest('.new');
+  removedComment.remove();
   });
 };
 
