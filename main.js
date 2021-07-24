8f33da83e411aba4144230450f3a4f0e08bc61ff
Ryan Passer <rpasser93@gmail.com>
2021-07-24 01:36:06 -0400

    Build comment creater and initial toggles

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 var newPostTemplate = function(postText,nameText) {
-  var postRemoveButton = '<button type="button" class="btn btn-link rem-post-btn">[X]</button>'
-  var postCommentButton = '<button type="button" class="btn btn-link view-comms-btn">[comments]</button>'
-  var insertPost = '<p><h4><strong>'+ postRemoveButton + postCommentButton + postText + '<small> - Posted by: ' + nameText + '</small></strong></h4></p><hr>';
+  var postRemoveButton = '<button type="button" class="btn btn-link rem-post-btn">[X]</button>';
+  var postCommentButton = '<button type="button" class="btn btn-link view-comms-btn">[comments]</button>';
+  var commentFormDisplay = '<div class="toggler hide-element"><form><div class="form-group"><input type="text" class="form-control commenter" id="comment-post" placeholder="Comment Text" /></div><div class="form-group"><input type="text" class="form-control commenter" id="comment-name" placeholder="Your Name" /></div></form>';
+  var commentSubmitButton = '<button type="button" class="btn btn-info submit-comment-btn">Submit Comment</button></div>';
+  var combinedCommentDisplay = commentFormDisplay + commentSubmitButton;
+
+  var insertPost = '<p><h4><strong>'+ postRemoveButton + postCommentButton + postText + '<small> - Posted by: ' + nameText + '</small></strong></h4></p>' + combinedCommentDisplay + '<hr>';
 
   $('.posts').append(insertPost);
+  
+  //make into own func
+  $('.view-comms-btn').click(function(){
+    console.log('click!');
+    $('.toggler').toggleClass("hide-element");
+  });
+
 }
 
-$('.submit-post-btn').click(function(){
+$('.submit-post-btn').click(function() {
   var postInput = $('#input-post').val();
   var nameInput = $('#input-name').val();
+  $('form')[0].reset();
   newPostTemplate(postInput,nameInput);
 });
