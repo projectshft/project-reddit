8afef8c69656c5c3127f4f5a2925a65509d3e4e3
timothybulla <83347577+timothybulla@users.noreply.github.com>
2021-07-24 14:02:41 -0400

    Attempting to remove bullet points

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 $('#submit').on('click', function () {
   var textInput = $('#message').val();
   var nameInput = $('#name').val();
-  
+  //need to remove bullet
   $('.posts').append('<li>' + textInput + ' - Posted By: ' + nameInput + '</li>');
-  //debugger;
 });
 
