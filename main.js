1b1651a085e96a8855ea6b98c370a8db1be20812
zdresser <42179244+zdresser@users.noreply.github.com>
2021-07-25 15:08:02 -0400

    click handlers all work

--- main.js
+++ main.js
@@ -1,52 +1,47 @@
-var posts = [];
-var id = 1;
-var comments_view = false;//use to toggle comments section on a post. Put here or in the post object? 
-
-var $comment_section = $('<form><div class="form-group><input type="text" class="form-control" id="comment-text" placeholder="Comment text"</div><div class="form-group"><input type="text" class="form-control" id="comment-author"placeholder="Your Name"</div><button type="button" class="btn btn-primary" id="submit-comment">Submit comment</button></form>'); //set layout for comment section. Here or in the submit-post click handler or elsewhere? 
-
 $("#submit-post").click(function () { 
    
   var author = $("#post-author").val();
   var text = $("#post-text").val();
-  var postData = {'id': id, 'author': author, 'text': text, comments: []};
-
-  posts.push(postData);
-
-  $("#post-author").html('Your Name');
-  $("#post-text").html('Post Text');
+  
 
   var remove = '<button class="remove">Remove</button>';
   var comments = '<button class="comments">Comments</button>';
+  var comment_section = '<div class="col-8 comment-section"><div id="new-comment"></div><form><div class="form-group">  <input type="text" class="form-control" id="comment-text"  placeholder="Comment text"</div><div class="form-group">  <input type="text" class="form-control" id="comment-author"  placeholder="Your Name"</div><button type="button" class="btn btn-primary" id="submit-comment">  Submit Comment</button></form></div>';  
   
-  $("#new-post").append('<p>' + remove + comments + text + " - Posted By: " + author + '<hr>'+ '</p>');//add comment section, too?
+  $("#new-post").append('<p class="post">' + remove + comments + text + " - Posted By: " + author  + '</p>' + comment_section );
 
-  id += 1;
+  $("form").trigger("reset");
+
+//need to reset form after submission
+ //$('#new-post').reset(); --doesnt work
 });
 
-$(document).on('click', '.remove',  function() {  
-  $(this).parent().next("hr").remove();
-  $(this).closest('p').remove();
+//removes post
+$(document).on('click','.remove',  function() { 
+  $(this).closest('div').find('.comment-section').remove();
+  $(this).parent().remove(); 
+  
+});
 
-});//would it be better to delete the post itself? This just hides the post.
+//opens and closes comment section on post
+$(document).on('click', '.comments', function () {  
+  $(this).closest('div').find('.comment-section').toggle();   
+});
 
-
-$(document).on('click', '.comments', function () {
-  if (comments_view) {//probably need to store comments_view in the post object, so the reference would be more complicated than 'comments_view'
-    //set display to none
-  } else {
-    //set display to block
-  }
-  $(this).next('p').append('comment');
-})
-
+//adds comment
 $(document).on('click', '#submit-comment', function() {
-  //add comment to post object (by id?) and display
-
+  
+  var commentAuthor = $("#comment-author").val();
+  var commentText = $("#comment-text").val();
   var remove_comment = '<button class="remove_comment">Remove</button>';
+  
+  
+ $(this).closest('.comment-section').find('#new-comment').append('<p class="comment">' + remove_comment + commentText + ' - Posted by: ' + commentAuthor + '<p>');
 
-  //$(this).append('<p>' + remove_comment + the rest of the comment + '<p>')
-})
+ $("form").trigger("reset");
+});
 
-$(document).on('click', '.remove_comment', function() {
-  $this.closest('p').remove();
+//removes comment
+$(document).on('click', '.remove_comment', function() { 
+  $(this).parent().remove();
 });
