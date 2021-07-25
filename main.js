91ccb4cd3aab7eeaac110ab2e4cedb3436732634
Nina Tan <ninavtan@gmail.com>
2021-07-25 17:18:21 -0400

    figuring out viewComments

--- main.js
+++ main.js
@@ -10,28 +10,44 @@ var loadPosts = {
   initial: function() {
     var $posts = $('.posts');
     if (postsAndCommentsArray.length == 1) {
-      $posts.append('<a data-id="0" class="remove">remove </a><a class="comments"> comments</a><li data-id="0">' + postsAndCommentsArray[0].originalPost + ' -  Posted by: ' + postsAndCommentsArray[0].author + '</li>');
+      $posts.append('<div data-id="0"><a data-id="0" class="remove">remove </a><a class="comments" data-id="0"> comments</a><li data-id="0">' + postsAndCommentsArray[0].originalPost + ' -  Posted by: ' + postsAndCommentsArray[0].author + '</li></div>');
       bindEvents.removePosts();
+      bindEvents.viewComments();
+
     };
   },
 
   allPosts: function() {
     var $posts = $('.posts');
     var i = postsAndCommentsArray.length - 1;
-    $posts.append('<a data-id="' + i + '" class="remove">remove </a><a class="comments"> comments</a><li data-id="' + i + '">' + postsAndCommentsArray[i].originalPost + ' -  Posted by: ' + postsAndCommentsArray[i].author + '</li>');
+    $posts.append('<div data-id="' + i + '"><a data-id="' + i + '"class="remove"> remove </a><a class="comments" data-id="' + i + '"> comments</a><li data-id="' + i + '">' + postsAndCommentsArray[i].originalPost + ' -  Posted by: ' + postsAndCommentsArray[i].author + '</li>');
     bindEvents.removePosts();
+    bindEvents.viewComments();
 
   }
 };
 
+
+
 var bindEvents = {
   removePosts: function () {
     $('.remove').on('click', function (e) {
-      // console.log($('li').data().id);
-      console.log($(e.target).data('id'));
       var dataId = $(e.target).data('id'); 
-      $('li[data-id="' + dataId + '"]').remove();
-    }) 
+      $('div[data-id="' + dataId + '"]').remove();
+    }); 
+  },
+
+  viewComments: function () {
+    $('.comments').on('click', function (e) {
+     var $commentBox = $('div.commentBox')[0];
+     console.log($commentBox);
+    
+     $commentBox.html(formTemplate);
+
+      var dataId = $(e.target).data('id');
+
+      console.log('clicked the comment box of ' + dataId + '!');
+    });
   }
 }
 
@@ -68,6 +84,7 @@ var submitPost = function() {
 loadPosts.initial();
 submitPost();
 
+var formTemplate = '<form id="commentInput" style="margin-top: 30px" onsubmit="event.preventDefault();"><div class="form-group"><textarea id="commentMessage" type="text"class="form-control"placeholder="comment text"></textarea><div class="form-group"><input id="commentName" type="text"class="form-control"placeholder="your name"></input></div><button id="submitComment" class="btn btn-primary">submit comment</button></form>';
 
 
 
