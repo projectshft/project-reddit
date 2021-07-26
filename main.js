cf1bc28949ae87bf01db1630c41abf627e6731a2
Nina Tan <ninavtan@gmail.com>
2021-07-26 19:56:51 -0400

    still figuring out comment functionality yuck

--- main.js
+++ main.js
@@ -7,27 +7,47 @@ var postsAndCommentsArray = [
 ]
 
 var loadPosts = {
-  initial: function() {
+ 
+  allPosts: function() {
+
     var $posts = $('.posts');
+
     if (postsAndCommentsArray.length == 1) {
       $posts.append('<div data-id="0"><a data-id="0" class="remove">remove </a><a class="comments" data-id="0"> comments</a><li data-id="0">' + postsAndCommentsArray[0].originalPost + ' -  Posted by: ' + postsAndCommentsArray[0].author + '</li></div>');
-      bindEvents.removePosts();
+      // this.attachComments();
       bindEvents.viewComments();
+    } else {
+      for (let i = (postsAndCommentsArray.length-1); i < postsAndCommentsArray.length; i++) {
+        $posts.append('<div data-id="' + i + '"><a data-id="' + i + '"class="remove"> remove </a><a class="comments" data-id="' + i + '"> comments</a><li data-id="' + i + '">' + postsAndCommentsArray[i].originalPost + ' -  Posted by: ' + postsAndCommentsArray[i].author + '</li>');
+        // this.attachComments();
+        bindEvents.viewComments();
 
-    };
+        
+
+      }
+    }
+    this.attachComments();
+    bindEvents.viewComments();
+    bindEvents.removePosts();
   },
 
-  allPosts: function() {
-    var $posts = $('.posts');
+  attachComments: function() {
+    // for (let i = 0; i < postsAndCommentsArray.length; i++) {
+
     var i = postsAndCommentsArray.length - 1;
-    $posts.append('<div data-id="' + i + '"><a data-id="' + i + '"class="remove"> remove </a><a class="comments" data-id="' + i + '"> comments</a><li data-id="' + i + '">' + postsAndCommentsArray[i].originalPost + ' -  Posted by: ' + postsAndCommentsArray[i].author + '</li>');
-    bindEvents.removePosts();
+    var formTemplate = '<form data-id="' + i + '" id="commentInput" style="margin-top: 30px" onsubmit="event.preventDefault();"><div class="form-group"><textarea id="commentMessage" type="text"class="form-control"placeholder="comment text"></textarea><div class="form-group"><input id="commentName" type="text"class="form-control"placeholder="your name"></input></div><button id="submitComment" class="btn btn-primary">submit comment</button></form>';
+
+    var $postCommentDiv = $('div[data-id="' + i + '"');
+    $postCommentDiv.append(formTemplate);
     bindEvents.viewComments();
 
+    // var i = postsAndCommentsArray.length - 1;
+  
   }
 };
 
 var bindEvents = {
+
   removePosts: function () {
     $('.remove').on('click', function (e) {
       var dataId = $(e.target).data('id'); 
@@ -40,23 +60,11 @@ var bindEvents = {
     var dataId = $(e.target).data('id');
     console.log('clicked the comment box of ' + dataId + '!');
 
-    var formTemplate = '<form data-id="' + dataId + '" id="commentInput" style="margin-top: 30px" onsubmit="event.preventDefault();"><div class="form-group"><textarea id="commentMessage" type="text"class="form-control"placeholder="comment text"></textarea><div class="form-group"><input id="commentName" type="text"class="form-control"placeholder="your name"></input></div><button id="submitComment" class="btn btn-primary">submit comment</button></form>';
-
-    
-    // grab the div we just clicked
-      var $clickedPostDiv = $('div[data-id="' + dataId + '"');
-    // append that div with the formTemplate containing the dataId
-      $clickedPostDiv.append(formTemplate);
-    
-      // grab the comment form of the div with our dataId
-      var $currentComments = $('form[data-id="' + dataId + '"');
-    
+    // grab the comment form of the div with our dataId
+    var $currentComments = $('form[data-id="' + dataId + '"');    
+    console.log('this is ' + dataId);
     // if form does not contain 'show' class, toggle class
-    if ($currentComments.hasClass('show')) {
-      $currentComments.removeClass('show');
-    } else {
-      $currentComments.addClass('show');
-    };
+    $currentComments.toggleClass('show');
     });
   }
 }
@@ -68,16 +76,18 @@ var submitPost = function() {
     var $userName = $('#name').val();      
     
     postsAndCommentsArray.push({originalPost: $userMessage, author: $userName});
-
-    console.log(postsAndCommentsArray);
-    loadPosts.allPosts();
+    console.log(postsAndCommentsArray.length);
     $('#postForm')[0].reset();
+    loadPosts.allPosts();
+    loadPosts.attachComments();
 
   });
-}
+};
 
-loadPosts.initial();
+
 submitPost();
+loadPosts.allPosts();
+
 
 
 
