8f94b37dd86adf0bec76c4210f760c7ff45f7ab2
timothybulla <83347577+timothybulla@users.noreply.github.com>
2021-07-26 08:46:38 -0400

    update

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 $('#submit').on('click', function () {
   var textInput = $('#message').val();
   var nameInput = $('#name').val();
-  //need to remove bullet
-  $('.posts').append('<li>' + textInput + ' - Posted By: ' + nameInput + '</li>');
-});
 
+    var removeComment = $('#remove-comment');
+    var addComment = $('#add-comment');
+
+  $('.posts').append('<li>' + textInput + ' - Posted By: ' + nameInput + '</li>');
+ 
+  $('li').on('click', function (e) {
+  
+    var $newPost = $(e.target);
+
+    $newPost.remove();
+  });
+});
