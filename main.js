5073fd317865ef6d06a2f045b55bfca330d5749a
Jason Stover <jstover8807@gmail.com>
2021-08-08 15:41:38 -0400

    Update 1.  Comment link issue

--- main.js
+++ main.js
@@ -1,44 +1,97 @@
-var removeClass = 'remove-link';
-var commentsClass = 'comments-link';
-
-
-
-
+var removeLinkClass = 'remove-link';
+var commentsLinkClass = 'comments-link';
+var commentsIdCounter = 0;
 
 var clicked = function () {    
     var userName = $('#name').val();
-    var post = $('#message').val();
-
+    var post = $('#message').val();     
     
     
-    
+    //Post variables for new HTML nodes
     var postsDiv = document.querySelector('.posts');
-    var newPostDiv = document.createElement('div');
+    var newPostDiv = document.createElement('div');    
     var newPostP = document.createElement('p');
     var newPostSpan = document.createElement('span');      
-    var newPostSpan2 = document.createElement('span');
-
-    newPostSpan.className = removeClass;
-    newPostSpan2.className = commentsClass;
-
+    var newPostSpan2 = document.createElement('span');   
+    
+    
+    //Assigning class names to post classes
+    newPostSpan.className = removeLinkClass;
+    newPostSpan2.className = commentsLinkClass;
+    
+    //Post Variables for new text and links
     var classOneTextNode = document.createTextNode('Remove ');
     var classTwoTextNode = document.createTextNode('Comments ');
-    var newPostTextNode = document.createTextNode(post + ' - Posted By: ' + userName);
+    var newPostTextNode = document.createTextNode(post + ' - Posted By: ' + userName);    
     
+    //Put text into span(s)
     newPostSpan.appendChild(classOneTextNode);
-    newPostSpan2.appendChild(classTwoTextNode);
+    newPostSpan2.appendChild(classTwoTextNode);     
+
+    //Put <span> (s) into <p>
     newPostP.appendChild(newPostSpan);
-    newPostP.appendChild(newPostSpan2)
-    newPostP.appendChild(newPostTextNode);
-
-    var newPostHR = document.createElement('hr');    
+    newPostP.appendChild(newPostSpan2);    
+    newPostP.appendChild(newPostTextNode);    
     
-    newPostDiv.append(newPostP);
-    newPostDiv.append(newPostHR);
+    //Add the new post to the div
+    newPostDiv.append(newPostP);   
+    $('#comments').clone().attr('id', 'comments' + commentsIdCounter++).insertAfter('#comments').appendTo(newPostDiv);
 
-    postsDiv.append(newPostDiv);
+    //Add new div to the HTML file
+    postsDiv.append(newPostDiv);   
+     
+    
+    
+    
+
+    
+
+    //Call click functions for remove and comment links
+    $('.remove-link').on('click', removed);
+    $('.comments-link').on('click', comments);
+
+}
+
+var removed = function () {    
+    $(this).parent().parent().remove();
+    //not removing comment text boxes as it should
+}
+
+var comments = function () {
+    if ($(this).closest('.posts').children().hide()) {
+        $(this).closest('.posts').children().show();
+    } else {
+        $(this).closest('.posts').children().hide();
+    }
+    
+    
 
 }
 
 $('#submit-post').on('click', clicked);
+$('.remove-link').on('click', removed);
+$('.comments-link').on('click', comments);
 
+
+/* Comment box for comment code
+//Comment variables for new HTML nodes
+var commentsDiv = document.querySelector('.comments');
+var newCommentDiv = document.createElement('div');
+var newCommentP = document.createElement('p');
+var newCommentSpan = document.createElement('span');
+
+//Assinging class names to comment class
+newCommentSpan.className = removeLinkClass;
+
+//Comment Variables for new text and links
+    var commentClassText = document.createTextNode('Remove ');
+    var commentTextNode = document.createTextNode(comment + ' - Posted By: ' + commentName);
+
+     newCommentSpan.appendChild(commentClassText);  
+
+     newCommentP.appendChild(commentClassText);
+    newCommentP.appendChild(commentTextNode);
+
+    newCommentDiv.append(newCommentP);  
+    
+    commentsDiv.append(newCommentDiv); */
