2f15e59122872da6e2f13261b604189d463f0304
Steve Husmann <stevehoose@Steves-iMac.local>
2021-08-12 16:02:06 -0500

    fixed edit function

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+var currentlyEditing = false;
+
 //new post function
 var newPost = function () {
   var userName = $('#user-name').val();
   var newPostText = $('#new-post-text').val();
   
   if (userName && newPostText) {
-    $('.posts').append('<div class="row individual-post"><div class="col-sm-10 each-post data-postID="' + postID +'"><span id="editThis">' + newPostText + '</span><br> <br>Posted by ' + userName + '</div>' +
-    '<div class="col-sm-1"><button type="button" class="btn btn-warning btn-sm editPost">Edit</button><br><button type="button" class="btn btn-danger btn-sm removePost">Remove</button></div></div>');
+    var d = new Date();
+    var n = d.toLocaleTimeString();
+    
+    $('.posts').append('<div class="row individual-post" data-postID=' + postID +'> <div class="col-sm-10 each-post"><span id="editThis">' + newPostText + '</span><br> <br>Posted by ' + userName + ' at ' + n + '.</div> <div class="col-sm-1"><button type="button" class="btn btn-warning btn-sm editPost">Edit</button><br><button type="button" class="btn btn-danger btn-sm removePost">Remove</button></div></div>');
 
     $('#post-form').trigger("reset");
   } else if (!userName){
@@ -26,33 +30,54 @@ var removePost = function() {
 var editPost = function() {
   // var $postLocation = $(this).closest('.individual-post').find('.editThis');
   // console.log($postLocation.html());
-  var $postLocation = $(this).parent().prev().children();
-  var tempStorage = $(this).parent().prev().children().html();
+  // var $postLocation = $(this).parent().prev().children();
+  // var tempStorage = $(this).parent().prev().children().html();
   // var tempStorage = $postLocation.html();
+  // editPostID = $(this).closest('.individual-post').attr('data-postID');
+  // editPostID = Number(editPostID);
+  // console.log(editPostID);
+  // var tempStorage = null;
 
-  $('#edited-post-text').html(tempStorage);
-  $('#post-comment').css('display','none');
-  $('#edit-comment').css('display','inline');
+  if (!currentlyEditing) {
+    currentlyEditing = true;
+    $(this).closest('.individual-post').append(editPostTemplate);
+    var $postLocation = $(this).closest('.individual-post').find('#editThis');
+    var tempStorage = $postLocation.html();
+    $('#edited-post-text').html(tempStorage);
 
-  $('#cancel-edit-button').click(function () {
-    $('#edit-form').trigger("reset");
-    $('#post-comment').css('display','inline');
-    $('#edit-comment').css('display','none');
-  });
+    $('#post-comment').css('display','none');
+    // $('#edit-comment').css('display','inline');
 
-  $('#edit-post-button').click(function () {
-    var editedPostText = $('#edited-post-text').val();
-    $postLocation.html(editedPostText);
-    $('#post-comment').css('display','inline');
-    $('#edit-comment').css('display','none');
-  });
+    $('#cancel-edit-button').click(function () {
+      currentlyEditing = false;
+      $('#post-comment').css('display','inline');
+      $(this).closest('#edit-comment').remove();
+    });
+
+    $('#edit-post-button').click(function () {
+      currentlyEditing = false;
+      var editedBy = $('#edited-by').val();
+      var d = new Date();
+      var n = d.toLocaleTimeString();
+      if(editedBy) {
+        var editedPostText = $('#edited-post-text').val();
+        $postLocation.html(editedPostText);
+        $('#post-comment').css('display','inline');
+        $(this).closest('.individual-post').find('.each-post').append('<span> (Edited by ' + editedBy + ' at ' + n + ')</span>');
+        $(this).closest('#edit-comment').remove();
+      } else {
+        alert("Before you can make these edits, you need to tell us who the editor is!");
+      }
+      
+    });
+  }
 }
+
+//event listeners
+$('#submit-post').click(newPost);
 
 //global variables
 var postID = 0;
 
-//event listeners
-$('#submit-post').click(newPost);
-$('.editPost').on("click", editPost);
-
+var editPostTemplate ='<div class="row" id="edit-comment"><div class="col-sm-10"><form id="edit-form"><p>Edit Comment</p><div class="form-group"><input type="text" class="form-control" id="edited-by" aria-describedby="Edited By" placeholder="Edited by"></div><div class="form-group"><textarea class="form-control" id="edited-post-text" rows="10"></textarea></div><button type="button" class="btn btn-warning" id="edit-post-button">Make Edits</button><button type="button" class="btn btn-default" id="cancel-edit-button">Cancel</button></form></div></div>';
 
