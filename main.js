49a3d352cf10e2bc6e9db91e5491da28911e32e6
j-d-hunt <jdhunt@Jordans-MacBook-Pro.local>
2021-08-24 21:31:45 -0500

    Newest Changes

--- main.js
+++ main.js
@@ -11,21 +11,3 @@ $('.posts').on('click', '#remove', function (e) {
   
   $element.remove();
 });
-
-
-
-//  step 1: append buttons on line 4, have all HTML wrapped in <div> with class of post -- add in entire comments section of html
-//      every posts div should have a div of comments
-//      in CSS you can hide comments
-//  step 2: remove 
-//  step 3: listen for clicks hide/show buttons every post 
-
-// //$('.posts').hover(function() {
-//   $(this).css('text-decoration','none');
-//   },
-//   function() {
-//   $(this).css('text-decoration','underline');
-//   });
-
-
-// "<button class='btn btn-sm' id='remove'><font color='#0000ff'>remove</font></button>" + " " + "<button class='btn btn-sm'><font color='#0000ff'>comments</font></button>"
