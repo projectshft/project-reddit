3a77e75d47a04d4b387a3e8c61e073e8bd39030c
j-d-hunt <jdhunt@Jordans-MacBook-Pro.local>
2021-08-24 21:46:08 -0500

    Updated

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 $('#submit').click(function () {
   var post = $('#post').val();
   var name = $('#name').val();
-  $('.posts').append("<div id='postContainer'><span id='remove'><font color='#0000ff'>Remove</font></span>" + " " + "<span id='comment'><font color='#0000ff'>Comment</font></span>" +  " " + post + ' - Posted By: ' + name + "</div>");
+  $('.posts').append("<div id='postContainer'><span id='remove'>Remove</span>" + " " + "<span id='comment'>Comment</span>" +  " " + post + ' - Posted By: ' + name + "</div>");
  
+});
+
+$('#comment').click(function () {
+
 });
 
 $('.posts').on('click', '#remove', function (e) {
