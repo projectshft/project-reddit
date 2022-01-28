feb594756981faa970a6efcf1103aaa0ce49d71f
Ian Holmes <iqholmes@gmail.com>
2022-01-28 17:28:27 -0500

    posts submitting and rendering to page

--- main.js
+++ main.js
@@ -2,16 +2,26 @@ var posts = [];
 
 var submitPost = function() {
   var text = $('#post-text').val();
-  var name = $('#poster-name').val()
+  var posterName = $('#poster-name').val();
 
   var post = {
-    name: name,
+    name: posterName,
     text: text,
+    comments: [],
   };
 
   posts.push(post);
+
+  $('.posts').append(`<div class="post"><p>${post.text} - Posted By: ${post.name}</p><hr></div>`)
+}
+
+var clearForm = function() {
+  $('#post-text').val('');
+  $('#poster-name').val('');
 }
 
 $('#submit').click(function(){
   submitPost();
+  clearForm();
 });
+
