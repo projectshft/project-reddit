ba1b3e9850ae966b9c8bec6943f5e4fd8b292395
Michael Grumbine <mgrumbine3@gmail.com>
2022-01-28 20:31:59 -0800

    added basic functionality to the submit button

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var 
+var $postSubButton = $('#post-button');
+var $authorInput = $('#post-author');
+var $messageInput = $('#post-message');
+var $posts = $('.posts');
+
+
+$postSubButton.on('click', function () {
+  var message = $messageInput.val();
+  var author = $authorInput.val();
+
+  $posts.append('<p>' + message + ' - <span class="author-span">Posted By: ' + author + '</span><hr>');
+});
