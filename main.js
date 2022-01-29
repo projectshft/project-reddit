b598d4bc6fc50de085f26d73e881658f6136a8b3
Nathan Drake <nathandrakedrums@gmail.com>
2022-01-29 13:02:55 -0600

    part 1 done

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
-document.getElementById('submit').addEventListener('click', () => {
-  const name = document.getElementById('name').value;
-  const message = document.getElementById('message').value;
+// document.getElementById('submit').addEventListener('click', () => {
+//   const name = document.getElementById('name').value;
+//   const message = document.getElementById('message').value;
 
-  const postsDiv = document.querySelector('.posts');
+//   const postsDiv = document.querySelector('.posts');
 
-  const newPostDiv = document.createElement('div');
+//   const newPostDiv = document.createElement('div');
 
-  const newMessageP = document.createElement('p');
-  const newMessageText = document.createTextNode(message);
-  newMessageP.appendChild(newMessageText);
+//   const newMessageP = document.createElement('p');
+//   const newMessageText = document.createTextNode(message);
+//   newMessageP.appendChild(newMessageText);
 
-  const newNameP = document.createElement('p');
-  const newNameText = document.createTextNode(`Posted By; ${name}`);
-  newNameP.appendChild(newNameText);
+//   const newNameP = document.createElement('p');
+//   const newNameText = document.createTextNode(`Posted By; ${name}`);
+//   newNameP.appendChild(newNameText);
 
-  const newPostHR = document.createElement('hr');
+//   const newPostHR = document.createElement('hr');
 
-  newPostDiv.append(newMessageP);
-  newPostDiv.append(newNameP);
-  newPostDiv.append(newPostHR);
+//   newPostDiv.append(newMessageP);
+//   newPostDiv.append(newNameP);
+//   newPostDiv.append(newPostHR);
  
-  postsDiv.append(newPostDiv);
-})
+//   postsDiv.append(newPostDiv);
+// })
+
+const submitButton = $('#submit');
+const postsDiv = $('.posts');
+
+const submitFunc = function() {
+  const message = $('#message').val();
+  const name = $('#name').val();
+  
+  const messageP = `<p>${message}</p>`;
+  const nameP = `<p><strong>Posted by : ${name}</strong></p>`;
+
+  const buttonClass = '<button class="postBtns btn btn-primary btn-xs">'
+  const commentButton = buttonClass + 'Comment</button>';
+  const editButton = buttonClass + 'Edit</button>';
+  const deleteButton = buttonClass + 'Delete</button>';
+
+  const newPost = messageP + nameP + commentButton + editButton + deleteButton + '<hr>';
+
+  postsDiv.append(newPost);
+}
+
+submitButton.on('click', submitFunc);
