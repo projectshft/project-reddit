276f4c12d76bd60674018c4493bc37e8e968f0ad
Nathan Drake <nathandrakedrums@gmail.com>
2022-01-29 13:19:59 -0600

    clear input on submit

--- main.js
+++ main.js
@@ -1,46 +1,27 @@
-// document.getElementById('submit').addEventListener('click', () => {
-//   const name = document.getElementById('name').value;
-//   const message = document.getElementById('message').value;
-
-//   const postsDiv = document.querySelector('.posts');
-
-//   const newPostDiv = document.createElement('div');
-
-//   const newMessageP = document.createElement('p');
-//   const newMessageText = document.createTextNode(message);
-//   newMessageP.appendChild(newMessageText);
-
-//   const newNameP = document.createElement('p');
-//   const newNameText = document.createTextNode(`Posted By; ${name}`);
-//   newNameP.appendChild(newNameText);
-
-//   const newPostHR = document.createElement('hr');
-
-//   newPostDiv.append(newMessageP);
-//   newPostDiv.append(newNameP);
-//   newPostDiv.append(newPostHR);
- 
-//   postsDiv.append(newPostDiv);
-// })
-
-const submitButton = $('#submit');
+const submitBtn = $('#submit');
 const postsDiv = $('.posts');
+const nameInput = $('#name');
+const messageInput = $('#message');
 
 const submitFunc = function() {
-  const message = $('#message').val();
-  const name = $('#name').val();
+  const nameVal = nameInput.val();
+  const messageVal = messageInput.val();
   
-  const messageP = `<p>${message}</p>`;
-  const nameP = `<p><strong>Posted by : ${name}</strong></p>`;
+  const messageP = `<p>${messageVal}</p>`;
+  const nameP = `<p><strong>Posted by : ${nameVal}</strong></p>`;
 
-  const buttonClass = '<button class="postBtns btn btn-primary btn-xs">'
-  const commentButton = buttonClass + 'Comment</button>';
-  const editButton = buttonClass + 'Edit</button>';
-  const deleteButton = buttonClass + 'Delete</button>';
+  // creates buttons for each post
+  const buttonPrefix = '<button class="postBtns btn btn-primary btn-xs'
+  const commentBtn = buttonPrefix + ' commentBtn">Comment</button>';
+  const editBtn = buttonPrefix + ' editBtn">Edit</button>';
+  const deleteBtn = buttonPrefix + ' deleteBtn">Delete</button>';
 
-  const newPost = messageP + nameP + commentButton + editButton + deleteButton + '<hr>';
+  const newPost = messageP + nameP + commentBtn + editBtn + deleteBtn + '<hr>';
 
   postsDiv.append(newPost);
+
+  nameInput.val('');
+  messageInput.val('');
 }
 
-submitButton.on('click', submitFunc);
+submitBtn.on('click', submitFunc);
