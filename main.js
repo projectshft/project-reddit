80f70735adce60fc7ea67fd37114838a2d9e855e
Michael Grumbine <mgrumbine3@gmail.com>
2022-01-29 20:50:09 -0800

    found syntax error in HTML

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ var $messageInput = $('.post-message');
 var $posts = $('.posts');
 
 
-$postSubButton.on('click', function () {
-  var message = $messageInput.val();
-  var author = $authorInput.val();
-  var postString = '<p>' + message + ' - <span class="author-span">Posted By: ' + author + '</span><hr>';
+  $postSubButton.on('click', function (event) {
+    var message = $messageInput.val();
+    var author = $authorInput.val();
+    
+    var postContent = '<p>' + message + ' - <span>Posted By: ' + author + '</span><hr>';
+  
+    $posts.append(postContent);
+  });
 
-  $posts.append(postString);
-});
