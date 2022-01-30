25958e7226454dc731736c49d516cbb56e819b96
Nathan Drake <nathandrakedrums@gmail.com>
2022-01-30 11:57:37 -0600

    delete works, working on comments

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
-const submitBtn = $('#submit');
-const postsDiv = $('.posts');
-const nameInput = $('#name');
-const messageInput = $('#message');
+const $submitBtn = $('#submit');
+const $postsDiv = $('.posts');
+const $nameInput = $('#name');
+const $messageInput = $('#message');
 
-const submitFunc = function() {
-  const nameVal = nameInput.val();
-  const messageVal = messageInput.val();
-  
+const submitFunc = () => {
+  const nameVal = $nameInput.val();
+  const messageVal = $messageInput.val();
+
+  // checks that input fields are not blank, creates post otherwise
+  if (!nameVal || !messageVal) {
+    alert('Input fields cannot be blank.');
+  } else {
+    const newPost = createPost(nameVal, messageVal);
+    $postsDiv.append(newPost);
+
+    const $posts = $('post');
+    $posts.on('click', '.delete-btn', e => $(e.target).parent().remove());
+
+    // clears input fields on submit
+    $nameInput.val('');
+    $messageInput.val('');
+  }
+}
+
+const createPost = (nameVal, messageVal) => {
   const messageP = `<p>${messageVal}</p>`;
   const nameP = `<p><strong>Posted by : ${nameVal}</strong></p>`;
 
   // creates buttons for each post
-  const buttonPrefix = '<button class="postBtns btn btn-primary btn-xs'
-  const commentBtn = buttonPrefix + ' commentBtn">Comment</button>';
-  const editBtn = buttonPrefix + ' editBtn">Edit</button>';
-  const deleteBtn = buttonPrefix + ' deleteBtn">Delete</button>';
+  const buttonPrefix = '<button class="post-btns btn btn-primary btn-xs';
+  const commentBtn = buttonPrefix + ' comment-btn">Comment</button>';
+  const editBtn = buttonPrefix + ' edit-btn">Edit</button>';
+  const deleteBtn = buttonPrefix + ' delete-btn">Delete</button>';
 
-  const newPost = messageP + nameP + commentBtn + editBtn + deleteBtn + '<hr>';
-
-  postsDiv.append(newPost);
-
-  nameInput.val('');
-  messageInput.val('');
+  // returns a string that creates the entire post
+  return '<post>' + messageP + nameP + commentBtn + editBtn + deleteBtn + '<hr></post>';
 }
 
-submitBtn.on('click', submitFunc);
+$submitBtn.on('click', submitFunc);
