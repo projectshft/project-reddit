5584f9c503edba9ae706920418c4d5e5a897cb3d
Dio <cdebon97@outlook.com>
2022-01-30 16:57:29 -0600

    Comments added but not functional

--- main.js
+++ main.js
@@ -6,22 +6,34 @@ $button.on('click', function () {
 
   var message = document.createElement("p")
   var name = document.createElement("p")
-  var newDiv = document.createElement("div");
-  var comments = document.createElement("div");
+  var $newDiv = $(document.createElement("div"));
+  var $posts = $('.posts');
 
-
-
+  //Creating variables for user messages and names
   message.append(document.createTextNode($('#message').val()));
   name.append(document.createTextNode("Posted By: " + $('#name').val()));
-  newDiv.append(message);
-  newDiv.append(name);
 
-  var comments = document.createElement("div");
-  newDiv.append(comments);
+  $newDiv.append(message);
+  $newDiv.append(name);
+  $newDiv.attr("class","newPost");
+
+  //Creating comment form section
+  var $newComment = $(document.createElement("form"));
+  $newComment.append($("<div></div>").attr("class", "form-group").append($("<input></input>").attr({class: "for-control", id: `comName-${Date($.now())}`, class: "comment", type: "text", placeholder: "Name"})), $("<div></div>").attr("class", "form-group").append($("<input></input>").attr({class: "for-control", id: `comMessage-${Date($.now())}`, class: "comment", type: "text", placeholder: "Message"})));
+
+  // $newComment.append($("<input></input>").attr({class: "for-control", class: `comMessage-${Date($.now())}`, type: "text", placeholder: "Message"}));
+  // $newComment.append($("<input></input>").attr({class: "for-control", type: "text", class: `comName-${Date($.now())}`, placeholder: "Name"}));
+
+  var $commentButton = $("<button></button>").attr({class: "btn btn-primary"});
+
+  $newComment.append($commentButton);
+
+  $newDiv.append($newComment);
+ 
+
   
-  var $posts = $('.posts');
-  $posts.append(newDiv);
-  
+  $posts.append($newDiv);
+
   // $posts.append(newDiv);
 
   // var newDiv = document.createElement('div');
