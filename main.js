a9d68e4ff61a126f4241afcb22b6802cac72a8b6
Michael Grumbine <mgrumbine3@gmail.com>
2022-01-30 16:14:46 -0800

    final commit. All done except extensions

--- main.js
+++ main.js
@@ -4,29 +4,36 @@ var $messageInput = $('.post-message');
 var $posts = $('.posts');
 var numOfPosts = 0;
 
-var addCommentButtonListener = function (postNumber, $comments) {
+var submitCommentListener = function (postNumber, $comments) {
+  var numOfComments = 0;
   var $button = $('.comment-button-' + postNumber);
 
   $button.on('click', function () {
+    numOfComments += 1;
     var commentAuthor = $('.comment-author-' + postNumber).val();
     var commentMessage = $('.comment-message-' + postNumber).val();
+    var $newComment = $('<div class=comment-num-' + numOfComments + '><p>' + commentMessage + ' - <span>Posted By: ' + commentAuthor + '</span></p><button class="btn btn-danger">Delete Comment</button><hr /></div>');
     
-    $comments.append('<p>' + commentMessage + ' - <span>Posted By: ' + commentAuthor + '</span></p><button class="btn btn-danger">Delete Comment</button><hr />');
+    $comments.append($newComment);
     
+    $comments.find($newComment).find('button').on('click', function () {
+      $newComment.remove();
+    });
   });
-  debugger;
 };
 
 var createInitialPost = function (postNumber, postMessage, postAuthor) {
-  var $mainPostDiv = $('<div class="post-' + postNumber + '"><p>' + postMessage + ' - <span>Posted By: ' + postAuthor + '</span></p><button class="btn btn-primary">Comments</button><button class="btn btn-danger">Delete</button><hr /></div>');
+  var $mainPostDiv = $('<div class="post-' + postNumber + '"><p><strong>' + postMessage + ' - <span>Posted By: ' + postAuthor + '</span></strong></p><button class="btn btn-primary show_">Comments</button><button class="btn btn-danger del">Delete</button><hr /></div>');
 
   var $commentsHideWrapper = $('<section class="hide-' + postNumber + '"></section>');
 
   var $commentsDiv = $('<div class="comments-' + postNumber + '"></div>');
 
-  var $commentsInputDiv = $('<div class="comments-input-' + postNumber + '"><div class="container"><div class="row"><div class="col-sm-7 col-offset-6"><textarea name="message" class="comment-message-' + postNumber + '" placeholder="Comment Text"></textarea></div></div></div><div class="container"><div class="row"><div class="col-sm-7 col-offset-6"><input type="text" name="author" class="comment-author-' + postNumber + '" placeholder="Your Name"/></div></div></div><div class="container"><div class="row"><div class="col-sm-7 col-offset-6"><button class="btn btn-primary comment-button-' + postNumber +'">Submit Comment</button></div></div></div></div>');
+  var $commentsInputDiv = $('<div class="comments-input-' + postNumber + '"><div class="container"><div class="row"><div class="col-sm-7 col-offset-6"><textarea name="message" class="width-handle comment-message-' + postNumber + '" placeholder="Comment Text"></textarea></div></div></div><div class="container"><div class="row"><div class="col-sm-7 col-offset-6"><input type="text" name="author" class="width-handle comment-author-' + postNumber + '" placeholder="Your Name"/></div></div></div><div class="container"><div class="row"><div class="col-sm-7 col-offset-6"><button class="btn btn-primary comment-button-' + postNumber +'">Submit Comment</button></div></div></div></div>');
+
 
   $commentsHideWrapper.append($commentsDiv, $commentsInputDiv);
+  $commentsHideWrapper.toggle(false);
 
   return $('<div class="spacer"></div>').append($mainPostDiv, $commentsHideWrapper);
 }
@@ -37,7 +44,17 @@ $postSubButton.on('click', function (event) {
   numOfPosts += 1;
   
   var postContent = createInitialPost(numOfPosts, message, author);
+
   $posts.append(postContent);
-  addCommentButtonListener(numOfPosts, $('.comments-' + numOfPosts));
+
+  var $showCommentsButton = $('.post-' + numOfPosts + ' .show_');
+  var $removePostButton = $('.post-' + numOfPosts + ' .del');
+  $showCommentsButton.on('click', function () {
+    postContent.find('.hide-' + numOfPosts).toggle();
+  });
+  $removePostButton.on('click', function () {
+    postContent.remove();
+  });
+  submitCommentListener(numOfPosts, $('.comments-' + numOfPosts));
 });
 
