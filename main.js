315cdd499861d89d52539dc0d9be2ad45708ed6c
Dio <cdebon97@outlook.com>
2022-01-31 00:40:01 -0600

    Comments are now working, but they still cannot be removed

--- main.js
+++ main.js
@@ -11,13 +11,14 @@ var forum = {
     $newPost.append($("<p></p>").text($message));
     $newPost.append($("<p></p>").text(`Posted By: ${$name}`));
 
-    var $comments = $("<div></div>").attr("class", "comment-section");
+    var $comments = $("<div></div>").attr("class", "comment-section").css("display","none");
 
-    var $commentToggle = $newPost.append("<p>Show/hide Comments</p>").attr("class", "toggle-comment");
+    var $commentToggle = $newPost.append("<p>Show/hide Comments</p>")
+  
+    // var $commentButton = $commentsInput.append($("<button></button>").attr("class", "btn btn-primary comment-button").text("Comment"));
 
-    var $commentsInput = $comments.append($("<form></form>").append($("<div></div>").attr("class", "form-group").append($("<input></input>").attr({type: "text", class: "form-control", placeholder: "Name"})), $("<div></div>").attr("class", "form-group").append($("<input></input>").attr({type: "text", class: "form-control", placeholder: "Message"}))));
-
-    var $commentButton = $comments.append($("<button></button>").attr("class", "btn btn-primary").text("Comment"));
+    var $commentsInput = $comments.append($("<form></form>").append($("<div></div>").attr("class", "form-group name").append($("<input></input>").attr({type: "text", class: "form-control commName", placeholder: "Name"})), $("<div></div>").attr("class", "form-group message").append($("<input></input>").attr({type: "text", class: "form-control commMessage", placeholder: "Message"}))), $("<button></button>").attr("class", "btn btn-primary comment-button").text("Comment"));
+    
 
     $comments.append($commentsInput);
     $newPost.append($comments);
@@ -29,13 +30,20 @@ var forum = {
       $posts.append($newPost);
     }
   },
-  createComment: function () {
-    
+  createComment: function (event) {
+    console.log($(event.target).hasClass("comment-button"));
+    if ($(event.target).hasClass("comment-button")) {
+    var parentComm = $(event.target).closest(".comment-section");
+    var $commName = parentComm.find(".commName").val();
+    var $commMessage = parentComm.find(".commMessage").val();
+
+    parentComm.append($(`<p>${$commMessage}</p>`), $(`<p>Posted by: ${$commName}</p>`));
+    }
+
   },
   hideComments: function (event) {
+  
    var $comments = $(event.target).siblings(".comment-section");
-
-   console.log($comments);
 
    $comments.toggle("display");
   //  if ($comments.attr("display") == )
@@ -46,3 +54,5 @@ var forum = {
 $button.on("click", forum.createPost);
 
 $("body").on("click", forum.hideComments);
+
+$("body").on("click", forum.createComment);
