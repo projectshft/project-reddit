3ab0b14bbc7c901e8365d3bd2856a35c827e82cb
Jeremynielsen <95871403+Jeremynielsen@users.noreply.github.com>
2022-02-13 12:47:11 -0500

    Add files via upload
    Functional but not perfect.

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
-var userName = $('#name');
-var message = $('#message');
+var $userName = $('#name');
+var $message = $('#message');
 var submit = $('.submit-btn');
-var $remCom = ('<strong>X </strong>')
 
 $('.submit-btn').on('click', function () {
-  var nameVal = $(userName).val();
-  var msgVal = $(message).val();
+  var $nameVal = $userName.val();
+  var $msgVal = $message.val();
 
-  if (msgVal === "") {
-    window.alert("Your message cannot be empty!");
+  if ($msgVal === "" || $nameVal === "") {
+    window.alert("please complete all fields");
   }
 
-  if (msgVal !== "") {
-    $('.comments').append($remCom + msgVal + ' - Written by: ' + nameVal + '<br>');
+  if ($msgVal !== "" && $nameVal !== "") {
+    $('.comments').append(`<div class ="post-row"<div><p id="post-content"><button class="deletebtn">Delete</button><button class="comment-btn">Comment</button> "${$msgVal}" - Posted By: <span><strong>${$nameVal}</strong></span></p></div></div><hr>`);
   }
 
-  $(userName).val("");
-  $(message).val("");
+  $userName.val("");
+  $message.val("");
+});
+ 
+
+$('.comments').on('click', '.deletebtn', function (e) { 
+  $(e.target).parent().remove();
 });
 
-$('.comments').on('click', function (e) {
-  var $element = $(e.target);
+$('.comments').on('click', '.comment-btn', function () {
+  $('.comment-form').toggle('slow');
+ }); 
 
-  $element.remove();
-  
-})
+$('#comment-submit').on('click', function () {
+ var $commentName = $('#comment-name').val();
+ var $commentMessage = $("#comment-message").val();
+
+$('.post-row').append(`<div class="comment-row"><p id="comment-content"><button class="deletebtn">Delete</button> "${$commentMessage}" - Posted By: <span><strong>${$commentName}</strong></span></p></div>`);  
+
+$('.comment-form').trigger('reset');
+
+$('.commentbtn').on('click', function () {
+    $('.comment-row').toggle();
+ });
+
+ $('.comment-row').on('click', '.deletebtn', function (e) { 
+  $(e.target).parent().remove();
+});
+});
