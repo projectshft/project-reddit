ac0177696205c0b30c5758aa8b8f463ed47b50dc
Emily Freeman <freeman.ecf@gmail.com>
2022-02-13 05:52:34 -0500

    add remove and comment button

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ $('.submit-button').on('click', function () {
   var $postText = $('.post-text').val();
   var $yourName = $('.your-name').val();
   $('.posts').append('<div>' + $postText + ' - Posted By: ' +$yourName + '</div>');
+  createButtons();
 })
 
+var createButtons = function () {
+  var $removeButton = $('<a>Remove</a>');
+  var $commentButton = $('<a>Comment</a>');
+  $('.posts').append($removeButton).append($commentButton);
+}
