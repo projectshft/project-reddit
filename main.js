56ff971ab1a67cbe22cdadb4c90e875e49d5285b
Seth Chun <schun.code@gmail.com>
2022-05-12 15:57:53 -0500

    added jQuery, removed Comment Btn

--- main.js
+++ main.js
@@ -2,18 +2,14 @@
 
     // "Create a new post" means the user can fill out info, hit submit, and see post appear in newsfeed
 
-// [] New posts to the newsfeed should have an option to leave a comment, and an option to remove the post
+// [] New posts to the newsfeed should have a button option to remove the post
 
-    // These should appear as buttons
-
-// New Post
+// New Post Creation/Submit
 
 var button = document.getElementById('post-creation-submit-btn');
 
-var postNumber = 0;
-
 button.addEventListener('click', function () {
-    var newsfeed = document.querySelector('.newsfeed');
+    var newsfeed = $('.newsfeed');
 
     var postText = document.getElementById('post-text').value;
     var postAuthor = document.getElementById('post-author').value;
@@ -29,23 +25,15 @@ button.addEventListener('click', function () {
     var newPostAuthorTextNode = document.createTextNode('Posted By: ' + postAuthor);
     newPostAuthor.appendChild(newPostAuthorTextNode);
 
-    var newPostCommentButton = document.createElement('button');
-    var newPostCommentButtonTextNode = document.createTextNode('Comments');
-    newPostCommentButton.appendChild(newPostCommentButtonTextNode);
-    newPostCommentButton.setAttribute('type', 'button');
-    newPostCommentButton.setAttribute('class', 'btn btn-info commentButton');
-
     var newPostDeleteButton = document.createElement('button');
     var newPostDeleteButtonTextNode = document.createTextNode('Delete Post');
     newPostDeleteButton.appendChild(newPostDeleteButtonTextNode);
-    newPostCommentButton.setAttribute('type', 'button');
     newPostDeleteButton.setAttribute('class', 'btn btn-danger deleteButton');
 
     var newPostHR = document.createElement('hr');
 
     newPostContainer.append(newPostBody);
     newPostContainer.append(newPostAuthor);
-    newPostContainer.append(newPostCommentButton);
     newPostContainer.append(newPostDeleteButton);
     newPostContainer.append(newPostHR);
 
@@ -57,12 +45,6 @@ button.addEventListener('click', function () {
     };
     var deleteButton = document.getElementsByClassName('deleteButton')[0];
     deleteButton.addEventListener('click', deletePost);
-
-    var commentOnPost = function () {
-        console.log('hello');
-    };
-    var commentButton = document.getElementsByClassName('commentButton')[0];
-    commentButton.addEventListener('click', commentOnPost);
 });
 
 /* Dummy Text:
