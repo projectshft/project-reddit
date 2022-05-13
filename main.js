718aeaeee548bdebf2a04f731a2abc30746f9bc8
Seth Chun <schun.code@gmail.com>
2022-05-13 16:14:00 -0500

    Refactored vanilla JS to jQuery

--- main.js
+++ main.js
@@ -1,51 +1,60 @@
-// [x] A user should be able to use the form at the bottom of the page to create a new post.
+// var button = document.getElementById('post-creation-submit-btn');
 
-    // "Create a new post" means the user can fill out info, hit submit, and see post appear in newsfeed
+// button.addEventListener('click', function () {
+//     var newsfeed = $('.newsfeed');
 
-// [] New posts to the newsfeed should have a button option to remove the post
+//     var postText = $('textarea#post-text').val();
+//     var postAuthor = $('textarea#post-author').val();
 
-// New Post Creation/Submit
+//     var newPostContainer = document.createElement('div');
+//     newPostContainer.setAttribute('class', 'user-post');
 
-var button = document.getElementById('post-creation-submit-btn');
+//     var newPostBody = document.createElement('p');
+//     var newPostBodyTextNode = document.createTextNode(postText);
+//     newPostBody.appendChild(newPostBodyTextNode);
 
-button.addEventListener('click', function () {
-    var newsfeed = $('.newsfeed');
+//     var newPostAuthor = document.createElement('p');
+//     var newPostAuthorTextNode = document.createTextNode('Posted By: ' + postAuthor);
+//     newPostAuthor.appendChild(newPostAuthorTextNode);
 
-    var postText = document.getElementById('post-text').value;
-    var postAuthor = document.getElementById('post-author').value;
+//     var newPostDeleteButton = document.createElement('button');
+//     var newPostDeleteButtonTextNode = document.createTextNode('Delete Post');
+//     newPostDeleteButton.appendChild(newPostDeleteButtonTextNode);
+//     newPostDeleteButton.setAttribute('class', 'btn btn-danger');
+//     newPostDeleteButton.addEventListener('click', function () {
+//         var post = $(this).closest('.user-post');
+//         post.remove();
+//     });
 
-    var newPostContainer = document.createElement('div');
-    newPostContainer.setAttribute('class', 'user-post');
+//     var newPostHR = document.createElement('hr');
 
-    var newPostBody = document.createElement('p');
-    var newPostBodyTextNode = document.createTextNode(postText);
-    newPostBody.appendChild(newPostBodyTextNode);
+//     newPostContainer.append(newPostBody);
+//     newPostContainer.append(newPostAuthor);
+//     newPostContainer.append(newPostDeleteButton);
+//     newPostContainer.append(newPostHR);
 
-    var newPostAuthor = document.createElement('p');
-    var newPostAuthorTextNode = document.createTextNode('Posted By: ' + postAuthor);
-    newPostAuthor.appendChild(newPostAuthorTextNode);
-
-    var newPostDeleteButton = document.createElement('button');
-    var newPostDeleteButtonTextNode = document.createTextNode('Delete Post');
-    newPostDeleteButton.appendChild(newPostDeleteButtonTextNode);
-    newPostDeleteButton.setAttribute('class', 'btn btn-danger');
-    newPostDeleteButton.addEventListener('click', function () {
-        var post = $(this).closest('.user-post');
-        post.remove();
-    });
-
-    var newPostHR = document.createElement('hr');
-
-    newPostContainer.append(newPostBody);
-    newPostContainer.append(newPostAuthor);
-    newPostContainer.append(newPostDeleteButton);
-    newPostContainer.append(newPostHR);
-
-    newsfeed.append(newPostContainer);
-});
+//     newsfeed.append(newPostContainer);
+// });
 
 /* Dummy Text:
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 
 Parzival, First to the Key
 */
+
+var postNumber = 0
+
+$('#post-creation-submit-btn').click(function () {
+    var postText = $('textarea#post-text').val();
+    var postAuthor = $('textarea#post-author').val();
+    postNumber++;
+    $('.newsfeed').append('<div class="user-post" id="user-post' + postNumber + '"></div>');
+    $('div#user-post' + [postNumber]).append('<p>' + postText + '</p>');
+    $('div#user-post' + [postNumber]).append('<p>- ' + postAuthor + '</p>');
+    $('div#user-post' + [postNumber]).append('<button class="btn btn-danger">Delete Post</button>');
+    $('div#user-post' + [postNumber]).append('<hr>');
+    $('.btn-danger').click(function () {
+        var post = $(this).closest('.user-post');
+        post.remove();
+    })
+});
