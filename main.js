91df35a06ee2f8bf6fd6e94c30c9ce1fc0c30cb1
Braydon Sutherland <geomatics@Braydons-MacBook-Air.local>
2022-05-15 15:37:55 -0400

    playing around with the style

--- main.js
+++ main.js
@@ -4,17 +4,18 @@ $(document).ready(() => {
     const $message = $('#commentInput').val();
 
     $('.posts').append(
-      `<div class="deleteMe"><p><strong>Name</strong>: ${$name} </p> <p><strong>Comment:</strong> ${$message} </p>  <button type="delete" class="delete btn-sm btn-primary">Remove</button><hr></div>`
+      `<div class="deleteMe"><p><strong>Name</strong>: ${$name} </p> <p><strong>Comment:</strong> ${$message} </p><button type="like" class="like btn-sm btn-primary m-1"><i class="bi bi-heart m-1"></i></button><button type="delete" class="delete btn-sm btn-primary">delete</button><hr></div>`
     );
 
     $($('#nameInput')).val('');
     $('#commentInput').val('');
 
     $('.delete').click((e) => {
-      e.preventDefault();
-      const test = e.currentTarget;
-      console.log(test);
       $(e.currentTarget).parent().remove();
+    });
+
+    $('.like').click((e) => {
+      $(e.currentTarget).css('color', 'red');
     });
   });
 });
