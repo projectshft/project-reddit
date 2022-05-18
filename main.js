3a0307d8769af05c10f4e49b125784b5378c3c00
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-05-18 17:31:21 -0400

    starting over. array map works

--- main.js
+++ main.js
@@ -1,60 +1,69 @@
-var clickFunction = function () {
+const postArray = ['asf', 'asdf']
+
+const postArrayMapped = postArray.map(array => `<p>${array}</p>`)
+
+
+const content = $('#content-post').append(postArrayMapped);
+
+
+
+// var clickFunction = function () {
   
-  var text = $('textarea').val();
+//   var text = $('textarea').val();
 
-  var name = $('input').val();
+//   var name = $('input').val();
 
-  $('.table').append(`<tr class="row">
-  <td><a id="remove">remove<a></td>
-  <td id="comment"><a>comment</a></td>
-  <td>${text}</td>
-  <td>–Posted by: ${name}</td>
-  </tr>`);
+//   $('.table').append(`<tr class="row">
+//   <td><a id="remove">remove<a></td>
+//   <td id="comment"><a>comment</a></td>
+//   <td>${text}</td>
+//   <td>–Posted by: ${name}</td>
+//   </tr>`);
 
-  $('.table').append(`<textarea rows="1" class="col" placeholder="Comment Text" id="comment-box"></textarea>`);
+//   $('.table').append(`<textarea rows="1" class="col" placeholder="Comment Text" id="comment-box"></textarea>`);
 
-  $('.table').append(`<input class="col" id="comment-name-box" placeholder="Your Name"/>`);
+//   $('.table').append(`<input class="col" id="comment-name-box" placeholder="Your Name"/>`);
 
-  $('.table').append(` <button class="btn btn-primary" id="comment-submit-button">Submit Comment</button>`);
-
-
-  $('#message').val('');
-  $('#name-box').val('');
-
-  var commentClick = function () {
-
-    $('#comment-box').css('display', 'inline');
-    $('#comment-name-box').css('display', 'inline');
-    $('#comment-submit-button').css('display', 'inline');
-  };
-  $('#comment').click(commentClick);
+//   $('.table').append(` <button class="btn btn-primary" id="comment-submit-button">Submit Comment</button>`);
 
 
-  var commentSubmitButtonClick = function () {
+//   $('#message').val('');
+//   $('#name-box').val('');
 
-    var text2 = $('#comment-box').val();
+//   var commentClick = function () {
 
-    var name2 = $('#comment-name-box').val();
+//     $('#comment-box').css('display', 'inline');
+//     $('#comment-name-box').css('display', 'inline');
+//     $('#comment-submit-button').css('display', 'inline');
+//   };
+//   $('#comment').click(commentClick);
 
-    $(`<tr class="row">
-    <td><a id="remove">remove<a></td>
-    <td>${text2}</td>
-    <td>–Comment by: ${name2}</td>
-    </tr>`).insertBefore('#comment-box');
+
+//   var commentSubmitButtonClick = function () {
+
+//     var text2 = $('#comment-box').val();
+
+//     var name2 = $('#comment-name-box').val();
+
+//     $(`<tr class="row">
+//     <td><a id="remove">remove<a></td>
+//     <td>${text2}</td>
+//     <td>–Comment by: ${name2}</td>
+//     </tr>`).insertBefore('#comment-box');
 
     
-  };
-  $('#comment-submit-button').click(commentSubmitButtonClick);
+//   };
+//   $('#comment-submit-button').click(commentSubmitButtonClick);
 
-  var removeClick = function () {
-    $('.table').children("tr").remove();
-  };
-  $('#remove').click(removeClick);
-  // consider $(this).remove();
-  // consider populating with objects
+//   var removeClick = function () {
+//     $('.table').children("tr").remove();
+//   };
+//   $('#remove').click(removeClick);
+//   // consider $(this).remove();
+//   // consider populating with objects
 
 
-};
+// };
 
-$('.btn').click(clickFunction);
+// $('.btn').click(clickFunction);
 
