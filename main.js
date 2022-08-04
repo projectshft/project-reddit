362e7378346eb2e708db7baf965fbccee722ce98
Daniel Zhang <daniel6628@gmail.com>
2022-08-03 23:12:20 -0400

    improved the delete post function and added remove button

--- main.js
+++ main.js
@@ -8,14 +8,10 @@ $("button").click(() => {
   const $divPosts = $(".posts").append("<div></div>");
 
   $divPosts.append(
-    `<div><p>${$userPost} - Posted By: ${$userName}</p><hr></div>`
+    `<div class="post-head"><button class="btn btn-link delete">remove</button> ${$userPost} - Posted By: ${$userName}<hr></div>`
   );
-
-  rmPost();
 });
 
-const rmPost = function () {
-  $("p").click(function () {
-    this.parentElement.remove();
-  });
-};
+$(".posts").on("click", ".delete", function () {
+  this.parentElement.remove();
+});
