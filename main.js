c22f374b6dc4d818285bdd3e301fad201e3d3c0f
Daniel Zhang <daniel6628@gmail.com>
2022-08-06 16:57:36 -0400

    git reset code

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 $("button").click(() => {
-  const $userPost = $("#post").val();
-  const $userName = $("#name").val();
+  let $userPost = $("#post").val();
+  let $userName = $("#name").val();
 
   $("#post").val("");
   $("#name").val("");
 
-  const $divPosts = $(".posts").append("<div></div>");
+  let $divPosts = $(".posts");
 
-  // $divPosts.append(
-  //   `<div><button class="btn btn-link delete">remove</button> ${$userPost} - Posted By: ${$userName}<hr></div>`
-  // );
-  const b = $("<button>").html("remove").addClass("btn btn-link delete");
-  // const p = $("<div>").html(`${$userPost} - Posted By: ${$userName}<hr>`);
+  $divPosts.append(
+    `<div><p>${$userPost} - Posted By: ${$userName}</p><hr></div>`
+  );
 
-  $divPosts.append(b);
-  $divPosts.append(`${$userPost} - Posted By: ${$userName}<hr>`);
+  rmPost();
 });
 
-$(".posts").on("click", ".delete", function () {
-  this.parentElement.remove();
-});
+const rmPost = function () {
+  $("p").click(function () {
+    this.parentElement.remove();
+  });
+};
