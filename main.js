feaba01428e2f233c517665a633377eb4701ec6f
Ryan <ryancaimano@gmail.com>
2022-08-05 21:59:33 -0500

    basic framework established in html...title, heading, form fields, button styled in bootstrap. Currently have a .js reference file from previous project. Yet to write the javascript w/ jQuery selectors

---
+++ main.js
@@ -0,0 +1,26 @@
+// use the script below as a reference in order to write with jQuery selectors
+
+// document.getElementById('submit').addEventListener('click', function () {
+// 	var name = document.getElementById('name').value;
+// 	var text = document.getElementById('message').value;
+
+// 	var postsDiv = document.querySelector('.posts');
+
+// 	var newPostDiv = document.createElement('div');
+
+// 	var newPostTextP = document.createElement('p');
+// 	var newPostTextNode = document.createTextNode(text);
+// 	newPostTextP.appendChild(newPostTextNode);
+
+// 	var newPostNameP = document.createElement('p');
+// 	var newPostNameNode = document.createTextNode('Posted By: ' + name);
+// 	newPostNameP.appendChild(newPostNameNode);
+
+// 	var newPostHR = document.createElement('hr');
+
+// 	newPostDiv.append(newPostTextP);
+// 	newPostDiv.append(newPostNameP);
+// 	newPostDiv.append(newPostHR);
+
+// 	postsDiv.append(newPostDiv);
+// });
