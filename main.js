c1eb523d9106c9009f5b379de5b8724881844451
Daniel Zhang <daniel6628@gmail.com>
2022-08-06 16:42:34 -0400

    fixing html

--- main.js
+++ main.js
@@ -7,9 +7,14 @@ $("button").click(() => {
 
   const $divPosts = $(".posts").append("<div></div>");
 
-  $divPosts.append(
-    `<div><button class="btn btn-link delete">remove</button> ${$userPost} - Posted By: ${$userName}<hr></div>`
-  );
+  // $divPosts.append(
+  //   `<div><button class="btn btn-link delete">remove</button> ${$userPost} - Posted By: ${$userName}<hr></div>`
+  // );
+  const b = $("<button>").html("remove").addClass("btn btn-link delete");
+  // const p = $("<div>").html(`${$userPost} - Posted By: ${$userName}<hr>`);
+
+  $divPosts.append(b);
+  $divPosts.append(`${$userPost} - Posted By: ${$userName}<hr>`);
 });
 
 $(".posts").on("click", ".delete", function () {
