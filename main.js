13c0701c9c80217a232d8adcc45d5012adf06a98
Ryan <ryancaimano@gmail.com>
2022-08-06 00:50:40 -0500

    created new elements using jQuery for the new posts rendering

--- main.js
+++ main.js
@@ -6,7 +6,23 @@ $('button').on('click', function () {
 
 	var newPostDiv = $('<div>');
 
+	var newPostTextSpan = document.createElement('span');
+	var newPostTextNode = document.createTextNode(text);
+	newPostTextSpan.appendChild(newPostTextNode);
+
+	var newPostNameSpan = document.createElement('span');
+	var newPostNameNode = document.createTextNode(' -Posted By: ' + name);
+	newPostNameSpan.appendChild(newPostNameNode);
+
+	var newPostHR = document.createElement('hr');
+
+	newPostDiv.append(newPostTextSpan);
+	newPostDiv.append(newPostNameSpan);
+	newPostDiv.append(newPostHR);
+
+	postsDiv.append(newPostDiv);
 });
+
 
 	
 
