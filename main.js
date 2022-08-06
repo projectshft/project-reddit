f44ddf2c90fc9d97ff7f0bb2a3a7c3e0caa2c3c6
Ryan <ryancaimano@gmail.com>
2022-08-06 12:19:49 -0500

    added previously written pseudo code...have yet to solve how to create a new span element using jQuery syntax

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
+/* <pseudo>
+//  -PART1-
+		(1)		create click handler using qJuery syntax
+		(2)		define name and text- set equal to the values that are inputted by user
+		(3)		define variable (newPostTextSpan) to store the creation of a new span element for each post (aka text/user message)
+		(4)		define variable (newPostNameSpan) to store the creation of a new span element for each name (aka user name) 
+		(5)		define variable (newPostTextNode) to store the creation of new node for the text (message) that is wrapped in span element
+		(6)		define variable (newPostNameNode) to store the creation of new node for the name (user name) that is wrapped in span element 
+		(7)		user input needs to be placed in the nodes mentioned above...use appendChild method for this
+		(8)		user input must be rendered on page upon triggering event (i.e. button), so...specify the location of such rendering by declaring variables that are associated with the appropriate elements (i.e. posts div and new posts div)
+		(9)		new posts should be rendered on separate rows. Declare a variable that stores the creation of a new element (i.e. row)
+		(10)	review code and assure that, when possible, jQuery syntax is used 
+
+		-PART2-
+		(11)  
+
+
+		-BEAUTIFY-
+		( )		clear input fields upon click event (i.e. submit button)
+		( )		stylize all text
+		( )		stylize background
+		( )		utilize 12-column grid from css.frameworks file
+*/
+
+
 $('button').on('click', function () {
-	var name = $('#name').val();
+	
 	var text = $('#message').val();
+	var name = $('#name').val();
 
 	var postsDiv = $('.posts');
 
