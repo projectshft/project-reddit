8e94723173ac3625c1ae757cd8603bec17999f3a
Ryan <ryancaimano@gmail.com>
2022-08-05 23:56:47 -0500

    added jQuery selector to write click handler

--- main.js
+++ main.js
@@ -1,26 +1,13 @@
-// use the script below as a reference in order to write with jQuery selectors
+$('button').on('click', function () {
+	var name = $('#name').val();
+	var text = $('#message').val();
 
-// document.getElementById('submit').addEventListener('click', function () {
-// 	var name = document.getElementById('name').value;
-// 	var text = document.getElementById('message').value;
+	var postsDiv = $('.posts');
 
-// 	var postsDiv = document.querySelector('.posts');
+	var newPostDiv = $('<div>');
 
-// 	var newPostDiv = document.createElement('div');
+});
 
-// 	var newPostTextP = document.createElement('p');
-// 	var newPostTextNode = document.createTextNode(text);
-// 	newPostTextP.appendChild(newPostTextNode);
+	
 
-// 	var newPostNameP = document.createElement('p');
-// 	var newPostNameNode = document.createTextNode('Posted By: ' + name);
-// 	newPostNameP.appendChild(newPostNameNode);
 
-// 	var newPostHR = document.createElement('hr');
-
-// 	newPostDiv.append(newPostTextP);
-// 	newPostDiv.append(newPostNameP);
-// 	newPostDiv.append(newPostHR);
-
-// 	postsDiv.append(newPostDiv);
-// });
