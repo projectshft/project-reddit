1e8aaec0bde3596701df063f3501c5534e22f6c5
Dillon Hess <dhess268@gmail.com>
2022-08-07 22:46:54 -0400

    encapsulate html templates from addPost() fucntion

--- index.js
+++ index.js
@@ -1,55 +1,64 @@
 function isValidName(name) {
+  // valid if only contains letters from a to Z
   const regex = /^[A-Za-z].*/;
   return regex.test(name);
 }
 function isValidMessage(msg) {
+  // no empty messages allowed
   return msg.length > 0;
 }
 function resetInputs() {
   $('#new-message').val('');
   $('#user-name').val('');
 }
+function buildPostTemplate(name) {
+  const commentInputSection = `<form style="margin-top:10px;" onsubmit="event.preventDefault();">
+  <div class="form-group">
+    <input class="commenter-name" name="commenter-name" type="text"
+      class="form-control"
+      placeholder="Name" pattern="^[A-Za-z].*" title="Please begin your username with a letter" ></input>
+  </div>
+  <div class="form-group">
+    <textarea class="comment-message" name="new-message" type="text"
+    class="form-control"
+    placeholder="Comment"></textarea>
+  </div>
+  <button class="btn btn-primary comment-btn">Post Comment</button>
+</form>`;
+
+  const commentSectionTemplate = `<section class="comment-list"></section>`;
+  const baseTemplate = `<div class="container post">
+  <div class="row">
+  <div class="col-xs-1 col-md-1 text-left post-header">
+    <div class="post-bubble text-center"><i class="fa-solid fa-${name[0].toLowerCase()}"></i></div>
+  </div>
+  <div class="col-xs-10 col-md-4">
+    <strong class = "post-author"></strong>
+  </div>
+  </div>
+  <div class="row">
+    <p class = "col-md-9 post-comment"></p>
+  </div>
+  <button class="btn btn-danger remove">Remove</button>
+  <button class="btn btn-info btn-comments">Comments</button>
+  <button type="button" class="btn btn-secondary edit">Edit Post</button>
+  <div class="comments hidden">
+    ${commentSectionTemplate}
+    ${commentInputSection}
+  </div>
+  </div>
+  <hr></hr>`;
+
+  return baseTemplate;
+}
 function addPost() {
+  // Need to grab user input to check if valid for a new post
   const message = $('#new-message').val();
   const name = $('#user-name').val();
   if (isValidMessage(message) && isValidName(name)) {
-    const commentInputSection = `<form style="margin-top:10px;" onsubmit="event.preventDefault();">
-    <div class="form-group">
-      <input class="commenter-name" name="commenter-name" type="text"
-        class="form-control"
-        placeholder="Name" pattern="^[A-Za-z].*" title="Please begin your username with a letter" ></input>
-    </div>
-    <div class="form-group">
-      <textarea class="comment-message" name="new-message" type="text"
-      class="form-control"
-      placeholder="Comment"></textarea>
-    </div>
-    <button class="btn btn-primary comment-btn">Post Comment</button>
-  </form>`;
-
-    const commentSectionTemplate = `<section class="comment-list"></section>`;
-    const template = `<div class="container post">
-    <div class="row">
-    <div class="col-xs-1 col-md-1 text-left post-header">
-      <div class="post-bubble text-center"><i class="fa-solid fa-${name[0].toLowerCase()}"></i></div>
-    </div>
-    <div class="col-xs-10 col-md-4">
-      <strong class = "post-author"></strong>
-    </div>
-    </div>
-    <div class="row">
-      <p class = "col-md-9 post-comment"></p>
-    </div>
-    <button class="btn btn-danger remove">Remove</button>
-    <button class="btn btn-info btn-comments">Comments</button>
-    <button type="button" class="btn btn-secondary edit">Edit Post</button>
-    <div class="comments hidden">
-      ${commentSectionTemplate}
-      ${commentInputSection}
-    </div>
-    </div>
-    <hr></hr>`;
+    const template = buildPostTemplate(name);
     const $post = $(template);
+    // This data attribute on the post element will hold the comments for this specific post
     $post.data('comments', []);
 
     // done for security reasons so no HTML can be injected via the html template
@@ -59,18 +68,27 @@ function addPost() {
     $('.posts').append($post);
     resetInputs();
 
-    // using jquery to scroll to the bottom of posts section
+    // using jquery to scroll to the bottom of posts section every time a post is added
     const postScrollHeight = $('.posts').prop('scrollHeight');
     $('.posts').scrollTop(postScrollHeight);
   }
 }
-function renderPosts(post) {
+
+function buildCommentTemplate(index, comment, name) {
+  return `<div class="comment" data-index="${index}"> <button class="btn btn-warning comment-removal">X</button>${comment} - Comment by: ${name} }</div>`;
+}
+
+function renderComments(post) {
   const $commentList = post.find('.comment-list');
   $commentList.empty();
   const commentArray = post.data('comments');
   let comments = '';
   for (let i = 0; i < commentArray.length; i += 1) {
-    comments += `<div class="comment" data-index="${i}"> <button class="btn btn-warning comment-removal remove">X</button>${commentArray[i].comment} - Comment by: ${commentArray[i].name} }</div>`;
+    comments += buildCommentTemplate(
+      i,
+      commentArray[i].comment,
+      commentArray[i].name
+    );
   }
   $commentList.append(comments);
 }
@@ -94,21 +112,21 @@ function addComment() {
       comment,
     };
     $post.data('comments').push(commentData);
-    renderPosts($post);
+    renderComments($post);
     $(this).parent().find('.comment-message').val('');
     $(this).parent().find('.commenter-name').val('');
   }
 }
 
 function remove() {
-  if ($(this).hasClass('comment-removal')) {
-    const dataArray = $(this).closest('.post').data('comments');
-    dataArray.splice($(this).closest('.comment').data('index'), 1);
-    renderPosts($(this).closest('.post'));
-  }
   $(this).parent().remove();
 }
-
+function removeComment() {
+  const dataArray = $(this).closest('.post').data('comments');
+  dataArray.splice($(this).closest('.comment').data('index'), 1);
+  renderComments($(this).closest('.post'));
+  remove();
+}
 function editPost() {
   const message = $(this).closest('.post').find('.post-comment');
   if (message.hasClass('edit-message')) {
@@ -127,6 +145,7 @@ function editPost() {
     message.replaceWith($editTemplate);
   }
 }
+
 // Event listeners
 $('#submit').click(addPost);
 
@@ -137,3 +156,5 @@ $('.posts').on('click', '.btn-comments', toggleComments);
 $('.posts').on('click', '.comment-btn', addComment);
 
 $('.posts').on('click', '.edit', editPost);
+
+$('.posts').on('click', '.comment-removal', removeComment);
