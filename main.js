8c3132bf1edeae9fba1e826082b3294d9727de39
ryrob <ryanrobertson242@gmail.com>
2022-08-30 18:56:39 -0500

    Change HTML templates to use Handlebar.js

--- main.js
+++ main.js
@@ -38,62 +38,20 @@ $(document).ready(function() {
     const $commentId = commentsObject.comments[`comment${$commentNumber}`];
 
     //HTML template for each new comment
-    let $newComment = 
-    `<div id="comment${commentsObject.numberOfComments}" class="user-comment container-fluid text-light mt-5  ">
-      <div class="row align-items-center p-1">
-        <div class='col-1 p-2'>
-          <img src='${faker.image.people('100', '100', true)}' class="rounded-circle img-fluid p-0" >
-        </div>
-        <div class='col-11'>
-          <div class="row justify-content-center bg-secondary rounded">
-            <div class="user-name row fw-light justify-content-between align-items-center mb-0 mt-2 pb-0">
-              <h6 class="col-11 px-0 mb-0"><strong>${$commentId.name}</strong></h6>
-              <button type="button" class="remove-comment btn-close col-1" aria-label="Close"></button>
-            </div>
-            <div class="user-message row fw-light mb-2">
-              <p class="col pb-0 mb-0 px-0">${$commentId.message}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class='row p-1'>  
-        <p class="like-button col-1 rounded border border-dark text-secondary text-center p-0 offset-1 mb-0 hover"><small>Like</small></p>
-        <p class="reply-button col-2 rounded border border-dark text-secondary text-center p-0 pl-2 mb-0 hover"><small>Reply<span class="reply-count"></span></small></p>
-        <p class="col-3 rounded border border-dark text-start text-secondary mb-0"><small>${getCurrentTime(new Date)}</small></p>
-      </div>  
-      <div id='reply-container' class='row justify-content-end d-none p-1'>
-      </div>
-      <div id="new-reply-form-container" class="row justify-content-end d-none p-1">
-        <form id='new-reply-form' class='col-10 p-0'>
-          <div class="mb-3">
-            <input
-              type="name"
-              name="reply-name"
-              id="reply-name"
-              class="form-control form-control-sm my-2 border-secondary bg-secondary text-light"
-              placeholder="name"
-              required
-            />
-          </div>
-          <div class="mb-3">
-            <textarea
-              name="reply-message"
-              id="reply-message"
-              class="form-control form-control-sm my-2 border-secondary bg-secondary text-light"
-              placeholder="comment"
-              required
-            ></textarea>
-          </div>
-          <button class="btn btn-primary" id="reply-submit" type="submit">
-            Reply
-          </button>
-        </form>
-      </div>
-    </div>
-    `
+    const newComment = $('#new-comment-template').html();
+    const commentTemplate = Handlebars.compile(newComment);
+    const commentHTML = commentTemplate(
+      {
+        id: `comment${commentsObject.numberOfComments}`,
+        faker: `${faker.image.people('100', '100', true)}`,
+        name: `${$commentId.name}`,
+        message: `${$commentId.message}`,
+        date: `${getCurrentTime(new Date)}`
+      }
+    )
 
     //comment will fade in on submission
-    $($newComment).hide().appendTo($commentContainer).fadeIn(200)
+    $(commentHTML).hide().appendTo($commentContainer).fadeIn(200)
 
     $(this)[0].reset() //resets form after submission
   })
@@ -118,8 +76,10 @@ $(document).ready(function() {
     if($currentReplyId) {
       //apply likes only to select reply
       commentsObject.comments[$currentCommentId]['replies'][$currentReplyId]['likes'] = commentsObject.comments[$currentCommentId]['replies'][$currentReplyId]['likes'] + 1 || 1
+
       const $currentLikes = commentsObject.comments[$currentCommentId]['replies'][$currentReplyId]['likes']
-      if($(this).parent().children().length ==2 ) {
+      
+      if($(this).parent().children().length === 2 ) {
         $(this).parent().append('<i class="col-1 text-end text-primary offset-5 pr-0 bi bi-hand-thumbs-up-fill"></i>')
         $(this).parent().append('<p class="col-1 text-start text-primary pl-0 mb-0"><small class="likes-text">1<small></p>')  
       } else {
@@ -129,6 +89,7 @@ $(document).ready(function() {
       //apply likes only to selected comment
       commentsObject.comments[$currentCommentId]['likes']++
       const $currentLikes = commentsObject.comments[$currentCommentId]['likes']
+      console.log($currentCommentId)
       if($(this).parent().children().length === 3) {
         $(this).parent().append('<i class="col-1 text-end text-primary offset-3 pr-0 bi bi-hand-thumbs-up-fill"></i>')
         $(this).parent().append('<p class="col-1 text-start text-primary pl-0 mb-0"><small class="likes-text">1<small></p>')  
@@ -168,36 +129,21 @@ $(document).ready(function() {
     //populates commentsObject with each reply data
     commentsObject.comments[$currentComment]['replies'][`reply${$numberOfReplies}`] = {name: $replyName, message: $replyMessage, likes: 0}
 
-    //HTML template for new replies
-    const $newReply = 
-    `<div id="reply${commentsObject.comments[$currentComment]['numberOfReplies']}" class="user-comment col-10 text-light my-3">
-      <div class="row align-items-center">
-        <div class='col-1 p-2'>
-          <img src='${faker.image.cats('100', '100', true)}' class="rounded-circle img-fluid p-0" >
-        </div>
-        <div class='col-11'>
-          <div class="row justify-content-center bg-secondary rounded">
-            <div class="user-name row fw-light justify-content-between align-items-center mb-0 mt-2 pb-0">
-              <h6 class="col-11 px-0 mb-0"><strong>${$replyName}</strong></h6>
-              <button type="button" class="remove-comment btn-close col-1" aria-label="Close"></button>
-            </div>
-            <div class="user-message row fw-light mb-2">
-              <p class="col pb-0 mb-0 px-0">${$replyMessage}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class='row text-secondary'>  
-        <p class="like-button col-1 rounded border border-dark text-center p-0 offset-1 mb-0 hover"><small>Like</small></p>
-        <p class="col-3 rounded border border-dark text-start mb-0"><small>${getCurrentTime(new Date)}</small></p>
-      </div>  
-    <div>`
+    const newReply = $('#new-reply-template').html();
+    const replyTemplate = Handlebars.compile(newReply);
+    const replyHTML = replyTemplate({
+      id: `reply${commentsObject.comments[$currentComment]['numberOfReplies']}`,
+      faker: `${faker.image.cats('100', '100', true)}`, 
+      name: `${$replyName}`,
+      message: `${$replyMessage}`,
+      date: `${getCurrentTime(new Date)}`
+    })
 
     //updates the reply text to show number of replies within each comment
     $(this).closest('.user-comment').find('.reply-count').text(` (${$numberOfReplies})`)
 
     const $replyContainer = $(this).parentsUntil($commentContainer).find('#reply-container')
-    $($newReply).hide().appendTo($replyContainer).fadeIn(200);
+    $(replyHTML).hide().appendTo($replyContainer).fadeIn(200);
 
     $(this)[0].reset();  //resets form after submission
   })
