9dfa6e401d89d89a6bb0f52fe80453218d3b9053
unknown <dgoodenough@proton.me>
2022-11-02 05:48:59 -0700

    Remove pseudo code

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// 1.get input values from forms
-// 2. create a template for a new post and inject the name and comment into it
-    // append that to the DOM
-//3. handle clicks on the delete buttons
-//4. handle clicks on the comments button
-    //show the comment section
-    //allow for comments to be made
-// 5. enable editing
-  //on click open up a form that will modify the text of the post
-
-// clear forms on submit
-
 $postComment = $('#post-form-comment');
 $postName = $('#post-form-name');
 $submit = $('.btn-submit');
