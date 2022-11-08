8f7268e8f72aa14546ad47e99c49dbdc4411f00a
david <david_k@fastmail.com>
2022-11-08 07:35:14 -0500

    init fix

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 $('button').click(function () {
-  let name = 'Posted By: ' + $('#name').val()
-  let message = $('#message').val()
-  let input = `<div class="post">` + message + ' ' + '- ' + name + '</div >'
-  let remove = '<div class="removeText">' + '<b>' + 'remove' + '</b>' + '</div>' + '<hr>'
-  let wholePost = '<div class="entire">' + input + remove + '</nav>'
+  let name = 'Posted By: ' + $('#name').val();
+  let message = $('#message').val();
+  let input = `<div class="post">` + message + ' ' + '- ' + name + '</div >';
+  let remove = '<div class="removeText">' + '<b>' + 'remove' + '</b>' + '</div>' + '<hr>';
+  let wholePost = '<div class="entire">' + input + remove + '</nav>';
 
   if (!$('#message').val().length > 0) {
-    alert('Type a message.')
+    alert('Type a message.');
   } else if (!$('#name').val().length > 0) {
-    alert('Enter a name.')
+    alert('Enter a name.');
   } else {
-    $('.posts').append(wholePost)
+    $('.posts').append(wholePost);
   }
 });
 
