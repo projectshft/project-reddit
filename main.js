527b064d89231ca15b54418abf86758c6570706e
Nils Landsberg <nils.landsberg78@gmail.com>
2022-11-22 16:49:11 -0500

    comments can be removed - working on getting the makeComment function to work.  At this point, the input boxes can be shown/hidden by clicking comment. However, it is not specific to the exact post. It shows all comment input boxes for each post.

--- main.js
+++ main.js
@@ -4,26 +4,34 @@ const makePost = function() {
     let userName = $('#name').val();
     let userMessage = $('#message').val();
 
-    let postTemplate = '<div class="post"><p><a class="remove">remove</a> <a class="comments">comments</a> ' + userMessage + ' - Posted By: ' + userName + '</p><hr></div>';
+    let postTemplate = '<div class="post"><p><a class="remove">remove</a> <a class="comments">comments</a> ' + userMessage + ' - Posted By: ' + userName + '</p>';
 
-    $('.posts').append(postTemplate);
+    let commentTemplate = '<form style="margin-top:30px;" onsubmit="event.preventDefault();"><div class="comment-input"><div class="form-group"><input id="comment" type="text" class="form-control" placeholder="Comment Text"></input></div>\n\n<div class="form-group"><input id="user" type="text" class="form-control" placeholder="Your Name"></input></div><button id="submit-comment" class="btn btn-primary">Submit Comment</button><hr></div>';
+
+    $('.posts').append(postTemplate, commentTemplate);
     $("form").trigger("reset");
-
+  
+    makeComments();
     removePost();
-    
-  });
-}
+  }) 
+};
+
 
 
 makePost();
 
 
-// const makeComments = function() {
-//   $('.comments').on('click'), function(e) {
-//     // if comments are hidden
-//       // show comments and show input boxes of e.target
-//   }
-// }
+const makeComments = function() {
+  $('.comments').on('click', function() {
+    if ($('.comment-input').css('display') == 'none') {
+      console.log('yes');
+      $('.comment-input').show();
+    } else {
+      $('.comment-input').hide();
+    }
+  });
+}
+// makeComments can show/hide comment inputs - <hr> is hidden - does not work for a specific post - only works for all comments
 const removePost = function() {
   $('.remove').on('click', function() {
   $(this).closest('div.post').remove();
