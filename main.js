83e0884e333677f3e7e5c1a127c6639b743af5cc
Nils Landsberg <nils.landsberg78@gmail.com>
2022-11-22 15:28:49 -0500

    remove comment working. background image, navbar icon, and other css elements added

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 
 const makePost = function() {
-  $('#submit').on('click', function() {
+  $('.btn').on('click', function() {
     let userName = $('#name').val();
     let userMessage = $('#message').val();
 
-    let postTemplate = '<div><p><a class="remove">remove</a> <a class="comments">comments</a> ' + userMessage + ' - Posted By: ' + userName + '</p><hr></div>';
+    let postTemplate = '<div class="post"><p><a class="remove">remove</a> <a class="comments">comments</a> ' + userMessage + ' - Posted By: ' + userName + '</p><hr></div>';
 
     $('.posts').append(postTemplate);
     $("form").trigger("reset");
+
+    removePost();
+    
   });
 }
 
@@ -21,5 +24,8 @@ makePost();
 //       // show comments and show input boxes of e.target
 //   }
 // }
-
-
+const removePost = function() {
+  $('.remove').on('click', function() {
+  $(this).closest('div.post').remove();
+  });
+}
