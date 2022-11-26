3228c7d753da7035f8a34898411039a12b9deb7a
Nils Landsberg <nils.landsberg78@gmail.com>
2022-11-26 11:52:54 -0500

    user can now add and remove comments - comments are visible when posted and hidden after clicking comments

--- main.js
+++ main.js
@@ -4,21 +4,49 @@ var makePost = function() {
     var userName = $('#name').val();
     var userMessage = $('#message').val();
 
-    var commentBox = '<form style="margin-top:30px;" onsubmit="event.preventDefault();"><div class="comment-input"><div class="form-group"><input id="comment" type="text" class="form-control" placeholder="Comment Text"></input></div><div class="form-group"><input id="user" type="text" class="form-control" placeholder="Your Name"></input></div><button id="submit-comment" class="btn btn-primary">Submit Comment</button></div>';
+    var commentBox = '<form id="new-comment" style="margin-top:30px;" onsubmit="event.preventDefault();"><div class="comment-input"><div class="form-group"><input id="comment" type="text" class="form-control" placeholder="Comment Text" required></input></div><div class="form-group"><input id="user-name" type="text" class="form-control" placeholder="Your Name" required></input></div><button id="submit-comment" type="submit" class="btn btn-primary">Submit Comment</button></div>';
 
-    var postTemplate = '<div class="post"><p><a class="remove">remove</a> <a class="comments">comments</a> ' + userMessage + ' - Posted By: ' + userName + '</p>' + commentBox + '<hr>';
+    var postTemplate = '<div class="post"><p><a class="remove">remove</a> <a class="comments">comments</a> ' + userMessage + ' - Posted By: ' + userName + '</p><div class="comment-list"></div>' + commentBox + '<hr>';
 
     // adds postTemplate to the .posts div
     $('.posts').append(postTemplate);
     // resets post input boxes
-    $("form").trigger("reset");
+    $('form').trigger('reset');
 
-    makeComments();
+    makeComment();
+    showComments();
     removePost();
   }) 
 };
 
-var makeComments = function() {
+var makeComment = function() {
+  $('#new-comment').on('submit', function () {
+    var userName = $('#user-name').val();
+    var commentText = $('#comment').val();
+
+    var commentTemplate = '<div class="comment comment-input"><a class="remove">remove</a> ' + commentText + ' Posted by: ' + userName + '</div>';
+
+    $('.comment-list').append(commentTemplate);
+    $('form').trigger('reset');
+
+    removeComment();
+  })
+}
+
+var removeComment = function() {
+  $('.remove').on('click', function() {
+    $(this).closest('div.comment').remove();
+  });
+}
+
+var removePost = function() {
+  $('.remove').on('click', function() {
+    // removes the closes post div above the remove that was clicked
+    $(this).closest('div.post').remove();
+  });
+}
+
+var showComments = function() {
   $('.comments').on('click', function() {
     if ($('.comment-input').css('display') == 'none') {
       $('.comment-input').show();
@@ -27,13 +55,9 @@ var makeComments = function() {
     }
   });
 }
-// makeComments can show/hide comment inputs - only works on bottom post && only works for all comments - need to find way to create unique id's for every post comment box
 
-var removePost = function() {
-  $('.remove').on('click', function() {
-    // removes the closes post div above the remove that was clicked
-    $(this).closest('div.post').remove();
-  });
-}
+// showComments can show/hide comment inputs - only works on bottom post && only works for all comments - need to find way to create unique id's for every post comment box
+
+
 
 makePost();
