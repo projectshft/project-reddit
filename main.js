b792c3f9fe7e6387acd6608fb9f8c76c392eba2a
chillyfest <111262007+chillyfest@users.noreply.github.com>
2022-11-28 01:04:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ $('button').on('click', function () {
   var inputPost = $('#post').val();
   var inputName = $('#name').val();
 
-  $('.posts').append('<p>' + inputPost + ' - Posted By: ' + inputName + '</p>' + '<hr>');
-
-  $('form :input').val('');
+  if (inputPost && inputName) {     // ensure user enters an entry in post AND name fields
+      $('.posts').append('<p>' + inputPost + ' - Posted By: ' + inputName + '</p>' + '<hr>');
+      
+      $('form :input').val('');     // reset the input fields for the next comment
+  }
 });
 
-$('body').on('click', 'p', function () {
+$('.posts').on('click', 'p', function () {    // erase a comment upon clicking on it
   $(this).remove();
 });
