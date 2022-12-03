a7bbd94998a010ea83d96407b5ef31c04988611f
ALocke333 <104593944+ALocke333@users.noreply.github.com>
2022-12-02 15:11:07 -1000

    updated commit

--- main.js
+++ main.js
@@ -1,43 +1,41 @@
 
   
 $('#submit').on('click', function () {
-  var name = $('#name').val();
-  var post = $('#post').val();
+  var myName = $('#name').val();
+  var post = $('#message').val();
 
 
-  
+  var postContainer = '<p>' + post + ' - posted by: '  + myName + '</p>' ;
 
-$('.posts').append('<p>' +  name + '</p>' + '<p>' + post + '</p>');
+  $(".posts").append(postContainer); 
 
-});
 
-$('.posts').on('click', function () {
+
+})
+
+
+  $('.posts').on('click', function () {
  $(this).remove();
  });
 
 
 
 
-  var postsDiv = document.querySelector('.posts');
-  
-  var newPostDiv = document.createElement('div');
-  
-  var newPostTextP = document.createElement('p');
-  var newPostTextNode = document.createTextNode(text);
-  newPostTextP.appendChild(newPostTextNode);
-  
-  var newPostNameP = document.createElement('p');
-  var newPostNameNode = document.createTextNode('Posted By: ' + name);
-  newPostTextP.appendChild(newPostNameNode);
-  
-  
-  var newPostHR = document.createElement('hr');
-  
-  newPostDiv.append(newPostTextP);
-  newPostDiv.append(newPostNameP);
-  newPostDiv.append(newPostHR);
-  
-  postsDiv.append(newPostDiv);
-  
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+ 
   
   
