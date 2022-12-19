037c86b8b71e2d2c6a4ed9d212f7ad8e8b1e4807
Chase Cousins <100642297+crcous@users.noreply.github.com>
2022-12-19 16:17:04 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,38 @@
-
 var posts = [];
 
-var parent = document.getElementById('#posts');
-var child = document.getElementsByClassName('.publish');
+var parent = document.getElementById("#posts");
+var child = document.getElementsByClassName(".publish");
 
-dltbtn = document.querySelector('.button');
-var inputs = document.querySelectorAll('input');
+dltbtn = document.querySelector(".button");
+var inputs = document.querySelectorAll("input");
 
 //  on click of a button
-$('.button').click(function(e) {
-e.preventDefault();
-// collect some input data
-    var inputName = $('.name').val();
-    var inputPost = $('.post').val();
-    // add that data to an array
-    var newEntry = inputPost + ' ' + '-' + ' ' + 
-        'Posted By:' + ' ' + inputName;
-    
-    posts.push(newEntry);
-    
-    $('#posts').append('<button class="rembutton">remove</button>',
-            `<div class="publish">${newEntry}</div>`);
-     
-}); 
+$(".button").click(function (e) {
+  e.preventDefault();
+  // collect some input data
+  var inputName = $(".name").val();
+  var inputPost = $(".post").val();
+  // add that data to an array
+  var newEntry = inputPost + " " + "-" + " " + "Posted By:" + " " + inputName;
 
-dltbtn.addEventListener('click', function() {
-    inputs.forEach(input => input.value = '');
-    
+  posts.push(newEntry);
+
+  $("#posts").append(
+    '<button class="rembutton">remove</button>',
+    `<div class="publish">${newEntry}</div>`
+  );
 });
 
+dltbtn.addEventListener("click", function () {
+  inputs.forEach((input) => (input.value = ""));
+});
 
 // 2. Make the remove button remove the post when clicked.
-var removeButton = document.querySelector('.rembutton');
 
-removeButton.click(function() {
+document.onload = function () {
+  var removeButton = document.querySelector(".rembutton");
+
+  removeButton.addEventListener("click", function () {
     parent.removeChild(child);
-})
-    
-
-
-
-
+  });
+};
