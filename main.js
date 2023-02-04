fbb2da2b072cb28719653a50af75218704e38b78
josephl37 <josephliow2000@gmail.com>
2023-02-04 16:44:55 -0600

    added comments section html

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 let createPost = () => {
   // get text & name and store in variable
   let text = $('#message').val();
-  let name = $('#name').val();
+  let name = $('#message-name').val();
+
+  console.log('clicked')
 
   // create post template
-  let template = "<div class='post'><p>" + text + " - Posted By: " + name + " </p> <a class='remove'>remove</a> <br> <hr></hr></div>" 
+  let template = "<div class='post'><p>" + text + " - Posted By: " + name + " </p> <a class='remove'>remove</a> <a class='comments'>comments</a> <br> <hr></hr></div>" 
 
   // append text & name to posts div
   $('.posts').append(template);
 }
 
+// remove post feature
 $('.posts').on('click','.remove', function() {
+  // references parent element of clicked element
   let element = $(this).parent();
   element.remove();
 });
 
-$('#submit').click(createPost);
+// comments feature
+$('.posts').on('click','.comments', function () {
+
+  console.log('clicked on comments')
+  // display comments section
+
+  // display input field for text & name
+
+  // display submit comment button
+})
+
+$('#submit-post').click(createPost);
