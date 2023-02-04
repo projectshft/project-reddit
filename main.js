145aa1eba36cfeec0682d275cc4c1e2b60d7903b
josephl37 <josephliow2000@gmail.com>
2023-02-03 23:57:47 -0600

    refactored html into template

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ let createPost = () => {
   let text = $('#message').val();
   let name = $('#name').val();
 
-  // append text & name to posts div
-  $('.posts').append("<p>" + text + " - Posted By: " + name + " </p>")
-  $('.posts').append("<hr></hr>")
+  // create post template
+  let template = "<div class='post'><p>" + text + " - Posted By: " + name + " </p> <a>remove</a> <br> <hr></hr></div>" 
 
+  // append text & name to posts div
+  $('.posts').append(template);
 }
 
 $('#submit').click(createPost);
