ef1d55ebcc73aa444cd33710d674de7da10f097c
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-02-04 22:50:29 -0500

    added variables in mainjs of text boxes to add posts and comments, tried editing some for style didn't work out great, added classname so i could make the variables, need to work on javascript

--- main.js
+++ main.js
@@ -3,6 +3,24 @@
 //Use jQuery
 //Make sure a user can add posts as well as remove them
 
-var submitButton = $('.btn')
+var postButton = $('.btnpost');
+var commentButton = $('.btncomment');;
+
+var deletePost = null;
+var deleteComment = null;
+
+var postName = $('.postnameinput');
+var postText = $('.posttextinput');
+
+var commentName = $('.commentname');
+var commentText = $('.commenttext');
+
+var postBoxName = $('.postname');
+var postBoxText = $('.postbody');
+
+var commentBoxName = $('.commentname');
+var commentBoxText = $('.commentbody');
+
+$(commentBoxName).css('color', 'green')
 
 $('.card').css('color', 'red')
