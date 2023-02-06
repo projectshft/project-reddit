45bd7259b7b2371afc10beee5f1d10e6012f068c
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-02-06 10:15:19 -0500

    redid project on another vs code need to figure how to clone without info, comment works post works, need to figure how to delete post

--- main.js
+++ main.js
@@ -1,33 +1,76 @@
+// // $(document).ready(function() {
+//   //form submit
+//   $('#postbtn').on('click', function() {
+//     // avoid page reload
+//     // e.preventDefault()
 
-var button = $('#only');
-var commentbtn = $('#commentbtn')
+    // get message
+//     let getMessage = $('#postname').val();
+//     var getPost = $('#posttext').val();
+//     console.log(click)
+//   //   console.log(getMessage)
+//   //   console.log(getPost);
 
-// var cloneFunction = $(button).on('click', function () {
-//   var clone = $('#everything');
-//   $(clone).clone().appendTo('.addarea');
-// });
+//   //   if(getMessage !== '') {
+//   //     // prepare messgage
+//       let newMessage = "<p class='message'>" + getMessage + "</p>" + "<p>" + getPost + "</p>";
 
-var addTextName =  $(button).on('click', function () {
-  // cloneFunction();
-  var postnamebox = $('#postnamein').val();
-  var textbox = $('#posttextin').val();
+// //   //     //apend message to box
+//       $(newMessage).appendTo('#postin');
 
-    $('#postname').append('<h3>'  + postnamebox +  '</h3>');
-    $('.postbody').append('<p>' + textbox + '</p>');
-    // cloneFunction();
+//   //     //clear the form
+//   //     $('input').val("");
+//   //   }
+//   // });
+
+// console.log('hello')
+//   
+// var commentBox = function test() {
+//   var r = "$('<input/>').attr({
+//                type: "button",
+//                id: "field"
+//           })";
+// }
+// $("body").append(r);
+
+var postButton = $ (".post")
+var commentLink = $('#commentlink')
+
+
+var clickPost = $(postButton).on('click', function () {
+
+  var place = $('#place')
+  var getMessage = $('#postname').val();
+  var getPost = $('#posttext').val();
+  
+  var commentBubble = $('<input id="commentbubble" class="alert alert-success" role="alert"/>')
+  var commentBtn = $('<input id="commentbar" type="button" value="Comment"/>');
+  var commentBar = $('<input id="commenttab" placeholder="comment" type="text" />')
+  var nameCommentBar = $('<input id="commentname" placeholder="name" type="text" />')
+
+
+  $('<h4>' + getMessage + '</h4>').appendTo(place);
+  $('<p>' + getPost + '</p>').appendTo(place);
+  $(commentBtn).appendTo(place);
+  console.log('click')
+
+$("#commentbar").on('click', function () {
+  $(commentBar).appendTo(place)
+  $(nameCommentBar).appendTo(place)
+  // $(commentBubble).appendTo('#commentsection');
+  $('#commentbar').css('display', 'none');
+
+  var postComment = $('#commentbtn').on('click', function () {
+    var getCommentName = $('#commenttab').val();
+    var getComment = $('#commentname').val();
+
+    $('<h4>' +  getCommentName + '</h4>').appendTo('#commentsection');
+    $('<p>' + getComment + '</p>').appendTo('#commentsection');
+    console.log('click')
+  })
+  postComment();
+  // $(commentPost).appendTo("#commenttab")
+})
 })
 
-// $(commentbtn).on('click', function () {
-//   console.log('im click')
-//   $('.postbody').append('<h6>' + commentName + '</h6>' )
-// })
-
-// var addComment = function () {
-//   var commentName = $('#commentname').val();
-//   var commentFrom = $('#commentfrom').val();
-  $(commentbtn).on('click', function () {
-    // var commentName = $('#commentname').val();
-    var commentFrom = $('#commentbox').val();
-    $('.postbody').append('<h6>' + commentFrom + '</h6>')
-    // $('postbody').append('<p>' + commentFrom + '</p>')
-  })
+clickPost();
