929c77d191658de85773704a75a53563135d523e
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-02-07 10:14:46 -0500

    reworked project to the point of making delete button work on new post, need to figure event delegation

--- main.js
+++ main.js
@@ -1,71 +1,33 @@
+var name = $('#postname')
+var post = $('#posttext')
 
-var newPost = $("#newpostbtn")
-var postButton = $ (".post")
-var commentLink = $('#commentlink')
-var clone = $('.clone')
-
-$(newPost).on('click', function () {
-
-  $('.myclone').first().clone(true).appendTo("body")
-  clickPost();
-})
+var deletePost = $('<button type="button" class="btn-close" aria-label="Close"></button>')
 
 
 
-var clickPost = $(postButton).on('click', function () {
+$("#postbtn").on('click', function () {
+  console.log('click')
+
+
+  var deletePost = $('<button type="button" class="btn-close" aria-label="Close"></button>').addClass("deletebtn")
+
+  var name = $("#postname").val();
+  var post = $("#posttext").val();
+  var box = $('.newname')
+  var delBtn = $('.btn-close')
+  var postFunc = function () {
+
+    
+    $('<h4 class="newname">' + name + '</h4>').append('<p class="newpost">' + post + '</p>').append(deletePost).appendTo('#post')
+    $("#postname").val('')
+    $("#posttext").val('')
+
+    }
+    postFunc();
+    $('.btn-close').on('click', '.newname', function () {
+      $(this).remove();
+  });
+  })
  
 
-//post variables
-  var place = $('#place')
-  var getMessage = $('#postname').val();
-  var getPost = $('#posttext').val();
-  
 
-  //Reset function
-  var resetInput = function () {
-    $('#posttext').val('');
-    $('#postname').val('');
-  }
-  //create comment varaibles
-  var commentBtn = $('<input id="commentbar" type="button" value="Comment"/>');
-  var commentBar = $('<input id="commenttab" placeholder="name" type="text" />')
-  var nameCommentBar = $('<input id="commentname" placeholder="comment" type="text" />')
-
-
-//append to postBox
-  $('<h4>' + getMessage + '</h4>').appendTo(place);
-  $('<p>' + getPost + '</p>').appendTo(place);
-  $(commentBtn).appendTo(place);
-  console.log('click')
-resetInput();
-
-//comment function
-$("#commentbar").on('click', function () {
-  //reset comments
-  var resetComments = function () {
-    $('#commentname').val('');
-    $('#commenttab').val('');
-  }
-  $(commentBar).appendTo(place)
-  $(nameCommentBar).appendTo(place)
-  // $(commentBubble).appendTo('#commentsection');
-  $('#commentbar').css('display', 'none');
-
-  var postComment = $('#commentbtn').on('click', function () {
-    var getCommentName = $('#commenttab').val();
-    var getComment = $('#commentname').val();
-
-    $('<h4>' +  getCommentName + '</h4>').appendTo('#commentsection');
-    $('<p>' + getComment +  '</p>').appendTo('#commentsection');
-    console.log('click')
-    resetComments();
-  })
-  postComment();
-  // $(commentPost).appendTo("#commenttab")
-})
-})
-
-// clickPost();
-$( document).on( "click", function( event ) {
-  $( event.target ).closest(clickPost);
-})
