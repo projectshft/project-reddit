df87dfdee6d50f1567841ead2d257b86f215ae3d
Max Blanton <max.blantonn@gmail.com>
2023-07-12 20:43:35 -0400

    Final commit

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var app = {
   posts: []
 };
+
 var postCount = 0;
 var postAction = function(el) {
+
   if (el.id === "post-submit") {
+    
     var post = el.parentElement.parentElement;
     
     var exPost = document.querySelector("#example-post");
@@ -53,23 +56,28 @@ var hideComment = function() {
 }
 
 var addComment = function(el) {
+
   var post = el.parentElement.parentElement;
 
   var exComment = document.querySelector("#example-comment");
   var commentText = el.querySelector("#comment-text");
   var commentUser = el.querySelector("#user-2");
-
   var newComment = exComment.cloneNode(true);
-  
   var date = new Date();
   var currentPost = post.id;
+
   var findPostApp = posts.findIndex((element) => {
     return element.post === currentPost;
+
   })
+
   posts[findPostApp].comments.push(`${currentPost}${date}`);
+
   newComment.classList.remove("hidden");
+
   newComment.querySelector("#comment-user").innerText = commentUser.value;
   newComment.querySelector("#comment-post").innerText = commentText.value;
+
   el.before(newComment);
 
   commentText.value = "";
@@ -82,22 +90,32 @@ var removeAction = function() {
     var findPost = document.querySelector(`#${item.post}`);
 
     findPost.addEventListener("click", (e) => {
+
       var indexItem = posts.indexOf(item);
       var removeButton = findPost.querySelectorAll("#remove");
+
       Array.from(removeButton).forEach((i) => {
         if (e.target === i) {
+
           e.preventDefault();
           if (i.classList.contains("remove-c")) {
+
             var currentComment = i.parentElement;
             currentComment.remove();
+
             var findCommApp = posts[indexItem].comments.findIndex((element) => {
+
               return element === currentComment.id;
+
             })
+
             posts[indexItem].comments.splice(findCommApp, 1);
             
           } else {
+
             findPost.remove();
             posts.splice(indexItem, 1);
+
           }
         } 
       });
@@ -109,20 +127,28 @@ var postForm = document.querySelector("#post-form");
 var postButtons = postForm.querySelectorAll("button");
 
 Array.from(postButtons).forEach(el => {
+
   el.addEventListener("click", () => {
+
     postAction(el);
     hideComment();
     removeAction();
+
   });
 });
 
 
 
 var renderComment = function(newPost) {
+
   var commentForm = newPost.querySelectorAll(".comment-form");
+
   Array.from(commentForm).forEach(el => {
+
     var commentButton = el.querySelector("button");
+
     commentButton.addEventListener("click", () => {
+
       addComment(el);
       removeAction();
     });
