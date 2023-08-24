51c2e4605b3834a7e7d0023860ea572c0c6704b3
Jordan Cox <jordancox747@outlook.com>
2023-08-24 08:32:21 -0600

    clear message box after post

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const button = document.querySelector("#submit");
+let postList = [];
 
 button.addEventListener("click", () => {
   const postContent = document.querySelector("#post-content").value;
@@ -13,6 +14,12 @@ const newPost = (postContent, userName) => {
     name: userName
   };
 
+  postList.push(post);
+
   const newPost = `<div>${post.text}<br/>Posted by: <b>${post.name}</b></div><hr/>`;
   document.querySelector(".posts").innerHTML += newPost;
+
+  // Clear message box
+  document.querySelector("#name").value = "";
+  document.querySelector("#post-content").value = "";
 };
