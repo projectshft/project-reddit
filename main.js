ccf2c5ce03c7c9b63c607fcd85ba591f2b3cdc36
Jordan Cox <jordancox747@outlook.com>
2023-08-24 08:52:05 -0600

    posts working and disallowing html additions

--- main.js
+++ main.js
@@ -1,25 +1,56 @@
 const button = document.querySelector("#submit");
 let postList = [];
+let postCount = 0;
 
 button.addEventListener("click", () => {
   const postContent = document.querySelector("#post-content").value;
   const userName = document.querySelector("#name").value;
-  
+
   newPost(postContent, userName);
 });
 
 const newPost = (postContent, userName) => {
-   const post = {
+  const post = {
     text: postContent,
-    name: userName
+    name: userName,
   };
 
   postList.push(post);
 
-  const newPost = `<div>${post.text}<br/>Posted by: <b>${post.name}</b></div><hr/>`;
-  document.querySelector(".posts").innerHTML += newPost;
+  // const newPost = `<div id="post-count-${postCount}">${post.text}<br/>Posted by: <b>${post.name}</b></div><hr/>`;
+  // document.querySelector(".posts").innerHTML += newPost;
+
+  // Populate new post div
+  const postDiv = document.createElement("div");
+  postDiv.setAttribute("id", `post-count-${postCount}`);
+  const postText = document.createTextNode(post.text);
+
+  postDiv.appendChild(postText);
+
+  const lineBreak = document.createElement("br");
+
+  postDiv.appendChild(lineBreak);
+
+  const postedByText = document.createTextNode("Posted by: ");
+
+  postDiv.appendChild(postedByText);
+  
+  const boldText = document.createElement("b");
+  const name = document.createTextNode(post.name);
+
+  boldText.appendChild(name);
+
+  postDiv.appendChild(boldText);
+
+  const hrDivider = document.createElement("hr");
+
+  document.querySelector(".posts").append(postDiv);
+  document.querySelector(".posts").append(hrDivider);
 
   // Clear message box
   document.querySelector("#name").value = "";
   document.querySelector("#post-content").value = "";
+
+  // Increment post count
+  postCount++;
 };
