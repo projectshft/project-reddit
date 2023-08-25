09b1bb09a3ab3cce4f2be3162ad7252ab65d090e
Jordan Cox <jordancox747@outlook.com>
2023-08-25 14:23:04 -0600

    edit post functionality works

--- main.js
+++ main.js
@@ -224,8 +224,14 @@ const editPost = (event) => {
   const selectedPost = event.target.closest(".single-post").getAttribute("id");
   const postNumber = selectedPost.match(/\d+$/)[0];
 
-  document.querySelector(".edit-post-textarea").innerHTML =
-  event.target.closest(".single-post").querySelector("br").previousSibling.textContent; // make this dynamic
+  document.querySelector(".edit-post-textarea").remove(); // make this dynamic
+  
+  const newTextarea = document.createElement("textarea");
+  newTextarea.setAttribute("class", "bg-dark text-light edit-post-textarea");
+  const oldText = document.createTextNode(postList[postNumber].text);
+  newTextarea.appendChild(oldText);
+
+  document.querySelector(".textarea-wrapper-div").append(newTextarea);
 
   // Submit edit
 
