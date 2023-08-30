09d9bd3f1a276d92d420ea602411473d31ede88a
Scott Bouchard <holdwhatdoor@gmail.com>
2023-08-30 10:38:30 -0700

    added MessageBoard class - refactored code into methods within the class and method calls on instance of the class

--- main.js
+++ main.js
@@ -14,66 +14,99 @@ var commentInput = document.getElementsByClassName('comment-text')[0];
 var commentInputName = document.getElementsByClassName('commentor-name')[0];
 var commentBtn = document.getElementsByClassName('comment-btn')[0];
 
+// MessageBoard Class
+class MessageBoard {
+  constructor(currentBoard, message, user){
+    this.currentBoard = currentBoard;
+    this.message = message;
+    this.user = user;
+  }
+
+  renderPage = function (){
+    var threadBody = document.getElementsByClassName('thread-body')[0];
+    while(threadBody.hasChildNodes()){
+      threadBody.removeChild(threadBody.firstChild);
+    }
+
+    threadBody.innerHTML = messageBoard.renderPosts();
+  }
+
+  addPost = function (){
+    switch(true){
+      case(postInput.value === ""):
+        alert("'Post Text' input cannot be blank.");
+        break;
+      case(postUserName.value === ""):
+        alert("'Your Name' input cannot be blank.");
+        break;
+      default:
+        var comments = [];
+        var newPost = new Post();
+        newPost.post = postInput.value;
+        newPost.postUserName = postUserName.value;
+        newPost.comments = comments;
+        // console.log("postlength: " + posts.length);
+        newPost.postId = posts.length;
+        // console.log("postId: " + newPost.postId);
+        posts.push(newPost);
+    }
+  }
+
+  renderPosts = function (){
+    var items = "";
+    for(var i = 0; i < posts.length; i++){
+      items += '<div class="post-item"><p class="delete-post"> Delete Post </p><p class="post-content">' + posts[i].post + ' - Posted By: ' + posts[i].postUserName + `</p><p id="post${posts[i]}" class="post-comments" onclick="messageBoard.toggleComments()"> show comments </p></div><hr/>`;
+      console.log(posts[i]);
+    }
+    return items;
+  }
+
+  toggleComments = function (){
+    // if(target.classList.contains('.post-comments')){
+      if(document.getElementsByClassName('comment')[0].classList.contains('show')){
+        document.getElementsByClassName('comment')[0].classList.remove('show');
+        document.getElementsByClassName('post-comments')[0].innerHTML = "show comments";
+      } else {
+        document.getElementsByClassName('comment')[0].className += ' show';
+        document.getElementsByClassName('post-comments')[0].innerHTML = "hide comments";
+      }
+    // } 
+  }
+
+}
+
 // Post class
 class Post {
-
-  constructor(post, postUserName, comments, postID){
+  constructor(post, postUserName, comments, postId){
     this.post = post; 
     this.postUserName = postUserName;
     this.comments = comments;
-    this.postId = postID;
+    this.postId = postId;
+  }
+
+  addComment = function (){
+
+  }
+
+  renderComments = function (){
+
   }
 
 }
 
-// click events for buttons
+// eventListeners for buttons
 postBtn.addEventListener('click', function(){
-  var comments = [];
-  var newPost = new Post();
-  newPost.post = postInput.value;
-  newPost.postUserName = postUserName.value;
-  newPost.comments = comments;
-  console.log("postlength: " + posts.length);
-  newPost.postId = posts.length;
-  console.log("postId: " + newPost.postId);
-
-  posts.push(newPost);
-  // console.log("postslength: " + posts.length);
-  // console.log("posts[newPost.postId].post: " + posts[newPost.postId].post);
-  // console.log("posts[newPost.postId].postUserName: " + posts[newPost.postId].postUserName);
+  messageBoard.addPost();
   postInput.value = "";
   postUserName.value = "";
-  renderPage();
+  messageBoard.renderPage();
 
 });
 
-// method to display page
-var renderPage = function (){
-  var threadBody = document.getElementsByClassName('thread-body')[0];
-  while(threadBody.hasChildNodes()){
-    threadBody.removeChild(threadBody.firstChild);
-  }
-  
-  var items = "";
-  for(var i = 0; i < posts.length; i++){
-    items += '<div class="post-item"><p class="delete-post"> Delete Post </p><p class="post-content">' + posts[i].post + ' - Posted By: ' + posts[i].postUserName + `</p><p id="post${posts[i]}" class="post-comments" onclick="toggleComments()"> show comments </p></div><hr/>`;
-    console.log(posts[i]);
-  }
-  threadBody.innerHTML = items;
-}
+commentBtn.addEventListener('click', function(){
 
-var toggleComments = function (e){
-  // for(e.target.closest('.post-comments')){
-    if(document.getElementsByClassName('comment')[0].classList.contains('show')){
-      document.getElementsByClassName('comment')[0].classList.remove('show');
-      document.getElementsByClassName('post-comments')[0].innerHTML = "show comments";
-    } else {
-      document.getElementsByClassName('comment')[0].className += ' show';
-      document.getElementsByClassName('post-comments')[0].innerHTML = "hide comments";
-    }
-  // }
-  
-}
+});
 
-
-renderPage();
+// Instantiates variable for new MessageBoard class & call to renderPage() funciton
+var messageBoard = new MessageBoard();
+messageBoard.renderPage();
