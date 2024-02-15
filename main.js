a2e753bb255551cd7f4a01b43d5f8b856c6ef21b
levydowell <33126924+levydowell@users.noreply.github.com>
2024-02-14 19:00:56 -0700

    Added div for individual posts and create light border

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ let posts = document.querySelector(".posts");
 clickable.addEventListener("click", function () {
   const newTextElement = document.createElement('p');
   const newNameElement = document.createElement('strong')
-  
+  const postElement = document.createElement('div');
+  postElement.setAttribute("class", "new-post");
+
   newNameElement.innerText = author.value;
   newTextElement.innerText = postText.value;
-  posts.appendChild(newNameElement);
-  posts.appendChild(newTextElement);
+  postElement.appendChild(newNameElement);
+  postElement.appendChild(newTextElement);
+  posts.appendChild(postElement);
 })
