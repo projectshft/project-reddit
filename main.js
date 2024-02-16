f05e190a46edddac515644536c92eee47da8bd36
levydowell <33126924+levydowell@users.noreply.github.com>
2024-02-16 07:52:17 -0700

    Finished toggle feature for comments display/hide

--- main.js
+++ main.js
@@ -1,32 +1,57 @@
 //define variables to access elements needed from `index.html`
-let author = document.querySelector(".author");
+let postAuthor = document.querySelector(".author");
 let postText = document.querySelector(".post")
-let clickable = document.querySelector(".btn");
+let PostBtn = document.querySelector(".btn");
 let posts = document.querySelector(".posts");
 
-//create element, fill element, and append to div
-clickable.addEventListener("click", function () {
+//create text elements to store post data
+PostBtn.addEventListener("click", function () {
   const newTextElement = document.createElement('p');
   const newNameElement = document.createElement('strong')
+  //create unique post element for every post
   const postElement = createElementsetAttributes('div', {"class": "new-post"});
-
-  newNameElement.innerText = author.value;
+  
+  //store post data in text elements and create button to post them
+  newNameElement.innerText = postAuthor.value;
   newTextElement.innerText = postText.value;
   const commentButton = createElementsetAttributes("btn", {"type": "button", "class": "btn btn-outline-secondary btn-sm"});
   commentButton.innerText = "Comment";
 
+  //Append post data and button to unique `postElement`
   postElement.appendChild(newNameElement);
   postElement.appendChild(newTextElement);
   postElement.appendChild(document.createElement('hr'));
   postElement.appendChild(commentButton);
+  //Append `postElement` to `posts` to display post on screen
   posts.appendChild(postElement);
 
+  //create comment div to hold all of the comment fields/data.
+  const commentDiv = createElementsetAttributes('div', {"class": "comments hide-comments"})
+  postElement.appendChild(commentDiv);
+
+  //create comment text fields and append them to the commentDiv
+  const commentAuthor = createElementsetAttributes('input', {"class": "form-control comment-box", "type": "text", "placeholder": "Your name"});
+  const commentField = createElementsetAttributes('input', {"class": "form-control comment-box", "type": "text", "placeholder": "Add a comment"});
+  commentDiv.appendChild(commentAuthor);
+  commentDiv.appendChild(commentField);
+
+  // create text elements to store comment field values and append them to `commentDiv`
+  commentField.addEventListener("keydown", function (e) {
+    if (e.key === 'Enter') {
+      let commentAuthorText = createElementsetAttributes('strong', {"class": "comment"});
+      let commentText = createElementsetAttributes('p', {"class": "comment"});
+      commentAuthorText.innerText = commentAuthor.value;
+      commentText.innerText = commentField.value;
+      commentDiv.appendChild(commentAuthorText);
+      commentDiv.appendChild(commentText);
+    }
+  });
+  
+
+  //Toggle this `hide-comments` class of `commentDiv` to enable hiding and displaying comment secton.
   commentButton.addEventListener("click", function () {
-    let commentField = createElementsetAttributes('input', {"class": "form-control", "type": "text", "placeholder": "Add a comment"});
-    let commentText = createElementsetAttributes('p', {"class": "comment"});
-    commentText.innerText = commentField.value;
-    postElement.appendChild(commentField);
-    postElement.appendChild(commentText);
+    const commentClasses = commentDiv.classList;
+    commentClasses.toggle("hide-comments");
   });
 })
 
