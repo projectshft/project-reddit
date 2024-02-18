2423ec099853108c6d706328db626e2bdf2a0ac6
levydowell <33126924+levydowell@users.noreply.github.com>
2024-02-18 09:46:01 -0700

    First pull request commit

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 //define variables to access elements needed from `index.html`
-let postAuthor = document.querySelector(".author");
-let postText = document.querySelector(".post")
-let PostBtn = document.querySelector(".btn");
-let posts = document.querySelector(".posts");
-
-///attempt at builder method for individual posts
+const postAuthor = document.querySelector(".author");
+const postText = document.querySelector(".post");
+const PostBtn = document.querySelector(".btn");
+const posts = document.querySelector(".posts");
 
 
-//
-
-//create text elements to store post data
+/**
+ * Add event listener to postBtn
+ * create text elements and unique div to style posts
+ * append text elements to unique div and create commentButton
+ */
 PostBtn.addEventListener("click", function () {
   const newTextElement = document.createElement('p');
-  const newNameElement = document.createElement('strong')
+  const newNameElement = document.createElement('strong');
   //create unique post element for every post
   const postElement = createElementsetAttributes('div', {"class": "new-post"});
   
@@ -20,7 +20,7 @@ PostBtn.addEventListener("click", function () {
   newNameElement.innerText = postAuthor.value;
   newTextElement.innerText = postText.value;
   const commentButton = createElementsetAttributes("btn", {"type": "button", "class": "btn btn-outline-secondary btn-sm"});
-  commentButton.innerText = "Comment";
+  commentButton.innerText = "Comments";
 
   //Append post data and button to unique `postElement`
   postElement.appendChild(newNameElement);
@@ -40,7 +40,11 @@ PostBtn.addEventListener("click", function () {
   commentDiv.appendChild(commentAuthor);
   commentDiv.appendChild(commentField);
 
-  // create text elements to store comment field values and button, then append them to `commentDiv`
+  /**
+   * Add event listener to commentField element for `keydown` event
+   * If enter is pressed, create text elements for the author and commentText
+   * Store text field values to text elements
+   */
   commentField.addEventListener("keydown", function (e) {
     if (e.key === 'Enter') {
       let commentAuthorText = createElementsetAttributes('strong', {"class": "comment"});
@@ -52,7 +56,10 @@ PostBtn.addEventListener("click", function () {
       commentDiv.append(commentAuthorText, deleteBtn);
       commentDiv.appendChild(commentText);
       
-      // remove all comment data upon clicking `delete`
+      /**
+       * Add event listener to deleteBtn element for `click` event
+       * Remove the author, deleteBtn, and commentText from the commentDiv
+       */
       deleteBtn.addEventListener("click", function () {
         commentAuthorText.remove();
         deleteBtn.remove();
@@ -61,7 +68,10 @@ PostBtn.addEventListener("click", function () {
     }
   });
   
-  //Toggle this `hide-comments` class of `commentDiv` to enable hiding and displaying comment secton.
+  /**
+   * Add event listener to commentButton for `click` event
+   * Toggle `hide-comments` class to hide/display comments section.
+   */
   commentButton.addEventListener("click", function () {
     const commentClasses = commentDiv.classList;
     commentClasses.toggle("hide-comments");
@@ -69,11 +79,16 @@ PostBtn.addEventListener("click", function () {
 });
 
 
-//function that creates element and sets attributes from an object parameter
+/**
+ * Create an element and set its attributes
+ * @param {*} elementString String of element to be created. I.e 'div'
+ * @param {*} attributeObject Object with {attribute: value} pairs.
+ * @returns The created element with attributes.
+ */
 function createElementsetAttributes(elementString, attributeObject) {
   let attributeTarget = document.createElement(elementString);
   for (let key of Object.keys(attributeObject)) {
     attributeTarget.setAttribute(key, attributeObject[key]);
   };
-  return attributeTarget
+  return attributeTarget;
 };
