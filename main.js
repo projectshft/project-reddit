2e9a9520b46459a95c3209207c717132cf7984e3
Stephen <stephen.swaringin@gmail.com>
2024-02-24 17:09:41 -0600

    Adding better formatting. Adding comments functionality

--- main.js
+++ main.js
@@ -8,21 +8,112 @@ let postMessage = document.getElementById('message');
 let postAuthor = document.getElementById('author');
 
 // Access Submit Post button
-let submitPostButton = document.getElementById('submit');
+let submitPostButton = document.getElementById('submit-post');
 
+// Example Data Structure
+// [{author: 'Jack Reacher', message: 'Hey', comments: []},{author: 'Frances Neagley', message: 'What's up?', comments: []}]
 
 let posts = [];
 
-// When the Submit Post button is clicked, create a new div under the posts div and include:
-// Text - Posted By: Name
+// When the Submit Post button is clicked, create a new div under the posts div.
+// Example: Text - Posted By: Name
 submitPostButton.addEventListener('click', function () {
   
-  let post = document.createElement('div');
-  let postContent = document.createTextNode(`${postMessage.value} - Posted By: ${postAuthor.value}`);
-  const divider = document.createElement('hr');
+  let authorName = postAuthor.value;
+  let messageContent = postMessage.value;
 
-  post.appendChild(postContent);
-  postsDiv.appendChild(post);
-  postsDiv.appendChild(divider);
+  let postObj = {
+    author: authorName,
+    message: messageContent,
+    comments: [],
+  }
+
+  posts.push(postObj);
+
+  postAuthor.value = "";
+  postMessage.value = "";
+
+  renderPosts();
 
 });
+
+const renderPosts = function() {
+  
+  // Empty the postsDiv
+  while (postsDiv.hasChildNodes()) {
+    postsDiv.removeChild(postsDiv.firstChild);
+  }
+  
+  // Go through posts Array and render each post (Not the most efficient)
+  for (post in posts) {
+    let newPost = document.createElement('div');
+    let postContent = document.createElement('p');
+    postContent.innerHTML = `${posts[post].message} - Posted By: ${posts[post].author}`;
+    const divider = document.createElement('hr');
+
+    let comments = posts[post].comments;
+
+    let commentsDiv = document.createElement('div');
+    commentsDiv.setAttribute('class', 'comments-form d-none');
+
+    let commentMessage = document.createElement('input');
+    commentMessage.setAttribute('placeholder', 'Comment Text');
+    commentMessage.setAttribute('id', 'comment-text');
+
+    let commentAuthor = document.createElement('input');
+    commentAuthor.setAttribute('placeholder', 'Your Name');
+
+    let commentsButton = document.createElement('button');
+    commentsButton.setAttribute('class', "btn btn-primary submit-comment")
+    commentsButton.innerHTML = "Submit Comment";
+
+    commentsButton.addEventListener('click', function () {
+  
+      let authorName = commentAuthor.value;
+      let messageContent = commentMessage.value;
+    
+      let commentObj = {
+        author: authorName,
+        message: messageContent,
+      }
+    
+      debugger;
+      comments.push(commentObj);
+    
+    });
+
+    commentsDiv.appendChild(commentMessage);
+    commentsDiv.appendChild(commentAuthor);
+    commentsDiv.appendChild(commentsButton);
+
+    let commentsLink = document.createElement('a');
+    commentsLink.innerHTML = 'comments';
+    commentsLink.addEventListener('click', function() {
+      if (commentsDiv.classList.contains('d-none')) {
+        commentsDiv.classList.remove('d-none');
+      } else {
+        commentsDiv.classList.add('d-none');
+      }
+    })
+
+    //add comments link
+    newPost.appendChild(commentsLink);
+
+    //insert new post div
+    newPost.appendChild(postContent);
+    
+    //insert content in the new post div
+    postsDiv.appendChild(newPost);
+
+    //add comments div
+    postsDiv.appendChild(commentsDiv);
+
+    //insert a horizontal rule to separate posts
+    postsDiv.appendChild(divider);
+  }
+
+};
+
+const renderComments = function() {
+
+};
