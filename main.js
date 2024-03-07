79d3877e2b7b42e114a4a330cd1642da39bf401d
Catie <catienice@gmail.com>
2024-03-07 15:28:24 -0700

    finalattempt

--- main.js
+++ main.js
@@ -1,148 +1,62 @@
-// Needs to have:
-// title "ReReddit"
-// posts formatted: post content - Posted By: Postername
-// needs to display as list items with break line 
-// need form input lines for post text and name
-// need sumbit post button that pushes content to list
-
-// let button = document.getElementById('submit')
-
-// button.addEventListener('click', function(){
-//   // retreive values of name and message inputs
-//   let nameInput = document.getElementById('name').value;
-//   let postInput = document.getElementById('message').value;
-
-//   // create new <p> inside <div class='posts'>
-//   let postDiv = document.querySelector('.posts');
-//   let element = document.createElement('p');
-
-//   // create text nodes for name and message
-//   // create new <hr> for seperating posts
-//   let elementText = document.createTextNode(`${postInput}`) 
-//   let elementText2 = document.createTextNode(` Posted By: ${nameInput}`)
-//   let newPostHR = document.createElement('hr');
-
-//   // create text node for comments
-//   // does this need to happen in here? 
-//   let commentText = document.createTextNode('Comment ');
-
-//   // append all new elements and nodes
-//   // oder matters ... why?
-//   postDiv.append(element);
-//   element.appendChild(commentText);
-//   element.appendChild(elementText);
-//   element.appendChild(elementText2);
-//   element.appendChild(newPostHR);
-
-//   // add class to commentText text node, does this order matter?
-//   commentText.className += 'comment-text';
-  
-// });
-
-// trying to connect listener to "comment" text node
-// how do I turn comment into a link/button using this js method above?
-// how to add class name/id to "comment" text node created above so I can reference it
-
-// do I need to make a hyperlink in order to add an eventlisterner?
-// and if so how can I make "comment" into a hyperlink while inside the event listener?
-
-
-// let viewCommentForm = document.getElementsByClassName('comment-form')[0];
-// let commentText = document.getElementsByClassName('comment-text')[0];
-
-// commentText.addEventListener('click', function () {
-//   if(viewCommentForm.className.indexOf('show') === -1) {
-//     viewCommentForm.className = 'comment-form';
-//   } else {
-//     viewCommentForm.className += ' show'
-//   }
-// });
-
-
-// post function with vaiable as posts
-// and variables with comments (array)
-// another function to add comment to post
-// then push comment to post 
-// seperate function to take data from comment function to 
-// create list of posts
-// good practice: do functions that are specific
-
-// -------------------------------------------
-
-const post = () => {
-  let button = document.getElementById('submit');
-  let postIdCounter = 1;
-
-  button.addEventListener('click', () => {
-    let nameInput = document.getElementById('name').value;
-    let postInput = document.getElementById('message').value;
-
-    let postDiv = document.querySelector('.posts');
-    let element = document.createElement('p');
-    let element2 = document.createElement('p');
-    
-    let elementText = document.createTextNode(postInput);
-    let elementText2 = document.createTextNode(' Posted By: ' + nameInput);
-    let newPostHR = document.createElement('hr');
-
-    let commentLink = document.createTextNode('Comment ');
-
-    element.setAttribute('id', postIdCounter);
-    postIdCounter++;
-
-    element2.setAttribute('id', 'comment-link');
-
-    postDiv.append(element2);
-    element2.appendChild(commentLink);
-    postDiv.append(element);
-    element.appendChild(elementText);
-    element.appendChild(elementText2);
-    element.appendChild(newPostHR);
-  
-  });
-}
-
-post();
-
-const comment = () => {
-  let button2 = document.getElementById('submit2');
-
-  button2.addEventListener('click', () => {
-    let nameInput2 = document.getElementById('name2').value;
-    let postInput2 = document.getElementById('message2').value;
-
-    let commentDiv = document.querySelector('p');
-    let element2 = document.createElement('div');
-
-    let commentText = document.createTextNode(postInput2);
-    let commentName = document.createTextNode(' Posted by: ' + nameInput2);
-    let newCommentHR = document.createElement('hr');
-
-    commentDiv.append(element2);
-    element2.appendChild(commentText);
-    element2.appendChild(commentName);
-    element2.appendChild(newCommentHR);
-
-  })
+const posts = [];
+// structure to compile post and method that pushes information to posts array. Info is later displayed by displayPosts
+const compilePost = function (message, name) {
+  let index = posts.length;
+  let post = {
+    message: message,
+    name: name,
+    index: index
+  }
+  posts.push(post);
 };
 
-comment();
-
-const commentToggle = () => {
-  let getCommentLink = document.getElementById('comment-link');
-  let getCommentForm = document.getElementById('comment-form');
-
-  console.log(getCommentLink)
-  
-  getCommentLink.addEventListener('click', () => {
-    if (getCommentForm.className.indexOf('show') === -1) {
-      getCommentForm.className += ' show';
-    } else {
-      getCommentForm.className = 'comment-form';
-    };
+// actual display of posts
+const postDiv = document.querySelector('.posts');
+const displayPosts = function () {
+  posts.forEach(function(eachPost) {
+    let index = eachPost.index;
+    let displayBoolean = eachPost.displayComments;
+    let createPost = '<button class="btn btn-primary remove-post-button">' + 'Remove' + '</button>' + '<b class="post-index">' + index + '</b>' + '<button class="btn btn-primary comment-button">' + 'Comment' + '</button>' + '<b class="display-comments">' + displayBoolean + '</b>' + eachPost.message + ' Posted by: ' + eachPost.name + '<div class="post-comments-div">' + createComments() +'</div>' + '<hr>';
+    let element = document.createElement('div');
+    postDiv.appendChild(element);
+    element.className = 'each-post';
+    element.innerHTML = createPost;
   })
 };
-
-commentToggle();
-
-// issue is that I am trying to getCommentForm before it is created. 
+// create comment HTML
+const createComments = function() {
+  let commentBoxHTML = '<form class="comment-form" style="margin-top:30px" onsubmit="event.preventDefault();">' +
+  '<div>' +
+    '<input id="comment" type="text" class="comment-input" placeholder="Comment Text"></input>' +
+  '</div>' +
+  '<div>' +
+    '<input id="comment-name" type="text" class="comment-name-input" placeholder="Name"></input>'+
+  '</div>' +
+  '<button id="sumbit-comment" class="btn btn-primary">Submit Comment</button>' +
+  '</form>';
+  return commentBoxHTML;
+} 
+// triggering post on submitPosts button by calling compilePost and displayPost
+const submitPostButton = document.getElementById('sumbit-post');
+submitPostButton.addEventListener('click', function() {
+  let nameInput = document.getElementById('name').value;
+  let messageInput = document.getElementById('message').value;
+  let postIndex = document.getElementById('post-index');
+  compilePost(messageInput, nameInput, postIndex);
+  postDiv.innerHTML = '';
+  displayPosts();
+  console.log(posts);
+});
+// removebutton event listener, also updateds post index
+document.querySelector('.posts').addEventListener('click', function(e){
+  if(e.target.closest('.remove-post-button')){
+    let thisPostIndex = e.target.nextElementSibling.innerHTML;
+    posts.splice(thisPostIndex, 1);
+    for (let i = 0; i < posts.length; i++){
+      posts[i].index = i 
+      }
+    }
+  postDiv.innerHTML = '';
+  displayPosts();
+  }
+);
